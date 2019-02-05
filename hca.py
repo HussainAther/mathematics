2123c8f546293a6b080a19ac545a7b00701cc991
Syed Hussain Ather <shussainather@gmail.com>
2019-02-05 16:28:57 -0500

    creating algorithm

--- hca.py
+++ hca.py
@@ -2,9 +2,31 @@
 General algorithm for hierchical clustering analysis.
 """
 
+# Returns minimum difference between any pair
+def findMinDiff(arr, m):
+  
+    # Sort array in non-decreasing order
+    arr = sorted(arr)
+  
+    # Initialize difference as infinite
+    diff = 10**20
+  
+    # Find the min diff by comparing adjacent
+    # pairs in sorted array
+    min_pair = []
+    for i in range(m-1):
+        if arr[i+1] - arr[i] < diff:
+            diff = arr[i+1] - arr[i]
+            min_pair = arr[i+1] - arr[i]
+  
+    # Return min diff
+    return diff, min_pair
 
-# Construct a graph T by assigning an 
-def hca(d, n): # n is the number of clusters to form. # d is the data itself.
+# Construct a graph T by assigning an isolated vertex to ecah cluster
+def hca(d, n): # n is the number of clusters to form. # d is the data itself in list form.
+    T = [] # graph to consctruct and output
+    while n > 1:
+        diff, closest = findMinDiff(d, len(d))
 
 
 
