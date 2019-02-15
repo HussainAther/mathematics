3e802ca314daaac9832c9c82058f7b283ac8d171
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 16:33:30 -0500

    math

--- hoeffding.py
+++ hoeffding.py
@@ -1,14 +1,13 @@
 import sympy
 import sypy.stats as ss
 
-
 """
 Hoeffding’s Inequality is similar, but less loose, than Markov’s Inequality. Let X1,...,Xn be iid observations
 such that E(Xi) = μ and a ≤ Xi ≤ b. Then, for any ε > 0, we have:
 
 P(|Xn − μ| ≥ ε) ≤ 2 exp(−2nε2/(b − a)2)
 
-Corollary: If X1,...,Xn are independent with P(a≤Xi ≤b) = 1 and all with
+Corollary: If X1,...,Xn are independent with P(a ≤ Xi ≤b) = 1 and all with
 E(Xi ) = μ. Then, we have
 
 |Xn−μ| ≤ sqrt((c/2n)(log2/δ))
@@ -18,3 +17,10 @@ Hoeffding bounds for the case of ten identically and uniformly distributed rando
 The solid line shows P(|Xn − 1/2| > ε). Note that the Hoeffding Inequality is tighter than the Markov Inequality and
 that both of them merge when ε gets big enough.
 """
+
+t = sympy.symbols("t", real=True)
+x = ss.ChiSquared("x", 1)
+
+r = ss.P((x-1) > e , x > 1) + ss.P(-(x-1) > t, x<1)
+w = (1 - ss.cdf(x)(t+1))+ss.cdf(x)(1-t)
+
