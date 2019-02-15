0a96fce96b96ca782a7b4a333173b9cbd7cbc2e4
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 14:25:33 -0500

    fixed comment

--- gradientboosting.py
+++ gradientboosting.py
@@ -1,15 +1,19 @@
 import numpy as np
+import math
 
 """
 Stochastic Gradient Boosting
-As mentioned in Sect. 8.6, Friedman et al. (2000) worked to provide statis- tical insight of the
-AdaBoost algorithm. For the classification problem, they showed that it could be interpreted as a
-forward stagewise additive model that minimizes an exponential loss function. This framework led to
-algorithmic generalizations such as Real AdaBoost, Gentle AdaBoost, and LogitBoost. Subsequently,
-these generalizations were put into a unifying framework called gradient boosting machines.
+As mentioned in Friedman et al. (2000) ("Additive logistic regression: a statistical view of boosting")
+worked to provide statistical insight of the AdaBoost algorithm. For the classification problem, they
+showed that it could be interpreted as a forward stagewise additive model that minimizes an exponential
+loss function. This framework led to algorithmic generalizations such as Real AdaBoost, Gentle AdaBoost,
+and LogitBoost. Subsequently, these generalizations were put into a unifying framework called gradient boosting machines.
 """
 
 # Initialized all predictions to the sample log-odds: f(0) = log p^ / (i 1−p^)
+
+f0 = math.log(p_hat)
+
 for iteration j = 1...M do
 Compute the residual (i.e. gradient) z_i = y_i − p_i
 Randomly sample the training data
