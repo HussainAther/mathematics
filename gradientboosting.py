651ae41ed6b6ec148fe8e73fd0de5bd8bcffa125
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 14:34:20 -0500

    finished algorithm

--- gradientboosting.py
+++ gradientboosting.py
@@ -1,6 +1,12 @@
+# Helper libraries
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 import random
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
 
 """
 Stochastic Gradient Boosting
@@ -11,6 +17,8 @@ loss function. This framework led to algorithmic generalizations such as Real Ad
 and LogitBoost. Subsequently, these generalizations were put into a unifying framework called gradient boosting machines.
 """
 
+# Simple gradient boosting for classification (2-class)
+
 # Initialized all predictions to the sample log-odds: f(0) = log p^ / (1−p^)
 
 f0 = math.log((p_hat / (1 - p_hat))
@@ -20,15 +28,8 @@ def gradient(x):
 
 for j in range(1, M+1):
     z_i = y_i − phat_i # Compute the residual (i.e. gradient) z_i = y_i − p_i
-    data = random.sample(data, len(data) / 10)# Randomly sample the training data
-        # Train a tree model on the random subset using the residuals as the outcome
-Compute the terminal node estimates of the Pearson residuals:
-  i
-1 / n i
-p􏰁 i ( 1 − p􏰁 i )
-1 / n 􏰃 n ( y i − p􏰁 i )
-i r= 􏰃n
-7 Update the current model using fi = fi + λf (j) i
-8 end
+    random_data = random.sample(data, len(data) / 10) # Randomly sample the training data
+    model.fit(random_data, data_labels, epochs=5) # Train a tree model on the random subset using the residuals as the outcome
+    terminal = ((1/n)*sum(y_i - phat_i) / (1/n)*sum(phat_i*(1 - phat_i)) # Compute the terminal node estimates of the Pearson residuals:
+    fi = fi + λ*f_i # Update the current model using fi = fi + λf (j) i
 
-Algorithm
