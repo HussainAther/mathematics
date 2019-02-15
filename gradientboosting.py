89a5781ae8ed206c60ab1d4a87f1fe7003706d1f
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 14:18:29 -0500

    intiial comit

---
+++ gradientboosting.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+"""
+Stochastic Gradient Boosting
+As mentioned in Sect. 8.6, Friedman et al. (2000) worked to provide statis- tical insight of the
+AdaBoost algorithm. For the classification problem, they showed that it could be interpreted as a
+forward stagewise additive model that minimizes an exponential loss function. This framework led to
+algorithmic generalizations such as Real AdaBoost, Gentle AdaBoost, and LogitBoost. Subsequently,
+these generalizations were put into a unifying framework called gradient boosting machines.
+"""
+
+ Initialized all predictions to the sample log-odds: f(0) = log p􏰁 . i 1−p􏰁
+2 for iteration j = 1...M do
+3 Compute the residual (i.e. gradient) zi = yi − p􏰁i
+4 Randomly sample the training data
+5 Train a tree model on the random subset using the residuals as
+the outcome
+6 Compute the terminal node estimates of the Pearson residuals:
+  i
+1 / n i
+p􏰁 i ( 1 − p􏰁 i )
+1 / n 􏰃 n ( y i − p􏰁 i )
+i r= 􏰃n
+7 Update the current model using fi = fi + λf (j) i
+8 end
+
+Algorithm
