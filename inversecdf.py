e494ba6065772e1234c111a28b0253cc2ba1ae52
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 15:49:55 -0500

    math

--- inversecdf.py
+++ inversecdf.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 
 """
+Inverse cumulative distribution function
+
 Suppose we want to generate samples from a fair six-sided die. Our workhouse uniform random variable is defined continuously
 over the unit interval and the fair six-sided die is discrete. We must first create a mapping between the continuous random
 variable u and the discrete outcomes of the die. Each individual segment is assigned to one of the die outcomes. For example,
@@ -12,4 +14,9 @@ length. Thus, our new random variable v is derived from u by this assignment.
 u = np.random.rand(100)
 df = pd.DataFrame(data=u,columns=["u"])
 
+labels = [1,2,3,4,5,6]
+df["v"]=pd.cut(df.u,np.linspace(0,1,7), include_lowest=True,labels=labels)
 
+df.head()
+
+df.groupby("v").count()
