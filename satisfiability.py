1152d67d776e6fa20b412d69af35e5519e549ff2
Syed Hussain Ather <shussainather@gmail.com>
2019-02-17 16:43:28 -0500

    defined functions

--- satisfiability.py
+++ satisfiability.py
@@ -8,8 +8,19 @@ a given Boolean formula. They have practical applications in planning and schedu
 and drug design and genetics.
 """
 
-def circuitSAT(C):
+def circuitSat(C):
     """
     Circuit satisfiability takes an electrical Boolean circuit with a large number of inputs nad one output.
     Returns the possiblity of there being an output 1. "Yes" if C circuit is satsifiable. "No" otherwise.
+    """
+
+def Sat(B):
+    """
+    Takes an input Boolean formula B in conjunctive normal form and returns "yes" if B is satisfiable (it is
+    possible to produce the value 1), and "no" otherwise.
+    """
+
+def 3Sat(B):
+    """
+    Same as Sat excpet all clauses in the input can have at most 3 literals.
     """
