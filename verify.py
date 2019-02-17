90a454a4e56c95015d1288275a1cbe7557ce22fd
Syed Hussain Ather <shussainather@gmail.com>
2019-02-17 15:11:30 -0500

    verifyTspd

--- verify.py
+++ verify.py
@@ -1,3 +1,7 @@
+"""
+Complexity classes in computer science have varying times in finding solutions. Verifying that
+solutions exist exist in complexity classes.
+"""
 
 def verifyFactor(I, S, H):
     """
@@ -8,4 +12,29 @@ def verifyFactor(I, S, H):
         return "unsure"
     M = int(I)
     m = int(S)
-    if m >= 2 and
+    if m >= 2 and m < M and M % m == 0:
+        # m is a nontrivial factor of M
+        return "correct"
+    else:
+        # m is nontrivial
+        return "unsure"
+
+"""
+In the next example uses the hint H to work in a reasonable amount of time.
+"""
+
+def verifyTspD(I, S, H):
+    if S == "no":
+        return "unsure"
+    # extract G, L, and I and convert to correct data types
+    (G, L) = I.split(";")
+
+    # split hte hint string into a list of verticies, which will
+    # form a Hamilton cycle of length at most L, if the hint is correct
+    cycle = Path(H.split(","))
+
+    # verify the hint is a Hamilton cycle, and has length at most L
+    if G.isHamiltonCycle(cycle) and G.cycleLength(cycle) <= L:
+        return "correct"
+    else:
+        return "unsure"
