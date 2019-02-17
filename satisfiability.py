1a968dfaa75365ca53101f268cb1bdbb1573260c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-17 16:39:10 -0500

    initial commit

---
+++ satisfiability.py
@@ -0,0 +1,12 @@
+
+"""
+Satisfiability problems check whether solutions exist satisfying an interpretation of
+a given Boolean formula. They have practical applications in planning and scheduling, hard and software verification,
+and drug design and genetics.
+"""
+
+def circuitSAT(circuit):
+    """
+    Circuit satisfiability takes an electrical Boolean circuit with a large number of inputs nad one output.
+    Returns the possiblity of there being an output 1. 
+    """
