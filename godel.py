f996a4e09769d8069ef58c8b6b7dc19c4f2c0c66
Syed Hussain Ather <shussainather@gmail.com>
2019-02-17 15:45:18 -0500

    initial commit

---
+++ godel.py
@@ -0,0 +1,5 @@
+"""
+Logician Kurt Gödel proved his first incompleteness theorem in the 1930s. It can loosely be descrbied as
+"there are true statements about theintegers that cant be proved from the axioms." We can' use computers to rpove all the true
+statements in mathematics.
+"""
