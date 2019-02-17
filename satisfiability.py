631de57486a98b794b685b71805bec694a1ae7a8
Syed Hussain Ather <shussainather@gmail.com>
2019-02-17 16:41:24 -0500

    import

--- satisfiability.py
+++ satisfiability.py
@@ -1,3 +1,6 @@
+import itertools as it
+import sys
+import os
 
 """
 Satisfiability problems check whether solutions exist satisfying an interpretation of
@@ -5,8 +8,8 @@ a given Boolean formula. They have practical applications in planning and schedu
 and drug design and genetics.
 """
 
-def circuitSAT(circuit):
+def circuitSAT(C):
     """
     Circuit satisfiability takes an electrical Boolean circuit with a large number of inputs nad one output.
-    Returns the possiblity of there being an output 1. 
+    Returns the possiblity of there being an output 1. "Yes" if C circuit is satsifiable. "No" otherwise.
     """
