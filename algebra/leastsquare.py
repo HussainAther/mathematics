7fb63775116822eb57a40425a345e8dfeeeb79d8
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 18:56:16 -0500

    import

--- algebra/leastsquare.py
+++ algebra/leastsquare.py
@@ -1,6 +1,11 @@
-
+import pylab as p
+from vpython import *
+from numpy import *
+from numpy.linalg import inv
+from numpy.linalg import solve
 
 """
 Perform least-square fit of a parabola to data using linalg package to solve the set
 of linear equations.
 """
+
