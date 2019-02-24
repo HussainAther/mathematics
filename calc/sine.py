63c5f2d426907d77c81bcbddf125a5124858ca70
Syed Hussain Ather <shussainather@gmail.com>
2019-02-23 23:02:47 -0500

    function

--- calc/sine.py
+++ calc/sine.py
@@ -1,4 +1,12 @@
+import math
 
 """
 Calcluate sin(x) as a finite sum.
 """
+
+def sine(x, N):
+    result = 0
+    for i in range(1, N+1):
+        num = ((-1)**i-1) * (x**(2*i-1)) # numerator
+        den = math.factorial(2*i-1) # denominator
+
