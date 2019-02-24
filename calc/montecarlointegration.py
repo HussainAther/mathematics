f7f39cc35ec0654dfab2a07c95f34bca8a755d3d
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 14:50:01 -0500

    finish

--- calc/montecarlointegration.py
+++ calc/montecarlointegration.py
@@ -1,9 +1,30 @@
-
 """
 Standard Monte Carlo technique for integration is based on the mean value theorem:
 
 I = integral from a to b of dx*f(x) = (b-a)<f>
 
 The value of the integral of some function f(x) between a and b equals the length of the interval (b-a)
-times hte mean value of the function <f> over that interval.
+times the mean value of the function <f> over that interval. This average function can be written roughly as:
+
+<f> ~ (1/N) * summation from i equals 1 to N of f(x_i) in which x_i is the function at i.
+
+We can simplify these two by putting them together:
+
+integral from a to b of dx*f(x) = (b-a)<f> = (b-a)*(1/N)*summation from i equals 1 to N of f(x_i) in which x_i is the function at i.
 """
+
+def x(i):
+    return 5*i
+
+def f(x):
+    return 10*x
+    
+N = 10000
+def mc(f, a, b): # Monte Carlo integration of f(x) from a to b:
+    length = b - a
+    outside = length * (1/N) # the value outside the summation
+    summation = 0
+    for i in range(a, b+1):
+        x = x(i)
+        f = f(x)
+        summation += f
