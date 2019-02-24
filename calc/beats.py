d0e8c5640966912c8a747e90d7d45e7de2563444
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 12:38:15 -0500

    visual and functino

---
+++ calc/beats.py
@@ -0,0 +1,13 @@
+from visual.graph import *
+
+"""
+When we add two sine functions with nearly identical frequences, we obtain beats.
+
+y(t) = A sin(30t) + A sin(33t)
+"""
+
+graph = gdisplay(x=0, y=0, width=500, height=300, title="Beats: f(x)=sin(30x)+sin(33x)"
+    xtitle="x", title="f(x)", xmax=5.0, xmin=0.0, ymax=2, ymin=-2, foreground=color.black,
+    background=color.white)
+
+function = gcurve(color=color.red)
