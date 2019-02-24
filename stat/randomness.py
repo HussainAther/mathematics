da6544117de97ba1eb15fe370131faa75e81f432
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 13:31:27 -0500

    initial commit

---
+++ stat/randomness.py
@@ -0,0 +1,6 @@
+
+"""
+We can define a sequence of numbers as random if there are no correlations among the numbers.
+If all the numbres in a sequence are equally likely to occur, it is uniform.
+"""
+# Power resideue method of generation a psuedo-random sequence of numbers
