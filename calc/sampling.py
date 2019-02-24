435b64c036ef2b309253e4eb4bcbde2c9bb4f232
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 14:29:43 -0500

    initial commit

---
+++ calc/sampling.py
@@ -0,0 +1,7 @@
+"""
+Imagine a pond enclosed by a 2-dimensional box.
+We'll use the sampling technique of throwing stones into a pond
+as a methof of measuring the area of the pond. The ratio of "hits" to
+the total number of stones thrown equals the artio of the area of the pond
+to that of the box.
+"""
