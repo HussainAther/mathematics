a6e6517b28f850589cac9508d0bdf8c533ee4494
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 17:57:52 -0500

    initial commit

--- calc/newton.py
+++ calc/newton.py
@@ -1,5 +1,10 @@
-
+from sys import version
+from math import cos
 
 """
 Use Newton-Raphson method to find zero of a function.
 """
+
+if int(version[0])>2: # Python 3 deprecated
+    raw_input = input
+
