18f76f0cbcb80630116a5c7b3800b99f4f537218
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 14:42:21 -0500

    initial commit

---
+++ calc/montecarlointegration.py
@@ -0,0 +1,9 @@
+
+"""
+Standard Monte Carlo technique for integration is based on the mean value theorem:
+
+I = integral from a to b of dx*f(x) = (b-a)<f>
+
+The value of the integral of some function f(x) between a and b equals the length of the interval (b-a)
+times hte mean value of the function <f> over that interval.
+"""
