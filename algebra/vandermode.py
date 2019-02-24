2b760b25dd548eeed312e3923400bab58a8b3ad0
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 18:54:30 -0500

    comment and import

--- algebra/vandermode.py
+++ algebra/vandermode.py
@@ -1,4 +1,13 @@
+from vpython import *
+import math
 
 """
 Interpolation using the Vandermode function.
+
+The Vandermonde matrix is an n × n matrix where the first row is the first point evaluated at
+each of the n monomials, the second row is the second point x2 evaluated at each of the n monomials,
+and so on.
+
+The easiest way to create this matrix is to write the functions above the matrix and the points
+to the left of the matrix as is shown below. Then evaluate the functions at the corresponding points.
 """
