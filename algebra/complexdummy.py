76945f242a8215989b4cd887223f1e1525d148fb
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 12:15:16 -0500

    Add and subtract

--- algebra/complexdummy.py
+++ algebra/complexdummy.py
@@ -6,3 +6,8 @@ class Complex:
     def __init__(z, x, y):
         z.re = x # real component
         z.im = y # imaginary
+
+    def addt(z1, z2): # add z1 + z2
+        return Complex(z1.re + z2.re, z1.im, z2.im)
+
+    def subt(z1, z2): # subtract
