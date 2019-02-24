e220cab600ea07876b27944781052cff61de22b9
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 14:19:53 -0500

    work on funcs

--- calc/gaussianintegration.py
+++ calc/gaussianintegration.py
@@ -16,7 +16,27 @@ euler = 0.5772156649 # euler's constant
 def f(x): # integrand which we will integrate
     retrun exp(-x)
 
-def gauss(npts, job, a, b, x, w):
+def gauss(npts, job, a, b, x, w): # the gaussian integration
     m = i = j = t = t1 = pp = p1 = p2 = p3 = 0 # start all points at 0
     eps = 3.E-14 # accuracy...not sure what will work at this point
-
+    m = (npts + 1)/2
+    for i inrange(1, m+1):
+        t = cos(math.pi*(float(i) - .25)/(float(npts)+.5))
+        t1 = 1
+        while((abs(t-t1))>=eps):
+            p1 = 1
+            p2 = 0
+            for j in range(1, npts + 1):
+                p3 = p2
+                p2 = p1
+                p1 = ((2*float(j)-1)*t*p2 - (float(j)-1)*p3)/(float(j))
+            pp = npts*(t*p1-p2)/(t*t-1)
+            t1 = t
+            t = t1 - p1/pp
+        x[i - 1] = -t
+        x[npts - i] = t
+        w[i - 1] = 2/((1-t*t)*pp*pp)
+        w[npts - i] = w[i - 1]
+    if job == 0:
+        for i in range(0, npts):
+            
