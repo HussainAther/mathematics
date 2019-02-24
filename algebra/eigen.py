6e6e5c8aacc650b1a5e0621620d1230832800a85
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 18:06:35 -0500

    initial commit

---
+++ algebra/eigen.py
@@ -0,0 +1,10 @@
+from numpy import *
+from numpy.linalg import eig
+
+"""
+Solve the matrix eigenvalue problem
+"""
+
+# change this to whatever matrix to solve.
+I = array( [[2/3, -1/4, -1/4], [-1/4, 2/3, -1/4], [-1/4, -1/4, 2/3]])
+
