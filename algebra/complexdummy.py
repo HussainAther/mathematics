13179e4586f2ce873f1ebd22317ad2bc3bebc657
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 12:18:35 -0500

    print

--- algebra/complexdummy.py
+++ algebra/complexdummy.py
@@ -11,3 +11,12 @@ class Complex:
         return Complex(z1.re + z2.re, z1.im, z2.im)
 
     def subt(z1, z2): # subtract
+        return Complex(z1.re - z2.re, z1.im - z2.im)
+    
+    def mult(z1, z2):
+        return Complex(z1.re * z1.re - z1.im * z2.im, z1.re * z2.im + z1.im * z2.re)
+
+    def __repr__(z): # convert to string for printing
+        return "(%f, %f) " %(z.re, z.im)
+
+print("Operations with two complex numbers\n")
