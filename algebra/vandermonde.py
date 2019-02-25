e4364c925e5cecd8df87f802751dc1526cde9f43
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 19:19:41 -0500

    create vandermonde matrix

--- algebra/vandermonde.py
+++ algebra/vandermonde.py
@@ -12,3 +12,34 @@ and so on. These are the terms of the geometric progression for each row.
 The easiest way to create this matrix is to write the functions above the matrix and the points
 to the left of the matrix as is shown below. Then evaluate the functions at the corresponding points.
 """
+
+def vandermonde(order, dim=1, syms='a b c d'):
+    """Computes a Vandermonde matrix of given order and dimension.
+    Define syms to give beginning strings for temporary variables.
+    Returns the Matrix, the temporary variables, and the terms for the
+    polynomials.
+    """
+    syms = syms.split()
+    n = len(syms)
+    if n < dim:
+        new_syms = []
+        for i in range(dim - n):
+            j, rem = divmod(i, n)
+            new_syms.append(syms[rem] + str(j))
+        syms.extend(new_syms)
+    terms = []
+    for i in range(order + 1):
+        terms.extend(comb_w_rep(dim, i))
+    rank = len(terms)
+    V = zeros(rank)
+    generators = [symbol_gen(syms[i]) for i in range(dim)]
+    all_syms = []
+    for i in range(rank):
+        row_syms = [next(g) for g in generators]
+        all_syms.append(row_syms)
+        for j, term in enumerate(terms):
+            v_entry = 1
+            for k in term:
+                v_entry *= row_syms[k]
+            V[i*rank + j] = v_entry
+    return V, all_syms, terms
