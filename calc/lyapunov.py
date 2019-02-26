3e4e69694d887c334023bfe5dff887954a5e4c05
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 23:44:54 -0500

    initial commit

---
+++ calc/lyapunov.py
@@ -0,0 +1,12 @@
+from vpython.graph import *
+
+"""
+Compute the Lyapunov exponents for the bifurcation plot of the logistic map as a
+function of growth rate.
+"""
+
+m.min = 3.5
+m.max = 4.5
+step = .25
+
+graph
