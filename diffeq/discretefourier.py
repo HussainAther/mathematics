d8ee5c8c6db818f61211ea2cc592c62ae7151f9f
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 21:57:48 -0500

    signal

--- diffeq/discretefourier.py
+++ diffeq/discretefourier.py
@@ -12,3 +12,18 @@ imagr = gdisplay(x=0,y=250,width=600,height=250,title ="Imag Fourier TF", \
                 xtitle = "x", ytitle="TF.Imag",xmax=10.,xmin=−1,ymax=100,ymin=−0.2)
 impart = gvbars(delta = 0.05, color = color.red, display = imagr)
 
+N = 50 # number of points
+signal = zeros((N+1), float)
+twopi = 2*pi
+sq2pi = 1/sqrt(twopi)
+h = twopi/N
+dftz = zeros((Np), complex) # sequence complex elements
+
+def f(signal): # signal function
+    step = twopi/N
+    x = 0
+    for i in range(0, N+1):
+        signal[i] = 30*cos(x**4)
+        sigfig.plot(pos = (x, signal[i]))
+
+def fourier(dftz):
