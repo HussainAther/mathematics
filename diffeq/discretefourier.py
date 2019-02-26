5822c203c0d1826fd03f7ca45cf0102cda855d99
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 21:54:48 -0500

    begin

--- diffeq/discretefourier.py
+++ diffeq/discretefourier.py
@@ -1,1 +1,10 @@
+from vpython.graph import *
+import cmath # for complex math functions
+
 # discretefourier.py: Discrete Fourier Transform using built-in complex numbers
+
+sgingr =  gdisplay(x=0, y=0, width=600, height=250, title ="Signal", \
+                xtitle="x", ytitle = "signal", xmax = 2.∗math.pi, xmin = 0,\
+                ymax = 30, ymin = 0)
+
+
