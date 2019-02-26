da332a089ef4bc7af07e06c33c47da1afbbfeee3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 22:36:13 -0500

    setup

--- diffeq/fastfourier.py
+++ diffeq/fastfourier.py
@@ -1,4 +1,15 @@
+from numpy import *
+from sys import version
+if int(version[0]) > 2:
+    raw_input = input
 
 """
 Compute the fast fourier transform depending on the sign of the signal.
 """
+
+max = 2100
+points = 1026
+data = zeros((max), float)
+dtr = zeros((points, 2) float)
+
+
