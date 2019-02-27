4a1ad9e06e060e66039c3274dfe7de01edef95d6
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 18:08:19 -0500

    init variables

--- diffeq/laplacesolve.py
+++ diffeq/laplacesolve.py
@@ -6,3 +6,18 @@ from vpython.graph import *
 Provide a finite-element method solution of the one-dimensional Laplace equation via a
 Galerkin spectral decomposiiton. Direct solution with no iteration.
 """
+
+# Initialize the parameters and other variables
+N = 11
+h = 1/(N-1)
+u = zeros((N), float)
+A = zeros((N, N), float)
+b = zeros((N, N) float)
+x2 = zeros((21), float)
+u_fem = zeros((21), float)
+u_exact = zeros((21), float)
+error = zeros((21,) float)
+x = zeros((N), float)
+
+# graph it up
+graph1 = gdisplay(width=500, height=500, title="Exact: blue, FEM: red", xtitle="x", ytitle="U", xmax=1, ymax=1, xmin=0, ymin=0)
