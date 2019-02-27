83165d7b992224b9275fa249c226e3cdfba96768
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 21:18:57 -0500

    graph

--- cs/fern.py
+++ cs/fern.py
@@ -1,6 +1,17 @@
 from vpython import *
+import random
 
 """
 Simulate the growth of ferns (fractial) in 3-dimensions. Simply beautiful.
 """
 
+imax = 200000 # points
+x = .5
+y = 0
+z = -2
+xn = 0
+yn = 0
+
+graph1 = display(width=500, height=500, forward=(-1,1),\
+                title="3D Fractal Fern (rotate via right mosue button)", range=10)
+graph1.show_rendertime = True
