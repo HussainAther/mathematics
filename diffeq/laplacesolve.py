551a42fe503dfa23d32e5d9d7202b90e9990f562
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 17:54:29 -0500

    initial commit

---
+++ diffeq/laplacesolve.py
@@ -0,0 +1,8 @@
+from numpy import *
+from numpy.linalg import solve
+from vpython.graph import *
+
+"""
+Provide a finite-element method solution of the one-dimensional Laplace equation via a
+Galerkin spectral decomposiiton. Direct solution with no iteration.
+"""
