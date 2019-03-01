3fed3ad76c2826aee8acd546b8da448a054d367a
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 19:04:20 -0500

    code and comment

--- stat/stochasticfrontieranalysis.py
+++ stat/stochasticfrontieranalysis.py
@@ -19,11 +19,32 @@ def get_px(stock, start, end):
 
 px = pd.DataFrame({n: get_px(n, '1/1/2009', '6/1/2012') for n in names})
 
+"""
+For the portfolio construction, we’ll compute momentum over a certain lookback, then
+rank in descending order and standardize:
+"""
+
 def calc_mom(price, lookback, lag):
     mom_ret = price.shift(lag).pct_change(lookback)
     ranks = mom_ret.rank(axis=1, ascending=False)
     demeaned = ranks - ranks.mean(axis=1)
     return demeaned / demeaned.std(axis=1)
 
+"""
+With this transform function in hand, we can set up a strategy backtesting function
+that computes a portfolio for a particular lookback and holding period (days between
+trading), returning the overall Sharpe ratio:
+"""
 compound = lambda x : (1 + x).prod() - 1
 daily_sr = lambda x: x.mean() / x.std()
+
+def strat_sr(prices, lb, hold):
+    # Compute portfolio weights
+    freq = '%dB' % hold
+    port = calc_mom(prices, lb, lag=1)
+    daily_rets = prices.pct_change()
+    # Compute portfolio returns
+    port = port.shift(1).resample(freq, how='first')
+    returns = daily_rets.resample(freq, how=compound)
+    port_rets = (port * returns).sum(axis=1)
+    return daily_sr(port_rets) * np.sqrt(252 / hold)
