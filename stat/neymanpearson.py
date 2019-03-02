c9e5f8498b5de74559ff46be90f40175d9b05d77
Syed Hussain Ather <shussainather@gmail.com>
2019-03-01 23:11:08 -0500

    initial commit

---
+++ stat/neymanpearson.py
@@ -0,0 +1,7 @@
+
+"""
+If we compare the power of two different tests that have the same size a, we can create
+a formula about the one test h(r) compared against the other that uses a likelihood ratio l(r).
+
+
+"""
