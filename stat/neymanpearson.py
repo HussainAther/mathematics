1c1bbbbac5056872f1f7cd844bee0795f2babefc
Syed Hussain Ather <shussainather@gmail.com>
2019-03-02 15:05:38 -0500

    funct

--- stat/neymanpearson.py
+++ stat/neymanpearson.py
@@ -1,8 +1,30 @@
 
 """
+Neyman-Pearson Lemma: Let L(theta|x) denote hte likelihood function for the random variable X corresponding to the
+probability measure P_theta. If there exists a critical region C of size alpha and a nonnegative constant
+k such that
+
+L(theta1|x)/L(theta0|x) >= k for x in region C and L(theta1|x) <= L(theta0|x) for x not in region C,
+
+then C is the most powerful critical region of size alpha.
+
 If a random sample is taken from a distribution with parameter t, a hypothesis is a simple hypothesis
 if the hypothesis uniquely specififes the distribution of the population from which the sample is taken.
 Any other hypothesis is a composite hypothesis.
 
-
+The critical region is the region of values that corresponds to the rejection of the null hypothesis at some chosen probability level.
 """
+
+def neymanPearson(funct):
+    if 
+
+def likelihoodFunction(theta):
+    """
+    For some likelihood function
+    """
+    if theta = theta1:
+        return .5
+    elif theta = theta0:
+        return .25
+    else:
+        return .1
