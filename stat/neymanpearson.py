fc7c672b2ef5d25c95143780d5ecb62758384c27
Syed Hussain Ather <shussainather@gmail.com>
2019-03-01 23:35:16 -0500

    typoe

--- stat/neymanpearson.py
+++ stat/neymanpearson.py
@@ -1,6 +1,8 @@
 
 """
-If a random sapmle is taken from a distribution with parameter t, a hypothesis is a simple hypothesis
-if the hypothesis uniquely specififes the distribution of the population from which the sample is taken. 
+If a random sample is taken from a distribution with parameter t, a hypothesis is a simple hypothesis
+if the hypothesis uniquely specififes the distribution of the population from which the sample is taken.
+Any other hypothesis is a composite hypothesis.
+
 
 """
