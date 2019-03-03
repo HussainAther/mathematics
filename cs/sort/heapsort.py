89ba83fdaf2aee8139afd58ec73cf695af46b978
Syed Hussain Ather <shussainather@gmail.com>
2019-03-02 22:50:23 -0500

    initial commit

--- cs/sort/heapsort.py
+++ cs/sort/heapsort.py
@@ -1,5 +1,8 @@
 """
-
+Heap sort is a comparison based sorting technique based on binary heap
+data structure. It is similar to selection sort where we first find the
+maximum element and place the maximum element at the end. We repeat the
+same process for remaining element.
 """
 
 def heapify(arr, n, i):
