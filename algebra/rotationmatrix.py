e6516fddff90b7cfc678bd0ef87676b7304c8a76
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 12:45:02 -0500

    return

--- algebra/rotationmatrix.py
+++ algebra/rotationmatrix.py
@@ -11,3 +11,10 @@ def getRotationMatrix(axis, angle):
     x = np.cos(angle)
     d = 1-c
     s = np.sin(angle)
+
+    R = [[c+d*x*x, d*x*y-s*z, d*x*z+s*y],
+         [d*y*x+s*z, c+d*y*y, d*y*z-s*x],
+         [d*z*x-s*y, d*z*y+s*x, c+d*z*z]]
+
+    return R
+
