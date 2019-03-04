6e6ed3ad974ea6d149816c800bae0299e8d9a912
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 12:43:48 -0500

    Angle

--- algebra/rotationmatrix.py
+++ algebra/rotationmatrix.py
@@ -4,3 +4,10 @@ import numpy as np
 Rotate a matrix along some axis.
 """
 
+def getRotationMatrix(axis, angle):
+    vLen = np.sqrt(sum([xyz*xyz for xyz in axis]))
+    x, y, z, = [xyz/vLen for xyz in axis]
+
+    x = np.cos(angle)
+    d = 1-c
+    s = np.sin(angle)
