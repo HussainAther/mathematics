0623a96ad2eaa2353c6fb3829615b684f1a387a1
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 16:37:59 -0500

    diagonal

--- algebra/jacobi.py
+++ algebra/jacobi.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 Jacobi's method is a way of computing eigenvalues and eigenvectors of a real symmetric matrix.
 """
 
-def Jacobi(a):
+def jacobi(a):
     """
     Compute all eigenvalues and eigenvectors of a real symmetric matrix a of side-length n and output
     d[0...n-1] that has the eigenvalues of a sorted into descending order while v[0...n-1][0...n-1] is a
@@ -17,5 +17,7 @@ def Jacobi(a):
         return
     n = np.size(a,0)
     v = np.matrix([0]*n,[0]*n)
+    np.fill_diagonal(v, 1.0)
+
 
 
