23c837677f3a12e4cf731819ff43e55b35b26fa1
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 14:39:13 -0500

    comment

--- algebra/laguerre.py
+++ algebra/laguerre.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 """
+Laguerre's polynomials are solutison to the second-order differential equation
 
+xy'' + (1 - x)y' + ny = 0
+
+or soutions of
+
+xy'' + (alpha + 1 - x)y' + ny = 0
 """
 
 lp = {
