c92a93e546a849e2c76b032d73dcd155a1024e13
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 16:29:01 -0500

    a size

--- algebra/jacobi.py
+++ algebra/jacobi.py
@@ -5,10 +5,15 @@ import matplotlib.pyplot as plt
 Jacobi's method is a way of computing eigenvalues and eigenvectors of a real symmetric matrix.
 """
 
-def Jacobi(a, i, j, ip, iq):
+def Jacobi(a):
     """
     Compute all eigenvalues and eigenvectors of a real symmetric matrix a of side-length n and output
     d[0...n-1] that has the eigenvalues of a sorted into descending order while v[0...n-1][0...n-1] is a
-    matrix whose columns contain the corresponding nromalized eigenvectors. nrot contains
+    matrix whose columns contain the corresponding nromalized eigenvectors. nrot contains the Jacobi rotations
+    that were required. Only the upper tirangle of a is accessed.
     """
-
+    if np.size(a,0) != np.size(a,1):
+        print("Error: matrix must be symmertic. E.g., of shape nxn")
+        return
+    v = np.matrix
+    for i in
