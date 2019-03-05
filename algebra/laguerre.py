d6dca783d7fb10a5e612d7eb85cb1875a4f00c1f
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 14:40:45 -0500

    bank

--- algebra/laguerre.py
+++ algebra/laguerre.py
@@ -10,6 +10,9 @@ or soutions of
 xy'' + (alpha + 1 - x)y' + ny = 0
 """
 
-lp = {
-
+lp = { # Laguerre polynomials
+    0: 1,
+    1: -x + 1,
+    2: (-1/2)*(x-4* x +2),
+    3: (1/6)*(-x**3 + 9*x**3 - 18*x +6),
 }
