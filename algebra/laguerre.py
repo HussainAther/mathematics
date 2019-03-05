0651b773c2511d4c1e15878a88ca6cb52cb95d78
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 14:53:18 -0500

    typo

--- algebra/laguerre.py
+++ algebra/laguerre.py
@@ -17,20 +17,21 @@ lp = { # Laguerre polynomials
     3: (1/6)*(-x**3 + 9*x**3 - 18*x + 6),
     4: (1/24)*(-x**4 - 16*x**3 + 72*x**2 - 96*x + 24),
     5: (1/120)*(-x**5 + 25*x**4 - 200*x**3 - 96*x + 24),
-    6: (1/720)*(x**6 -36*x**5 + 450*x**4 - 2400*x**3 + 5400*x**@ - 4320*x + 720)
+    6: (1/720)*(x**6 -36*x**5 + 450*x**4 - 2400*x**3 + 5400*x**2 - 4320*x + 720)
 }
 
 def laguerre(x, k):
     """
     Return up to kth order Laguerre polynomials
     """
-    results = [1, 1-x] # the first two polynomials are by definition
+    results = [] # the first two polynomials are by definition
     if k == 0:
-        return results[:1]
+        return [1]
     if k == 1:
-        return result
+        return [1, 1-x]
     else:
         for i in range(2, k+1):
-            poly = # calculate the polynomial using the recurrence relation
+            results.append(((2 + 1 - x) * lp(i) - i*lp(i-1) / (i+1))) # calculate the polynomial using the recurrence relation
+    return results
 
 
