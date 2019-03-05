27e2e2085fe71ceaaffeb97325b086b2feb5001f
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 13:42:30 -0500

    comment

--- algebra/hermite.py
+++ algebra/hermite.py
@@ -1,5 +1,9 @@
 """
-
+Hermite polynomials are a classical orthogonal polynomial sequence. In probability, they give
+rise to the Edgeworth series. In combinatorics, they're an example of an Appell sequence. In numerical
+analysis, they're a Gaussian quadrature. In physics, they're used in eigenstars of the quantum
+harmonic oscillator. In systemsm ,theory, they're used with nonlinear operations on Gaussian noise.
+In random matrix theory, they're used in Wigner-Dyson ensembles.
 """
 
 
