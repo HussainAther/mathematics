1ad2a27346e7caec68da9f81f71761720b932b9c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 13:32:13 -0500

    forward

--- algebra/fourier.py
+++ algebra/fourier.py
@@ -4,3 +4,22 @@ import numpy as np
 Calculate Fourier transform of a set of n real-valued data points.
 """
 
+def forwardFourier(d):
+    """
+    Forward Fourier Transform on data d
+    """
+    for n in range (0 ,Np) :
+        imag = 0.
+        for k in range(0, N):
+            imag += signal [k]∗ sin (( twopi∗k∗n) /N) # imaginary component
+        d [n] = −imag∗sq2pi
+        if d[n] !=0:
+            impart.plot(pos=(n,d[n]))
+
+def realFourier(a):
+    """
+    Perform Fourier transform on list (a) of data points.
+    """
+    if isign == 1:
+        c2 = 0.5
+        four1
