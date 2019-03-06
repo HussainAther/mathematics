1e8de0aa22cbd347f4bf2e3100c0b91b916624c7
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 15:55:13 -0500

    def

--- algebra/convolve.py
+++ algebra/convolve.py
@@ -7,7 +7,11 @@ Fourier transform of the convolution of two functions is equal to the product of
 has peaks that fall to zero in both directions from its maximum.
 """
 
-def convolve(s, t):
+def convolve(s, t, isign=1):
     """
-    Convolve or deconvolve 
+    Convolve or deconvolve a real set of data (s[0...n-1]) with a response functino (r[0...m-1]) in which
+    m is an odd integer <= n. The response should be in wraparound order such that the first half has the impulse
+    response function at positive times while the second half has the impulse function at negative times.
+    isign = 1 for convolution or -1 for deconvolution.
     """
+    
