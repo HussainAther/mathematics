d0d14b1439c13f2b4274ca014d7d134eb101106d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 15:50:25 -0500

    comment

--- algebra/convolve.py
+++ algebra/convolve.py
@@ -3,4 +3,7 @@ import numpy as np
 """
 Convolution or deconvolution of a real set of data in array form a of two functions s(t) and r(t) (written as s(t) * r(t)).
 Fourier transform of the convolution of two functions is equal to the product of their individual Fourier transforms.
+(s) is typicall a signal or data stream that goes on indefinitely in time while r(t) is a "response" function that typically
+has peaks that fall to zero in both directions from its maximum.
+"""
 
