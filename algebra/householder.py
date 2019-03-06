f7bcb6da9b995779e0e7dad346400140bb931995
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 23:48:55 -0500

    comment

--- algebra/householder.py
+++ algebra/householder.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 """
-Householder algorithm reduces an nxn matrix A to tridiagonal form by n - 2 orthogonal transformations.
+Householder algorithm reduces an n x n matrix A to tridiagonal form by n - 2 orthogonal transformations.
+Each transformation annihilates the required part of a whole column and whole corresponding row.
+The Householder matrix P has the form:
 
+P = 1 - 2w dot w^T
+
+in which w is a real vector with |w|^2 = 1. 
 """
