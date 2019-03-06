ef53e822c5f779c6ea4a3f4523a91ba0e3b95442
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 15:49:29 -0500

    initial commit

---
+++ algebra/convolve.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+"""
+Convolution or deconvolution of a real set of data in array form a of two functions s(t) and r(t) (written as s(t) * r(t)).
+Fourier transform of the convolution of two functions is equal to the product of their individual Fourier transforms.
+
