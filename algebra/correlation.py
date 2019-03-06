78c10b58e400a0073cbee7b67025ec45784715e5
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 16:16:18 -0500

    comment

--- algebra/correlation.py
+++ algebra/correlation.py
@@ -1,5 +1,8 @@
 import numprt as np
 
 """
+Similar to convolution, correlation represents two functions by different but generally similar data sets
+and investigates their "correlation" by comparing them both directly superposed and with one of them
+shifted either right or left.
 
 """
