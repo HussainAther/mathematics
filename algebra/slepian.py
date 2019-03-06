90d15031332d2dabfdc3a58a4c070fda2d4c3d11
Syed Hussain Ather <shussainather@gmail.com>
2019-03-06 17:25:21 -0500

    slepian comment

--- algebra/slepian.py
+++ algebra/slepian.py
@@ -5,11 +5,27 @@ Multitaper methods use a principled approach to the trade-off between low leakag
 variance of a power spectral density estimate. Minimizing leak can let you find the tail of a spectrum
 at high frequencies that can be spuriously dominated by leakage from lower frequencies.
 
-Broadening the main lobe of the leakge function (W(s)) lets us give up some frequency resolution
-(parameterized using j_res).
+Broadening the main lobe of the leakge function, or windowing of the fourier transform, (W(s)), lets us give
+up some frequency resolution (parameterized using j_res).
 
 Multitaper models use data length N and choice j_res to solve for the best possible weights
-w_j such that the ones that
+w_j such that the ones that make the leakge smallest amogn all possible choices.
+
+The vector of optimal weights is the eigenvector corresponding to the smallest eigenvalue of the
+symmetric tridiagonal matrix with diagonal elements:
+
+(1/4)[ N^2 - (N - 1 - 2j)^(2)cos(2pi(r)(j_res)/N)] for j in 0 to N - 1
+
+and off-diagonal elements
+
+(-1/2)j(N-j) for j in 1 to N - 1
+
+Multittaper methods also use the next few eigenvectors of this same matrix as good window functions.
+They're orthogonal to the first eigenvector and they give statistically independent estimates which can be
+averaged toegehter to decrease the variance of the final answer. In this case we use k_T (for "taper") to denoate
+the number of such estimates that are averaged. The functions are actually discrete sequences obtained
+as eigenvectors of the previous two equations. We call them Slepian functions or discrete prolate spheroidal
+sequences.
 """
 
 def slepian():
