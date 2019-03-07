cd1021e29c9116d170eeae528c040a49da9cab10
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 18:03:21 -0500

    bg

--- algebra/fasper.py
+++ algebra/fasper.py
@@ -3,6 +3,7 @@ import numpy as np
 from fourier import *
 
 """
-Fast computation of the Lomb Periodogram. We use the Fast Fourier transform
+Fast computation of the Lomb Periodogram. We use the Fast Fourier transform to evluate
+equations of the Lomb normalized periodogram.
 """
 
