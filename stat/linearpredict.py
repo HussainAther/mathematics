fcac8ce732570884db807298fbb9aafea7242402
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 14:49:34 -0500

    init variables

--- stat/linearpredict.py
+++ stat/linearpredict.py
@@ -5,3 +5,11 @@ Linear prediction uses data points equally spaced along a line such that we use
 values to autocorrelate and predict based on them.
 """
 
+def memcof(a, m=5):
+    """
+    Takes a vector of list a. Return the mean square discrepancy as xms and m linear prediction coefficients
+    as a list d.
+    """
+    p = 0
+    n = len(a)
+    wk1 = [0]*len(n)
