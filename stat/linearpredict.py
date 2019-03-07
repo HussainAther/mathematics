b741bbfdf811454a21bd2d05c30aa528349c9b38
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 14:57:04 -0500

    working thru

--- stat/linearpredict.py
+++ stat/linearpredict.py
@@ -10,6 +10,7 @@ def memcof(a, m=5):
     Takes a vector of list a. Return the mean square discrepancy as xms and m linear prediction coefficients
     as a list d.
     """
+    d = [0]*m
     p = 0
     n = len(a)
     wk1 = [0]*len(n)
@@ -20,4 +21,15 @@ def memcof(a, m=5):
     xms = p/n
     wk1[0] = a[0]
     wk2[n-2] = a[n-1]
-    
+    for j in range(1, n-1):
+        wk1[j] = a[j]
+        wk2[j-1] = a[j]
+    for k in range(0, m):
+        num = 0 # numerator
+        den = 0 # denominator
+        for j in range(0, n-k):
+            num += wk[j]*wk2[j]
+            den += np.sqrt(wk1[j]) + np.sqrt(wk2[j])
+        d[k] = 2*num/den
+        xms *= (1-np.sqrt(d[k]))
+
