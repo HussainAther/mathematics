f3c22793a951b8e1a9b065643c78237b8a3b2e4d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 18:05:23 -0500

    bg

--- algebra/fasper.py
+++ algebra/fasper.py
@@ -11,5 +11,9 @@ of equations of Lomb normalized periodogram.
 
 def fastper(x, y, ofac=4, hifac):
     """
-    
+    Given list of data points x[0...n-1] and ordinates y[0...n-1] and oversampling factor ofac, this
+    fills array px[0...nout-1] with increasing sequence of frequencies up to hifac times the "average"
+    Nyquist freuqnecy. It fills array py[0...nout-1] with the Lomb normalized periodogram values at those
+    frequencies. x and y are not altered. The vectors px and py resize to nout if their initial size is less
+    than this. Otehrwise the first nout components are filled.
     """
