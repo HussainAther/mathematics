99f1097ec5478ed7381e91669674f41985b9ca33
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 18:06:04 -0500

    typo

--- algebra/fasper.py
+++ algebra/fasper.py
@@ -15,5 +15,6 @@ def fastper(x, y, ofac=4, hifac):
     fills array px[0...nout-1] with increasing sequence of frequencies up to hifac times the "average"
     Nyquist freuqnecy. It fills array py[0...nout-1] with the Lomb normalized periodogram values at those
     frequencies. x and y are not altered. The vectors px and py resize to nout if their initial size is less
-    than this. Otehrwise the first nout components are filled.
+    than this. Otherwise the first nout components are filled.
     """
+    MACC = 4
