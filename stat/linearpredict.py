c581037ad4e8f82b880ce7a99735529a88c6fd83
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 14:52:29 -0500

    begin vari

--- stat/linearpredict.py
+++ stat/linearpredict.py
@@ -13,3 +13,11 @@ def memcof(a, m=5):
     p = 0
     n = len(a)
     wk1 = [0]*len(n)
+    wk2 = [0]*len(n)
+    wkm = [0]*len(m)
+    for j in range(0, n):
+        p += np.sqrt(a[j])
+    xms = p/n
+    wk1[0] = a[0]
+    wk2[n-2] = a[n-1]
+    
