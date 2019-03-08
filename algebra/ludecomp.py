df215369c2b53b29399d39683e77464b87756b65
Syed Hussain Ather <shussainather@gmail.com>
2019-03-08 00:51:12 -0500

    return lu

--- algebra/ludecomp.py
+++ algebra/ludecomp.py
@@ -12,7 +12,8 @@ def ludecomp(a):
     indx is an output vector that records the row permutation effected by the partial pivoting.
     d is output as +/- 1 depending on whether the number of row interchanges was even or add.
     """
-    n = len(np.size(a,0))
+    n = len(np.size(a,0)) # matrix size
+    vv = [0]*n # implicit scaling of each row
     lu = a
     big = 0
     temp = 0
@@ -26,3 +27,26 @@ def ludecomp(a):
         if big == 0:
             print("Singular matrix in LU decomposition")
             return
+    for k in range(0, n):
+        big = 0
+        for i in range(k, n):
+            temp = vv[i]*abs(lu[i][k])
+            if temp > big:
+                big = temp
+                imax = i
+        if k != imax:
+            for j in range(0, n):
+                temp = lu[imax][j]
+                lu[imax][j] = lu[k][j]
+                lu[k][j] = temp
+            d = -d
+            vv[imax] = vv[k]
+        indx[k] = imax
+        if lu[k][k] == 0:
+            lu[k][k] = TINY
+        for i in range(k+1,n):
+            temp = lu[i][k] /= lu[k][k] # divide by pivot element
+            for j in range(k+1, n): # reduce remaining submatrix
+                lu[i][j] -= temp*lu[k][j]
+    return lu
+
