285c5a1cce53528dd49e7902e66b60c0b6d67f86
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 23:13:27 -0500

    bg

--- algebra/wavelet.py
+++ algebra/wavelet.py
@@ -4,4 +4,8 @@ import numpy as np
 Like the fast Fourier transform, discrete wavelet transform is a fast, linear operation
 that operates on a data vector with length of an integer power of 2 to trasnform it into a numerically
 different vector of the same length.
+
+Wavelet filter coefficients are a particular set of numbers to describe wavelets.
+DAUB4 is the simplest and most localized member. It has only four coefficients.
 """
+
