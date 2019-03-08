c50b213d045d58c0a12a53bddefe6b58f84f8ea9
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 23:11:07 -0500

    initial commit

---
+++ algebra/wavelet.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+"""
+Like the fast Fourier transform, discrete wavelet transform is a fast, linear operation
+that operates on a data vector with length of an integer power of 2 to trasnform it into a numerically
+different vector of the same length.
+"""
