cee7b238eff8736c79a11b5ab7006d15efd11929
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 23:17:17 -0500

    pyramid commet

--- algebra/wavelet.py
+++ algebra/wavelet.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 """
-Like the fast Fourier transform, discrete wavelet transform is a fast, linear operation
+Like the fast Fourier transform (FFT), discrete wavelet transform (DWT) is a fast, linear operation
 that operates on a data vector with length of an integer power of 2 to trasnform it into a numerically
 different vector of the same length.
 
 Wavelet filter coefficients are a particular set of numbers to describe wavelets.
-DAUB4 is the simplest and most localized member. It has only four coefficients.
+DAUB4 is the simplest and most localized member. It has only four coefficients. DAubs and Daub4i are
+other examples.
+
+DWT applies the pyramidal algorithm: wavelet coefficient matrix hierarchically, first to the full data vector of length n, then to
+the "smooth" vector of length n/2, then to the "smooth-smooth" vector of length n/4, and so on until a trivial
+number of smooth components remain. It outputs the remaining components and all the "detail" components
+that were accumulated along the way.
 """
 
 def WT1(a):
@@ -17,6 +23,6 @@ def WT1(a):
     if n <4:
         print("a must be larger than three members")
         return
-    
+
 
 
