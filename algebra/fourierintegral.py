b8160df93789bd1f92e536b5fb8aaabd35891ba1
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 21:38:13 -0500

    def

--- algebra/fourierintegral.py
+++ algebra/fourierintegral.py
@@ -15,6 +15,14 @@ enough to imply several cycles in the interval [a, b], then the value of I is ty
 by the first-order error.
 
 Given sampled points h_j, we approximate h(t) everywhere in the interval [a, b] by interpolation on nearby h_j's.
-
 """
 
+def DFTcor(w, delta, a, b):
+    """
+    For an integral approximated by a discrete Fourier transform, compute the correction factor
+    that multiplies the DFT and the endpoint correction to be added. Input is angular frequency w, stepsize delta,
+    lower and upper limits of integral a and b, and array endpoints contain the first 4 and last 4 functiona values.
+    The correction factor is returned as crofac while the real and imaginary parts of the endpoint
+    correction are returned as corre and corim.
+    """
+    
