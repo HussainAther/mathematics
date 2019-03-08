7f20ea291d981461baab6626287c45a893b934b5
Syed Hussain Ather <shussainather@gmail.com>
2019-03-08 16:21:36 -0500

    opt comment

--- algebra/ellipsoid.py
+++ algebra/ellipsoid.py
@@ -9,6 +9,17 @@ A hyperplane is the set of points that satisfies the linear equation ax = b (whe
 A convex set K is a set of points such that  ∀x, y ∈ K, λx+(1−λ)y ∈ K, where λ ∈ [0, 1]. A Convex Body is a closed and bounded
 convex set.
 
+We observe if K ⊆ Rn is a convex set and p ∈ Rn is a point, then one of the following holds:
+(i) p ∈ K
+(ii) there is a hyperplane that separates p from K
+
+A polynomial time Separating Oracle for a convex set K is a procedure which given x, either tells that x ∈ K or returns a hyperplane
+separating x from K. The procedure should run in polynomial time. Note that from the previous observation, such a plane is guaranteed to exist.
+
+We optimize by reducing an objective function to a series of feasibility problems by doing the following.
+We start with an estimate of the maximum value, co, and check for feasibility of the folloiwng system:
+
+
 """
 
 def ellipsoid(Eo, Vl):
