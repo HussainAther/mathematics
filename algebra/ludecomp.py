236040796fed5a72c9f86fc7d67818032bc3ffd8
Syed Hussain Ather <shussainather@gmail.com>
2019-03-08 00:42:11 -0500

    initial commit

---
+++ algebra/ludecomp.py
@@ -0,0 +1,8 @@
+import numpy as numpy
+
+"""
+Lower-upper (LU) decomposition or factorization factors a matrix as the product of a lower triangular
+matrix and an upper triangular matrix. The product sometimes includes a permutation matrix as well.
+It can be viewed as the matrix form of Gaussian elimination.
+"""
+
