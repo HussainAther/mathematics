cc8550ac8cf4d4eb1abc4ce541e8fa66027f0bd6
Syed Hussain Ather <shussainather@gmail.com>
2019-03-08 00:24:55 -0500

    input

--- algebra/gauss.py
+++ algebra/gauss.py
@@ -1,6 +1,12 @@
 import numpy as np
 
-def gj()
+def gj(a, n)
     """
-    Gaussian-Jordan elimination
+    Gaussian-Jordan elimination on two input matrices represented by a and b.
     """
+    n = np.size(a,0)
+    indxc = [0]*n
+    ipiv = [0]*n
+    indxr = [0]*n
+    for j in range(0, n):
+
