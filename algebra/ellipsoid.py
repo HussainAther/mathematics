cb4ce7dffabce0154a030075e8a67aff018b7c31
Syed Hussain Ather <shussainather@gmail.com>
2019-03-08 16:16:17 -0500

    definitions lol

--- algebra/ellipsoid.py
+++ algebra/ellipsoid.py
@@ -5,7 +5,8 @@ Ellipsoid algorithm for linear programming uses the ellipsoid method to derive
 the first polynomial time algorithm for linear programming. Theoretically better than
 Simplex algorithm which has an exponential running time in its worst case.
 
-A hyperplane is the
-
+A hyperplane is the set of points that satisfies the linear equation ax = b (where a, x, and b are ∈ R^n).
+A convex set K is a set of points such that  ∀x, y ∈ K, λx+(1−λ)y ∈ K, where λ ∈ [0, 1]. A Convex Body is a closed and bounded
+convex set.
 
 """
