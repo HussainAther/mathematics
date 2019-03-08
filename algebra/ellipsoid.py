397eb74539788cd413d26e07011f5f784c300c4d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-08 16:13:31 -0500

    initial commit

---
+++ algebra/ellipsoid.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+"""
+Ellipsoid algorithm for linear programming uses the ellipsoid method to derive
+the first polynomial time algorithm for linear programming. Theoretically better than
+Simplex algorithm which has an exponential running time in its worst case.
+"""
