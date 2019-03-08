1bdb09d42d95ecd0af723f3b40eed74b907f45e9
Syed Hussain Ather <shussainather@gmail.com>
2019-03-08 00:25:30 -0500

    comment

--- algebra/gauss.py
+++ algebra/gauss.py
@@ -5,8 +5,10 @@ def gj(a, n)
     Gaussian-Jordan elimination on two input matrices represented by a and b.
     """
     n = np.size(a,0)
-    indxc = [0]*n
+    indxc = [0]*n # bookkeeping during pvots
     ipiv = [0]*n
     indxr = [0]*n
-    for j in range(0, n):
+    for j in range(0, n): # main loop over columns that we reduce
+        ipiv[j] = 0
+
 
