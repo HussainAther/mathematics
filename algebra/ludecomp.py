7128e3982d261abdb20911972cf7c26ee28a8a1e
Syed Hussain Ather <shussainather@gmail.com>
2019-03-08 00:43:33 -0500

    func comment

--- algebra/ludecomp.py
+++ algebra/ludecomp.py
@@ -6,3 +6,9 @@ matrix and an upper triangular matrix. The product sometimes includes a permutat
 It can be viewed as the matrix form of Gaussian elimination.
 """
 
+def ludecomp(a):
+    """
+    Given a matrix a, replace it iwth the LU decomposition of a rowwise permutation of itself.
+    indx is an output vector that records the row permutation effected by the partial pivoting.
+    d is output as +/- 1 depending on whether the number of row interchanges was even or add.
+    """
