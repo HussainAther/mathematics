307b0bef21aa7078c76a63b4b9b1601e4c5e5c11
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 17:58:42 -0500

    new def

--- algebra/coemission.py
+++ algebra/coemission.py
@@ -50,3 +50,8 @@ def similarity(m1, m2):
     s1 = coemission(m1, m2)/np.sqrt(coemission(m1,m1) * coemission(m2,m2))
     s2 = 2*coemission(m1, m2)/(np.sqrt(coemission(m1, m1) + coemission(m2, m2)))
     return s1, s2
+
+def coemissionProbs(m1, m2):
+    """
+    Return co-emission probabilites at all pairs of states in the same class as p, p'.
+    """
