7835f0c53d778950f92991978c86512c43f18df1
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 00:44:00 -0500

    eq

--- algebra/coemission.py
+++ algebra/coemission.py
@@ -3,4 +3,15 @@ import numpy as np
 """
 We use coemission probability to compute the L2-distance between the probability distributions
 of two Markov models.
+
+When using a profile hidden Markov model, it is sometimes sufficient just to focus on the most probable path through the model,
+e.g. when using a profile hidden Markov model to generate alignments. It is, however, well known that profile hidden Markov models
+possess a lot more information than the most probable paths, as they allow the generation of an infinity of sequences, each by a multitude of paths.
+
+Thus, when comparing two profile hidden Markov models, one should look at the entire spectrum of sequences and probabilities.
+The probability two profile hidden Markov models independently generate the same sequence, we compute the co-emission probability:
+
+the summation over PM1(s) * PM2 (s)
+
+in which PM1 and PM2 are the probabilities for either model to produce a sequence s.
 """
