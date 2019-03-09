996c07dc72c9fcc971eeca245a5ebb2f5c028525
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 01:24:48 -0500

    finish sim

--- algebra/coemission.py
+++ algebra/coemission.py
@@ -48,4 +48,6 @@ def similarity(m1, m2):
     """
     Return the similary between two hidden Markov models m1 and m2.
     """
-    
+    s1 = coemission(m1, m2)/np.sqrt(coemission(m1,m1)*coemission(m2,m2))
+    s2 = 2*coemission(m1, m2)/(np.sqrt(coemission(m1, m1) + coemission(m2, m2)))
+    return s1, s2
