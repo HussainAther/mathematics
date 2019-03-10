adb22ee780e9dc9b1dc3d32741168bc2b4b6b4c0
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:19:26 -0500

    initial commit

---
+++ cs/puzzles/courtyard.py
@@ -0,0 +1,15 @@
+
+"""
+Consider the following tiling problem. We have a courtyard with 2n × 2n squares and we
+need to tile the courtyard using L-shaped tiles or trominoes. Each trominoe consists of
+three square tiles attached to form an L shape as shown below.
+
+Can this be done without spilling over the boundaries, breaking a tromino or having
+overlapping trominoes? The answer is no, simply because 2^n × 2^n = 2^2n is not divisible
+by 3, only by 2. However, if there is one square that can be left untiled, then 22n – 1 is
+divisible by 3. Can you show this? We, therefore, have hope of properly tiling a 2^n × 2^n
+courtyard with one square that we can leave untiled because, for example, there is a statue
+of your favorite President on it.
+
+We’ll call this square that can be left untiled the missing square.
+"""
