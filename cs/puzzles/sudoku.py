78f4e347880cb1e495d758f3cd693c91b5d4adbb
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:26:22 -0500

    def check

--- cs/puzzles/sudoku.py
+++ cs/puzzles/sudoku.py
@@ -27,3 +27,21 @@ def findNextCellToFill(grid):
             if grid[x][y] == 0:
                 return x,y
     return -1,-1
+
+def isValid(grid, i, j, e):
+    """
+    Check if the position is valid.
+    """
+    rowOk = all([e != grid[i][x] for x in range(9)])
+    if rowOk:
+        columnOk = all([e != grid[x][j] for x in range(9)])
+        if columnOk:
+            #finding the top left x,y co-ordinates of
+            #the section or sub-grid containing the i,j cell
+            secTopX, secTopY = 3 *(i//3), 3 *(j//3)
+            for x in range(secTopX, secTopX+3):
+                for y in range(secTopY, secTopY+3):
+                    if grid[x][y] == e:
+                        return False
+            return True
+    return False
