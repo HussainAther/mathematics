83f5ad34a79cf7c3b12d59432af9479353b29073
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 19:42:23 -0500

    bg

---
+++ cs/puzzles/party.py
@@ -0,0 +1,12 @@
+
+"""
+There is a party to celebrate celebrities that you get to attend because you won a ticket at
+your office lottery. Because of the high demand for tickets you only get to stay for one
+hour but you get to pick which one since you received a special ticket. You have access
+to a schedule that lists when exactly each celebrity is going to attend the party. You want
+to get as many pictures with celebrities as possible to improve your social standing. This
+means you wish to go for the hour when you get to hob-nob with the maximum number of
+celebrities and get selfies with each of them.
+
+
+"""
