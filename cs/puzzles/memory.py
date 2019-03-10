a23810531b098e518b281ec456b9b031800795b5
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:47:39 -0500

    bg

---
+++ cs/puzzles/memory.py
@@ -0,0 +1,7 @@
+"""
+Here’s a cute coin row game that corresponds to an optimization problem. We have a set
+of coins in a row all with positive values. We need to pick a subset of the coins to
+maximize their sum, but with the additional constraint that we are not allowed to pick two
+adjacent coins.
+
+"""
