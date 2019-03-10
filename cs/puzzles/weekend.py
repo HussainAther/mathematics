c4bc4532b6af98e0ffa43601d651af378919ac82
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:43:27 -0500

    it's like high school musical

--- cs/puzzles/weekend.py
+++ cs/puzzles/weekend.py
@@ -7,5 +7,9 @@ every one of your friends is going to get invited on exactly one of those two da
 are still worried about pairs of your friends who intensely dislike each other and want to
 invite them on different days.
 
-
+Now, you are a bit worried because you don’t know if you can pull this off. If you
+had a small social circle like the one below, it would be easy. Recall that in the
+graph below, the vertices are friends and an edge between a pair of vertices mean
+that the two corresponding friends dislike each other, and cannot be invited to the
+party on the same night.
 """
