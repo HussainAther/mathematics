b3b4e3d38af5c4244e2c8e75146ea5f3b926fb92
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:47:58 -0500

    question

--- cs/puzzles/memory.py
+++ cs/puzzles/memory.py
@@ -4,4 +4,15 @@ of coins in a row all with positive values. We need to pick a subset of the coin
 maximize their sum, but with the additional constraint that we are not allowed to pick two
 adjacent coins.
 
+Given:
+
+14    3    27    4    5    15    1
+
+You should pick 14, skip 3, pick 27, skip 4 and 5, pick 15 and skip 1. This gives a total of
+56, which is optimal. Note that alternately picking and skipping coins does not work for
+this example (or in general). If we picked 14, 27, 5 and 1, we would only get 47. And if
+we picked 3, 4, and 15, we would get a pathetic score of 22.
+
+Can you find the maximum value for the coin row problem below?
+
 """
