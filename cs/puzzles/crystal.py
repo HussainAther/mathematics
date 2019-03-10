3fd0881398157230f43ae98c0f9f8498992e166c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 20:33:40 -0500

    more info

--- cs/puzzles/crystal.py
+++ cs/puzzles/crystal.py
@@ -6,5 +6,12 @@ You are tasked with determining the “hardness coefficient” of a set of ident
 balls. The famous Shanghai Tower completed in 2015 has 128 floors, and you have to
 figure out from how high you can drop one of these balls so it doesn’t break but rather
 bounces off the ground below. We will assume that the surrounding area has been
-evacuated while you conduct this important experiment. 
+evacuated while you conduct this important experiment.
+
+What you have to report to your boss is the highest floor number of the Shanghai Tower
+from which the ball does not break when dropped. This means that if you report floor f,
+the ball does not break at floor f, but does break at floor f + 1. Else you would have
+reported f + 1. Your bonus depends on how high a floor you report, and if you report a
+floor f from which the ball breaks, you face a stiff fine, which you want to avoid at all
+costs.
 """
