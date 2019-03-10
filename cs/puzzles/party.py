da2cfe61d565e1fd6ff3c79e8378ad547f28f21b
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 19:43:05 -0500

    question

--- cs/puzzles/party.py
+++ cs/puzzles/party.py
@@ -8,5 +8,12 @@ to get as many pictures with celebrities as possible to improve your social stan
 means you wish to go for the hour when you get to hob-nob with the maximum number of
 celebrities and get selfies with each of them.
 
+We are given a list of intervals that correspond to when each celebrity comes and goes.
+Assume that these intervals are , where  and  correspond to hours. That is, the
+interval is closed on the left hand side and open on the right hand side. This just means
+that the celebrity will be partying on and through the th hour, but will have left when the
+th hour begins. So even if you arrive on dot on the th hour, you will miss this particular
+celebrity.
 
+When is the best time to attend the party? That is, which hour should you go to?
 """
