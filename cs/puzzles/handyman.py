bee5fc714edd5c81ea1aa502b7f95b576c0c43c7
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:34:00 -0500

    initial commit

---
+++ cs/puzzles/handyman.py
@@ -0,0 +1,9 @@
+
+"""
+A handyman has a whole collection of nuts and bolts of different sizes in a bag. Each nut
+is unique and has a corresponding unique bolt, but the disorganized handyman has
+dumped them all into one bag and they are all mixed up. How best to “sort” these nuts
+and attach each to its corresponding bolt?
+
+
+"""
