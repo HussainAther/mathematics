bc6b298303ef60328e471de7aa2e92b927cbb7bf
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 20:47:07 -0500

    def

--- cs/puzzles/crystal.py
+++ cs/puzzles/crystal.py
@@ -1,4 +1,3 @@
-
 """
 Solving without import.
 
@@ -79,5 +78,11 @@ and 19 drops of the second ball for a total of 21. Onto floor 60 and so on. Tryi
 Our purpose here is not to just solve a specific 128-floor problem. Is there a general
 algorithm where given an n-floor building and given two balls we can show a symbolic
 worst-case bound of func(n) where func is some function? Then, we can apply this
-algorithm to our specific 128-floor problem. 
+algorithm to our specific 128-floor problem.
 """
+
+def howHardIsTheCrystal(n, d):
+    """
+    If d is too large in the setting the first digit to 1
+    exceeds the number of floors.
+    """
