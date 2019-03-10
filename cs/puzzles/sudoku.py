aa5b975e4af30ad59a7be9b43389b07fc890a196
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:24:45 -0500

    bg

--- cs/puzzles/sudoku.py
+++ cs/puzzles/sudoku.py
@@ -1,3 +1,9 @@
 """
+Sudoku is a popular number-placement puzzle. The objective is to fill a partially filled 9
+× 9 grid with digits so that each column, each row, and each of the nine 3 × 3 sub-grids or
+sectors that compose the grid contains all of the digits from 1 to 9.
 
+These constraints are used to determine the missing numbers. In the puzzle below,
+several sub-grids have missing numbers. Scanning rows (or columns as the case may be)
+can tell us where to place a missing number in a sector. 
 """
