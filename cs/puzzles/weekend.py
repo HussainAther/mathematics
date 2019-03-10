9bad75b1a7d8dedeefddd85b8547c9be05f31b7c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:43:07 -0500

    bg

--- cs/puzzles/weekend.py
+++ cs/puzzles/weekend.py
@@ -1,4 +1,11 @@
 
 """
+You have gotten into trouble with some of your, shall we say, difficult friends because
+they have realized that they are not being invited to your house parties. So you announce
+that you are going to have dinners on two consecutive nights, Friday and Saturday, and
+every one of your friends is going to get invited on exactly one of those two days. You
+are still worried about pairs of your friends who intensely dislike each other and want to
+invite them on different days.
+
 
 """
