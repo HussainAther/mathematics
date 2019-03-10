d6dcefef0423be101887003215506f3a93e378ac
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:00:28 -0500

    initail commit

---
+++ cs/puzzles/queens.py
@@ -0,0 +1,12 @@
+"""
+A lovely chess problem.
+
+The 8-Queens problem on a chessboard corresponds to finding a placement of 8 queens
+such that no queen attacks any other queen. This means that
+1. No two queens can be on the same column.
+2. No two queens can be on the same row.
+3. No two queens can be on the same diagonal.
+
+
+
+"""
