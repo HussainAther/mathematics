7bf1bbdfff663f909a721a9a00de4e77d065b4ae
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 20:17:45 -0500

    first card

--- cs/puzzles/mindreading.py
+++ cs/puzzles/mindreading.py
@@ -48,3 +48,7 @@ def outputFirstCard(numbers, oneTwo, cards):
         hidden = oneTwo[1]
         other = oneTwo[0]
         encode = (numbers[oneTwo[1]] - numbers[oneTwo[0]]) % 13
+        
+    print ('First card is:', cards[other])
+
+    return hidden, other, encode
