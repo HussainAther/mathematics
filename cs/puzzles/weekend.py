bfa2345d5f3329309059c28b7c7ef0f1cf742eae
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 22:44:09 -0500

    graphs

--- cs/puzzles/weekend.py
+++ cs/puzzles/weekend.py
@@ -15,8 +15,47 @@ party on the same night.
 
 To summarize:
 1. Each of your friends must attend exactly one of the two dinners.
-2. If A dislikes B or B dislikes A, they cannot both be in the same dinner party. 
+2. If A dislikes B or B dislikes A, they cannot both be in the same dinner party.
 
 Can you invite all of your friends A through I above following your self-imposed rules?
 Or are you going to have go to back on your word, and not invite someone?
 """
+
+dgraph = { 'B': ['C'],
+           'C': ['B', 'D'],
+           'D': ['C', 'E', 'F'],
+           'E': ['D'],
+           'F': ['D', 'G', 'H', 'I'],
+           'G': ['F'],
+           'H': ['F'],
+           'I': ['F'],
+          'F1': ['D1', 'I1', 'G1', 'H1'],
+          'B1': ['C1'],
+          'D1': ['C1', 'E1', 'F1'],
+          'E1': ['D1'],
+          'H1': ['F1'],
+          'C1': ['D1', 'B1'],
+          'G1': ['F1'],
+          'I1': ['F1']}
+
+dgraph2 = {'F': ['D', 'I', 'G', 'H'],
+           'B': ['C'],
+           'D': ['C', 'E', 'F'],
+           'E': ['D'],
+           'H': ['F'],
+           'C': ['D', 'B'],
+           'G': ['F'],
+           'I': ['F'],
+           'A1': ['B1', 'C1'],
+           'B1': ['A1', 'C1'],
+           'C1': ['A1', 'B1']}
+
+dgraph3 = {'A': ['B'],
+           'B': ['A'],
+           'C': ['D'],
+           'D': ['C', 'E', 'F'],
+           'E': ['D'],
+           'F': ['D', 'G', 'H', 'I'],
+           'G': ['F'],
+           'H': ['F'],
+           'I': ['F']}
