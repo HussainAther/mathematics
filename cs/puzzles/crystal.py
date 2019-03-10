10e3a6f2830812d64f5c9c091f3c9099e393d54a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 20:32:10 -0500

    initial commit

---
+++ cs/puzzles/crystal.py
@@ -0,0 +1,10 @@
+
+"""
+Solving without import.
+
+You are tasked with determining the “hardness coefficient” of a set of identical crystal
+balls. The famous Shanghai Tower completed in 2015 has 128 floors, and you have to
+figure out from how high you can drop one of these balls so it doesn’t break but rather
+bounces off the ground below. We will assume that the surrounding area has been
+evacuated while you conduct this important experiment. 
+"""
