770462e307ab6cc6f0fd862d55b40ef6e2dffe9b
Syed Hussain Ather <shussainather@gmail.com>
2019-03-09 19:30:43 -0500

    initial commit

---
+++ cs/puzzles/conform.py
@@ -0,0 +1,6 @@
+
+"""
+Input is a vector of F's and B's, in terms of forwards and backwards caps
+Output is a set of commands (printed out) to get either all F's or all B's
+Our goal is to perform this in the fewest number of commands.
+"""
