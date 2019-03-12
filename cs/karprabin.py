4a1c1099772b69262367642d82b925b952d9d0c9
Syed Hussain Ather <shussainather@gmail.com>
2019-03-12 00:39:27 -0400

    def

--- cs/karprabin.py
+++ cs/karprabin.py
@@ -1,5 +1,18 @@
 
 """
 The Karp-Rabin algorithm matches a pattern to a string, the basic principle of database search.
+
+It computes scores of the substrings against the main string
 """
+
+
+def search(pat, txt, q):
+    M = len(pat)
+    N = len(txt)
+    i = 0
+    j = 0
+    p = 0    # hash value for pattern
+    t = 0    # hash value for txt
+    h = 1
+  
 
