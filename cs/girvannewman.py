4bf255109ab0336f665828bead47e3c29682ebac
Syed Hussain Ather <shussainather@gmail.com>
2019-03-11 23:30:24 -0400

    import

--- cs/girvannewman.py
+++ cs/girvannewman.py
@@ -1,7 +1,10 @@
+import networkx as nx
+import math
+import csv
+import random as rand
+import sys
 
 """
 The Girvan–Newman algorithm (named after Michelle Girvan and Mark Newman) is a hierarchical
 method used to detect communities in complex systems.
-
-
 """
