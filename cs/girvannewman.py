39b07923ba85e1a829a979863a1ec7f2b4b50c30
Syed Hussain Ather <shussainather@gmail.com>
2019-03-11 23:39:32 -0400

    format

--- cs/girvannewman.py
+++ cs/girvannewman.py
@@ -13,3 +13,6 @@ def buildG(G, file_, delimiter_):
     # read the graph from cgraph.dat file
     reader = csv.reader(open(file_), delimiter=delimiter_)
     for line in reader:
+        if len(line) > 2:
+            if float(line[2]) != 0.0: # for the format of cgraph.dat
+                G.add_edge(int(line[0]),int(line[1]),weight=float(line[2]))
