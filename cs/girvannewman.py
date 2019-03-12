241c2d9c656e3c57aa6665be1fbb8e558d638de3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-11 23:38:45 -0400

    read graph

--- cs/girvannewman.py
+++ cs/girvannewman.py
@@ -6,5 +6,10 @@ import sys
 
 """
 The Girvan–Newman algorithm (named after Michelle Girvan and Mark Newman) is a hierarchical
-method used to detect communities in complex systems. 
+method used to detect communities in complex systems.
 """
+
+def buildG(G, file_, delimiter_):
+    # read the graph from cgraph.dat file
+    reader = csv.reader(open(file_), delimiter=delimiter_)
+    for line in reader:
