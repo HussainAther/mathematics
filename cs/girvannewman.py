7687fa68b262d4fa6d4238fcbda98bc95eb20505
Syed Hussain Ather <shussainather@gmail.com>
2019-03-11 23:53:27 -0400

    centrality

--- cs/girvannewman.py
+++ cs/girvannewman.py
@@ -41,3 +41,4 @@ def gnStep(g):
     init_ncomp = nx.number_connected_components(g) # get the number of components
     ncomp = init_ncomp # initialize with the initial number of components
     while ncomp <= init_ncomp: # loop through the steps as the algorithm dictates
+        w = nx.edge_betweenness_centrality(g, weight="weight")
