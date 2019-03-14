5780ef1206871809fc160498487725aedbf4adff
Syed Hussain Ather <shussainather@gmail.com>
2019-03-14 18:27:40 -0400

    comment

--- calc/sine.py
+++ calc/sine.py
@@ -5,6 +5,9 @@ Calcluate sin(x) as a finite sum.
 """
 
 def sine(x, N):
+    """
+    Discrete method of calculating
+    """
     result = 0
     for i in range(1, N+1):
         num = ((-1)**i-1) * (x**(2*i-1)) # numerator
