a39dea3ee9ba2bee6e8e19d66f1f60aceff2c64a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-15 00:47:23 -0400

    np trapz

--- calc/richardson.py
+++ calc/richardson.py
@@ -8,3 +8,28 @@ y'(t) = - y^2, y(0) = 1 with the trapezoidal method.
 ts = 0 # time start
 te = 5 # time end
 
+y0 = 1 # initial position
+eps = 10e-11 # accuracy
+
+def f(y):
+    """
+    Our function.
+    """
+    return -y**2
+
+maxRows = 20
+initialH = ts - te # step size
+foundSolution = False # to check if we have a solution
+
+h = initialH
+
+# use the 2d matrix to hold Richardson extrapolates
+a = np.zeros(maxRows, maxRows)
+
+integratearray = []
+for i in range(ts, te):
+    integratearray.append(f(i))
+
+a(1, 1) = np.trapz(integratearray, dx = h)
+
+
