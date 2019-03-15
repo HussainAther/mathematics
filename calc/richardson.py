ad92193b91324db4397fb69bef4ce514906fb85f
Syed Hussain Ather <shussainather@gmail.com>
2019-03-15 00:42:15 -0400

    time

--- calc/richardson.py
+++ calc/richardson.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 """
-
+Use Richardson extrapolation to solve the ordinary differential equation
+y'(t) = - y^2, y(0) = 1 with the trapezoidal method.
 """
+
+ts = 0 # time start
+te = 5 # time end
+
