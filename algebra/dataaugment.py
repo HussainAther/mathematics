aaa6f05e7f55a4e46b11034449513d14510b6bb3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-16 17:05:14 -0400

    initial commit

---
+++ algebra/dataaugment.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+"""
+With the data augmentation algorith, we alternate between the I-step (imputation step, analogous to
+the E step of the EM algorithm) and the P-step (posterior step, analogous to M).
+"""
