25a9e22787f7dac343e4d514a7644b50fe253d83
Syed Hussain Ather <shussainather@gmail.com>
2019-03-16 17:06:51 -0400

    initial commit

---
+++ algebra/em.py
@@ -0,0 +1,6 @@
+
+"""
+We use the expectation-maximization (EM) algorithm to iterate over data
+to find a maximum likelihood or maximum a posterior (MAP) estimate of parameters
+for models affected by unobserved latent variables.
+"""
