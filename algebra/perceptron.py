da3147f9e97d4c9b8b7f9b5fac0de80a17ef4362
Syed Hussain Ather <shussainather@gmail.com>
2019-03-15 22:05:41 -0400

    def predict

--- algebra/perceptron.py
+++ algebra/perceptron.py
@@ -9,5 +9,18 @@ of the form
 
 y(x) = f(w^T*phi(x))
 
+with a nonlinear activation function f(.) with as a step function:
 
+f(a) 1 when a >=0 and -1 when a < 0.
 """
+
+def predict(row, weights):
+    """
+    Make predcitions by evaluating candidate weight values in stoachastic
+    gradient descent. After the model is finalized we start making predictions on test data.
+    """
+    activation = weights[0]
+    for i in range(len(row)-1):
+        activation += weights[i + 1] * row[i]
+    return 1.0 if activation >= 0.0 else 0.0
+
