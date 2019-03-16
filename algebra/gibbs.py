a7f3ecb291104fb8384dcb48449a90e095341f0a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-16 01:28:48 -0400

    beta0

--- algebra/gibbs.py
+++ algebra/gibbs.py
@@ -1,4 +1,6 @@
 import numpy as np
+from seaborn import plt
+import pandas as pd
 
 """
 Gibbs sampling works as follows: suppose we have two parameters theta1 and theta 2 and some data x.
@@ -11,3 +13,16 @@ the conditional distributions p(theta1||theta2, x) and p(theta2||theaa1, x) so t
 
 Increment i and repeat K times to draw K samples. No tuning paramters required.
 """
+
+def samplebeta0(y, x, beta_1, tau, mu_0, tau_0):
+    """
+    Find the posterior distributions as described above. Use the log-posterior conditional density
+    in a quadratic form such that the coefficient of x^2 will be tau mu and the coefficient of x^2 will be -tau/2.
+    We can find a log-dependence on beta0
+    """
+    N = len(y)
+    assert len(x) == N
+    precision = tau_0 + tau * N
+    mean = tau_0 * mu_0 + tau * np.sum(y - beta_1 * x)
+    mean /= precision
+    return np.random.normal(mean, 1 / np.sqrt(precision))
