b2eb5af6ae39059844b4f97a82337c6a5f9d6541
Syed Hussain Ather <shussainather@gmail.com>
2019-03-15 23:58:26 -0400

    functions

--- algebra/bayesianlearning.py
+++ algebra/bayesianlearning.py
@@ -2,7 +2,11 @@ import numpy as np
 
 """
 Sequential Sparse Bayesian Learning Algorithm
-If solving a regression problem, initialize beta using a basis function phi1 with hyperparameter
+
+In analyzing sparsity automatic relevance determination causes a subset of paramters
+to be driven to zero. Sparsiy is a mechnism in the context of relevance vector machine.
+
+If solving a regression problem, initialize beta (isotropic noise) using a basis function phi1 with hyperparameter
 alpha1 set using
 
 alphai = si^2 / (qi^2 - si)
@@ -14,9 +18,33 @@ and evaluate hyperparameter alphai = infinity. If qi^2 <= si and alphai < infini
 function phi from teh model and set alphai = infinity.
 
 Then, ff solving regression problem, update beta. If converged terminate repeat the steps of the previous paragraph.
+
+The marginal likelihood is p(t|alpha, beta) = N(t|0, C) in which the covariance
+matrix takes the form
+
+C = (1/beta) I + (1/alpha) phi*phi^T
 """
+
+def logistic(x):
+    """
+    Binary classificaiton logistic function
+    """
+    return 1/(1+np.exp(-x))
+
+def mlf(x):
+    """
+    Multinomial link function. This lets us use multiple GLMs (multiclass) to separate
+    data into more than two classes. Each class is modeled by its own set of weights,
+    and the probability of a data point being a member of class c is returned.
+    """
+    num = np.exp(x) # numerator
+    den = # denominator
+
+def prior():
+    np.random.normal()
 
 def ssbla():
     """
     Sequential Sparse Bayesian Learning Algorithm.
     """
+
