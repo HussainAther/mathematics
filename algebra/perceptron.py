fecd529d6e861bc4c9d5e54384984b271c7f4c00
Syed Hussain Ather <shussainather@gmail.com>
2019-03-15 22:04:02 -0400

    bg

---
+++ algebra/perceptron.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+"""
+Rosenblatt's pecreptron is an example of a linear discriminant model.
+
+It uses a two-class model with an input vector x transformed using a fixed nonlinear
+transformation to give a feature vector phi(x) and create a generalized linear model
+of the form
+
+y(x) = f(w^T*phi(x))
+
+
+"""
