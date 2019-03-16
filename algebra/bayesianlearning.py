d66a52bd0886de25c97b29c38f044e04761a0718
Syed Hussain Ather <shussainather@gmail.com>
2019-03-15 23:17:10 -0400

    import

--- algebra/bayesianlearning.py
+++ algebra/bayesianlearning.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 """
 Sequential Sparse Bayesian Learning Algorithm
@@ -8,6 +9,11 @@ alphai = si^2 / (qi^2 - si)
 
 and evaluate summation and m along with qi and si for all basis functions. Then we select a candidate
 basis function phi. If qi^2 > si and alphai < infinity such that the basis vector phi is already included
-in the model, update alphai using its equation. If qi^2 > si and alpha = infinity
+in the model, update alphai using its equation. If qi^2 > si and alpha = infinity, then add phii to the model
+and evaluate hyperparameter alphai = infinity. If qi^2 <= si and alphai < infinity, then remove the basis
+function phi from teh model and set alphai = infinity.
+
+Then, ff solving regression problem, update beta. If converged terminate repeat the steps of the previous paragraph.
 """
+
 
