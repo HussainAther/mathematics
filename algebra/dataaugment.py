1341cc35ed0eb679431c932df44fe5e465e90f7e
Syed Hussain Ather <shussainather@gmail.com>
2019-03-16 17:19:15 -0400

    typo

--- algebra/dataaugment.py
+++ algebra/dataaugment.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 """
-With the data augmentation algorith, we alternate between the I-step (imputation step, analogous to
+With the data augmentation algorithm, we alternate between the I-step (imputation step, analogous to
 the E step of the EM algorithm) and the P-step (posterior step, analogous to M).
 """
