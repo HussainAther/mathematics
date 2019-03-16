421ef32ec60c581a0ce24e5f912c8943880a0c50
Syed Hussain Ather <shussainather@gmail.com>
2019-03-16 16:26:23 -0400

    def hess

--- algebra/hessian.py
+++ algebra/hessian.py
@@ -13,11 +13,33 @@ by the Hessian matrix (Bishop and Nabney, 2008).
 2. The Hessian forms the basis of a fast procedure for re-training a feed-forward network
 following a small change in the training data (Bishop, 1991).
 
-3. TheinverseoftheHessianhasbeenusedtoidentifytheleastsignificantweights in a network as part
-of network ‘pruning’ algorithms (Le Cun et al., 1990).
+3. The inverse of the Hessian has been used to identify the least significant weights in a
+network as part of network ‘pruning’ algorithms (Le Cun et al., 1990).
 
 4. The Hessian plays a central role in the Laplace approximation for a Bayesian neural network
 (see Section 5.7). Its inverse is used to determine the predic- tive distribution for a trained network,
 its eigenvalues determine the values of hyperparameters, and its determinant is used to evaluate
 the model evidence.
 """
+
+def hessian ( x, the_func, epsilon=1e-8):
+    """Numerical approximation to the Hessian matrix
+    x: array-like
+        The evaluation point
+    the_func: function
+        The function. We assume that the function returns the function value and
+        the associated gradient as the second return element
+    epsilon: float
+        The size of the step
+    """
+
+    N = x.size
+    h = np.zeros((N,N))
+    df_0 = the_func ( x )[1]
+    for i in xrange(N):
+        xx0 = 1.*x[i]
+        x[i] = xx0 + epsilon
+        df_1 = the_func ( x )[1]
+        h[i,:] = (df_1 - df_0)/epsilon
+        x[i] = xx0
+    return h
