3683605ae3731468a0de12839d33c53233c5829c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 03:20:31 -0400

    spacing

--- cs/cmos.py
+++ cs/cmos.py
@@ -6,7 +6,8 @@ We can use complementary metal–oxide–semiconductor (cmos) circuits to implem
 functions using logic gates. We can also use metal-oxide-semiconductor field-effect
 transistor (mosfet) as a type of field-effect transistor to generate different voltages.
 
-This circuit is on page 6 of the following document: https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-004-computation-structures-spring-2009/labs/MIT6_004s09_lab01.pdf
+This circuit is on page 6 of the following document: 
+https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-004-computation-structures-spring-2009/labs/MIT6_004s09_lab01.pdf
 """
 
 Vmeter = 0 # 1st voltage source
