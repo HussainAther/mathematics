8b2c33772846c4b9e2216dd551a61c865030315c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-16 21:14:28 -0400

    comment

--- algebra/sampling/sem.py
+++ algebra/sampling/sem.py
@@ -10,6 +10,9 @@ maximization itself, and the code for standard matrix operations.
 """
 class GMM(object): # Gaussian mixture model
     def __init__(self, X, k=2):
+        """
+        Initialize the Gaussian mixture model with a set of data X.
+        """
         # dimension
         X = np.asarray(X)
         self.m, self.n = X.shape
@@ -18,6 +21,9 @@ class GMM(object): # Gaussian mixture model
         self.k = k
 
     def _init(self):
+        """
+        Initialize the various values pertaining to the data set.
+        """
         # init mixture means/sigmas
         self.mean_arr = np.asmatrix(np.random.random((self.k, self.n)))
         self.sigma_arr = np.array([np.asmatrix(np.identity(self.n)) for i in range(self.k)])
@@ -27,6 +33,9 @@ class GMM(object): # Gaussian mixture model
         #print(self.sigma_arr)
 
     def fit(self, tol=1e-4):
+        """
+        Fit the model using the log likelihood function.
+        """
         self._init()
         num_iters = 0
         ll = 1
@@ -39,4 +48,18 @@ class GMM(object): # Gaussian mixture model
             print("Iteration %d: log-likelihood is %.6f"%(num_iters, ll))
         print("Terminate at %d-th iteration:log-likelihood is %.6f"%(num_iters, ll))
 
-
+    def loglikelihood(self):
+        """
+        Borrow the log likelihood from statistics in determining the best way to fit.
+        """
+        ll = 0
+        for i in range(self.m):
+            tmp = 0
+            for j in range(self.k):
+                #print(self.sigma_arr[j])
+                tmp += sp.stats.multivariate_normal.pdf(self.data[i, :],
+                                                        self.mean_arr[j, :].A1,
+                                                        self.sigma_arr[j, :]) *\
+                       self.phi[j]
+            ll += np.log(tmp)
+        return ll
