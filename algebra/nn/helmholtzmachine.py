4d0faa63d802b7814f3a7346f2b8076cd2f41d0b
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 00:36:21 -0400

    bg

--- algebra/nn/helmholtzmachine.py
+++ algebra/nn/helmholtzmachine.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 """
+The Helmholtz machine was designed to accommodate hierarchical architectures that construct complex
+multilayer representations. The model in- volves two interacting networks, one with parameters G
+that is driven in the top-down direction to implement the generative model, and the other, with
+parameters W , driven bottom-up to implement the recogni- tion model.
 
+The parameters are determined by a modified EM algorithm that results in roughly symmetric updates for the two networks.
 """
