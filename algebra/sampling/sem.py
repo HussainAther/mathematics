70df68d596b5b0d2718d47d51ead9b00b63fe3f6
Syed Hussain Ather <shussainather@gmail.com>
2019-03-16 21:09:01 -0400

    gmm

--- algebra/sampling/sem.py
+++ algebra/sampling/sem.py
@@ -1,32 +1,16 @@
 import numpy as np
 import random
-import skimage as sk
-
-from scipy import ndarray
-from skimage import transform
-from skimage import util
-
 """
 With the supplemental expectation maximization algorithm, we get the covariance matrix by
 using only the code for computing the complete-data covariance matrix, the code for the expectation
 maximization itself, and the code for standard matrix operations.
 """
+class GMM(object):
+    def __init__(self, X, k=2):
+        # dimension
+        X = np.asarray(X)
+        self.m, self.n = X.shape
+        self.data = X.copy()
+        # number of mixtures
+        self.k = k
 
-def random_rotation(image_array: ndarray):
-    """
-    Random degree of rotation.
-    """
-    random_degree = random.uniform(-25, 25)
-    return sk.transform.rotate(image_array, random_degree)
-
-def random_noise(image_array: ndarray):
-    """
-    Make some noise.
-    """
-    return sk.util.random_noise(image_array)
-
-def horizontal_flip(image_array: ndarray):
-    """
-    As easy as 1, 2, 3.
-    """
-    return image_array[:, ::-1]
