317e9acb070f84e2e0054630c251e087a02e44f9
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 04:01:36 -0400

    space

--- stat/randomness.py
+++ stat/randomness.py
@@ -3,6 +3,7 @@ We can define a sequence of numbers as random if there are no correlations among
 If all the number in a sequence are equally likely to occur, it is uniform.
 """
 # Linear congruent power resideue method of generation a psuedo-random sequence of numbers
+
 def lcg(prev): # generate the next number in the sequence. prev is the previous number. M is the upper bound of the interval of numbers we can choose from.
     a = 699224 # some constant
     c = 520499118 # another constant lol
