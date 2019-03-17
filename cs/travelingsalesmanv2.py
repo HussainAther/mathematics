e4f47ac1be4dd9d8eef274ca09c5ccf645fb0a7e
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 16:03:36 -0400

    init variables

--- cs/travelingsalesmanv2.py
+++ cs/travelingsalesmanv2.py
@@ -6,3 +6,13 @@ from visual import sphere,curve,display
 """
 Simpler functions
 """
+
+N = 25
+R = 0.02
+Tmax = 10.0
+Tmin = 1e-3
+tau = 1e4
+
+# Function to calculate the magnitude of a vector
+def mag(x):
+    return sqrt(x[0]**2+x[1]**2)
