4e65282bdee4a53ee81fee986a108a81c31039e3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 15:55:26 -0400

    initia commit

---
+++ calc/gaussianintegrationv2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+"""
+Functions to calculate integration points and weights for Gaussian quadrature.
+
+x,w = gaussxw(N) returns integration points x and integration
+#          weights w such that sum_i w[i]*f(x[i]) is the Nth-order
+#           Gaussian approximation to the integral int_{-1}^1 f(x) dx
+# x,w = gaussxwab(N,a,b) returns integration points and weights
+#           mapped to the interval [a,b], so that sum_i w[i]*f(x[i])
+#           is the Nth-order Gaussian approximation to the integral
+#           int_a^b f(x) dx
+#
+# This code finds the zeros of the nth Legendre polynomial using
+# Newton's method, starting from the approximation given in Abramowitz
+# and Stegun 22.16.6.  The Legendre polynomial itself is evaluated
+# using the recurrence relation given in Abramowitz and Stegun
+# 22.7.10.  
+"""
