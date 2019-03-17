142c982710957b42a2f0bac2be651447c99e994a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 16:04:04 -0400

    def distance

--- cs/travelingsalesmanv2.py
+++ cs/travelingsalesmanv2.py
@@ -16,3 +16,10 @@ tau = 1e4
 # Function to calculate the magnitude of a vector
 def mag(x):
     return sqrt(x[0]**2+x[1]**2)
+
+# Function to calculate the total length of the tour
+def distance():
+    s = 0.0
+    for i in range(N):
+        s += mag(r[i+1]-r[i])
+    return s
