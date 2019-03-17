cb92991c2ea58176374a254ce82e36e5b43522a6
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 16:01:09 -0400

    normalize

--- algebra/linear/banded.py
+++ algebra/linear/banded.py
@@ -29,3 +29,11 @@ def banded(Aa,va,up,down):
         for k in range(1,down+1):
             if m+k<N:
                 v[m+k] -= A[up+k,m]*v[m]
+
+        # Now normalize the pivot row of A and subtract from lower ones
+        for i in range(up):
+            j = m + up - i
+            if j<N:
+                A[i,j] /= div
+                for k in range(1,down+1):
+                    A[i+k,j] -= A[up+k,m]*A[i,j]
