1f49dab64308b85529db90e6b42948b87f8c97f7
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 17:06:08 -0400

    soft

--- algebra/linear/glmnet.py
+++ algebra/linear/glmnet.py
@@ -8,3 +8,13 @@ import matplotlib.pyplot as plot
 
 """
 
+def S(z, gamma):
+    """
+    Lasso coefficient shrinkage function used as a soft limiter such
+    that, if the first input is larger than the second, the output is the
+    first input reduced by the magnitude of the second.
+    """
+    if gamma >= abs(z):
+        return 0.0
+    return (z/abs(z))*(abs(z) - gamma)
+
