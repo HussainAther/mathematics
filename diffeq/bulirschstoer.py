1e6a1124aef3e2635b33b77a72ddb9082ad86cd3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 16:15:46 -0400

    init var

--- diffeq/bulirschstoer.py
+++ diffeq/bulirschstoer.py
@@ -1,6 +1,6 @@
-from math import sin,pi
-from numpy import empty,array,arange
-from pylab import plot,show
+from math import sin, pi
+from numpy import empty, array, arange
+from pylab import plot, show
 
 """
 In numerical analysis, the Bulirsch–Stoer algorithm is a method for the numerical solution of
@@ -12,3 +12,20 @@ Bulirsch and Josef Stoer. It is sometimes called the Gragg–Bulirsch–Stoer (G
 because of the importance of a result about the error function of the modified midpoint method,
 due to William B. Gragg.
 """
+
+g = 9.81 # gravitational acceleration
+l = 0.1 # length
+theta0 = 179*pi/180 #
+
+a = 0.0
+b = 10.0
+N = 100 # Number of "big steps"
+H = (b-a)/N # Size of "big steps"
+delta = 1e-8 # Required position accuracy per unit time
+
+def f(r):
+    theta = r[0]
+    omega = r[1]
+    ftheta = omega
+    fomega = -(g/l)*sin(theta)
+    return array([ftheta,fomega],float)
