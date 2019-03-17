9b639b63b05d0c8c2f3266679ceaac6a07d87ede
Syed Hussain Ather <shussainather@gmail.com>
2019-03-16 21:12:25 -0400

    initial commit

--- algebra/sampling/jensen.py
+++ algebra/sampling/jensen.py
@@ -10,20 +10,20 @@ in various sampling algorithms.
 x = np.linspace(.1, 5, 100)
 y = np.log(x)
 fig, ax = plt.subplots(figsize=(8,4))
-ax.scatter(x, y, color='green', marker='+')
+ax.scatter(x, y, color="green", marker="+")
 ## (p=0.5)
-ax.vlines(x=1, ymin=-2.5, ymax=0, linestyle='--', color='green')
-ax.hlines(y=0, xmin=-.1, xmax=1, linestyle='--', color='green')
-ax.text(x=1.1, y=np.log(.7), s='$f(1)=0$',
-        fontdict={'size': 13})
+ax.vlines(x=1, ymin=-2.5, ymax=0, linestyle="--", color="green")
+ax.hlines(y=0, xmin=-.1, xmax=1, linestyle="--", color="green")
+ax.text(x=1.1, y=np.log(.7), s="$f(1)=0$",
+        fontdict={"size": 13})
 ## (p=0.5)
-ax.vlines(x=4, ymin=-2.5, ymax=np.log(4), linestyle='--', color='green')
-ax.hlines(y=np.log(4), xmin=-.1, xmax=4, linestyle='--', color='green')
+ax.vlines(x=4, ymin=-2.5, ymax=np.log(4), linestyle="--", color="green")
+ax.hlines(y=np.log(4), xmin=-.1, xmax=4, linestyle="--", color="green")
 ax.text(x=4.1, y=np.log(3.1),
-        s='$f(4)=%.2f$'%(np.log(4)),
-        fontdict={'size': 13}
+        s="$f(4)=%.2f$"%(np.log(4)),
+        fontdict={"size": 13}
        )
-ax.plot([1, 4], np.log([1, 4]), color='green', linestyle='dotted')
+ax.plot([1, 4], np.log([1, 4]), color="green", linestyle="dotted")
 # E(x) = (1+4)/2 = 2.5
-ax.vlines(x=2.5, ymin=-2.5, ymax=np.log(2.5), linestyle='--', color='green')
-ax.hlines(y=np.log(2.5), xmin=-.1, xmax=2.5, linestyle='--', color='green')
+ax.vlines(x=2.5, ymin=-2.5, ymax=np.log(2.5), linestyle="--", color="green")
+ax.hlines(y=np.log(2.5), xmin=-.1, xmax=2.5, linestyle="--", color="green")
