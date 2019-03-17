a128d87e693e8b6cf917f6648ac91db198cd8008
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 16:14:14 -0400

    comment

--- diffeq/bulirschstoer.py
+++ diffeq/bulirschstoer.py
@@ -1,4 +1,11 @@
 
 """
-
+In numerical analysis, the Bulirsch–Stoer algorithm is a method for the numerical solution of
+ordinary differential equations which combines three powerful ideas: Richardson extrapolation,
+the use of rational function extrapolation in Richardson-type applications, and the modified
+midpoint method, to obtain numerical solutions to ordinary differential equations (ODEs)
+with high accuracy and comparatively little computational effort. It is named after Roland
+Bulirsch and Josef Stoer. It is sometimes called the Gragg–Bulirsch–Stoer (GBS) algorithm
+because of the importance of a result about the error function of the modified midpoint method,
+due to William B. Gragg.
 """
