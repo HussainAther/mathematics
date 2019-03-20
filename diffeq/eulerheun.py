ea481096baf58ecab0a2f0bbdc9d195e6b0251d4
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 19:04:00 -0400

    riemann

--- diffeq/eulerheun.py
+++ diffeq/eulerheun.py
@@ -4,4 +4,12 @@ import matplotlib.pyplot as plt
 """
 We use the second-order Euler-Heun method to approximate an integral through which we can
 use in integrating an ordinary differential equation.
+
+x(t_n+1) = x(t_n) + integral from t_n to t_n+1 of f(x(s), s)ds
+
+such that x(t) is an exact solution of ẋ = f(x, t).
+
+We use a trapezoild approximation (in contrast to Euler's method of using a Riemann sum).
 """
+
+
