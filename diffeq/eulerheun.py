a5ab0ba6521700d0c038c0ae15b1cf5913d21772
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 19:08:23 -0400

    init arrays

--- diffeq/eulerheun.py
+++ diffeq/eulerheun.py
@@ -9,9 +9,14 @@ x(t_n+1) = x(t_n) + integral from t_n to t_n+1 of f(x(s), s)ds
 such that x(t) is an exact solution of ẋ = f(x, t).
 
 We use a trapezoild approximation (in contrast to Euler's method of using a Riemann sum).
+In this example, we look at a one-dimensional oscillating model.
 """
-a = np.zeros
+
+
+# Initialize arrays
+a = np.zeros(N)
+b = np.zeros(N)
 
 # Step forward
-for n in range(Nt):
+for n in range(N):
     a[n+1] = a[n] + dt*b[n]
