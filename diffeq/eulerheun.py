c516439572cafa286ad8167d9be3e365bd1fb4a8
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 19:02:35 -0400

    comment

--- diffeq/eulerheun.py
+++ diffeq/eulerheun.py
@@ -2,5 +2,6 @@ from numpy import zeros, linspace, pi, cos, array
 import matplotlib.pyplot as plt
 
 """
-
+We use the second-order Euler-Heun method to approximate an integral through which we can
+use in integrating an ordinary differential equation.
 """
