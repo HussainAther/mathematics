cd4b057c06539aead5e5b3b530aa5a1399b12353
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 18:40:59 -0400

    initial commit

---
+++ diffeq/lyapunov.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+"""
+Lyapunov's "second method" or "direct method" for ordinary differential
+equations of the form:
+
+dx/dt = ẋ = f(x)
+
+in which the first equals sign denotes the definition of dx/dt
+"""
