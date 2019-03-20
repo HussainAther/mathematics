da8b6b8a99fd8d909c0dd30fdd80279ed9f6eac6
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 18:42:50 -0400

    comment

--- diffeq/lyapunov.py
+++ diffeq/lyapunov.py
@@ -6,5 +6,10 @@ equations of the form:
 
 dx/dt = ẋ = f(x)
 
-in which the first equals sign denotes the definition of dx/dt
+in which the first equals sign denotes the definition of dx/dt. Suppose
+the equation above has an isolated fixed point at x = 0. If there's a differential function V(x)
+that is positive definite and for which dV/dt = ∇V · f is negative definite on some domain D ∋ 0,
+then 0 is asymptotically stable. if dV/dt is negative semidefinite (e.g., dV/dt = 0 is allowed),
+then 0 is Lypunov stable.
 """
+
