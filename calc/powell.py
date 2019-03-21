ae91ea04cd38d1c0aa3b7615f7fd3b3afa5be819
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 21:59:25 -0400

    weight

--- calc/powell.py
+++ calc/powell.py
@@ -11,4 +11,7 @@ def p(x):
     lam = 100 # to normalize the penalty on a scale of 100
     c = 2*np.sqrt(2) # pull this out in front for simplifying the polynomial
     A = np.array([[c*x[1] + x[2], -x[2], x[2]], [-x[2], x[2], -x[2]], [x[2], -x[2],  c*x[0] + x[2]]])/c]) # polynomials
-    
+    b = np.array([0, -1, 0])
+    v = gaussElim(A, b) # perform Gaussian elimination to find a linear solution
+    weight = x[0] + x[1] + np.sqrt(2)*x[2] # weight used for our solution
+
