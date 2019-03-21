17f9a27f189bbd6edcb0b47027676c24e862efa8
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 20:49:55 -0400

    be

--- diffeq/backwardeuler.py
+++ diffeq/backwardeuler.py
@@ -4,5 +4,9 @@ import numpy as np
 We use the Backward Euler method for evaluating differential eqiations.
 """
 
-
+def be(f, y0, x_vals, h):
+    """
+    Compute the Backawrd Euler method for a function f with initial condition y0,
+    over values x_vals, with step size h.
+    """
 
