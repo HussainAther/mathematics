f05ae0cec846156ae9846e7ac68ee4bdb8296e85
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 21:54:37 -0400

    def

--- calc/powell.py
+++ calc/powell.py
@@ -4,4 +4,8 @@ import numpy as numpy
 Powell's algorithm for finding local minima.
 """
 
-
+def p(x):
+    """
+    For a series of values x, find local minima using the Powell method.
+    """
+    
