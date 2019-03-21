45cd81752890956afdba64c503309acd81f0bc4d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 21:57:35 -0400

    poly

--- calc/powell.py
+++ calc/powell.py
@@ -9,3 +9,6 @@ def p(x):
     For a series of values x, find local minima using the Powell method.
     """
     lam = 100 # to normalize the penalty on a scale of 100
+    c = 2*np.sqrt(2) # pull this out in front for simplifying the polynomial
+    A = np.array([[c*x[1] + x[2], -x[2], x[2]], [-x[2], x[2], -x[2]], [x[2], -x[2],  c*x[0] + x[2]]])/c]) # polynomials
+    
