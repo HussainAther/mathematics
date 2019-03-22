c06ab3015f877475b10a4919f20d62e7f689e72e
Syed Hussain Ather <shussainather@gmail.com>
2019-03-22 15:42:54 -0400

    grad

--- algebra/neuralnetworks/nesterov.py
+++ algebra/neuralnetworks/nesterov.py
@@ -9,3 +9,6 @@ def grad(f, x, deltax):
     Compute the gradient numerically using our function f that takes in x as a variable.
     """
     xval = f(x) # evaluate the function f at x.
+    step = f(x + deltax) # step forward with deltax
+    return (step - xval) / deltax # return the difference over the deltax value
+
