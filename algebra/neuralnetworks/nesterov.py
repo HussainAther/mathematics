938c537b1e64e1a12f14d1433301c985076d9a82
Syed Hussain Ather <shussainather@gmail.com>
2019-03-22 15:34:33 -0400

    grad

--- algebra/neuralnetworks/nesterov.py
+++ algebra/neuralnetworks/nesterov.py
@@ -1,4 +1,10 @@
+import numpy as np
 
 """
 Nesterov's method for proximal gradient descent.
 """
+
+def grad(f, x, deltax):
+    """
+    Compute the gradient numerically using our function f that takes in x as a variable.
+    """
