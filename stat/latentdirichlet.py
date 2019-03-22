3b8dbc2881f6486b7934acb530a34677cb49f5e3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-22 17:09:57 -0400

    initial commit

---
+++ stat/latentdirichlet.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+"""
+In the area of topic modeling, we use latent dirichlet allocation (lda) to classify
+text in a document about a specific topic. We build a topic-per-document model and
+words-per-topic model using Dirichlet distributions.
+"""
+
