67dd106818859d803e8f0f7925088202a65c5a5c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-22 15:45:26 -0400

    def nes

--- algebra/neuralnetworks/nesterov.py
+++ algebra/neuralnetworks/nesterov.py
@@ -12,3 +12,9 @@ def grad(f, x, deltax):
     step = f(x + deltax) # step forward with deltax
     return (step - xval) / deltax # return the difference over the deltax value
 
+def nes(f, L, dim, xinit=None):
+    """
+    Use the Nesterov method for proximal gradient descent. We improve convergence
+    and can add a momentum term such that we relax the descent property. We update our
+    function with a momenutm value that accounts ofr hte step size.
+    """
