a6e586c2bf02fc40ab8f55935c6665b49f4da058
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 16:43:25 -0400

    bg

--- calc/neldermead.py
+++ calc/neldermead.py
@@ -5,4 +5,21 @@ We use the Nelder-Mead method (a.k.a downhill simplex, amoeba, or polytope metho
 find the minimum or maximum of an objective function in multidmensional space through
 direct search. It's often used in nonlinear optimization problems for which derivatives
 may not be known. It's a heuristic search method.
+
+We use this method in requiring that the fundamental circular frequency of a stepped shaft which
+is required to be higher than a certain value (w0). We determine the diameters d1 and d2
+to minimize the volume of the material without violating the frequency constraint. The approximate
+value of the fundamental frequency can be computed by solving the eigenvalue problem of
+the two corresponding matrices.
+
+
 """
+
+def nm():
+    """
+    """
+    l = 1e6
+    evalmin = .4
+    a = np.array([[4.0*(x[0]**4 + x[1]**4), 2.0*x[1]**4], [2.0*x[1]**4, 4.0*x[1]**4]])
+    b = np.array([[4.0*(x[0]**2 + x[1]**2), -3.0*x[1]**2], [-3*x[1]**2, 4.0*x[1]**2]])
+
