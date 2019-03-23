fac1bab861bac5af827c94149bdaf22faed5ec38
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 17:02:39 -0400

    bg

--- calc/neldermead.py
+++ calc/neldermead.py
@@ -7,8 +7,12 @@ find the minimum or maximum of an objective function in multidmensional space th
 direct search. It's often used in nonlinear optimization problems for which derivatives
 may not be known. It's a heuristic search method.
 
-We examine a two-dimensinoal simplex (n=2) that
+We examine a two-dimensional simplex (n=2) that we perform actions upon (expansion, reflection,
+contraction, and shrinkage) such that we return hte vertex with the highest value
+of a function. We control the magnitude with a distance (d) measured from the function's vertex
+at the highest value to the centroid of the opposing face of the simplex.
 
+Stepped shaft
 We use this method in requiring that the fundamental circular frequency of a stepped shaft which
 is required to be higher than a certain value (w0). We determine the diameters d1 and d2
 to minimize the volume of the material without violating the frequency constraint. The approximate
@@ -51,10 +55,29 @@ def dh(f, xinit, s=.1, eps=1e-6):
             xnew = x[ihi] + d # expand the reflection
             fnew = f(xnew) # evaluate it at this expansion
             if fnew <= o[ilo]: # should we accept the expansion?
+                x[ihi] = xnew # update our input x vector values
+                o[ihi] = fnew # update output array
+        else:
+            if fnew <= o[ihi]: # should we accept a new reflection?
+                x[ihi] = xnew
+                o[ihi] = fnew
+            else:
+                xnew = x[ihi] + .5*d # contraction
+                fnew = f(xnew) # get new output value
+                if fnew <= f[ihi]: # should we accept the contraction?
+                    x[ihi] = xnew # update our arrays
+                    o[ihi] = fnew
+                else:
+                    for i in range(len(x)): # shrinkage
+                        if i != ilo: # should we accept the shrinkage?
+                            x[i] = (x[i] - x[ilo])*.5
+                            o[i] = f(x[i])
+        return x[ilo]
 
 
-def nm():
+def ss():
     """
+    Stepped shaft problem.
     """
     l = 1e6
     evalmin = .4
