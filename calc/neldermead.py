4ff34bd53d6d1685613049d903e5614dceb04d79
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 17:10:52 -0400

    choleski

--- calc/neldermead.py
+++ calc/neldermead.py
@@ -125,6 +125,22 @@ def ip(a, s, eps=1e-6):
         if np.sqrt(np.dot(xold - x, xold-x)) < eps:
             return s + sign/xmag, x
 
+def choleski(a):
+    """
+    Perform Choleski decomposition such that [L][L]transpose = [a]
+    """
+    n = len(a)
+    for k in range(n):
+        try:
+            a[k,k] = math.sqrt(a[k,k]  \
+                   - np.dot(a[k,0:k],a[k,0:k]))
+        except ValueError:
+            error.err(’Matrix is not positive definite’)
+        for i in range(k+1,n):
+            a[i,k] = (a[i,k] - np.dot(a[i,0:k],a[k,0:k]))/a[k,k]
+    for k in range(1,n): a[0:k,k] = 0.0
+    return a
+
 
 def stdForm(a,b):
     """
