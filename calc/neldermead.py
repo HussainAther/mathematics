34a59f041581d528ef98ca5f891b20ac58721d56
Syed Hussain Ather <shussainather@gmail.com>
2019-03-23 16:46:30 -0400

    def dh

--- calc/neldermead.py
+++ calc/neldermead.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 """
 We use the Nelder-Mead method (a.k.a downhill simplex, amoeba, or polytope method) to
@@ -6,14 +7,24 @@ find the minimum or maximum of an objective function in multidmensional space th
 direct search. It's often used in nonlinear optimization problems for which derivatives
 may not be known. It's a heuristic search method.
 
+We examine a two-dimensinoal simplex (n=2) that
+
 We use this method in requiring that the fundamental circular frequency of a stepped shaft which
 is required to be higher than a certain value (w0). We determine the diameters d1 and d2
 to minimize the volume of the material without violating the frequency constraint. The approximate
 value of the fundamental frequency can be computed by solving the eigenvalue problem of
 the two corresponding matrices.
 
+D1 θ = c D2 θ
 
+in which D1 and D2 are the two corresponding matrices for the distances
 """
+
+def dh():
+    """
+    Downhill simplex (Nelder-Mead) method for minimizing the user-supplied scalar function f(x)
+    with respect to the vector x.
+    """
 
 def nm():
     """
