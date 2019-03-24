5b2de4a79873a531c4c63c097a2627d2324b92c2
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 16:16:38 -0400

    comment

--- calc/differentiation.py
+++ calc/differentiation.py
@@ -12,8 +12,15 @@ def fd(y, t, h): # function f at point t at interval h
 
 # central differentiation
 def cd(y, t, h):
+    """
+    Differentiate around both sides of a point t of interval h.
+    """
     return ( y(t+h/2) -y(t-h/2))/h
 
 # extrapolation
 def ed(y, t, h):
+    """
+    Extrapolate by splitting the derivative equation into four parts
+    that we sum toghether.
+    """
     return (8*(y(t+h/4)-y(t-h/4)) - (y(t+h/2)-y(t-h/2)))/3/h
