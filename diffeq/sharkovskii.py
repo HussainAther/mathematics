054a4000aeaa65290ac3f9f1f7c5cf61a17bd265
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 21:36:10 -0400

    bg

--- diffeq/sharkovskii.py
+++ diffeq/sharkovskii.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 """
-Sharkovskii proved 
+In 1964, Sharkovskii published a paper in which he proved that if a solution 
+of an odd (>=3) period exists for a value r3 for a discrete logistic-type nonlinear model,
+then aperiodic or chaotic solutions exist for r > r3. Tehy oscillate in an apparently
+random manner. The bifurcation at r3 is a tangent bifurcation. This may correspond to
+population explosion, crashback, and slow recovery
 """
