2030a8fe5c4e1cc1cba9fd3a9acbc4756f75a5bd
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 07:42:46 -0400

    return

--- algebra/linear/strassen.py
+++ algebra/linear/strassen.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 """
 Strassen's (Strassen) algorithm for matrix multiplication.
@@ -7,3 +8,10 @@ def mm(a, b):
     """
     For a and b square n x n matrices, we find the product c = a . b.
     """
+    n = a.shape[0]
+    c = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                c[i][j] += np.dot(a[i][k], b[k][j]) # from the definition of each entry in c
+    return c
