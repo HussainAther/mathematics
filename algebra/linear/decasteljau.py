ba9099ad867df2e2d5c9e29ef61a250c837dc785
Syed Hussain Ather <shussainather@gmail.com>
2019-03-27 19:33:28 -0400

    def bez

--- algebra/linear/decasteljau.py
+++ algebra/linear/decasteljau.py
@@ -1,3 +1,6 @@
+import matplotlib.patches as patch
+import matplotlib.path as Path
+
 """
 We use the de Casteljau algorithm as a recursive method to evaluate polynomials in Bernstein form or
 Bézier (Bezier) curves. We construct parametric equations of an arc of a polynomial of order 4. 
@@ -6,3 +9,6 @@ of degree n using:
 
 r(t) = summation from i=0 to n of b_i*B_in(t) for 0<=t<=1  
 """
+
+def bezier_parabola(P1, P2, P3):
+    return Path([P1, P2, P3], [Path.MOVETO, Path.CURVE3, Path.CURVE3])
