5144aee135263d3b1d7bf3056a7c41068d52a298
Syed Hussain Ather <shussainather@gmail.com>
2019-03-27 19:14:21 -0400

    comment

--- algebra/linear/minkowski.py
+++ algebra/linear/minkowski.py
@@ -4,3 +4,8 @@ from decimal import Decimal
 Minkowski distance in machine learning lets us find the distance similarity of a vector
 in the context of two or more vectors.
 """
+
+def rootdist(a, root):
+    """
+    Calculate distance value to a given root value.
+    """
