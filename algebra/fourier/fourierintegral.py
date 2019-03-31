5a4fa9c42f7f89ad7e9b009fa834a69cc6498fcd
Syed Hussain Ather <shussainather@gmail.com>
2019-03-30 21:07:19 -0400

    typo

--- algebra/fourier/fourierintegral.py
+++ algebra/fourier/fourierintegral.py
@@ -10,7 +10,7 @@ integral from a to be of exp(i*omega*t) * h(t) * dt
 which can be split up into real and imaginary parts
 
 integral from a to be of cos(omega*t) * h(t) * dt and integral from a to be of sin(omega*t) * h(t) * dt
-Taking advantage of hte oscillatory nature of the integral, if h(t) is at all smooth and omega is large
+Taking advantage of the oscillatory nature of the integral, if h(t) is at all smooth and omega is large
 enough to imply several cycles in the interval [a, b], then the value of I is typically very small that it's sswamped
 by the first-order error.
 
