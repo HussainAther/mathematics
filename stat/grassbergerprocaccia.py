0053c1217051916d4f47f982c3730df48234e897
Syed Hussain Ather <shussainather@gmail.com>
2019-04-02 18:50:50 -0400

    return embed

--- stat/grassbergerprocaccia.py
+++ stat/grassbergerprocaccia.py
@@ -1,4 +1,16 @@
+import numpy as np
+import scipy.stats as ss
+
 """
 Use the Grassberger-Procaccia algorithm to estimate the correalation dimension of a set.
 Correlation dimension is a fractal dimension characteristic of the set of points.
 """
+
+def embed(a, d, tau):
+    """
+    Return the time delay embedding for an array a with embedding dimension d and time delay tau.
+    """
+    i = range(0, d)*tau # indices
+    return np.arrange([i + j for j in range((d-1)*(tau))])
+
+
