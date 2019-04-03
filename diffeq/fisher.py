7d8df4c791bd07d81a242e2317b094f3fd72a03d
Syed Hussain Ather <shussainather@gmail.com>
2019-04-03 01:42:41 -0400

    bg

--- diffeq/fisher.py
+++ diffeq/fisher.py
@@ -2,8 +2,16 @@ import numpy as np
 
 """
 Fisher's (Fisher fisher) equation or Kolmogorov-Petrovsky-Piskunov equation or KPP equation or Fisher-KPP equation
-is the partidal differential equation:
+is the partial differential equation:
 
 du/dt - Dd^2u/dx^2 = ru(1-u)
 
+in describing diffusion in one dimension. We can propagate wave solutions that travel in this dimension.
+
+Fisher originally proposed the equation to describe the spatial spread of an advantageous allele with the corresponding
+wave solutions. These solutions would be of the form
+
+u(x,t) = v(x+/- ct) = v(z)
+
+such that v(z) approaches 0 as z approaches -infinity and v(z) approaches 1 as z approaches infinity.
 """
