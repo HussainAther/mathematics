a6a9e83d7cae1bd237df88d0bc10d33c28efee1c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-03 01:29:46 -0400

    initial commit

---
+++ diffeq/fisher.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+"""
+Fisher's (Fisher fisher) equation or Kolmogorov-Petrovsky-Piskunov equation or KPP equation or Fisher-KPP equation
+is the partidal differential equation:
+
+du/dt - Dd^2u/dx^2 = ru(1-u)
+
+"""
