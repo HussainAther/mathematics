42928d2bc800a09cb51b2a20c75979dee617dbd6
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 15:32:21 -0400

    init var

--- diffeq/langevin.py
+++ diffeq/langevin.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 """
 In stoachstic differential equations we can model dynamical systems subject to noise for use
 in physics, biology, and neuroscience. We will simlulate the Ornstein-Uhlenbeck process as a solution
@@ -6,3 +9,8 @@ under the influence of friction. We account for movement due ot collusions with 
 and find the difference with the Brownian motion and friction. The Ornstein-Uhlenbeck process is
 stationary, Gaussian, and Markov, so it can represent stationary random noise.
 """
+
+# Let's get Gaussian
+sigma = 1 # standard deviation
+mu = 20 # average
+tau = .05 # time constant
