5ca2a0fede000ba904a7096c604b803aa103c66c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 15:30:24 -0400

    bg

--- diffeq/langevin.py
+++ diffeq/langevin.py
@@ -1,4 +1,8 @@
 """
 In stoachstic differential equations we can model dynamical systems subject to noise for use
-in physics, biology, and neuroscience.
+in physics, biology, and neuroscience. We will simlulate the Ornstein-Uhlenbeck process as a solution
+of the Langevin (langevin) equation to describe the stochastic evolution of a particle in a fluid
+under the influence of friction. We account for movement due ot collusions with the fluid (diffusion) 
+and find the difference with the Brownian motion and friction. The Ornstein-Uhlenbeck process is
+stationary, Gaussian, and Markov, so it can represent stationary random noise.
 """
