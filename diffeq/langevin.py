44baff09e05621a12b1b16cc1f11d1720d5b1938
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 15:35:01 -0400

    eq

--- diffeq/langevin.py
+++ diffeq/langevin.py
@@ -8,6 +8,10 @@ of the Langevin (langevin) equation to describe the stochastic evolution of a pa
 under the influence of friction. We account for movement due ot collusions with the fluid (diffusion) 
 and find the difference with the Brownian motion and friction. The Ornstein-Uhlenbeck process is
 stationary, Gaussian, and Markov, so it can represent stationary random noise.
+
+Langevin equation is:
+
+dx = -((x-mu)/tau)dt + sigma * sqrt(2/tau) dW
 """
 
 # Let's get Gaussian
@@ -31,3 +35,25 @@ for i in range(n - 1):
     but with an extra stochastic term (scaled normal random variable).
     """
     x[i + 1] = x[i] + dt * (-(x[i] - mu) / tau) + sigma_bis * sqrtdt * np.random.randn()
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+ax.plot(t, x, lw=2)
+
+ntrials = 10000
+X = np.zeros(ntrials)
+# We create bins for the histograms.
+bins = np.linspace(-2., 14., 100)
+fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+
+for i in range(n):
+    # Update the process independently for all trials
+    X += dt * (-(X - mu) / tau) + \
+        sigma_bis * sqrtdt * np.random.randn(ntrials)
+    # Display the histogram for a few points in time
+    if i in (5, 50, 900):
+        hist, _ = np.histogram(X, bins=bins)
+        ax.plot((bins[1:] + bins[:-1]) / 2, hist,
+                {5: '-', 50: '.', 900: '-.', }[i],
+                label=f"t={i * dt:.2f}")
+    ax.legend()
+
