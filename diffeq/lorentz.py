732dc675ac077856ee58271ad6d85aa44315ef14
Syed Hussain Ather <shussainather@gmail.com>
2019-04-08 00:06:57 -0400

    rename

--- diffeq/lorentz.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from scipy.integrate import odeint
-
-"""
-We use the Lorentz system to discuss issues of dynamical systems.
-
-"""
---
+++ diffeq/lorenz.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy.integrate import odeint
+
+"""
+We use the Lorenz system to discuss issues of dynamical systems. It illustrates non-linear systems theory and
+chaos theory (like with the butterfly effect).
+
+"""
