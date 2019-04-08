0b13990d0ca8cb7f7a5fa2a9f09339279c74cdac
Syed Hussain Ather <shussainather@gmail.com>
2019-04-08 00:09:14 -0400

    def

--- diffeq/lorenz.py
+++ diffeq/lorenz.py
@@ -3,6 +3,24 @@ from scipy.integrate import odeint
 
 """
 We use the Lorenz system to discuss issues of dynamical systems. It illustrates non-linear systems theory and
-chaos theory (like with the butterfly effect).
-
+chaos theory (like with the butterfly effect). We can simulate this.
 """
+
+def Lorenz(s,t):
+    """
+    For three coupled differential equations with three state variables (indicated by the three-member s),
+    we have three constants (sigma, rho, beta) to demonstrate chaotic behavior.
+    """
+    x = s[0]
+    y = s[1]
+    z = s[2]
+  
+    # constants for the equations
+    sigma = 10.0
+    rho = 28.0
+    beta = 8.0/3.0
+  
+    # state derivatives
+    xd = sigma * (y-x)
+    yd = (rho-z)*x - y
+    zd = x*y - beta*z
