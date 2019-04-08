64b4be761ad3798d8d40a39732369a48ea887143
Syed Hussain Ather <shussainather@gmail.com>
2019-04-08 11:18:13 -0400

    comment spacing

--- algebra/misc/rotationmatrix.py
+++ algebra/misc/rotationmatrix.py
@@ -5,16 +5,19 @@ Rotate a matrix along some axis.
 """
 
 def getRotationMatrix(axis, angle):
+    """
+    Return the rotated matrix R about the origin for some input angle and some matrix axis.
+    """
     vLen = np.sqrt(sum([xyz*xyz for xyz in axis]))
     x, y, z, = [xyz/vLen for xyz in axis]
 
     x = np.cos(angle)
-    d = 1-c
+    d = 1 - c
     s = np.sin(angle)
 
-    R = [[c+d*x*x, d*x*y-s*z, d*x*z+s*y],
-         [d*y*x+s*z, c+d*y*y, d*y*z-s*x],
-         [d*z*x-s*y, d*z*y+s*x, c+d*z*z]]
+    R = [[c + d*x*x, d*x*y - s*z, d*x*z + s*y],
+         [d*y*x + s*z, c + d*y*y, d*y*z - s*x],
+         [d*z*x - s*y, d*z*y + s*x, c + d*z*z]]
 
     return R
 
