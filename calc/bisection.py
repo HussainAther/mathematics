11500bb8ccf51fce77e54404c9b7f74c9875eae4
Syed Hussain Ather <shussainather@gmail.com>
2019-04-08 00:40:04 -0400

    .

--- calc/bisection.py
+++ calc/bisection.py
@@ -2,7 +2,7 @@ from vpython import *
 from vpython.graph import *
 
 """
-Biscetion algorithm is a simple way for finding a zero of a function
+Biscetion algorithm is a simple way for finding a zero of a function.
 """
 
 eps = 1e-6 # precision
