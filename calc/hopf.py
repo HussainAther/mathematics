df23a96120335b12594da260240af41a8591d67c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-08 00:37:53 -0400

    quotes and spacing

--- calc/hopf.py
+++ calc/hopf.py
@@ -11,19 +11,21 @@ dynamical system, a small-amplitude limit cycle branches from the fixed point.
 """
 
 def make_A(tau):
-    """Make the Jacobian matrix"""
+    """
+    Jacobian matrix
+    """
     return array([[-1./20, 0, 0, -1./20],
                [1./tau, -1./tau, 0, 0],
                [0, 6./50, -1./50, 0],
                [0, 0, 1./tau, -1./tau]])
 
 
-cols = ['r', 'g', 'k', 'b']
+cols = ["r", "g", "k", "b"]
 
 plt.plot([-0.3, 0.01], [0, 0], 'k')
 plt.plot([0, 0], [-0.15, 0.15], 'k')
-plt.xlabel('real')
-plt.ylabel('imag')
+plt.xlabel("real")
+plt.ylabel("imag")
 plt.show()
 
 # Boolean flag initialization
@@ -34,7 +36,7 @@ taus = linspace(0.05, 15, 100)
 # keep the "handle" of the text object so that we can update it later
 t = plt.text(-0.2, 0.12, 'tau = %.3f' % 0.05, fontsize=20)
 
-plt.title('Eigenvalues of f.p.')
+plt.title("Eigenvalues of f.p.")
 
 for i, tau in enumerate(taus):
     # [0] is eigenvalues, [1] is eigenvectors
@@ -46,8 +48,8 @@ for i, tau in enumerate(taus):
             # assumes that we know the eigenvalues started negative real part
             print "Hopf found near tau = %.4f (index %i)" % (tau, i)
             Hopf_found = True
-        plt.plot(re, im, cols[j]+'.')
+        plt.plot(re, im, cols[j]+".")
         plt.xlim([-0.3, 0.01])
         plt.ylim([-0.15, 0.15])
-        t.set_text('tau = %.3f' % tau)
+        t.set_text("tau = %.3f" % tau)
         plt.draw()
