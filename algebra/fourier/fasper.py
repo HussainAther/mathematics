6427e211bbb37dcaa142e05b6e5725d8b7cbed3b
Syed Hussain Ather <shussainather@gmail.com>
2019-04-11 14:41:08 -0400

    spacing

--- algebra/fourier/fasper.py
+++ algebra/fourier/fasper.py
@@ -6,8 +6,7 @@ from math import floor
 """
 Fast computation of the Lomb Periodogram. We use the Fast Fourier transform to evluate
 equations of the Lomb normalized periodogram. The fast algorithm makes feasible the application
-of the Lomb method to data sets at least as large as 10^6 data points. It's faster than straightforward evaluation
-of equations of Lomb normalized periodogram.
+of the Lomb method to data sets at least as large as 10^6 data points. It's faster than straightforward evaluation of equations of Lomb normalized periodogram.
 """
 
 def spread(y, yy, x, m):
