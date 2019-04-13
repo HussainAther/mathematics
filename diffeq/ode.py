77d2284f95d2488a30e3f648e0e59e5aa459d20b
Syed Hussain Ather <shussainather@gmail.com>
2019-04-13 16:53:25 -0400

    comments

--- diffeq/ode.py
+++ diffeq/ode.py
@@ -1,3 +1,5 @@
+from numpy import zeros, linspace
+
 """
 How can we verify that the programming of an ODE model is correct? The best method is to find a problem where
 there are no unknown numerical approximation errors, because we can then compare the exact solution of the
@@ -8,6 +10,9 @@ tests and writing test functions) for automatic verification of our implementati
 """
 
 def ode_FE(f, U_0, dt, T):
+    """
+    Verify our ODE model.
+    """
     N_t = int(round(float(T)/dt))
     u = zeros(N_t+1)
     t = linspace(0, N_t*dt, len(u))
@@ -17,12 +22,20 @@ def ode_FE(f, U_0, dt, T):
     return u, t
 
 def test_ode_FE():
-    """Test that a linear u(t)=a*t+b is exactly reproduced."""
+    """
+    Test that a linear u(t)=a*t+b is exactly reproduced.
+    """
 
     def exact_solution(t):
+        """
+        Gauge this as a measure of performance.
+        """
         return a*t + b
 
     def f(u, t):  # ODE
+        """
+        Use the ODE to test.
+        """
         return a + (u - exact_solution(t))**m
 
     a = 4
@@ -34,6 +47,6 @@ def test_ode_FE():
 
     u, t = ode_FE(f, exact_solution(0), dt, T)
     diff = abs(exact_solution(t) - u).max()
-    tol = 1E-15           # Tolerance for float comparison
+    tol = 1E-15 # Tolerance for float comparison
     success = diff < tol
     assert success
