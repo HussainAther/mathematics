aa8d1567ed219f9679933eab89d7daa796ac9393
Syed Hussain Ather <shussainather@gmail.com>
2019-04-14 19:48:01 -0400

    typo

--- calc/samplingintegration.py
+++ calc/samplingintegration.py
@@ -3,8 +3,8 @@ import numpy as np
 """
 Imagine a pond enclosed by a 2-dimensional box.
 We'll use the sampling technique of throwing stones into a pond
-as a methof of measuring the area of the pond. The ratio of "hits" to
-the total number of stones thrown equals the artio of the area of the pond
+as a method of measuring the area of the pond. The ratio of "hits" to
+the total number of stones thrown equals the ratio of the area of the pond
 to that of the box. We can also determine pi through this method (as described
 by Buffon's needle.)
 """
