2781dfd041d4e8aba40186a28f553ca665281721
Syed Hussain Ather <shussainather@gmail.com>
2019-04-14 19:58:13 -0400

    np

--- calc/vonNeuman.py
+++ calc/vonNeuman.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+
 from vpython.graph import *
 
 """
@@ -26,25 +28,25 @@ def fx(x):
     return x*sin(x)*sin(x)
 
 def plotfunction(): # Plot the function and the box
-    inc = 2*pi/N
+    inc = 2*np.pi/N
     for i in range(0, N):
         xx = i*incr
-        xsinx.x[i] = ((80/pi)*xx-80)
+        xsinx.x[i] = ((80/np.pi)*xx-80)
         xsinx.y[i] = 20*fx(xx)-50
         box = curve(pos=[(-80, -50), (-80, 50), (80, 50), (80, -50), (-80, -50)], color=color.white)
 
 plotfunction() # get to it!
 j = 0
 Npts = 3001 # poiints generated inside the box
-analyt = pi**2 # analytical integral
+analyt = np.pi**2 # analytical integral
 areanal.text = "analytical=%8.5f"%analyt # output analytical integral
 genpts = points(size=2)
 
 for i in range(1, Npts): # generate points inside the box
     rate(500)
-    x = 2*pi*random.random()
+    x = 2*np.pi*random.random()
     y= 5*random.random()
-    xp = x*80/pi-80
+    xp = x*80/np.pi-80
     yp = 20*y-50
     pts2.text = "4%s" %i
     if y <= fx(x):
@@ -53,6 +55,6 @@ for i in range(1, Npts): # generate points inside the box
         inside2.text="%4s"%j
     else:
         genpts.append(pos=(xp,yp), color=color.green)
-    boxarea = 2*pi*5
+    boxarea = 2*np.pi*5
     area =  boxarea*j/(Npts-1)
     arealbl2.text = "%8.5f"%area
