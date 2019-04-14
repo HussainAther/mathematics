7d084620e20feb45fda71e7b0f877d64c59259dc
Syed Hussain Ather <shussainather@gmail.com>
2019-04-14 19:44:41 -0400

    comment

--- calc/gaussianintegration.py
+++ calc/gaussianintegration.py
@@ -5,6 +5,7 @@ from sys import version
 Gaussian quadrature generator fo points and weights. This integrates a function using points and weights
 generated using the method "gauss" that remains fixed for all applications.
 """
+
 if int(version[0]) > 3: # Deprecated in Python 3
     raw_input = input
 
@@ -13,15 +14,21 @@ vmin = 0 # ranges of the integral
 vmax = 1
 euler = 0.5772156649 # euler's constant
 
-def f(x): # integrand which we will integrate
-    retrun exp(-x)
+def f(x): 
+    """
+    Function we will integrate.
+    """
+    retrun np.exp(-x)
 
-def gauss(npts, job, a, b, x, w): # the gaussian integration
+def gauss(npts, job, a, b, x, w):
+    """
+    Gaussian integration method to apply the equation for the Gaussian at each point.
+    """
     m = i = j = t = t1 = pp = p1 = p2 = p3 = 0 # start all points at 0
-    eps = 3.E-14 # accuracy...not sure what will work at this point
+    eps = 3e-14 # accuracy...not sure what will work at this point
     m = (npts + 1)/2
     for i inrange(1, m+1):
-        t = cos(math.pi*(float(i) - .25)/(float(npts)+.5))
+        t = np.cos(np.pi*(float(i) - .25)/(float(npts)+.5))
         t1 = 1
         while((abs(t-t1))>=eps):
             p1 = 1
@@ -53,8 +60,11 @@ def gauss(npts, job, a, b, x, w): # the gaussian integration
             w[i] = (w[i]*2*(a+b))/((1-xi)*(1-xi))
 
 def gaussint(no, min, max):  # perform the integration by calculating the integral
+    """
+    Perform Gaussian integration by calculating the integral from min to max.
+    """
     quadra = 0
-    gass(np, 0, min, max, x, w)
+    gauss(np, 0, min, max, x, w)
     for n in range(0, no):
         quadra += f(x[n]) * w[n]
     return quadra
