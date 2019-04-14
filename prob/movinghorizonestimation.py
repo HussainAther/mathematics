462f0b533391af2d2300acbde38ae0e91b0b4d97
Syed Hussain Ather <shussainather@gmail.com>
2019-04-14 14:40:55 -0400

    comment and spacing

--- prob/movinghorizonestimation.py
+++ prob/movinghorizonestimation.py
@@ -1,35 +1,50 @@
-from gekko import GEKKO
 import numpy as np
 import matplotlib.pyplot as plt
+
+from gekko import GEKKO
+
+"""
+Moving horizon estimation (MHE) is an optimization approach that uses a series of measurements
+observed over time containing noise and other inaccuracies. It produces estimates of unknown variables
+or parameters and requires an iterative approach that relies on linear programming or nonlinear programming
+solvers to find a solution.
+"""
 
 # Estimator Model
 m = GEKKO()
 m.time = p.time
+
 # Parameters
 m.u = m.MV(value=u_meas) #input
 m.K = m.FV(value=1, lb=1, ub=3)    # gain
 m.tau = m.FV(value=5, lb=1, ub=10) # time constant
+
 # Variables
 m.x = m.SV() #state variable
 m.y = m.CV(value=y_meas) #measurement
+
 # Equations
 m.Equations([m.tau * m.x.dt() == -m.x + m.u,
              m.y == m.K * m.x])
+
 # Options
 m.options.IMODE = 5 #MHE
 m.options.EV_TYPE = 1
+
 # STATUS = 0, optimizer doesn't adjust value
 # STATUS = 1, optimizer can adjust
 m.u.STATUS = 0
 m.K.STATUS = 1
 m.tau.STATUS = 1
 m.y.STATUS = 1
+
 # FSTATUS = 0, no measurement
 # FSTATUS = 1, measurement used to update model
 m.u.FSTATUS = 1
 m.K.FSTATUS = 0
 m.tau.FSTATUS = 0
 m.y.FSTATUS = 1
+
 # DMAX = maximum movement each cycle
 m.K.DMAX = 2.0
 m.tau.DMAX = 4.0
