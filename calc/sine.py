7d33c6db4b7eb92f06ca6ebde14f3c4a0107f4a9
Syed Hussain Ather <shussainather@gmail.com>
2019-04-14 20:24:48 -0400

    comment

--- calc/sine.py
+++ calc/sine.py
@@ -6,7 +6,8 @@ Calcluate sin(x) as a finite sum.
 
 def sine(x, N):
     """
-    Discrete method of calculating
+    Discrete method of calculating the sine function.
+    Helpful for certain Fourier transforms of noisy data.
     """
     result = 0
     for i in range(1, N+1):
