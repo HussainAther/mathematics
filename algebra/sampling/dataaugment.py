64ea10177592d74b7a776482025c39ee6af19298
Syed Hussain Ather <shussainather@gmail.com>
2019-04-16 12:49:46 -0400

    spacing

--- algebra/sampling/dataaugment.py
+++ algebra/sampling/dataaugment.py
@@ -6,7 +6,6 @@ from scipy import ndarray
 from skimage import transform
 from skimage import util
 
-
 """
 With the data augmentation algorithm, we alternate between the I-step (imputation step, analogous to
 the E step of the EM algorithm) and the P-step (posterior step, analogous to M).
