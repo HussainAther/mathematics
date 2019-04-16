1a6d2bf6abf96d347ed3ec2e4674a117fc50b587
Syed Hussain Ather <shussainather@gmail.com>
2019-04-16 18:26:13 -0400

    typo

--- algebra/fourier/convolve.py
+++ algebra/fourier/convolve.py
@@ -5,7 +5,7 @@ from fourier import * # from the fourier.py script in the same dirctory.
 """
 Convolution or deconvolution of a real set of data in array form a of two functions s(t) and r(t) (written as s(t) * r(t)).
 Fourier transform of the convolution of two functions is equal to the product of their individual Fourier transforms.
-(s) is typicall a signal or data stream that goes on indefinitely in time while r(t) is a "response" function that typically
+(s) is typically a signal or data stream that goes on indefinitely in time while r(t) is a "response" function that typically
 has peaks that fall to zero in both directions from its maximum.
 """
 
