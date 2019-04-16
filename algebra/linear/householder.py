577d5db5889b2b0d1fe6a5fe224c7d0c943f6b36
Syed Hussain Ather <shussainather@gmail.com>
2019-04-16 19:35:38 -0400

    spacing

--- algebra/linear/householder.py
+++ algebra/linear/householder.py
@@ -3,6 +3,7 @@ import numpy as np
 """
 Householder algorithm reduces an n x n matrix A to tridiagonal form by n - 2 orthogonal transformations.
 Each transformation annihilates the required part of a whole column and whole corresponding row.
+
 The Householder matrix P has the form:
 
 P = 1 - 2w dot w^T
