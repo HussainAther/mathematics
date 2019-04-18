bd2e3a5a9b143fd91959e9d4640c23b2acd0ddfc
Syed Hussain Ather <shussainather@gmail.com>
2019-04-18 16:48:26 -0400

    keyword

--- calc/neldermead.py
+++ calc/neldermead.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 """
-We use the Nelder-Mead method (a.k.a downhill simplex, amoeba, or polytope method) to
+We use the Nelder-Mead method (nelder mead a.k.a downhill simplex, amoeba, or polytope method) to
 find the minimum or maximum of an objective function in multidmensional space through
 direct search. It's often used in nonlinear optimization problems for which derivatives
 may not be known. It's a heuristic search method.
