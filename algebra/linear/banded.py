a740a4aacd2c86a36a879b4f84045d43731bd4fc
Syed Hussain Ather <shussainather@gmail.com>
2019-04-18 17:02:18 -0400

    comment

--- algebra/linear/banded.py
+++ algebra/linear/banded.py
@@ -1,24 +1,26 @@
 import numpy as np
 
 """
-Solve a banded system of linear equaitons using Gaussian elimination and backsubstitution.
+Solve a banded system of linear equations using Gaussian elimination and backsubstitution.
 
 Return the vector solution to x = banded(A, v, up, down) and A.x = v in which v
-is an array representing a vector of N elements that are eitehr real or complex. A
+is an array representing a vector of N elements that are either real or complex. A
 is an N by N banded matrix with "up" nonzero elements above the diagonal and "down" nonzero
 elements below the diagonal. The matrix is specified as a 2-d array of (1+up+down) by N
 elements with the diagonals of the original matrix along its rows.
 """
 
 
-def banded(Aa,va,up,down):
-
+def banded(Aa, va, up, down):
+    """
+    The banded system is described above. 
+    """
     # Copy the inputs and determine the size of the system
-    A = np.copy(Aa)
+    A = np.copy(Aa) # create copies using numpy
     v = np.copy(va)
     N = len(v)
 
-    # Gaussian elimination
+    # Gaussian elimination (Gaussian-Jordan gaussian jordan)
     for m in range(N):
 
         # Normalization factor
