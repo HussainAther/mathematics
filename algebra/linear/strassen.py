a026888497fa1edd95d990fb4e2faa788c555051
Syed Hussain Ather <shussainather@gmail.com>
2019-04-18 17:08:31 -0400

    comment

--- algebra/linear/strassen.py
+++ algebra/linear/strassen.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 """
-Strassen's (Strassen) algorithm for matrix multiplication.
+Strassen's (Strassen strassen) algorithm for matrix multiplication. A straightforward
+iteration that works for smaller matrices.
 """
 
 def mm(a, b):
@@ -10,7 +11,7 @@ def mm(a, b):
     """
     n = a.shape[0]
     c = np.zeros((n,n))
-    for i in range(n):
+    for i in range(n): # iterate with nested for loops
         for j in range(n):
             for k in range(n):
                 c[i][j] += np.dot(a[i][k], b[k][j]) # from the definition of each entry in c
