bed22cd338a611be7c58bcef40ebdbf9a090978b
Syed Hussain Ather <shussainather@gmail.com>
2019-04-22 13:51:05 -0400

    comment

--- algebra/misc/complexdummy.py
+++ algebra/misc/complexdummy.py
@@ -7,16 +7,28 @@ class Complex:
         z.re = x # real component
         z.im = y # imaginary
 
-    def addt(z1, z2): # add z1 + z2
+    def addt(z1, z2): 
+        """
+        Add two complex vectors
+        """
         return Complex(z1.re + z2.re, z1.im, z2.im)
 
-    def subt(z1, z2): # subtract
+    def subt(z1, z2): 
+        """
+        Subtract two complex vectors
+        """
         return Complex(z1.re - z2.re, z1.im - z2.im)
     
     def mult(z1, z2):
+	"""
+	Multiply
+	"""
         return Complex(z1.re * z1.re - z1.im * z2.im, z1.re * z2.im + z1.im * z2.re)
 
     def __repr__(z): # convert to string for printing
+	"""
+	Convert to string
+	"""
         return "(%f, %f) " %(z.re, z.im)
 
 print("Operations with two complex numbers\n")
