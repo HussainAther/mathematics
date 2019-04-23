3b3ef1ecfd6da46515c728c8ef9048813c043cee
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:13:41 -0400

    moved

--- stat/latentdirichlet.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-"""
-The Latent Dirichlet allocation (LDA lda) is a generative statistical model to 
-use unobserved groups that explain why parts of the data are similar to make 
-observations.
-"""
-    
