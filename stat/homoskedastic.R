c389276a5035daa2b22fc311bf34c57509de4523
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 16:28:33 -0400

    homo and rename

---
+++ stat/homoskedastic.R
@@ -0,0 +1,10 @@
+"Linear regression for homoskedastic Gaussian noise with one input variable x and a response vairable y."
+sim.lm <- function(linfit, test.x) {
+    n <- length(test.x)
+    sim.frame <- data.frame(x = test.x)
+    sigma <- summary(linfit)$sigma * (n - 2)/n
+    y.sim <- predict(linfit, newdata = sim.frame)
+    y.sim <- y.sim + rnorm(n, 0, sigma)
+    sim.frame <- data.frame(sim.frame, y = y.sim)
+    return(sim.frame)
+}
