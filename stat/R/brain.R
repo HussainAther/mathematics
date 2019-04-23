cee65665d58b5b71151ab467c06f014ff79d0f11
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:51:15 -0400

    df's

--- stat/R/brain.R
+++ stat/R/brain.R
@@ -4,7 +4,19 @@ library(np)
 df <- read.csv(file="data/n90_pol.csv", header=TRUE, sep=",")
 bw <- npudensbw(df)
     fhat <- npudens(bw)
-pdfy <- function(df) {
+
+# Probability distribution function 
+pdfxy <- function(x, y) (x^2 * y + x * y^2)/2
+pdfxy(df$amygdala, df$acc)
+
+# Integrate over possible x values
+pdf <- function(pdfxy) {
   result <- sapply(df, function(b) integrate(function(a) pdfxy(a, b), 0, 2)$value)
+  return(result)
+}
+
+# Cumulative distirbution function
+cdfy <- function(pdf) {
+  result <- sapply(y, function(a) integrate(function(b) pdfy(b), 0, a)$value)
   return(result)
 }
