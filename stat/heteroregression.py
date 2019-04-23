0a55d83a4e6d838b325f921dd531c77f49b0ad85
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 15:53:09 -0400

    comment

--- stat/heteroregression.py
+++ stat/heteroregression.py
@@ -1,4 +1,19 @@
 "Linear regression of heteroskedastic data, using weighted least-squared regression."
+"Generate heteroskedastic data and fit ordinary least squares (OLS ols) regression."
+
+ols.heterosked.example = function(n) {
+    y = 3 - 2 * x + rnorm(n, 0, sapply(x, function(x) {
+        1 + 0.5 * x^2
+    }))
+    fit.ols = lm(y ~ x)
+    return(fit.ols$coefficients - c(3, -2))
+}
+ols.heterosked.error.stats = function(n, m = 10000) {
+    ols.errors.raw = t(replicate(m, ols.heterosked.example(n)))
+    intercept.se = sd(ols.errors.raw[, "(Intercept)"])
+    slope.se = sd(ols.errors.raw[, "x"])
+    return(c(intercept.se = intercept.se, slope.se = slope.se))
+}
 
 wls.heterosked.example = function(n) {
     y = 3 - 2 * x + rnorm(n, 0, sapply(x, function(x) {
