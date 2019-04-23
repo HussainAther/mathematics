47d54333e5197abdfcfd056eac3dc595c3ce4a4d
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 16:37:19 -0400

    initial commit

--- stat/weightmatrix.R
+++ stat/weightmatrix.R
@@ -5,3 +5,13 @@ reconstruction.weights.2 <- function(x, neighbors, alpha) {
     w = t(w)
     return(w)
 }
+"Manifold coordinates from approximation weights by finding eigenfunctions."
+coords.from.weights <- function(w, q, tol = 1e-07) {
+    n = nrow(w)
+    stopifnot(ncol(w) == n)
+    stopifnot(all(abs(rowSums(w) - 1) < tol))
+    M = t(diag(n) - w) %*% (diag(n) - w)
+    soln = eigen(M)
+    coords = soln$vectors[, ((n - q):(n - 1))]
+    return(coords)
+}
