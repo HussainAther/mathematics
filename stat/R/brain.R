4930cb4766a5b6fd553617426d013cc4decd36ec
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 18:01:28 -0400

    npudens

--- stat/R/brain.R
+++ stat/R/brain.R
@@ -1,7 +1,12 @@
 "Estimate joint probability density for the volumes of the amygdala and acc (anterior
 cingulate cotrtex)."
+# Load the np library
 library(np)
+
+# Read in the csv as a dataframe
 df <- read.csv(file="data/n90_pol.csv", header=TRUE, sep=",")
+
+# Calculate the bandwidth 
 bw <- npudensbw(df)
     fhat <- npudens(bw)
 
@@ -15,8 +20,19 @@ pdf <- function(pdfxy) {
   return(result)
 }
 
-# Cumulative distirbution function
+# Cumulative distirbution function shoudl give us the pdf of a joint distribution
 cdfy <- function(pdf) {
   result <- sapply(y, function(a) integrate(function(b) pdfy(b), 0, a)$value)
   return(result)
 }
+
+# Or using npudens to find the Gaussian kernel estimate 
+# of the joint distribution 
+jpd <- npudens(~popgro+inv, data=df) # joint probability density
+fhat <- plot(jpd, plot.behavior="data")
+fhat <- fhat$orientation
+
+# Draw a lattice plot of the distribution
+library(lattice)
+contourplot(fhat$dens~fhat$eval$Var1*fhat$eval$Var2,cuts=20,
+xlab="popgro",ylab="inv",labels=list(cex=0.5))
