e8b60e3e7c92ee8d1f8c4a5a30947e368bc68f06
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:01:25 -0400

    linear

--- stat/R/mass.R
+++ stat/R/mass.R
@@ -7,4 +7,29 @@ using (i) linear regression, (ii) logistic regression, and (iii) lda
 (using the function lda()). Compare the results."
 library(MASS)
 data(fgl)
-help(fgl)
+
+# Combine first three classes 
+attach(fgl)
+X=fgl[,1:9]
+Type=rep(0, 214)
+Type[1:163]=1
+data1=cbind(X,Type)
+k=nrow(X)
+testa=rbinom(k,1, .25)
+data=data1[testa==0,]
+test=data1[testa==1,]
+x=data[,1:9]
+tx=test[,1:9]
+ty=test[,10]
+y=data[,10]
+n=nrow(data)
+p=ncol(X)
+q=nrow(tx)
+
+# Linear
+lin = lm(Type~. ,data=data)
+predlin = predict(lin,tx)
+Typehat = rep(0,q)
+Typehat[predlin > .5]=1
+print(table(ty, Typehat))
+print(sum(ty != Typehat)/q)
