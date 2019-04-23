1b697a056030baba5a039f105030b1889cbec973
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:43:30 -0400

    pdf

--- stat/R/brain.R
+++ stat/R/brain.R
@@ -1,2 +1,10 @@
 "Estimate joint probability density for the volumes of the amygdala and acc (anterior
 cingulate cotrtex)."
+library(np)
+df <- read.csv(file="data/n90_pol.csv", header=TRUE, sep=",")
+bw <- npudensbw(df)
+    fhat <- npudens(bw)
+pdfy <- function(df) {
+  result <- sapply(df, function(b) integrate(function(a) pdfxy(a, b), 0, 2)$value)
+  return(result)
+}
