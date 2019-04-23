f2ff29060eca674e7b81e885b72747e1b65caa42
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:09:17 -0400

    spam

--- stat/R/kernel.R
+++ stat/R/kernel.R
@@ -29,3 +29,28 @@ risk = mean((r - mX)^2)
 print(risk)
 par(mfrow=c(2,5))
 plot(output)
+
+# SPaM (Sparsed additive model sparse)
+#spam, select bandwidth and regularizer using GCV
+H=seq(0.01, 0.05, 0.002) #candidate bandwidth
+cvScores=rep(0, length(H))
+outputs=list()
+indexL=rep(0, length(H))
+for(j in 1:length(H)){ #choose bandwidth
+h=H[j] #current bandwidth
+lambdas=seq(0, 0.4, 0.02) #candidate lambda
+cvScoresEx=rep(0, length(lambdas))
+outputsEx=list()
+for(i in 1:length(lambdas)){
+outputsEx[[i]] = spam(X, Y, h, lambdas[i], 10, 1e-3)
+cvScoresEx[i] = outputsEx[[i]]$cvScore
+}
+
+# Plot
+## plot(lambdas, cvScoresEx, "l");
+## record the best lambda for this bandwidth
+indexL[j]=which.min(cvScoresEx)
+cvScores[j] = cvScoresEx[indexL[j]]
+outputs[[j]] = outputsEx[[indexL[j]]]
+print(sprintf("*****H=%f", h))
+}
