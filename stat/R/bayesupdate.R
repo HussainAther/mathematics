ebf3321d4745c3c525730a8fe6df1b4fc0ebe9bd
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 19:13:33 -0400

    n theta vals

--- stat/R/bayesupdate.R
+++ stat/R/bayesupdate.R
@@ -1,3 +1,6 @@
 # Bayesian updating of beliefs about the bias of a coin. 
 # The prior and posterior distributions indicate probability masses at discrete candidate values of θ.
 # Theta is the vector of candidate values for the parameter theta.
+# nThetaVals is the number of candidate theta values.
+# To produce the examples in the book, set nThetaVals to either 3 of 63.
+nThetaVals = 3
