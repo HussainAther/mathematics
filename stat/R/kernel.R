aaa282bbd882bdd267506ba2408f30e0be7c5394
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:07:25 -0400

    regression

--- stat/R/kernel.R
+++ stat/R/kernel.R
@@ -8,3 +8,12 @@ X = matrix(runif(n*p),n,p)
 mX = cos(5*pi*X[,1]) + 5*X[,2]^2
 e = rnorm(n, 0, noise.sd)
 Y = mX + e
+
+# kernel regression
+H=seq(0.1, 1.0, 0.01)
+cvScore = krgcv(X, Y, H)
+h=H[which.min(cvScore)]
+print(h)
+r = kr(X, Y, h, X)
+risk=mean((r - mX)^2)
+print(risk)
