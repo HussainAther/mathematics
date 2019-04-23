1255669a42ef0fa0be0df3b3dd38508791f08046
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:12:40 -0400

    init

--- stat/lle.R
+++
@@ -1,8 +0,0 @@
-"Locally (locally) linear embedding."
-lle <- function(x, q, k = q + 1, alpha = 0.01) {
-    stopifnot(q > 0, q < ncol(x), k > q, alpha > 0)
-    kNNs = find.kNNs(x, k)
-    w = reconstruction.weights(x, kNNs, alpha)
-    coords = coords.from.weights(w, q)
-    return(coords)
-}
