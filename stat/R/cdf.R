71071d811c98c2fa23037785bf08ff9498d79a9c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:26:12 -0400

    band and simulate

--- stat/R/cdf.R
+++ stat/R/cdf.R
@@ -1,1 +1,10 @@
 "Calculate cumulative distribution using CDF function and confidence bands."
+# Two confidence bands for the CDF
+cdf.band <- function(n.unif, alpha=.05){
+U = runif(n.unif)
+x = sort(U) # find the cutoffs
+Y = abs(1:n.unif/n.unif - x)
+return(quantile(Y, 1-alpha))
+}
+# Simulate
+c = mean(replicate(1000,cdf.band(250)))
