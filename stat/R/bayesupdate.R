2841c8d0a97d8e6f50aa194f344acddaefa43e08
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 19:08:35 -0400

    initial commit

---
+++ stat/R/bayesupdate.R
@@ -0,0 +1,3 @@
+# Bayesian updating of beliefs about the bias of a coin. 
+# The prior and posterior distributions indicate probability masses at discrete candidate values of θ.
+# Theta is the vector of candidate values for the parameter theta.
