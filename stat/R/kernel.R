1fca5408dbe76f91d6aa5cf4254ecdcee6a2e8a4
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 17:07:08 -0400

    data

--- stat/R/kernel.R
+++ stat/R/kernel.R
@@ -1,1 +1,10 @@
 "Kernel (kernel) regression."
+# Generate data
+n = 100
+p = 10
+noise.sd = 0.1
+set.seed(123456)
+X = matrix(runif(n*p),n,p)
+mX = cos(5*pi*X[,1]) + 5*X[,2]^2
+e = rnorm(n, 0, noise.sd)
+Y = mX + e
