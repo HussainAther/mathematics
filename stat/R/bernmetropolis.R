9672b0cb6ce4c136f293460a63999157c166f441
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 22:58:42 -0400

    data

--- stat/R/bernmetropolis.R
+++ stat/R/bernmetropolis.R
@@ -1,2 +1,6 @@
 "Apply the Metropolis (Metropolis-Hastings metropolis hastings) algorithm to estimate
 a binomial proportion."
+# Specify the data, to be used in the likelihood function.
+# This is a vector with one component per flip,
+# in which 1 means a "head" and 0 means a "tail".
+myData=c(1,1,1,1,1,1,1,1,1,1,1,0,0,0)
