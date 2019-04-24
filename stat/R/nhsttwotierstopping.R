f4e5dcd1b398e420ea991001edc6f70a11ec0794
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 14:28:10 -0400

    initial ocmmit

---
+++ stat/R/nhsttwotierstopping.R
@@ -0,0 +1,6 @@
+"Suppose an experimenter plans to collect data based on a two-tier stopping criterion. The experimenter will"
+"collect an initial batch of data with N = 30 and then do a NHST. If the result is not significant, then an"
+"additional 15 subjects’ data will be collected, for a total N of 45. Suppose the researcher intends to use the"
+"standard critical values for determining significance at both the N = 30 and N = 45 stages. Our goal is determine"
+"the actual false-alarm rate for this two-stage procedure, and to ponder what the mere intention of doing a second"
+"phase implies for interpreting the first stage, even if data collection stops with the first stage."
