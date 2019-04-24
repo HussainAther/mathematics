02b4c6519ea7d8a1e39a19ff03672f66f0093a14
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 17:10:36 -0400

    keywrod

--- stat/R/runningproportion.R
+++ stat/R/runningproportion.R
@@ -1,3 +1,4 @@
+# Bayesian (Bayes bayes) testing of independent attributes. Verify their independence. 
 # Toss a coin N times and compute the running proportion of heads.
 N = 500 # Specify the total number of flips, denoted N.
 
