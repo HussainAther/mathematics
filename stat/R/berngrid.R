729f53076edfaa93eac8cbe668f011c4d46582d7
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 20:42:15 -0400

    Summary

--- stat/R/berngrid.R
+++ stat/R/berngrid.R
@@ -1,2 +1,17 @@
 "Grid approximation to infer binomial proportion."
 BernGrid = function(Theta, pTheta, Data, credib=.95, nToPlot=length(Theta)) {
+    # Bayesian updating for Bernoulli likelihood and prior specified on a grid.
+    # Theta is a vector of theta values between 0 and 1.
+    # pTheta is a vector of corresponding probability masses.
+    # Data is a vector of 1s and 0s (corresponding to a and b, respectively).
+    # credib is the probability of mass of the credible interval, default is .95.
+    # nToPlot is the number of grid points to plot. default is all.
+    # Return pThetaGivenData, vector of posterior probability masses over Theta.
+    # Create a three-panel graph of prior, likelihood, and posterior probability
+    # masses with credible interval.
+
+    # Create summary values of Data
+    z = sum(Data==1) # number of 1's in Data
+    N = length( Data ) # number of flips in Data
+
+    # Compute the likelihood of the Data for each value of Theta. 
