861d92b86635aabc88e1ef27dcf755f28a04526c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 14:32:34 -0400

    nt

--- stat/R/nhsttwotierstopping.R
+++ stat/R/nhsttwotierstopping.R
@@ -20,3 +20,6 @@ hiCritN1 = (N1:0)[max(which(cumsum(dbinom(N1:0,N1,theta)) <= FAmax/2))]
 FA1 = sum((0:N1 <= loCritN1 | 0:N1 >= hiCritN1) * dbinom(0:N1,N1,theta))
 cat("N1:", N1, ", lo:", loCritN1, ", hi:",hiCritN1, ", FA:", FA1, "\n")
 
+# Determine critical values for NT.
+loCritNT = (0:NT)[max(which(cumsum(dbinom(0:NT,NT,theta)) <= FAmax/2))]
+hiCritNT = (NT:0)[max(which(cumsum(dbinom(NT:0,NT,theta)) <= FAmax/2))]
