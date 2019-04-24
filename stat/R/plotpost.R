e0b55b9050b0e45004e5d375c1b1d3e428671205
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 00:20:21 -0400

    initial commit

---
+++ stat/R/plotpost.R
@@ -0,0 +1,4 @@
+plotPost = function(paramSampleVec, credMass=0.95, compVal=NULL,
+    HDItextPlace=0.7, ROPE=NULL, yaxt=NULL, ylab=NULL,
+    xlab=NULL, cex.lab=NULL, cex=NULL, xlim=NULL, main=NULL,
+    showMode=F, ... ) {
