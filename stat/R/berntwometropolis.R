5ae665aa4cacf702b4fc5d3c23e7a900a235d70c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 00:04:35 -0400

    likelihood function

--- stat/R/berntwometropolis.R
+++ stat/R/berntwometropolis.R
@@ -7,3 +7,9 @@ library(MASS)
 # Define the likelihood function.
 # The input argument is a vector: theta = c(theta1, theta2)
 likelihood = function(theta) {
+    # Data are constants, specified here:
+    z1 = 5; N1 = 7; z2 = 2; N2 = 7
+    likelihood = (theta[1]^z1 * (1-theta[1])^(N1-z1)
+        * theta[2]^z2 * (1-theta[2])^(N2-z2))
+    return(likelihood)
+}
