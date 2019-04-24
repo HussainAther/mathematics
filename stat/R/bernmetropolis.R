b864690b2083481e1be364278ad1c7b8bd3fb19b
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 23:12:39 -0400

    prior

--- stat/R/bernmetropolis.R
+++ stat/R/bernmetropolis.R
@@ -11,3 +11,15 @@ likelihood = function( theta , data ) {
     z=sum(data==1)
     N = length(data)
     pDataGivenTheta = theta^z * (1-theta)^(N-z)
+    # The theta values passed into this function are generated at random,
+    # and therefore might be inadvertently greater than 1 or less than 0.
+    # The likelihood for theta > 1 or for theta < 0 is zero: 
+    pDataGivenTheta[theta > 1 | theta < 0] = 0
+    return(pDataGivenTheta)
+}
+
+# Define the prior density function. For purposes of computing p(D),
+# at the end of this program, we want this prior to be a proper density. 
+# The argument theta could be a vector, not just a scalar.
+prior = function(theta) {
+    prior = rep(1, length(theta)) # uniform density over [0,1]
