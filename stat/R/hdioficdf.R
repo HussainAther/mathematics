17a82096c0ec38989cca28fc0b632dcb203d2a11
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 17:45:09 -0400

    interval

--- stat/R/hdioficdf.R
+++ stat/R/hdioficdf.R
@@ -7,4 +7,7 @@ HDIofICDF = function(ICDFname, credMass=0.95, tol=1e-8, ...) {
     # credMass is the desired mass of the HDI region.
     # tol is the tolerance to optimize the function.
     # Return the highest density interval (HDI) limmits in a vector.
-     
+    incredMass = 1.0 - credMass # inverse of the input
+    intervalWidth = function(lowTailPr, ICDFname, credMass, ...) {
+        ICDFname(credMass + lowTailPr, ...) - ICDFname(lowTailPr, ...)
+    } 
