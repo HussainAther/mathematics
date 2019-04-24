2b12f29cc093e3b6481a34346d205363796592f1
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 20:34:09 -0400

    store

--- stat/R/betaposteriorpredictions.R
+++ stat/R/betaposteriorpredictions.R
@@ -40,3 +40,10 @@ simSampleZrecord = vector(length=nSimSamples)
 
 # Now generate samples from the posterior.
 for (sampleIdx in 1:nSimSamples) {
+    # Generate a theta value for the new sample from the posterior.
+    sampleTheta = rbeta(1 , postA , postB)
+    # Generate a sample, using sampleTheta.
+    sampleData = sample(x=c(0,1), prob=c(1-sampleTheta, sampleTheta),
+        size=simSampleSize , replace=TRUE)
+    # Store the number of heads in sampleData.
+    simSampleZrecord[sampleIdx] = sum(sampleData)	
