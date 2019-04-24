06d891d520bae371aacd79c58b08760bded8e92c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 17:42:34 -0400

    initail commit

---
+++ stat/R/hdioficdf.R
@@ -0,0 +1,5 @@
+# Highest (posterior) density interval (highest Posterior) (HDI hdi) of the 
+# inverse cumulative density function (icdf ICDF) for the corresponding
+# probability distribution.
+
+HDIofICDF = function( ICDFname , credMass=0.95 , tol=1e-8 , ... ) {
