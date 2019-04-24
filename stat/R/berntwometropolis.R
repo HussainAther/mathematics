1e003052f4c4088aeb090cb1f9c67002f5865ea3
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 00:02:29 -0400

    initial commit

---
+++ stat/R/berntwometropolis.R
@@ -0,0 +1,9 @@
+# Use this program as a template for experimenting with the Metropolis
+# algorithm applied to two parameters called theta1,theta2 defined on the
+
+# Load MASS
+library(MASS)
+
+# Define the likelihood function.
+# The input argument is a vector: theta = c(theta1, theta2)
+
