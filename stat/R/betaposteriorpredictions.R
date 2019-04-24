1f3cfafc4f5df33205ad56b31aaa806a3d5df674
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 20:28:25 -0400

    initial commit

---
+++ stat/R/betaposteriorpredictions.R
@@ -0,0 +1,4 @@
+"Following the coin-flipping examples, suppose we flip the coin a 
+total of N = 12 times and it comes up heads in z = 8 of those flips. 
+Suppose we let a beta(θ; 100, 1) distribution describe the head-biased 
+trick coin, and we let a beta(θ; 1, 100) distribution describe the tail-biased trick coin."
