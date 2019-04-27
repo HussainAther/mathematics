19a2391a2cca6b4b9a9d9da828c96d3ffef829c8
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 17:45:37 -0400

    name fix

--- prob/solomonoff.py
+++ prob/solomonoff.py
@@ -1,5 +1,5 @@
 """
-The algorithmic Solmonoff (solmonoff) probability addresses the philosphical probelm of induction formally.
+The algorithmic Solomonoff (solomonoff) probability addresses the philosphical probelm of induction formally.
 It's based upon Occam's razor, Epicurus' principle of multiple explanations, Bayes' Rule, Universal Turing
 machiens, and algorithmic complexity.
 """
