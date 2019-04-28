c0eee885054837040c180f0938ef307a4039bfdd
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 20:33:02 -0400

    comment

--- diffeq/neimarcksacker.py
+++ diffeq/neimarcksacker.py
@@ -1,10 +1,17 @@
 """
 The Neimarck-Sacker bifurcation is a closed invariant curve from a fixed point
 in dynamical systems with discrete time when the fixed point changes stability
-via a pair of complex eigenvalues with unit modulus.
+via a pair of complex eigenvalues with unit modulus. This is the Hopf
+bifurcation equivalent for maps.
 
 We observe a Neimark-Sacker bifurcation as either subcritical or supercritical 
 by the sign of the first Lyapunov coefficient.
 """
 
-
+def ns(d):
+    """
+    For a map in dictionary form d with a Jacobian matrix of the map
+    such that this matrix has a pair of eigenvalues λ(a) = R(a)exp(i*θ(a))
+    for d(a) over an interval of a-values of interest.
+    """
+    
