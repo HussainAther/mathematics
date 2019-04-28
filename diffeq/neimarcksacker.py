63e1fa0054c991b95fc7684bcfdda5ee6691d0da
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 20:28:59 -0400

    initial commit

---
+++ diffeq/neimarcksacker.py
@@ -0,0 +1,9 @@
+"""
+The Neimarck-Sacker bifurcation is a closed invariant curve from a fixed point
+in dynamical systems with discrete time when the fixed point changes stability
+via a pair of complex iegenvalues with unit modulus.
+
+We observe a Neimark-Sacker bifurcation as either subcritical or supercritical 
+by the sign of the first Lyapunov coefficient.
+"""
+
