e2e3932073530d9e9f67d59df892c337ca54a414
Syed Hussain Ather <shussainather@gmail.com>
2019-04-28 21:38:51 -0400

    spacing and matplotlib

--- diffeq/bulirschstoer.py
+++ diffeq/bulirschstoer.py
@@ -1,6 +1,5 @@
-from pylab import plot, show
-
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 In numerical analysis, the Bulirsch–Stoer algorithm is a method for the numerical solution of
@@ -40,35 +39,28 @@ r = np.array([theta0,0.0],float)
 
 # big steps of size H
 for t in tpoints:
-
     thetapoints.append(r[0])
-
     # modified midpoint step to get things started
     n = 1
     r1 = r + 0.5*H*f(r)
     r2 = r + H*f(r1)
-
     # array R1 stores the first row of the
     # extrapolation table, which contains only the single
     # modified midpoint estimate of the solution at the
     # end of the interval
     R1 = np.empty([1,2],float)
     R1[0] = 0.5*(r1 + r2 + 0.5*H*f(r2))
-
     # Now increase n until the required accuracy is reached
     error = 2*H*delta
     while error>H*delta:
-
         n += 1
         h = H/n
-
         # Modified midpoint method
         r1 = r + 0.5*h*f(r)
         r2 = r + h*f(r1)
         for i in range(n-1):
             r1 += h*f(r2)
             r2 += h*f(r1)
-
         # Calculate extrapolation estimates.  Arrays R1 and R2
         # hold the two most recent lines of the table
         R2 = R1
@@ -78,12 +70,10 @@ for t in tpoints:
             epsilon = (R1[m-1]-R2[m-1])/((n/(n-1))**(2*m)-1)
             R1[m] = R1[m-1] + epsilon
         error = abs(epsilon[0])
-
     # Set r equal to the most accurate estimate we have,
     # before moving on to the next big step
     r = R1[n-1]
 
 # Plot the results
-plot(tpoints,thetapoints)
-plot(tpoints,thetapoints,"b.")
-show()
+plt.plot(tpoints, thetapoints)
+plt.show()
