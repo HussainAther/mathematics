e30cd36f9839f02901017e9d9f1c6f41e1eecf49
Syed Hussain Ather <shussainather@gmail.com>
2019-04-28 21:33:26 -0400

    quotes and spacing

--- diffeq/timediff.py
+++ diffeq/timediff.py
@@ -7,18 +7,18 @@ from gekko import GEKKO
 Simple GEKKO usage with an equation differentiated by time.
 """
 
-m = GEKKO()    # create GEKKO model
-k = 0.3        # constant
+m = GEKKO() # create GEKKO model
+k = 0.3 # constant
 y = m.Var(5.0) # create GEKKO variable
 m.Equation(y.dt()==-k*y) # create GEKKO equation
-m.time = np.linspace(0,20) # time points
+m.time = np.linspace(0, 20) # time points
 
 # solve ODE
 m.options.IMODE = 4
 m.solve()
 
 # plot results
-plt.plot(m.time,y)
-plt.xlabel('time')
-plt.ylabel('y(t)')
+plt.plot(m.time, y)
+plt.xlabel("time")
+plt.ylabel("y(t)")
 plt.show()
