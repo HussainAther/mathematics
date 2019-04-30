72b336f985f596642b2b3bf067582c9b3b7ee91b
Syed Hussain Ather <shussainather@gmail.com>
2019-04-30 18:42:33 -0400

    bg

--- algebra/neuralnetworks/hessian.py
+++ algebra/neuralnetworks/hessian.py
@@ -3,8 +3,8 @@ import numpy as np
 """
 We can use backpropagation to evaluate the second derivates of the error, given by
 the second derivate of the error with respect to weights. We use the element H_ij
-of the Hessian (hessian) matrix H in which i, j are included in the set {1,...W} in which W is the total
-number of weights and biases.
+of the Hessian (hessian) matrix H in which i, j are included in the set {1,...W} in which W is the 
+total number of weights and biases.
 
 1. Several nonlinear optimization algorithms used for training neural networks are based
 on considerations of the second-order properties of the error surface, which are controlled
@@ -16,8 +16,8 @@ following a small change in the training data (Bishop, 1991).
 3. The inverse of the Hessian has been used to identify the least significant weights in a
 network as part of network ‘pruning’ algorithms (Le Cun et al., 1990).
 
-4. The Hessian plays a central role in the Laplace approximation for a Bayesian neural network
-(see Section 5.7). Its inverse is used to determine the predic- tive distribution for a trained network,
+4. The Hessian plays a central role in the Laplace approximation for a Bayesian neural network. 
+Its inverse is used to determine the predic- tive distribution for a trained network,
 its eigenvalues determine the values of hyperparameters, and its determinant is used to evaluate
 the model evidence.
 """
