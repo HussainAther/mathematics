bb4a4e99cc29505803750cfc392cd3c10b0cb428
Syed Hussain Ather <shussainather@gmail.com>
2019-04-30 17:24:10 -0400

    import

--- stat/python/likelihood.py
+++ stat/python/likelihood.py
@@ -1,3 +1,5 @@
+import numpy as np
+import scipy.stats.norm as norm
 
 """
 Compare the likelihood of the random samples to the two
@@ -8,6 +10,9 @@ def compare_data_to_dist(x, mu_1=5, mu_2=7, sd_1=3, sd_2=3):
     ll_1 = 0
     ll_2 = 0
     for i in x:
+        """
+        Calculate likelihood estimates using a normal distribution.
+        """
         ll_1 += np.log(norm.pdf(i, mu_1, sd_1))
         ll_2 += np.log(norm.pdf(i, mu_2, sd_2))
     
