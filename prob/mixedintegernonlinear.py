96c646a79c075900e49eb154fbc02b7116d65c05
Syed Hussain Ather <shussainather@gmail.com>
2019-04-30 17:43:47 -0400

    quotes

--- prob/mixedintegernonlinear.py
+++ prob/mixedintegernonlinear.py
@@ -8,19 +8,19 @@ m = GEKKO() # Initialize gekko
 m.options.SOLVER=1  # APOPT is an MINLP solver
 
 # optional solver settings with APOPT
-m.solver_options = ['minlp_maximum_iterations 500', \
+m.solver_options = ["minlp_maximum_iterations 500", \
                     # minlp iterations with integer solution
-                    'minlp_max_iter_with_int_sol 10', \
+                    "minlp_max_iter_with_int_sol 10", \
                     # treat minlp as nlp
-                    'minlp_as_nlp 0', \
+                    "minlp_as_nlp 0", \
                     # nlp sub-problem max iterations
-                    'nlp_maximum_iterations 50', \
+                    "nlp_maximum_iterations 50", \
                     # 1 = depth first, 2 = breadth first
-                    'minlp_branch_method 1', \
+                    "minlp_branch_method 1", \
                     # maximum deviation from whole number
-                    'minlp_integer_tol 0.05', \
+                    "minlp_integer_tol 0.05", \
                     # covergence tolerance
-                    'minlp_gap_tol 0.01']
+                    "minlp_gap_tol 0.01"]
 
 # Initialize variables
 x1 = m.Var(value=1,lb=1,ub=5)
