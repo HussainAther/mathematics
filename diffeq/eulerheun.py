6003a7c07b79aa2e1b6ba9733b77bdb60725b701
Syed Hussain Ather <shussainather@gmail.com>
2019-05-01 13:36:26 -0400

    indent

--- diffeq/eulerheun.py
+++ diffeq/eulerheun.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 """
-We use the second-order Euler-Heun (euler Euler Heun heun) method to approximate an integral through which we can
-use in integrating an ordinary differential equation.
+We use the second-order Euler-Heun (euler Euler Heun heun) method to approximate an integral 
+through which we can use in integrating an ordinary differential equation.
 
 x(t_n+1) = x(t_n) + integral from t_n to t_n+1 of f(x(s), s)ds
 
