d1df9d7b5e45b146567c99bc01237ec077f0070b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-01 13:44:36 -0400

    df

--- prob/hoeffding.py
+++ prob/hoeffding.py
@@ -12,15 +12,17 @@ E(Xi ) = μ. Then, we have
 
 |Xn−μ| ≤ sqrt((c/2n)(log2/δ))
 
-where c = (b−a)2. We will see this inequality again in the machine learning chapter. Figure 2.23 shows the Markov and
-Hoeffding bounds for the case of ten identically and uniformly distributed random variables, X i ∼ U [0, 1].
-The solid line shows P(|Xn − 1/2| > ε). Note that the Hoeffding Inequality is tighter than the Markov Inequality and
-that both of them merge when ε gets big enough.
+where c = (b−a)2. There are Hoeffding bounds for the case of ten identically and uniformly distributed 
+random variables, X i ∼ U [0, 1]. The solid line shows P(|Xn − 1/2| > ε). Note that the Hoeffding 
+Inequality is tighter than the Markov Inequality and that both of them merge when ε gets big enough.
 """
 
 t = sympy.symbols("t", real=True)
 x = ss.ChiSquared("x", 1)
 
+# probabilities for the inequality 
 r = ss.P((x-1) > e , x > 1) + ss.P(-(x-1) > t, x<1)
-w = (1 - ss.cdf(x)(t+1))+ss.cdf(x)(1-t)
+
+# draw from the cumulative density function
+w = (1 - ss.cdf(x)*(t+1)) + ss.cdf(x)*(1-t)
 
