1df9bccffdcb9d3f5a72b69d80c78a9656e16677
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 23:35:49 -0400

    9 cluster

--- stat/R/mixturemodel.R
+++ stat/R/mixturemodel.R
@@ -66,3 +66,8 @@ for (k in candidate.cluster.numbers) {
 }
 plot(x=1:10, y=loglikes,xlab="Number of mixture clusters",
     ylab="Log-likelihood on testing data")
+snoq.k9 <- normalmixEM(snoq,k=9,maxit=400,epsilon=1e-2)
+plot(hist(snoq,breaks=101),col="grey",border="grey",freq=FALSE,
+    xlab="Precipitation (1/100 inch)",main="Precipitation in Snoqualmie Falls")
+lines(density(snoq),lty=2)
+invisible(sapply(1:9,plot.gaussian.clusters,mixture=snoq.k9))
