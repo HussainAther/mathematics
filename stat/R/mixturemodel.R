11db431772cf5c28830cd809c893ac16c70784c4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 23:29:15 -0400

    lines

--- stat/R/mixturemodel.R
+++ stat/R/mixturemodel.R
@@ -5,4 +5,4 @@ library(mixtools)
 snoq.k2 <- normalmixEM(snoq, k=2, maxit=100, epsilon=0.01)
 plot(hist(snoq, breaks=101), col="grey", border="grey", freq=FALSE,
     xlab="Precipitation (1/100 inch)", main="Precipitation in Snoqualmie Falls")
-
+lines(density(snoq), lty="dashed")
