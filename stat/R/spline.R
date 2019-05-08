9706efdddea04e0417760d5d02a264a35f8a49af
Syed Hussain Ather <shussainather@gmail.com>
2019-05-08 15:34:29 -0400

    pdfetch

--- stat/R/spline.R
+++ stat/R/spline.R
@@ -2,3 +2,6 @@
 expectation or reegression function μ. We were controlling how smooth we make indirectly through
 the bandwith of the kernels, but we can use a more direct method of controlling smoothness itself by
 minimizing the spline objective function."
+require(pdfetch)
+sp <- pdfetch_YAHOO("SPY", fields = "adjclose", from = as.Date("1993-02-09"),
+    to = as.Date("2015-02-09"))
