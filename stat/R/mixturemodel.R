ea0c0337eeb2ba4a9ab409f49f2270ef276e45a7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 23:27:56 -0400

    library

--- stat/R/mixturemodel.R
+++ stat/R/mixturemodel.R
@@ -1,3 +1,5 @@
 snoqualmie <- scan("http://www.stat.washington.edu/peter/book.data/set1",skip=1)
 snoq <- snoqualmie[snoqualmie > 0]
 summary(snoq)
+library(mixtools)
+snoq.k2 <- normalmixEM(snoq,k=2,maxit=100,epsilon=0.01)
