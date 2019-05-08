7c0221e9a3e1a13ee4f429f6f78f63c647248af9
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 23:28:57 -0400

    plot

--- stat/R/mixturemodel.R
+++ stat/R/mixturemodel.R
@@ -2,4 +2,7 @@ snoqualmie <- scan("http://www.stat.washington.edu/peter/book.data/set1",skip=1)
 snoq <- snoqualmie[snoqualmie > 0]
 summary(snoq)
 library(mixtools)
-snoq.k2 <- normalmixEM(snoq,k=2,maxit=100,epsilon=0.01)
+snoq.k2 <- normalmixEM(snoq, k=2, maxit=100, epsilon=0.01)
+plot(hist(snoq, breaks=101), col="grey", border="grey", freq=FALSE,
+    xlab="Precipitation (1/100 inch)", main="Precipitation in Snoqualmie Falls")
+
