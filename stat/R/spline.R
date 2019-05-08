9e481b626541918a802255eb193aa60c506f6864
Syed Hussain Ather <shussainather@gmail.com>
2019-05-08 15:32:21 -0400

    initial commit

---
+++ stat/R/spline.R
@@ -0,0 +1,4 @@
+"When smoothing one dimensional data, we want to find an approximation to the true conditional
+expectation or reegression function μ. We were controlling how smooth we make indirectly through
+the bandwith of the kernels, but we can use a more direct method of controlling smoothness itself by
+minimizing the spline objective function."
