1988891c7eb6931f82758d821ee7da9876362450
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 15:17:24 -0400

    library and data

--- stat/R/rulebasedmodels.R
+++ stat/R/rulebasedmodels.R
@@ -1,2 +1,5 @@
 "Rule-based models. Build single classification trees. Use predictors for the data
 to model classes for groupes categories."
+library(rpart)
+cartModel <- rpart(factorForm, data = training[pre2008,])
+rpart(Class ~ NumCI + Weekday, data = training[pre2008,])
