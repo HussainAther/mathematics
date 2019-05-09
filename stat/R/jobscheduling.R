27fac69dda27868ff79358b13134342406e17be9
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 16:14:31 -0400

    data train

--- stat/R/jobscheduling.R
+++ stat/R/jobscheduling.R
@@ -1,1 +1,6 @@
 "Job scheduling problem in high-performance computing (high performance HPC hpc)."
+library(AppliedPredictiveModeling)
+data(HPC)
+set.seed(100)
+inTrain <- createDataPartition(schedulingData$Class, p = .8, list = FALSE)
+
