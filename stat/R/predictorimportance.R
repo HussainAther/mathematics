528aad46a0d6d9e764660fd146f37ec86f601db3
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 16:39:37 -0400

    correlation

--- stat/R/predictorimportance.R
+++ stat/R/predictorimportance.R
@@ -5,4 +5,10 @@ library(CORElearn)
 library(minerva)
 library(pROV)
 library(randomForest)
-
+data(solubility)
+cor(solTrainXtrans$NumCarbon, solTrainY)
+fpCols <- grepl("FP", names(solTrainXtrans))
+numericPreds <- names(solTRainXtrans)[!fpCols]
+corrValues <- apply(solTrainXtrans[, numericPreds], MARGIN =2,
+                    FUN = function(x, y) cor(x, y),
+                    y = solTrainY)
