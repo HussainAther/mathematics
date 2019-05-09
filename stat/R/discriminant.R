a271a7bc4462238926295aa007aef3d8139bda6c
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 14:20:37 -0400

    comment

--- stat/R/discriminant.R
+++ stat/R/discriminant.R
@@ -10,6 +10,7 @@ fullCovMat <- cov(training[, fullSet])
 fullSetResults <- trim.matrix(fullCovMat)
 "May choose what to exclude using this function."
 fullSetREsults$names.discarded
+"Train samples using estimated parameters with index argument to identify the samples."
 ctrl <- trainControl(summaryFunction = twoClassSummary, classProbs = TRUE)
 ctrl <- trainControl(method = "LGOCV", summaryFunction = twoClassSummary,
                      classProbs = TRUE, index = list(TrainSet = pre2008))
