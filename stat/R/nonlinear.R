2b115b4e014a336d582c08bf5963750d303d0143
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 15:00:14 -0400

    mda

--- stat/R/nonlinear.R
+++ stat/R/nonlinear.R
@@ -1,4 +1,16 @@
 "Nonlinear (nonlinear) classification models."
 library(mda)
+library(AppliedPredictiveModeling)
+library(caret)
+set.seed(100)
+simulatedTrain <- quadBoundaryFunc(500)
+simulatedTest <- quadBoundaryFunc(1000)
 mdaModel <- mda(Class ~ ., data = training[pre2008, c("Class", reducedSet)],
                 subclasses = 3)
+mdaModel
+predict(mdaModel, newdata = head(training[-pre2008, reducedSet]))
+"Mixture discriminant analysis. mixture"
+mdaFit <- train(training[, reducedSet], training$Class, method = "mda",
+                metric = "ROC", tuneGrid = expand.grid(.subclasses = 1:8),
+                trControl = ctrl)
+
