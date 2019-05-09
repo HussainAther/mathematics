ed87e15a450df1f993765fcaa5d2d3fb90838cbe
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 15:20:38 -0400

    fit

--- stat/R/rulebasedmodels.R
+++ stat/R/rulebasedmodels.R
@@ -9,3 +9,11 @@ cartModel <- rpart(factorForm, data = training[pre2008,])
 rpart(Class ~ NumCI + Weekday, data = training[pre2008,])
 library(RWeka)
 J48(Class ~ NumCI + Weekday, data = training[pre2008,])
+library(C50)
+C5tree <- C5.0(Class ~ NumCI + Weekday, data = training[pre2008,])
+C5tree
+summary(C5tree)
+"Fit the grouped category model for CART."
+rpartGrouped <- train(x = training[, factorPredictors], y = training$Class,
+                      method = "rpart", tuneLength = 30, metric = "ROC",
+                      trControl = ctrl)
