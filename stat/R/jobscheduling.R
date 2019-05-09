5a7aabfbf5b7bbdf7050c0dcaa4916daa7e6287e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 16:17:50 -0400

    cost function

--- stat/R/jobscheduling.R
+++ stat/R/jobscheduling.R
@@ -3,4 +3,22 @@ library(AppliedPredictiveModeling)
 data(HPC)
 set.seed(100)
 inTrain <- createDataPartition(schedulingData$Class, p = .8, list = FALSE)
-
+schedulingData$NumPending <- schedulingData$NumPending + 1
+trainData <- schedulingData[inTrain,]
+testData <- schedulingData[-inTrain,]
+cost <- function(pred, obs) {
+    isNA <- is.na(pred)
+    if(!all(isNA)) {
+        pred <- pred[!isNA]
+        obs <- obs[!isNA]
+        if(any(pred == "VF" & obs == "L"))
+            cost[pred == "L" & obs == "VF"] <- 10
+        if(any(pred =="F" & obs == "L"))
+            cost[pred == "F" & obs == "L"] <- 5
+        if(any(pred == "F" & obs == "M"))
+            cost[pred == "F" & obs == "M"] <- 5
+        if(any(pred == "VF" & obs == "M"))
+            cost[pred == "VF" & obs == "M"] <- 5
+        out <- mean(cost)
+    } else out <- NA
+} 
