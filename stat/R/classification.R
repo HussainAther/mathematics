01cfe116a525e0050b934b7bf91851b49673d413
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 13:45:57 -0400

    library and data

--- stat/R/classification.R
+++ stat/R/classification.R
@@ -1,2 +1,6 @@
 "Measuring performance in classification models."
+library(AppliedPredictiveModeling)
+set.seed(100)
+simulatedTrain <- quadBoundaryFunc(500)
+simulatedTest <- quadBoundaryFunc(1000)
 
