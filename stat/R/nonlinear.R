2b35293008549b7df37265e2c07f956bf9ab5de2
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 15:03:38 -0400

    final modell

--- stat/R/nonlinear.R
+++ stat/R/nonlinear.R
@@ -16,3 +16,8 @@ mdaFit <- train(training[, reducedSet], training$Class, method = "mda",
 "Neural networks neural."
 nnetMod <- nnet(Class ~ NumCI + CI.1960, data = training[pre2008,],
                 size = 3, decay = .1)
+nnetMod
+predict(nnetMod, newdata = head(testing), type = "class")
+nnetGrid <- expand.grid(.size = 1:10, .decay = c(0, .1, 1, 2))
+maxSize <- max(nnetGrid$.size)
+numWts - 1*(maxSize * (length(reducedSet) + 1) + maxSize + 1)
