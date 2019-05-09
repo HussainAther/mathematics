5a8aac464c746725084c1ea75c000ecc0e578ecd
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 15:18:02 -0400

    J48 and more library

--- stat/R/rulebasedmodels.R
+++ stat/R/rulebasedmodels.R
@@ -1,5 +1,11 @@
 "Rule-based models. Build single classification trees. Use predictors for the data
 to model classes for groupes categories."
 library(rpart)
+library(mda)
+library(AppliedPredictiveModeling)
+library(caret)
+set.seed(100)
 cartModel <- rpart(factorForm, data = training[pre2008,])
 rpart(Class ~ NumCI + Weekday, data = training[pre2008,])
+library(RWeka)
+J48(Class ~ NumCI + Weekday, data = training[pre2008,])
