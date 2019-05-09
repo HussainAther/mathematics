adb83c3a60ad8fe0ddcd839ba08c47b77fb68a9d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 17:07:46 -0400

    data in

--- stat/R/typethree.R
+++ stat/R/typethree.R
@@ -1,1 +1,6 @@
 "Type III (three) errors are models that answer the wrong question."
+library(AppliedPredictiveModeling)
+data(solubility)
+set.seed(100)
+indx <- createFolds(solTrainY, returnTRain = TRUE)
+ctrl <- trainControl(method = "cv", index = indx)
