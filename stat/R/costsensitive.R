713c8e1902345a94fe0ee7878eaae3189bf6fd60
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 15:45:41 -0400

    predictors

--- stat/R/costsensitive.R
+++ stat/R/costsensitive.R
@@ -17,3 +17,13 @@ isFactor <- unlist(lapply(ticdata, is.factor))
 convertCols <- names(isOdrered)[isOrdered | isFactor]
 for(i in convertCols) ticdata[, i] <- recodeLevels(ticdata[, i])
 ticdata$CARAVAN <- factor(as.character(ticdata$CARAVAN), levels = rev(levels(ticdata$CARAVAN)))
+"Use stratified random sampling for training set."
+split1 <- createDataPartition(ticdata$CARAVAN, p = .7)[[1]]
+other <- ticdata[-split1, ]
+training <- ticdata[split1,]
+"Evaluation and test sets."
+split2 <- createDataPartition(other$CARAVAN, p = 1/3)[[1]]
+evaluation <- other[split2,]
+testing <- other[-split2,]
+"Determine predictors."
+predictors <- names(training)[names(training) != "CARAVAN"]
