fd137e84d4871bcd4217ecc2af1787e65388f1bb
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 16:52:02 -0400

    alz

--- stat/R/featureselection.R
+++ stat/R/featureselection.R
@@ -7,3 +7,8 @@ library(MASS)
 library(pROC)
 library(rms)
 library(stats)
+data(AlzheimerDisease)
+predictors$E2 <- predictors$E3 < predictors$E4 <- 0
+predictors$E2[grepl("2", predictors$Genotype)] <- 1
+predictors$E3[grepl("3", predictors$Genotype)] <- 1
+predictors$E4[grepl("4", predictors$Genotype)] <- 1
