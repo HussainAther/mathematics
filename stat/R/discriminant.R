15e8786e272d653428cf1684c4daacd807e562e1
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 14:17:42 -0400

    linear matrix

--- stat/R/discriminant.R
+++ stat/R/discriminant.R
@@ -1,3 +1,7 @@
 "Discriminant analysis and other linear classification models."
 library(AppliedPredictiveModeling)
 set.seed(100)
+reduceCovMat <- cov(training[, reducedSet])
+"Eliminate linear combinations using trim.matrix."
+library(subselect)
+trimmingResults <- trim.matrix(reducedCovMat)
