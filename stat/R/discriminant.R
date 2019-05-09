2a598f40877e7633afa1931ddaf6e4853c21f862
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 14:19:17 -0400

    choose predictors

--- stat/R/discriminant.R
+++ stat/R/discriminant.R
@@ -5,3 +5,8 @@ reduceCovMat <- cov(training[, reducedSet])
 "Eliminate linear combinations using trim.matrix."
 library(subselect)
 trimmingResults <- trim.matrix(reducedCovMat)
+"Identify predictors."
+fullCovMat <- cov(training[, fullSet])
+fullSetResults <- trim.matrix(fullCovMat)
+"May choose what to exclude using this function."
+fullSetREsults$names.discarded
