b19fb3350f8f18b02cd39e1ee69104fcb8738a5a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-09 13:47:25 -0400

    rf and qda

--- stat/R/classification.R
+++ stat/R/classification.R
@@ -1,6 +1,13 @@
 "Measuring performance in classification models."
+"quadBoundaryFunc generates predictors and outcomes."
 library(AppliedPredictiveModeling)
 set.seed(100)
 simulatedTrain <- quadBoundaryFunc(500)
 simulatedTest <- quadBoundaryFunc(1000)
-
+"Random forest and quadratic discriminant models fit to data.
+library(randomForest)
+rfModel <- randomForest(class ~ X1 + X2,
+                        data = simulatedTrain,
+                        ntree = 2000)
+library(MASS)
+qdaModel <- qda(class ~ X1 + X2, data = simulatedTrain)
