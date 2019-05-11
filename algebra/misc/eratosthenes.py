5f516f5815323c2d178209fca8c975809100b24f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-11 02:17:17 -0400

    return

--- algebra/misc/eratosthenes.py
+++ algebra/misc/eratosthenes.py
@@ -1,4 +1,19 @@
+import numpy as np
+
 """
 The Sieve of Eratosthenes is an ancient algorithm to find all
 prime numbers up to a limit.
 """
+
+def erato(n):
+    """
+    For a given limit n, find the prime numbers up to it.
+    """
+    n = (n + 1) >> 1 # bit-wise operator for the limit
+    p = np.ones(n, dtype=np.int8)
+    i, j = 1, 3
+    while i < n:
+        if p[i]:
+            p[j * j >> 1::j] = 0
+        i, j = i+1, j+2
+    return p.sum()
