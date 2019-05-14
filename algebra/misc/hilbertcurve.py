a18824598d82cdb6aa702942b136ad2988668499
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 15:49:24 -0400

    tree char

--- algebra/misc/hilbertcurve.py
+++ algebra/misc/hilbertcurve.py
@@ -11,4 +11,26 @@ def hilbertCurve(n):
     """
     w = 1024
     return svgFromPoints(w)(hilbertPoints(w)(hilbertTree(n)))
- 
+
+def hilbertTree(n):
+    """
+    Nth (nth) application of a rule to a seedling tree.
+    """
+    # rule :: Dict Char [Char]
+    rule = {
+        "a": ["d", 'a", "a", "b"],
+        "b": ["c", "b", "b", "a"],
+        "c": ["b", "c", "c", "d"],
+        "d": ["a", "d", "d", "c"]
+    }
+    def go(tree): # go :: Tree Char -> Tree Char
+        """
+        Connecting characters from trees.
+        """
+        c = tree["root"]
+        xs = tree["nest"]
+        return Node(c)(
+            map(go, xs) if xs else map(flip(Node)([]), rule[c]))
+    seed = Node("a")([])
+    return list(islice(iterate(go)(seed), n))[-1] if 0 < n else seed
+  
