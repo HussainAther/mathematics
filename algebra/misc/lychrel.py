923c6ad677b7fb3636c8650fd0c6c78a16c9693e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:08:54 -0400

    initial commit

---
+++ algebra/misc/lychrel.py
@@ -0,0 +1,7 @@
+"""
+A Lychrel (lychrel) number is a natural number that cannot form a palindrome through the iterative process of 
+repeatedly reversing its digits and adding the resulting numbers. 
+1. Take an integer n, greater than zero.
+2. Form the next n of its series by reversing the digits of the current n and adding the result to the current n.
+3. Stop when n becomes palindromic - i.e. the digits of n in reverse order == n.
+"""
