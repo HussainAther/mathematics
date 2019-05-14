cc4c5605fb9ba36de5c42d656ba9340e1b563e1f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:54:44 -0400

    def

--- algebra/misc/mandelbrot.py
+++ algebra/misc/mandelbrot.py
@@ -1,3 +1,16 @@
+import math
+
 """
 Mandelbrot (mandelbrot) set.
 """
+
+def mandelbrot(z , c , n=40):
+    """
+    Formula variables z and c for Mandelbrot set.
+    """
+    if abs(z) > 1000:
+        return float("nan")
+    elif n > 0:
+        return mandelbrot(z ** 2 + c, c, n - 1) 
+    else:
+        return z ** 2 + c
