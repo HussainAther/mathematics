51e4f5b77b954115c02769dde4e750759285533c
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 16:07:11 -0400

    def

--- algebra/misc/vandercorput.py
+++ algebra/misc/vandercorput.py
@@ -2,3 +2,14 @@
 A van der Corput (corput) sequence is one of the simplest one-dimensional 
 low-discrepancy sequence over the unit interval. 
 """
+
+def vdc(n, base=2):   
+    """
+    Given n, generates the nth term of the van der Corput sequence in base 2.
+    """
+    vdc, denom = 0,1
+    while n:
+        denom *= base
+        n, remainder = divmod(n, base)
+        vdc += remainder / denom
+    return vdc
