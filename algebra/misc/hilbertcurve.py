93b91cd3dde2602081d556b35f0f8012599848cc
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 15:46:37 -0400

    def curve

--- algebra/misc/hilbertcurve.py
+++ algebra/misc/hilbertcurve.py
@@ -4,3 +4,11 @@ from inspect import signature
 """
 Hilbert (hilbert) curve.
 """
+
+def hilbertCurve(n):
+    """
+    An SVG (svg scalable vector graphics) string representing a Hilbert curve of degree n.
+    """
+    w = 1024
+    return svgFromPoints(w)(hilbertPoints(w)(hilbertTree(n)))
+ 
