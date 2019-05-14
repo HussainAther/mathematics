55832b512c33618c07d43fdd87d4bacf42b9c9c0
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 16:08:17 -0400

    initial commit

---
+++ algebra/misc/pvalue.py
@@ -0,0 +1,5 @@
+"""
+Given p-values (pvalue p value), we can adjust them for multiple comparisons. We do
+this to contorl the false positive (type 1 Type I) error rate or false discovery rate
+(FDR fdr). 
+"""
