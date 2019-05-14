929fa7cbcf0a38cca80148c432720c05acaa2101
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 15:56:55 -0400

    return

--- calc/kahan.py
+++ calc/kahan.py
@@ -2,3 +2,15 @@
 Kahan (kahan) summation reduces numerical error in total we get from adding a sequence
 of finite precision floating point numbers.
 """
+
+def kahansum(l):
+    """
+    For a list of input numbers l, perform Kahan summation.
+    """
+    summ = c = 0
+    for num in l:
+        y = num - c
+        t = summ + y
+        c = (t - summ) - y
+        summ = t
+    return summ
