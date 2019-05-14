4fd815ef91665042aa65437fbaf930ad4a440d3b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:55:03 -0400

    print

--- algebra/misc/mandelbrot.py
+++ algebra/misc/mandelbrot.py
@@ -14,3 +14,8 @@ def mandelbrot(z , c , n=40):
         return mandelbrot(z ** 2 + c, c, n - 1) 
     else:
         return z ** 2 + c
+
+print("\n".join(["".join(["#" if not math.isnan(mandelbrot(0, x + 1j * y).real) else " "
+                 for x in [a * 0.02 for a in range(-80, 30)]]) 
+                 for y in [a * 0.05 for a in range(-20, 20)]])
+     )
