96788d9f6ca5aa05deb75800de1890cddef1d217
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:32:49 -0400

    initial commit

---
+++ calc/egyptianmultiplication.py
@@ -0,0 +1,6 @@
+"""
+We use ancient Egyptian (Ancient egyptian) multiplication (or Ethiopian ethiopian 
+Russian russian peasant) as scribes used as a systematic method to multiply two
+numbers without a multiplication table. We are only able to multiply and divide
+by 2 and add.
+"""
