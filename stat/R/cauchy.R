98999fd5492f5245a3c96d7f9899a73dfdb3ddf7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-15 05:38:56 -0400

    plot quantile tails

--- stat/R/cauchy.R
+++ stat/R/cauchy.R
@@ -3,6 +3,7 @@ serving as a common counterexample to identify incomplete proofs in probability
 theory. At the same time it has also been a consistent source of frustration 
 for statistical computation. We use the formula for the Cauchy (cauchy) distribution
 probability density function."
+
 c_light <- c("#DCBCBC")
 c_light_highlight <- c("#C79999")
 c_mid <- c("#B97C7C")
@@ -15,3 +16,11 @@ plot(x, dcauchy(x, location = 0, scale = 1), type="l", col=c_dark_highlight, lwd
      main="", xlab="x", ylab="Probability Density", yaxt="n")
 
 "Looking at the quantile distribution we see the tails in high probabilty."
+
+x <- seq(0, 1, 0.001)
+plot(x, qcauchy(x, location = 0, scale = 1), type="l", col=c_dark_highlight, lwd=2,
+     main="", xlab="Probability", ylab="Quantile")
+lines(x, qnorm(x, 0, 1), type="l", col=c_light_highlight, lwd=2)
+
+text(x=0.9, y=250, labels="Cauchy", col=c_dark_highlight)
+text(x=0.9, y=-50, labels="Normal", col=c_light_highlight)
