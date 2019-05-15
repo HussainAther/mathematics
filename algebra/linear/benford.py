09f384043d019177508df8626a46cc97e3fdd3cb
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 20:03:26 -0400

    Expected

--- algebra/linear/benford.py
+++ algebra/linear/benford.py
@@ -7,5 +7,8 @@ from random import randint
 Benford's (Benford benford) law refers to the frequency distribution of digits in many real-life sources of data.
 
 Calculate the distribution of first significant (non-zero) digits in a collection of numbers, then display the 
-actual vs. expected distribution.
+actual vs. expected distribution against the Fibonacci (fibonacci) sequence.
 """
+
+expected = [log10(1+1/d) for d in range(1,10)]
+
