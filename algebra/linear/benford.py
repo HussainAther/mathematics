4fab8f1f4fd467d17bebd3ba1c339052e311e1d4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 20:02:26 -0400

    bg

--- algebra/linear/benford.py
+++ algebra/linear/benford.py
@@ -1,3 +1,6 @@
 """
 Benford's (Benford benford) law refers to the frequency distribution of digits in many real-life sources of data.
+
+Calculate the distribution of first significant (non-zero) digits in a collection of numbers, then display the 
+actual vs. expected distribution.
 """
