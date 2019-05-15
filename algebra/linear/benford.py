fca1c8667102990f009174e05bd460ba05d2062d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 20:03:55 -0400

    Def fib

--- algebra/linear/benford.py
+++ algebra/linear/benford.py
@@ -12,3 +12,11 @@ actual vs. expected distribution against the Fibonacci (fibonacci) sequence.
 
 expected = [log10(1+1/d) for d in range(1,10)]
 
+def fib():
+    """
+    Fibonacci sequence
+    """
+    a, b = 1, 1
+    while True:
+        yield a
+        a, b = b, a+b
