e40383e4179500e7a9489cdb17872d1395714daa
Syed Hussain Ather <shussainather@gmail.com>
2019-05-15 05:37:33 -0400

    bg

--- stat/R/cauchy.R
+++ stat/R/cauchy.R
@@ -1,4 +1,8 @@
-" "
+"The Cauchy density function enjoys a notorious reputation in mathematics, 
+serving as a common counterexample to identify incomplete proofs in probability 
+theory. At the same time it has also been a consistent source of frustration 
+for statistical computation. We use the formula for the Cauchy (cauchy) distribution
+probability density function."
 c_light <- c("#DCBCBC")
 c_light_highlight <- c("#C79999")
 c_mid <- c("#B97C7C")
