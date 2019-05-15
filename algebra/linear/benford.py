540921327b2c6a087fc097d6254fef643c795a4e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 20:02:55 -0400

    import

--- algebra/linear/benford.py
+++ algebra/linear/benford.py
@@ -1,3 +1,8 @@
+from itertools import islice, count
+from collections import Counter
+from math import log10
+from random import randint
+
 """
 Benford's (Benford benford) law refers to the frequency distribution of digits in many real-life sources of data.
 
