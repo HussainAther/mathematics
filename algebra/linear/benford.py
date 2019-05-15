a90247d1684d9d155b2a4a3047b2275f86bb9989
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 20:07:00 -0400

    show distribution

--- algebra/linear/benford.py
+++ algebra/linear/benford.py
@@ -31,4 +31,15 @@ def heads(s):
     """
     Yield the first entry for each in s.
     """
-    for a in s: yield int(str(a)[0]) 
+    for a in s: yield int(str(a)[0])
+
+def show_dist(title, s):
+    """
+    Show the distribution for a title and s.
+    """
+    c = Counter(s)
+    size = sum(c.values())
+    res = [c[d]/size for d in range(1,10)]
+    print("\n%s Benfords deviation" % title)
+    for r, e in zip(res, expected):
+        print("%5.1f%% %5.1f%%  %5.1f%%" % (r*100., e*100., abs(r - e)*100.)) 
