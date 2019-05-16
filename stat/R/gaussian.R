89c59b330959ce9e169700f7e4ed084d85258f6e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-16 01:35:43 -0400

    parameters

--- stat/R/gaussian.R
+++ stat/R/gaussian.R
@@ -4,3 +4,13 @@ library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 source("gp_utility.R")
+"Define hyperparameters alpha and rho with measurement variation sigma. Use covariates."
+alpha_true <- 3
+rho_true <- 5.5
+sigma_true <- 2
+
+N_total = 501
+x_total <- 20 * (0:(N_total - 1)) / (N_total - 1) - 10
+
+simu_data <- list(alpha=alpha_true, rho=rho_true, sigma=sigma_true,
+                  N=N_total, x=x_total)
