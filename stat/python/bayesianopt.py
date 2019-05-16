a05382722e06803aef824303b9ad1f8adccfd882
Syed Hussain Ather <shussainather@gmail.com>
2019-05-16 09:37:04 -0400

    plot

--- stat/python/bayesianopt.py
+++ stat/python/bayesianopt.py
@@ -16,3 +16,16 @@ def f(X, noise=noise):
 
 X_init = np.array([[-0.9], [1.1]])
 Y_init = f(X_init)
+
+# Dense grid of points within bounds
+X = np.arange(bounds[:, 0], bounds[:, 1], 0.01).reshape(-1, 1)
+
+# Noise-free objective function values at X 
+Y = f(X,0)
+
+# Plot optimization objective with noise level 
+plt.plot(X, Y, "y--", lw=2, label="Noise-free objective")
+plt.plot(X, f(X), "bx", lw=1, alpha=0.1, label="Noisy samples")
+plt.plot(X_init, Y_init, "kx", mew=3, label="Initial samples")
+plt.legend()
+
