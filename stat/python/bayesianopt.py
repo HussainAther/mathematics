fdbfb98ad3c9739b4231ee8a4d8bc9847667a029
Syed Hussain Ather <shussainather@gmail.com>
2019-05-16 09:42:09 -0400

    propose

--- stat/python/bayesianopt.py
+++ stat/python/bayesianopt.py
@@ -36,26 +36,43 @@ plt.legend()
 def expected_improvement(X, X_sample, Y_sample, gpr, xi=0.01):
     """
     Compute the EI (expected improvement) at points X based on existing samples X_sample and Y_sample 
-    using a Gaussian process surrogate model. Args: X: Points at which EI shall be computed (m x d). 
-    X_sample: Sample locations (n x d). Y_sample: Sample values (n x 1). gpr: A GaussianProcessRegressor 
-    fitted to samples. xi: Exploitation-exploration trade-off parameter. Returns: Expected improvements at 
+    using a Gaussian process surrogate model. X is the list of points at which EI shall be computed (m x d). 
+    X_sampleis the list of sample locations (n x d). Y_sample are the sample values (n x 1). gpr 
+    is a GaussianProcessRegressor fitted to samples. xi is an exploitation-exploration trade-
+    off parameter. Returns expected improvements at 
     points X. """
     pushforward_samples = sapply(stan_samples, function(x) (iota(x) - 5)**2)
     compute_mc_stats(pushforward_samples)
     mu, sigma = gpr.predict(X, return_std=True)
     mu_sample = gpr.predict(X_sample)
-
     sigma = sigma.reshape(-1, X_sample.shape[1])
-    
     # Needed for noise-based model,
     # otherwise use np.max(Y_sample).
     # See also section 2.4 in [...]
     mu_sample_opt = np.max(mu_sample)
-
     with np.errstate(divide='warn'):
         imp = mu - mu_sample_opt - xi
         Z = imp / sigma
         ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
         ei[sigma == 0.0] = 0.0
-
     return ei
+
+def propose_location(acquisition, X_sample, Y_sample, gpr, bounds, n_restarts=25):
+    """
+    Propose the next sampling point by optimizing the acquisition function. acquisition 
+    is the acquisition function. X_sample are the sample locations (n x d). Y_sample are sample 
+    values (n x 1). gpr is a GaussianProcessRegressor fitted to samples. Returns the location 
+    of the acquisition function maximum."""
+    dim = X_sample.shape[1]
+    min_val = 1
+    min_x = None
+    def min_obj(X):
+        # Minimization objective is the negative acquisition function
+        return -acquisition(X.reshape(-1, dim), X_sample, Y_sample, gpr)
+    # Find the best optimum by starting from n_restart different random points.
+    for x0 in np.random.uniform(bounds[:, 0], bounds[:, 1], size=(n_restarts, dim)):
+        res = minimize(min_obj, x0=x0, bounds=bounds, method='L-BFGS-B')        
+        if res.fun < min_val:
+            min_val = res.fun[0]
+            min_x = res.x           
+    return min_x.reshape(-1, 1)
