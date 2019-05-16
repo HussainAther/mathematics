7c47a63e1d1f124907b3fe6e17e53e820096ffff
Syed Hussain Ather <shussainather@gmail.com>
2019-05-16 09:36:01 -0400

    func

--- stat/python/bayesianopt.py
+++ stat/python/bayesianopt.py
@@ -2,5 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """
-Bayesian optimization.
+Bayesian optimization of objective function f, approximate iteratively with Gaussian process.
 """
+
+bounds = np.array([[-1.0, 2.0]])
+noise = 0.2
+
+def f(X, noise=noise):
+    """
+    Function to optimize.
+    """
+    return -np.sin(3*X) - X**2 + 0.7*X + noise * np.random.randn(*X.shape)
+
+X_init = np.array([[-0.9], [1.1]])
+Y_init = f(X_init)
