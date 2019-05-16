d84c63226db8615d71b26a8139e2101ea683452d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-16 09:38:49 -0400

    ei

--- stat/python/bayesianopt.py
+++ stat/python/bayesianopt.py
@@ -5,6 +5,7 @@ from scipy.stats import norm
 
 """
 Bayesian optimization of objective function f, approximate iteratively with Gaussian process.
+Use an expectation function.
 """
 
 bounds = np.array([[-1.0, 2.0]])
@@ -31,3 +32,29 @@ plt.plot(X, f(X), "bx", lw=1, alpha=0.1, label="Noisy samples")
 plt.plot(X_init, Y_init, "kx", mew=3, label="Initial samples")
 plt.legend()
 
+def expected_improvement(X, X_sample, Y_sample, gpr, xi=0.01):
+    """
+    Compute the EI (expected improvement) at points X based on existing samples X_sample and Y_sample 
+    using a Gaussian process surrogate model. Args: X: Points at which EI shall be computed (m x d). 
+    X_sample: Sample locations (n x d). Y_sample: Sample values (n x 1). gpr: A GaussianProcessRegressor 
+    fitted to samples. xi: Exploitation-exploration trade-off parameter. Returns: Expected improvements at 
+    points X. """
+    pushforward_samples = sapply(stan_samples, function(x) (iota(x) - 5)**2)
+    compute_mc_stats(pushforward_samples)
+    mu, sigma = gpr.predict(X, return_std=True)
+    mu_sample = gpr.predict(X_sample)
+
+    sigma = sigma.reshape(-1, X_sample.shape[1])
+    
+    # Needed for noise-based model,
+    # otherwise use np.max(Y_sample).
+    # See also section 2.4 in [...]
+    mu_sample_opt = np.max(mu_sample)
+
+    with np.errstate(divide='warn'):
+        imp = mu - mu_sample_opt - xi
+        Z = imp / sigma
+        ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        ei[sigma == 0.0] = 0.0
+
+    return ei
