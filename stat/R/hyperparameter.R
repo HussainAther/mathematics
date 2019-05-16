5826b0ddf859e578557eab08ea8b549145a55f93
Syed Hussain Ather <shussainather@gmail.com>
2019-05-16 09:30:35 -0400

    cv

--- stat/R/hyperparameter.R
+++ stat/R/hyperparameter.R
@@ -5,3 +5,5 @@ library(Matrix)
 data(agaricus.train, package = "xgboost")
 dtrain <- xgb.DMatrix(agaricus.train$data,
                       label = agaricus.train$label)
+cv_folds <- KFold(agaricus.train$label, nfolds = 5,
+                  stratified = TRUE, seed = 0)
