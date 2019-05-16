2b3baf7f3bf0131da6cd15e9c1f44a5a128b0985
Syed Hussain Ather <shussainather@gmail.com>
2019-05-16 01:34:08 -0400

    load

--- stat/R/gaussian.R
+++ stat/R/gaussian.R
@@ -1,2 +1,6 @@
 "Gaussian processes in Stan (stan). Use Gaussian process prior and analytic
 and non-analytic Gaussian process posteriors."
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+source("gp_utility.R")
