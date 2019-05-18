1318f3ede9a44d6733829fcf5e672624e696e4da
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 01:11:28 -0400

    corpus

--- stat/R/smsspam.R
+++ stat/R/smsspam.R
@@ -1,4 +1,4 @@
 "Filtering mobile phone spam with naive Bayes algorithm."
 sms_raw <- read.csv("spam.csv", stringsAsFactors = FALSE)
 sms_raw$type <- factor(sms_raw$type) # Convert to factor
- 
+sms_corpus <- Corpus(VectorSource(sms_raw$text)) 
