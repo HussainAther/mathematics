5685f97f8e40b9f9ac45d97114e86d8a25292e0a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 18:10:33 -0400

    import

--- stat/python/bernoulli.py
+++ stat/python/bernoulli.py
@@ -1,3 +1,16 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import sympy as sp
+import pymc
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+from mpl_toolkits.mplot3d import Axes3D
+from scipy import stats
+from scipy.special import gamma
+from sympy.interactive import printing
+
 """
 Bayesian estimation of Bernoulli trials.
 """
