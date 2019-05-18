af3521dfa87b62e711114bfaf0d6de7fab58ea07
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 18:13:32 -0400

    plot

--- stat/python/bernoulli.py
+++ stat/python/bernoulli.py
@@ -21,3 +21,20 @@ np.random.seed(123)
 nobs = 100
 theta = 0.3
 Y = np.random.binomial(1, theta, nobs)
+
+# Plot
+fig = plt.figure(figsize=(7,3))
+gs = gridspec.GridSpec(1, 2, width_ratios=[5, 1]) 
+ax1 = fig.add_subplot(gs[0])
+ax2 = fig.add_subplot(gs[1])
+
+ax1.plot(range(nobs), Y, "x")
+ax2.hist(-Y, bins=2)
+
+axn.yaxis.set(ticks=(0,1), ticklabels=("Failure", "Success"))
+ax2.xaxis.set(ticks=(-1,0), ticklabels=("Success", "Failure"));
+
+ax1.set(title=r"Bernoulli Trial Outcomes $(\theta=0.3)$", xlabel="Trial", ylim=(-0.2, 1.2))
+ax2.set(ylabel="Frequency")
+
+fig.tight_layout()
