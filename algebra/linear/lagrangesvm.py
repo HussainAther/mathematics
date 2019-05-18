328df8c8aa725622fa35ed2e042f1edf547efa54
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 19:38:06 -0400

    def primal

--- algebra/linear/lagrangesvm.py
+++ algebra/linear/lagrangesvm.py
@@ -28,10 +28,17 @@ Using the Karush-Kuhn-Tucker conditions, we derive the dual Lagrangian
 
 def dualL(alpha, y, x):
     """
-    For Lagrange multipliers alpha with x and y dictating parameters for the constraint.
+    For Lagrange multipliers alpha with x and y dictating parameters for the constraint,
+    calculate dual Lagrangian.
     """
+    return np.sum(alpha, axis = 0) - .5*(sum(sum(alpha*y*x.transpose(), sum=0), sum=1) 
 
 """
-We can attain convex duality by subjecting the Lagrange multipler
+We can attain convex duality by subjecting the Lagrange multipler at the conditional minimization
+of the objective function with our constraints. This gives the highest possible margin in the case 
+in which classification errors are inevitable due to the linearity of the separating hyperplane.
+We can calculate the Lagrangian functional for the primal problem. 
 """
 
+def primalL(alpha, y, x, b, mu, w, C, eps):
+     
