f0026e05d0274724fcc6c1f0bf401b3db2c8ea5a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 01:11:57 -0400

    preprocess

--- stat/R/smsspam.R
+++ stat/R/smsspam.R
@@ -2,3 +2,6 @@
 sms_raw <- read.csv("spam.csv", stringsAsFactors = FALSE)
 sms_raw$type <- factor(sms_raw$type) # Convert to factor
 sms_corpus <- Corpus(VectorSource(sms_raw$text)) 
+corpus_clean <- tm_map(sms_corpus, tolower) # Convert to lowercase
+corpus_clean <- tm_map(corpus_clean, removeNumbers) # Remove numbers
+
