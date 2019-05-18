db3fd7fde13e7fa9f04b08ae1804af12cdb85875
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 01:13:35 -0400

    split

--- stat/R/smsspam.R
+++ stat/R/smsspam.R
@@ -7,4 +7,7 @@ corpus_clean <- tm_map(corpus_clean, removeNumbers) # Remove numbers
 corpus_clean <- tm_map(corpus_clean, removeWords, stopwords()) # Remove stop words
 corpus_clean <- tm_map(corpus_clean, removePunctuation) # Remove punctuation
 corpus_clean <- tm_map(corpus_clean, stripWhitespace) # Remove whitespace
-
+sms_dtm <- DocumentTermMatrix(corpus_clean) # Create spares matrix
+sms_raw_train <- sms_raw[1:4169, ] # Split to training and test
+sms_raw_test  <- sms_raw[4170:5559, ] 
+sms_dtm_train <- sms_dtm[1:4169, ]
