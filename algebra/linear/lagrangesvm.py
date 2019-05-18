0fb5852c0d050c453dec97315c957334bf13b055
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 19:24:29 -0400

    def obj

--- algebra/linear/lagrangesvm.py
+++ algebra/linear/lagrangesvm.py
@@ -1,5 +1,12 @@
 """
-Use Lagrange multipler of a constraint to determine the Lagrangian.
+Use Lagrange multipler of a constraint to determine the Lagrangian for SVM (support
+vector machine) classification.
 """
+
+def obj(w, C, eps):
+    """
+    Objective function of the penalized margin maximation for a classifiation error 
+    eps, generalization ability of the machine C, sensitivity v, and vector w.
+    """
 
 
