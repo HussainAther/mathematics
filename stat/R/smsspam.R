a72245900cfbbe0410d96660dc7c021a94b24acd
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 01:12:28 -0400

    rm

--- stat/R/smsspam.R
+++ stat/R/smsspam.R
@@ -4,4 +4,5 @@ sms_raw$type <- factor(sms_raw$type) # Convert to factor
 sms_corpus <- Corpus(VectorSource(sms_raw$text)) 
 corpus_clean <- tm_map(sms_corpus, tolower) # Convert to lowercase
 corpus_clean <- tm_map(corpus_clean, removeNumbers) # Remove numbers
-
+corpus_clean <- tm_map(corpus_clean, removeWords, stopwords()) # Remove stop words
+corpus_clean <- tm_map(corpus_clean, removePunctuation) # Remove punctuation
