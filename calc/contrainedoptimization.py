ec133d5a22ceabba84337317eead0c4b34e790c5
Syed Hussain Ather <shussainather@gmail.com>
2019-05-19 01:36:26 -0400

    bg

--- calc/contrainedoptimization.py
+++ calc/contrainedoptimization.py
@@ -4,5 +4,5 @@ from scipy.optimize import minimize
 
 """
 We can find a point on a plane closest to the origin by minimizing a function along it.
- 
+This way we are constraining the problem to a certain condition. 
 """
