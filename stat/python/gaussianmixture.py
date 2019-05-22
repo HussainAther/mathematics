21524060ae7a8db2521dd48322cc1649ff60bf70
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 23:57:56 -0400

    make and plot data

--- stat/python/gaussianmixture.py
+++ stat/python/gaussianmixture.py
@@ -2,6 +2,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 import numpy as np
 
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.cluster import KMeans
+
 """
 Gaussian mixture models (gmm).
 """
+
+# Make data
+X, y_true = make_blobs(n_samples=400, centers=4,
+                       cluster_std=0.60, random_state=0)
+X = X[:, ::-1] # flip axes for better plotting
+kmeans = KMeans(4, random_state=0)
+labels = kmeans.fit(X).predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap="viridis")
