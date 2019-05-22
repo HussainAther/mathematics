a9d2d800c0d3980b779d85e7cd1cc9ae113f0cfc
Syed Hussain Ather <shussainather@gmail.com>
2019-05-22 00:14:05 -0400

    bg

--- stat/python/manifold.py
+++ stat/python/manifold.py
@@ -3,4 +3,10 @@ import seaborn as sns; sns.set()
 import numpy as np
 
 """
+Manifold learning is a class of unsupervised estimators that seeks to describe datasets 
+as low-dimensional manifolds embedded in high-dimensional spaces. When you think of a 
+manifold, I'd suggest imagining a sheet of paper: this is a two-dimensional object that 
+lives in our familiar three-dimensional world, and can be bent or rolled in that two 
+dimensions. In the parlance of manifold learning, we can think of this sheet as a two-
+dimensional manifold embedded in three-dimensional space.
 """
