ed41301bb045540cdc76d77be3ff75f108a00a2b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-22 00:00:41 -0400

    gmm

--- stat/python/gaussianmixture.py
+++ stat/python/gaussianmixture.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.cluster import KMeans
 from sklearn.datasets.samples_generator import make_blobs
+from sklearn.mixture import GMM
 from scipy.spatial.distance import cdist
 
 """
@@ -34,3 +35,14 @@ def plot_kmeans(kmeans, X, n_clusters=4, rseed=0, ax=None):
              for i, center in enumerate(centers)]
     for c, r in zip(centers, radii):
         ax.add_patch(plt.Circle(c, r, fc="#CCCCCC", lw=3, alpha=0.5, zorder=1))
+
+kmeans = KMeans(n_clusters=4, random_state=0)
+plot_kmeans(kmeans, X)
+rng = np.random.RandomState(13)
+X_stretched = np.dot(X, rng.randn(2, 2))
+plot_kmeans(kmeans, X_stretched)
+
+# GMM
+gmm = GMM(n_components=4).fit(X)
+labels = gmm.predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap="viridis")
