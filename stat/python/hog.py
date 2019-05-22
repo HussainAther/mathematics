b5c8e546000d6982af83a72cc558c588e7a126c4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-22 00:31:06 -0400

    img

--- stat/python/hog.py
+++ stat/python/hog.py
@@ -3,10 +3,11 @@ import seaborn as sns; sns.set()
 import numpy as np
 import skimage.data
 
-from skimage import data, color, feature
+from skimage import data, color, feature, transform
+from sklearn.datasets import fetch_lfw_people
 
 """
-Histogram of gradients pipeline.
+Histogram of gradients (HOG) pipeline.
 """
 
 image = color.rgb2gray(data.chelsea())
@@ -17,3 +18,13 @@ ax[0].imshow(image, cmap="gray")
 ax[0].set_title('input image')
 ax[1].imshow(hog_vis)
 ax[1].set_title("visualization of HOG features")
+
+faces = fetch_lfw_people()
+positive_patches = faces.images
+positive_patches.shape
+
+imgs_to_use = ["camera", "text", "coins", "moon",
+               "page", "clock", "immunohistochemistry",
+               "chelsea", "coffee", "hubble_deep_field"]
+images = [color.rgb2gray(getattr(data, name)())
+          for name in imgs_to_use]
