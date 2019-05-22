9d48409fac41b8bad1ccc8c37575632cb818d85b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 23:32:21 -0400

    class

--- stat/python/linearpredict.py
+++ stat/python/linearpredict.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
@@ -243,3 +244,50 @@ poly = PolynomialFeatures(3, include_bias=False)
 poly.fit_transform(x[:, None])
 
 poly_model = make_pipeline(PolynomialFeatures(7), LinearRegression())
+rng = np.random.RandomState(1)
+x = 10 * rng.rand(50)
+y = np.sin(x) + 0.1 * rng.randn(50)
+
+poly_model.fit(x[:, np.newaxis], y)
+yfit = poly_model.predict(xfit[:, np.newaxis])
+
+plt.scatter(x, y)
+plt.plot(xfit, yfit)
+
+"""
+Gaussian basis function.
+"""
+
+class GaussianFeatures(BaseEstimator, TransformerMixin):
+    """
+    Uniformly spaced Gaussian features for one-dimensional input.
+    """
+    def __init__(self, N, width_factor=2.0):
+        """
+        Initialize with N number of features.
+        """
+        self.N = N
+        self.width_factor = width_factor
+    
+    @staticmethod
+    def _gauss_basis(x, y, width, axis=None):
+        """
+        Basis for data in (x, y) format.
+        """
+        arg = (x - y) / width
+        return np.exp(-0.5 * np.sum(arg ** 2, axis))
+        
+    def fit(self, X, y=None):
+        """
+        Create N centers spread along the data range.
+        """
+        self.centers_ = np.linspace(X.min(), X.max(), self.N)
+        self.width_ = self.width_factor * (self.centers_[1] - self.centers_[0])
+        return self
+        
+    def transform(self, X):
+        """
+        Transform using the Gaussian basis.
+        """
+        return self._gauss_basis(X[:, :, np.newaxis], self.centers_,
+                                 self.width_, axis=1)
