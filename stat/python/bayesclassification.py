d8a921237754a1756220a2565ac87e570682d6fe
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 23:17:27 -0400

    blobs

--- stat/python/bayesclassification.py
+++ stat/python/bayesclassification.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 
+from sklearn.datasets import make_blobs
+
 """
 Naive Bayes classification.
 """
+
+X, y = make_blobs(100, 2, centers=2, random_state=2, cluster_std=1.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap="RdBu")
