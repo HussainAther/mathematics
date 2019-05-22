46817a48a87fb7b67c10c076c5605f0677d5bd79
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 23:42:47 -0400

    make data

--- stat/python/kernel.py
+++ stat/python/kernel.py
@@ -5,3 +5,14 @@ import numpy as np
 """
 Kernel density estimation with histograms.
 """
+
+def make_data(N, f=0.3, rseed=1):
+    """
+    Generate random data.
+    """
+    rand = np.random.RandomState(rseed)
+    x = rand.randn(N)
+    x[int(f * N):] += 5
+    return x
+
+x = make_data(1000)
