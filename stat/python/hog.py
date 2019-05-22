ab6e892143601f9350761cd49ea7dff56550239e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-22 00:31:37 -0400

    extract

--- stat/python/hog.py
+++ stat/python/hog.py
@@ -5,6 +5,7 @@ import skimage.data
 
 from skimage import data, color, feature, transform
 from sklearn.datasets import fetch_lfw_people
+from sklearn.feature_extraction.image import PatchExtractor
 
 """
 Histogram of gradients (HOG) pipeline.
@@ -28,3 +29,19 @@ imgs_to_use = ["camera", "text", "coins", "moon",
                "chelsea", "coffee", "hubble_deep_field"]
 images = [color.rgb2gray(getattr(data, name)())
           for name in imgs_to_use]
+
+def extract_patches(img, N, scale=1.0, patch_size=positive_patches[0].shape):
+    """
+    Extract patches from images.
+    """
+    extracted_patch_size = tuple((scale * np.array(patch_size)).astype(int))
+    extractor = PatchExtractor(patch_size=extracted_patch_size,
+                               max_patches=N, random_state=0)
+    patches = extractor.transform(img[np.newaxis])
+    if scale != 1:
+        patches = np.array([transform.resize(patch, patch_size)
+                            for patch in patches])
+    return patches
+negative_patches = np.vstack([extract_patches(im, 1000, scale)
+                              for im in images for scale in [0.5, 1.0, 2.0]])
+negative_patches.shape
