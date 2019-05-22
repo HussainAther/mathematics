e17f8db6a8f050ea81ddbbb58a3a4ccb63d581f4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 23:18:29 -0400

    naive code

--- stat/python/bayesclassification.py
+++ stat/python/bayesclassification.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 
 from sklearn.datasets import make_blobs
+from sklearn.naive_bayes import GaussianNB
 
 """
 Naive Bayes classification.
@@ -10,3 +11,14 @@ Naive Bayes classification.
 
 X, y = make_blobs(100, 2, centers=2, random_state=2, cluster_std=1.5)
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap="RdBu")
+model = GaussianNB()
+model.fit(X, y)
+rng = np.random.RandomState(0)
+Xnew = [-6, -14] + [14, 18] * rng.rand(2000, 2)
+ynew = model.predict(Xnew)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='RdBu')
+lim = plt.axis()
+plt.scatter(Xnew[:, 0], Xnew[:, 1], c=ynew, s=20, cmap='RdBu', alpha=0.1)
+plt.axis(lim)
+yprob = model.predict_proba(Xnew)
+yprob[-8:].round(2)
