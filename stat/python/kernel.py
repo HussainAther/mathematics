a45387bb72f67eda5c667f85997004f2b0602a76
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 23:43:32 -0400

    bin size

--- stat/python/kernel.py
+++ stat/python/kernel.py
@@ -16,3 +16,19 @@ def make_data(N, f=0.3, rseed=1):
     return x
 
 x = make_data(1000)
+hist = plt.hist(x, bins=30, normed=True)
+density, bins, patches = hist
+widths = bins[1:] - bins[:-1]
+
+# Effect of bin size 
+x = make_data(20)
+bins = np.linspace(-5, 10, 10)
+fig, ax = plt.subplots(1, 2, figsize=(12, 4),
+                       sharex=True, sharey=True,
+                       subplot_kw={'xlim':(-4, 9),
+                                   'ylim':(-0.02, 0.3)})
+fig.subplots_adjust(wspace=0.05)
+for i, offset in enumerate([0.0, 0.6]):
+    ax[i].hist(x, bins=bins + offset, normed=True)
+    ax[i].plot(x, np.full_like(x, -0.01), '|k',
+               markeredgewidth=1)
