abdee0484792b107832ce1b41bfd44efcfc4c6d7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 18:29:46 -0400

    begin code

--- stat/python/wald.py
+++ stat/python/wald.py
@@ -1,4 +1,11 @@
+import numpy as np
+
+from scipy import stats
+
 """
 Wald test as an asymptotic test. We decide whether to reject the 
 Wald statistics. 
 """
+
+theta0 = .5 # Null hypothesis
+k = np.random.binomial(1000, .3)
