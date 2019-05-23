29ce2138075ddf19912ddc5dd08a5a7cceb797c2
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 18:56:30 -0400

    bg

--- stat/python/perceptron.py
+++ stat/python/perceptron.py
@@ -1,9 +1,14 @@
 from sklearn.linear_model import Perceptron
 
 """
-The perceptron uses bagging. With a two-dimensional plane that 
+The perceptron uses bagging. Bagging is bootstrap aggregating that uses
+sampling and resampling to reduce collective variance in high-variance
+models. With a two-dimensional plane that 
 partitions into two regions at a boundary, we can identify points
 above the boundary as one and the ones below as zero. We take
 samples from each of these regions as a linear classifier that
 finds the line to separate the two categories.
 """
+
+p = Perceptron()
+
