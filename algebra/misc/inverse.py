a74dda32d7f489730ca7eac4f6b2cd52414adb2d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 00:59:17 -0400

    import

--- algebra/misc/inverse.py
+++ algebra/misc/inverse.py
@@ -1,5 +1,12 @@
+from euclid import extendedcgd
+
 """
 Let m and i be integers. Then [i]m is an invertible element of Z/(m) if and only if 1 is a gcd of m and i.
 
 Determine if an element of Z/(m) is invertible, and if so, how to find its inverse.
 """
+
+def inverse(i, m):
+    """
+    
+    """
