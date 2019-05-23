9f735186bc3c814ca200a63c3db37db2bbda4681
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 01:05:25 -0400

    comment

--- algebra/misc/homer.py
+++ algebra/misc/homer.py
@@ -1,3 +1,11 @@
 """
-Homer's (homer) algorithm to evaluate a polynomial.
+Homer's (homer) algorithm to evaluate a polynomial method.
 """
+
+def evaluate(f, a):
+    """
+    For an array of polynomial coefficients f and a value a, return the
+    polynomial evaluated at a. The coefficients should be given such that
+    the index of the arraay corresponded to that power polynomial. For example,
+    3x^2 + 1 would be [1, 0, 3]. 
+    """
