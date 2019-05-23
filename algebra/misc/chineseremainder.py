4df2125fd9302c04464f5939007566c75f2abc20
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 02:20:48 -0400

    Def mul

--- algebra/misc/chineseremainder.py
+++ algebra/misc/chineseremainder.py
@@ -18,4 +18,17 @@ def chinese_remainder(n, a):
         p = prod // n_i
         sum += a_i * mul_inv(p, n_i) * p
     return sum % prod
- 
+
+def mul_inv(a, b):
+    """
+    Modular multiplicative inverse.
+    """
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1 
