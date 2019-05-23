f51b343c78449344308218a23622abf0c14c5fcd
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 18:55:33 -0400

    bg

--- stat/python/perceptron.py
+++ stat/python/perceptron.py
@@ -1,4 +1,9 @@
 from sklearn.linear_model import Perceptron
 
 """
+The perceptron uses bagging. With a two-dimensional plane that 
+partitions into two regions at a boundary, we can identify points
+above the boundary as one and the ones below as zero. We take
+samples from each of these regions as a linear classifier that
+finds the line to separate the two categories.
 """
