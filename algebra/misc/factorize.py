db6f717207b8e0f846f61c4ee67d68ff6e00a7c3
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 01:11:59 -0400

    def

--- algebra/misc/factorize.py
+++ algebra/misc/factorize.py
@@ -1,3 +1,9 @@
 """
 Integer factorization (factorize).
 """
+
+def intfact(n):
+    """
+    Integer factorization for integer n > 1.
+    Return [p1,...pk) such that p1,...pk are positive primes, p1 <= p2 <= ... <= pk and n = p1*p2...pk
+    """
