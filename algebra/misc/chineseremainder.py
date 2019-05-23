9824e36e13b18fff658ac8e22cc899ce443ac739
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 02:17:45 -0400

    init commit

---
+++ algebra/misc/chineseremainder.py
@@ -0,0 +1,5 @@
+from functools import reduce
+
+"""
+The Chinese remainder theorem is a theorem of number theory, which states that if one knows the remainders of the Euclidean division of an integer n by several integers, then one can determine uniquely the remainder of the division of n by the product of these integers, under the condition that the divisors are pairwise coprime.
+"""
