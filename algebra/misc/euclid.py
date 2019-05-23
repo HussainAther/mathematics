0232d3206bbbc4d090d180e03a9e0546116d8c66
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 00:55:51 -0400

    def exteneded

--- algebra/misc/euclid.py
+++ algebra/misc/euclid.py
@@ -4,9 +4,15 @@ Greatest common divisor (gcd) Euclid's (euclid) algorithm.
 
 def gcd(a, b):
     """
-    For two numbers a and b, find g such that g is a gcd of a and b.
+    For two numbers a and b, find g such that g is a gcd of a and b
+    using Euclid's algorithm.
     """
     if b == 0:
         return a
     else:
         return gcd(b, a % b)
+
+def extendedcgd(a, b):
+    """
+    Output g, s, and t such that g is a gcd of a and b and g = sa + tb.
+    """
