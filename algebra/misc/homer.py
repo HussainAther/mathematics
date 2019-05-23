aa34b68ec0e12db24a8151ee18be07c86f3a491f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 01:08:47 -0400

    ldcf

--- algebra/misc/homer.py
+++ algebra/misc/homer.py
@@ -9,3 +9,11 @@ def evaluate(f, a):
     the index of the arraay corresponded to that power polynomial. For example,
     3x^2 + 1 would be [1, 0, 3]. 
     """
+    if f == []:
+        return 0
+    result = f[-1] # begin with leading coefficient
+    i = len(f) - 1 # number of times to iterate 
+    while i >= 0:
+        result = f[i] + a*result
+        i -= 1
+    return result
