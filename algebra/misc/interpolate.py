220ded5e53fc84b5ae1bf509fc05c1eb1ab3e6ec
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 00:41:13 -0400

    def

--- algebra/misc/interpolate.py
+++ algebra/misc/interpolate.py
@@ -2,3 +2,9 @@
 Let F = Q, R, C, or Z/(p), p prime (or indeed, let F be any field). Suppose we are given α1,...,αn,β1,...,βn ∈ F. Does there exist an f ∈ F[x] such that f(α1) = β1,...,f(αn) = βn? 
 If so, can we calculate one of degree smaller than n?
 """
+
+def interpolate(a, b):
+    """
+    For arrays a and b of the same length n, return a function f such that
+    f(a1) = b1,...f(an) = bn and deg(f) < n.
+    """
