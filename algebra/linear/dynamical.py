c3570d2bfc02627a2906a4d0e5ae3c8fcbf5ab81
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 00:39:55 -0400

    pe

--- algebra/linear/dynamical.py
+++ algebra/linear/dynamical.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Dynamical approach to stochastic sampling based off physical systems that evolve with
 Hamiltonian dynamics. In a Markov chain Monte Carlo simulation, we sample from a given 
@@ -15,3 +17,10 @@ def K(r):
         summ += i ** 2
     return summ/2
 
+def E(z):
+    """
+    Potential energy of a system for state variables z and probability
+    distribution p. For this example, we assume the probability distribution
+    is Gaussian (normal). 	
+    """
+    return -np.random.normal(z) * np.exp(z)
