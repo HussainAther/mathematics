57c6596b7069ba655e02df7ab99e48e0faf07f97
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 00:36:49 -0400

    ke

--- algebra/linear/dynamical.py
+++ algebra/linear/dynamical.py
@@ -1,4 +1,17 @@
 """
 Dynamical approach to stochastic sampling based off physical systems that evolve with
-Hamiltonian dynamics.
+Hamiltonian dynamics. In a Markov chain Monte Carlo simulation, we sample from a given 
+probabilitry distribution. Hamiltonian dynamics uses the probabilistic simulation in the
+form of a Hamiltonian system. 
 """
+
+def K(r):
+    """
+    Kinetic energy for intermediate momentunm variables r, which correspond
+    to change of state rate of state variablse z such that r = dz/dt.
+    """
+    summ = 0
+    for i in r:
+        summ += i ** 2
+    return summ/2
+
