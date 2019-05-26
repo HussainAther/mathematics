95bd6cb67ec752903705e0e864c97fe304cb36fe
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 00:57:16 -0400

    prbo values

--- stat/python/bushmosteller.py
+++ stat/python/bushmosteller.py
@@ -3,12 +3,18 @@ import numpy as np
 """
 The Bush-Mosteller (bush mosteller) stochastic model can analyze data with changing
 probabilities.
+
+For t events, we may present a column vector with each operator Tj in an r x r matrix.
+However, since we're only concerned with a single probability that can represent two 
+alternative events A1 and A2, we don't need to use matrixes. Instead, we'll use a trans-
+formed probability vector.  
 """
 
-def event(t):
+# Event probabilities
+a = np.linspace(0, 100, 10)
+
+def Q(n):
     """
-    For t events, we may present a column vector with each operator Tj in an r x r matrix.
-    However, since we're only concerned with a single probability that can represent two 
-    alternative events A1 and A2, we don't need to use matrixes. Instead, we'll use a trans-
-    formed probability vector. 
+    Apply operator Q to our event probabilities n times. 
     """
+     
