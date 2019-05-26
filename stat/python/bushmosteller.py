3146466b3c043c80b28040e7b8ff3d9c90bd157e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 00:37:59 -0400

    event bg

--- stat/python/bushmosteller.py
+++ stat/python/bushmosteller.py
@@ -7,5 +7,8 @@ probabilities.
 
 def event(t):
     """
-    For t events, we present a column vector with each operator Tj in an r x r matrix.
+    For t events, we may present a column vector with each operator Tj in an r x r matrix.
+    However, since we're only concerned with a single probability that can represent two 
+    alternative events A1 and A2, we don't need to use matrixes. Instead, we'll use a trans-
+    formed probability vector. 
     """
