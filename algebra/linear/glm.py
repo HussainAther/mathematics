6744e15022ee362bf4b72beea9a05bb8b07f2303
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 05:08:46 -0400

    hold constant

--- algebra/linear/glm.py
+++ algebra/linear/glm.py
@@ -19,3 +19,14 @@ print(res.summary())
 print("Total number of trials:",  data.endog[0].sum())
 print("Parameters: ", res.params)
 print("T-values: ", res.tvalues)
+
+# Hold explanatory variables constant at their means and manipulate the percentage
+# of low income households to find its impact on response
+means = data.exog.mean(axis=0)
+means25 = means.copy()
+means25[0] = stats.scoreatpercentile(data.exog[:,0], 25)
+means75 = means.copy()
+means75[0] = lowinc_75per = stats.scoreatpercentile(data.exog[:,0], 75)
+resp_25 = res.predict(means25)
+resp_75 = res.predict(means75)
+diff = resp_75 - resp_25
