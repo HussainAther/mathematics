65a0b18ca78c8f0e645c4bbf4df06a01abe91ddf
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 00:35:11 -0400

    def

--- stat/python/bushmosteller.py
+++ stat/python/bushmosteller.py
@@ -4,3 +4,8 @@ import numpy as np
 The Bush-Mosteller (bush mosteller) stochastic model can analyze data with changing
 probabilities.
 """
+
+def event(t):
+    """
+    For t events, we present a column vector with each operator Tj in an r x r matrix.
+    """
