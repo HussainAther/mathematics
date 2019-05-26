9bdd3629f702528380fe82cfbae6610401f2bb33
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 05:08:13 -0400

    print

--- algebra/linear/glm.py
+++ algebra/linear/glm.py
@@ -12,7 +12,10 @@ Generalized linear model (glm GLM)
 data = sm.datasets.star98.load(as_pandas=False)
 data.exog = sm.add_constant(data.exog, prepend=False)
 
-# Fit and summary
+# Fit and print
 glm_binom = sm.GLM(data.endog, data.exog, family=sm.families.Binomial())
 res = glm_binom.fit()
 print(res.summary())
+print("Total number of trials:",  data.endog[0].sum())
+print("Parameters: ", res.params)
+print("T-values: ", res.tvalues)
