7648647a53f1772cbb237a71bc8f08bc9871bd13
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 04:57:06 -0400

    lag

--- stat/python/var.py
+++ stat/python/var.py
@@ -10,10 +10,21 @@ Vector autoregression (VAR var) for multiple time series along with Vector Error
 Correction Models (VECM vecm).
 """
 
+# Load and process data
 mdata = sm.datasets.macrodata.load_pandas().data
 dates = mdata[["year", "quarter"]]
 quarterly = dates["year"] + "Q" + dates["quarter"].astype(int).astype(str)
 quarterly = dates_from_str(quarterly)
 mdata.index = pd.DatetimeIndex(quarterly)
 data = np.log(mdata).diff().dropna()
+
+# Generate model 
 model = VAR(data)
+results = model.fit(2)
+results.summary()
+results.plot()
+results.plot_acorr() # autocorrelation function
+
+# Lag order selection
+model.select_order(15) # Choice of lag order
+results = model.fit(maxlags=15, ic="aic") # Fit to AIC
