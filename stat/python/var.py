753f43d8f7e86cc1d533e08530183d175831b5b4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 04:54:26 -0400

    load data

--- stat/python/var.py
+++ stat/python/var.py
@@ -3,8 +3,13 @@ import pandas as pd
 import statsmodels.api as sm
 
 from statsmodels.tsa.api import VAR, DynamicVAR
+from statsmodels.tsa.base.datetools import dates_from_str
 
 """
 Vector autoregression (VAR var) for multiple time series along with Vector Error
 Correction Models (VECM vecm).
 """
+
+mdata = sm.datasets.macrodata.load_pandas().data
+dates = mdata[["year", "quarter"]]
+quarterly = dates["year"] + "Q" + dates["quarter"]
