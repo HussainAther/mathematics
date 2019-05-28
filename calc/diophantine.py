b7af5e9038bbb413362ad3db590f930485ff17ec
Syed Hussain Ather <shussainather@gmail.com>
2019-05-27 21:19:34 -0400

    return linear

--- calc/diophantine.py
+++ calc/diophantine.py
@@ -2,7 +2,14 @@
 Diophantine (diophantine) equations solutions.
 """
 
-def dio(a, b, c):
+def lineardio(a, b, c):
     """
     Solving linear ax + by = c.
     """
+    q, r = divmod(a, b) # return the quotient and remainder of a / b
+    if r == 0:
+        return([0, c/b]) # solution
+    sol = lineardio(b, r, c)
+    u = sol[0]
+    v = sol[1]
+    return([v, u-q*v])
