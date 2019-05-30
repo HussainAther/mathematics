adb901555e3114bcd137ff2d9f6da5e7c3a4b630
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 19:54:36 -0400

    Spacing

--- stat/R/pca.R
+++ stat/R/pca.R
@@ -1,20 +1,20 @@
 "Principal component analysis (PCA)."
 install.packages("ISLR")
 library(ISLR)
-states=row.names(USArrests)
+states = row.names(USArrests)
 states
 names(USArrests)
 apply(USArrests, 2, mean)
 apply(USArrests, 2, var)
-pr.out=prcomp(USArrests, scale=TRUE)
+pr.out = prcomp(USArrests, scale=TRUE)
 names(pr.out)
 pr.out$center
 pr.out$scale
 pr.out$rotation
 dim(pr.out$x)
 biplot(pr.out, scale=0)
-pr.out$rotation=-pr.out$rotation
-pr.out$x=-pr.out$x
+pr.out$rotation = -pr.out$rotation
+pr.out$x = -pr.out$x
 biplot(pr.out, scale=0)
 pr.out$sdev
 pr.var=pr.out$sdev^2
