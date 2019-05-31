b9d6d1e3784d54e016945fc7ff293bc4a3fd1b54
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 22:05:02 -0400

    no import *

--- algebra/linear/leastsquare.py
+++ algebra/linear/leastsquare.py
@@ -1,10 +1,6 @@
-import pylab as p
-
-from vpython import *
-from numpy import *
-from numpy.linalg import inv
-from numpy.linalg import solve
-from numpy import zeros
+import pylab as pl
+import numpy as np
+import vpython as vp
 
 """
 Perform least-square fit of a parabola to data using linalg package to solve the set
@@ -15,16 +11,16 @@ of linear equations.
 t = arange(1, 2, .1) # time range
 x = array([1, 1.1, 1.24, 1.35, 1.451, 1.5, 1.92]) # x range curve
 y = array([.52, .8, .7, 1.8, 2.9, 3.6])
-p.plot(x, y, "bo") # plot data in blue
+pl.plot(x, y, "bo") # plot data in blue
 sig = array([.1, .1, .2, .3, .2, .1, .1]) # error bar lengths (sigma)
-p.errorbar(x, y, sig) # plot them
-p.title("Linear least square fit")
-p.xlabel("x")
-p.ylabel("y")
-p.grid(True)
+pl.errorbar(x, y, sig) # plot them
+pl.title("Linear least square fit")
+pl.xlabel("x")
+pl.ylabel("y")
+pl.grid(True)
 Nd = 7
-A = zeros((3,3), float))
-bvec = zeros((3,1), float)
+A = np.zeros((3,3), float))
+bvec = np.zeros((3,1), float)
 ss = sx = sxx = sy = sxxx = sxxxx = sxy = sxy = sxxy = 0 # s and its derivatives
 
 for i in range(0, Nd):
@@ -40,20 +36,21 @@ for i in range(0, Nd):
     sxxxx += rh1*rh1/sig2
 
 A = array([[ss, sx, sxx], [sx, sxx, sxxx], [sxx, sxxx, sxxx]])
-
 bvec = array([sy, sxy, sxxy])
 
-xvec = multiply(inv(A), bvec)
-Itest = multiply(A, inv(A))
+xvec = np.multiply(np.linalg.inv(A), bvec)
+Itest = np.multiply(A, np.linalg.inv(A))
 print("\n x vector via inverse" + str(xvec) + "\n")
 print("A*inverse(A) " + Itest + "\n")
 
-xvec = solve(A, bvec) # Solve via elimination
+xvec = np.linalg.solve(A, bvec) # Solve via elimination
 print("x Matrix via direct" + str(xvec))
+
 for i in range(0, Nd):
     s = xvec[0] + xvec[1]*x[i] + xvec[2]*x[i]*x[i]
     print(" %d %5.3f %5.3f %8.7f \n" %(i, x[i], y[i], x))
+    
 curve = xvec[0] + xvec[1]*t + xvec[2]*t**2
 points = xvec[0] + xvec[1]*c + xvec[2]*x**2
-p.plot(t, curve, "r", x, points, "ro") # Plot the fit line in red.
-p.show() 
+pl.plot(t, curve, "r", x, points, "ro") # Plot the fit line in red
+pl.show()
