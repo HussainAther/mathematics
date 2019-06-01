1b610859575b6dbbbc0e646cbb509f79fff7473c
Syed Hussain Ather <shussainather@gmail.com>
2019-06-01 00:28:27 -0400

    init commit

---
+++ calc/delaunay.py
@@ -0,0 +1,5 @@
+"""
+In mathematics and computational geometry, a Delaunay triangulation (also known as a Delone triangulation) 
+for a given set P of discrete points in a plane is a triangulation DT(P) 
+such that no point in P is inside the circumcircle of any triangle in DT(P).
+"""
