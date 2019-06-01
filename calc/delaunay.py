2d0de006e4886d536715b969e6c7af32e277cafa
Syed Hussain Ather <shussainather@gmail.com>
2019-06-01 00:29:31 -0400

    import

--- calc/delaunay.py
+++ calc/delaunay.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 """
 In mathematics and computational geometry, a Delaunay triangulation (also known as a Delone triangulation) 
 for a given set P of discrete points in a plane is a triangulation DT(P) 
 such that no point in P is inside the circumcircle of any triangle in DT(P).
+This methods uses the Bowyer-Watson algorithm
 """
