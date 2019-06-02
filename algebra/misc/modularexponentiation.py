bf7328032888100edccfb6cf75c12fe29ebff32a
Syed Hussain Ather <shussainather@gmail.com>
2019-06-02 08:20:33 -0400

    code

--- algebra/misc/modularexponentiation.py
+++ algebra/misc/modularexponentiation.py
@@ -4,3 +4,18 @@ performed over a modulus. It is useful in computer science,
 especially in the field of public-key cryptography.
 """
 
+def modularExponential(base, power, mod):
+    """
+    For a base, power, and modulus, perform modular
+    exponentiation.
+    """
+    if power < 0:
+        return -1
+    base %= mod
+    result = 1
+    while power > 0:
+        if power & 1:
+            result = (result * base) % mod
+	power = power >> 1
+	base = (base * base) % mod
+    return result
