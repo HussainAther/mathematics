b7f3b469ce1ce0780ec639675558234d50d73a3c
Syed Hussain Ather <shussainather@gmail.com>
2019-06-10 13:49:08 -0400

    scan

--- algebra/linear/deepbeliefnetwork.py
+++ algebra/linear/deepbeliefnetwork.py
@@ -113,3 +113,77 @@ class DBN(object):
             pretrain_fns.append(fn)
         return pretrain_fns
 
+    def build_finetune_functions(self, datasets, batch_size, learning_rate):
+        """
+        Generates a function train that implements one step of
+        finetuning, a function validate that computes the error on a
+        batch from the validation set, and a function test that
+        computes the error on a batch from the testing set.
+        datasets is a list of pairs of tensors that have all the datasets.
+        batch_size is the size of a minibatch, and learning_rate is the learning
+        learning rate for finetune stage.        
+        """
+        (train_set_x, train_set_y) = datasets[0]
+        (valid_set_x, valid_set_y) = datasets[1]
+        (test_set_x, test_set_y) = datasets[2]
+        # Compute number of minibatches for training, validation and testing
+        n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
+        n_valid_batches //= batch_size
+        n_test_batches = test_set_x.get_value(borrow=True).shape[0]
+        n_test_batches //= batch_size
+        index = T.lscalar('index')  # index to a [mini]batch
+        # Compute the gradients with respect to the model parameters
+        gparams = T.grad(self.finetune_cost, self.params)
+        # Compute list of fine-tuning updates
+        updates = []
+        for param, gparam in zip(self.params, gparams):
+            updates.append((param, param - gparam * learning_rate))
+        train_fn = theano.function(
+            inputs=[index],
+            outputs=self.finetune_cost,
+            updates=updates,
+            givens={
+                self.x: train_set_x[
+                    index * batch_size: (index + 1) * batch_size
+                ],
+                self.y: train_set_y[
+                    index * batch_size: (index + 1) * batch_size
+                ]
+            }
+        )
+
+        test_score_i = theano.function(
+            [index],
+            self.errors,
+            givens={
+                self.x: test_set_x[
+                    index * batch_size: (index + 1) * batch_size
+                ],
+                self.y: test_set_y[
+                    index * batch_size: (index + 1) * batch_size
+                ]
+            }
+        )
+        valid_score_i = theano.function(
+            [index],
+            self.errors,
+            givens={
+                self.x: valid_set_x[
+                    index * batch_size: (index + 1) * batch_size
+                ],
+                self.y: valid_set_y[
+                    index * batch_size: (index + 1) * batch_size
+                ]
+            }
+        )
+        def valid_score():
+            """
+            Scan the entire validation set from the valid batches.
+            """
+            return [valid_score_i(i) for i in range(n_valid_batches)]
+        def test_score():
+            """
+            Scan the validation set for test batches.
+            """
+            return [test_score_i(i) for i in range(n_test_batches)]
+        return train_fn, valid_score, test_score
