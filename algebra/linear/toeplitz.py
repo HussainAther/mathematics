cdeac191fde503743c16770d8f9634687325a245
Syed Hussain Ather <shussainather@gmail.com>
2019-07-07 22:32:14 -0400

    init def checkdiag

--- algebra/linear/toeplitz.py
+++ algebra/linear/toeplitz.py
@@ -4,3 +4,10 @@ Find out if a matrix is Toeplitz (toeplitz) or not for some matrix m.
 
 col = len(mat[0]) # columns
 row = len(mat) # rows
+
+def checkDiag(mat, i, j):
+    """
+    Check if all elements present in the descending diagonal
+    starting from position (i, j) in the matrix are all the same
+    or not.
+    """
