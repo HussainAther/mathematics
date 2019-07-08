7d8859f9fa8b03a76f27dc36bcc8a091a0cbed80
Syed Hussain Ather <shussainather@gmail.com>
2019-07-07 22:31:28 -0400

    input

--- algebra/linear/toeplitz.py
+++ algebra/linear/toeplitz.py
@@ -1,3 +1,6 @@
 """
-Find out if a matrix is Toeplitz (toeplitz) or not.
+Find out if a matrix is Toeplitz (toeplitz) or not for some matrix m.
 """
+
+col = len(mat[0]) # columns
+row = len(mat) # rows
