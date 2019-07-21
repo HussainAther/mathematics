2d651d46f36f074f82caa67808907a9cc29cbb9a
Syed Hussain Ather <shussainather@gmail.com>
2019-07-20 22:42:50 -0400

    fix equals

--- algebra/misc/morlet.m
+++ algebra/misc/morlet.m
@@ -1,6 +1,6 @@
-function m¼morlet(t, params)
-sigma ¼ params(1);
-m ¼ pi∧-0.25*exp(-i*sigma.*t-0.5*t.∧2);
+function m=morlet(t, params)
+sigma = params(1);
+m = pi∧-0.25*exp(-i*sigma.*t-0.5*t.∧2);
 In plot_cwt.m:
 function plot_cwt(t, coefs, scales)
 imagesc(t, scales, coefs);
