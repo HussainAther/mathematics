f61505379dfec6234d4590404443003502483c71
Syed Hussain Ather <shussainather@gmail.com>
2019-07-20 22:35:39 -0400

    indent

--- algebra/misc/ramp.m
+++ algebra/misc/ramp.m
@@ -2,8 +2,8 @@
 % This script generates the image that creates the Mach band visual illusion.
 In=10*ones(64,128); %initiates the visual stimulus with a constant value of 10
 for i=1:65
-In(:,32+i)=10+i;
-%ramps up the value for the middle matrix elements (column 33 to column 97)
+    In(:,32+i)=10+i;
+    %ramps up the value for the middle matrix elements (column 33 to column 97)
 end
 In(:,98:end)=75; %sets the last columns of the matrix to the final brightness value of 75
 figure
