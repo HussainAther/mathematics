b7fc36cc97bce3dbd3a608b7be825037e17fe43d
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:31:34 -0400

    plot together

--- stat/MATLAB/kmeans.m
+++ stat/MATLAB/kmeans.m
@@ -12,3 +12,7 @@ for i=1:3
     plot(d(gidx==i,1),d(gidx==i,2),[lineCol(i) 'o’]) 
 end
 plot(cents(:,1),cents(:,2),'ko’)
+for i=1:length(d)
+    plot([ d(i,1) cents(gidx(i),1) ], ...
+         [ d(i,2) cents(gidx(i),2) ],lineCol(gidx(i)))
+end
