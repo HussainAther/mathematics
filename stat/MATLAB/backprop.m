d9e6e123412e6b8074656a7ef681b56e00e8f86a
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:33:31 -0400

    init var

--- stat/MATLAB/backprop.m
+++ stat/MATLAB/backprop.m
@@ -3,3 +3,10 @@ Neural network with backpropagation.
 }%
 inputs = [ 1 0 1; 0 1 1; 1 1 1; 0 0 1 ]; 
 output = [ 0; 0; 1; 1 ];
+nInputsNodes = size(inputs,2);
+nHiddenNodes = 3;
+nOutputNodes = 1;
+% random initial weights
+weights_i2h = randn(nInputsNodes,nHiddenNodes); 
+weights_h2o = randn(nHiddenNodes,nOutputNodes); 
+l_rate = .2;
