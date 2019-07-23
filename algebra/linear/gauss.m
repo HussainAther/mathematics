9aff9fd1c273cc3e851888a712d1e2ee0157a7fd
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 16:06:38 -0400

    fit it

--- algebra/linear/gauss.m
+++ algebra/linear/gauss.m
@@ -6,8 +6,11 @@ fwhm = 1;
 cent = 3;
 nois = .5;
 x = -10:.1:10;
+
 gaus = peak*exp(-(x-cent).^2 / (2*fwhm^2));
 gaus = gaus + nois*randn(size(gaus));
+
 InitParms = [ 2 2 –2 ];
 funch = @(initParms) fitGaussian(initParms,x,gaus); [outparams,sse] = fminsearch(funch,initParms);
-
+funch = @(x) -sin(x)./x;
+[xval,funcval] = fminsearch(funch,0);
