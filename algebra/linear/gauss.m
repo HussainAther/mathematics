e4ac0df2ce652f68f5e18e2a7299b4026e6e2d0e
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 16:05:58 -0400

    params

--- algebra/linear/gauss.m
+++ algebra/linear/gauss.m
@@ -1,3 +1,13 @@
 %{
 Fitting Gaussian model.
 }%
+peak = 4;
+fwhm = 1;
+cent = 3;
+nois = .5;
+x = -10:.1:10;
+gaus = peak*exp(-(x-cent).^2 / (2*fwhm^2));
+gaus = gaus + nois*randn(size(gaus));
+InitParms = [ 2 2 –2 ];
+funch = @(initParms) fitGaussian(initParms,x,gaus); [outparams,sse] = fminsearch(funch,initParms);
+
