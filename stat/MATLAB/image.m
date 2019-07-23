addffd508c0ed3be8d9215f77d51b354709023de
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 17:29:39 -0400

    rgb

--- stat/MATLAB/image.m
+++ stat/MATLAB/image.m
@@ -1,5 +1,16 @@
+%{
+Image reading functionality.
+}%
 pic = imread('saturn.png');
 imagesc(pic)
 pic2 = pic;
 pic2(:,:,4) = pic(:,:,1);
 imagesc(pic2)
+colorchans = { ’red';'green';'blue' };
+for chani=1:3
+    subplot(2,2,chani)
+    imagesc(pic(:,:,chani))
+    axis off
+    set(gca,'clim',[0 255])
+    title([ colorchans{chani} ' channel' ])
+end
