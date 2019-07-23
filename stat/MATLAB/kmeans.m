f8e130b06988657fdbf55c6229f86cc686a78ac4
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:29:51 -0400

    hold up wait a minute

--- stat/MATLAB/kmeans.m
+++ stat/MATLAB/kmeans.m
@@ -6,3 +6,7 @@ for i=1:length(d)
 [ d(i,2) cents(gidx(i),2) ],lineCol(gidx(i)))
 end
 [gidx,cents,sumdist,distances] = kmeans(d,3);
+lineCol = 'rbk';
+hold on
+for i=1:3
+plot(d(gidx==i,1),d(gidx==i,2),[lineCol(i) 'o’]) end
