8fe84f10226ff9ce794559f28a928413c0d63042
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 15:34:01 -0400

    hidden layer

--- stat/MATLAB/backprop.m
+++ stat/MATLAB/backprop.m
@@ -10,3 +10,5 @@ nOutputNodes = 1;
 weights_i2h = randn(nInputsNodes,nHiddenNodes); 
 weights_h2o = randn(nHiddenNodes,nOutputNodes); 
 l_rate = .2;
+hdLayerResp = inputs(ini,:) * weights_i2h; 
+hdLayerResp = 2./(1+exp(-hdLayerResp'*2))-1;
