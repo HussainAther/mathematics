374e29c7b9a2730f100bf4c803a25c337cfe4c87
Syed Hussain Ather <shussainather@gmail.com>
2019-07-23 16:03:28 -0400

    params

--- algebra/linear/fitmodel.m
+++ algebra/linear/fitmodel.m
@@ -2,4 +2,14 @@
 Model fitting practice.
 }%
 funch = @(t) t.^2;
-[xval,funcval] = fminsearch(funch,-2);
+[xval,funcval] = fminbnd(funch,-30,-2);
+
+% parameters
+a=.2; c=.6; b=.9;
+
+% generate random data and then modulate
+x = rand(1,10000);
+y(x<c) = a + sqrt(x(x<c).*(b-a).*(c-a));
+y(x>c) = b—sqrt((1-x(x>c)).*(b-a).*(b-c));
+% plot distribution
+hist(y,100)
