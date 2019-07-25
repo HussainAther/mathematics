f7df1ece0d2ba6a48ef1beb9701a99bd61f5e2d1
Syed Hussain Ather <shussainather@gmail.com>
2019-07-24 23:13:43 -0400

    data

--- stat/MATLAB/interexterpol.m
+++ stat/MATLAB/interexterpol.m
@@ -10,3 +10,7 @@ interpY = linspace(min(eY),max(eY),intFact); [gridX,gridY] = ndgrid(interpX,inte
 F = scatteredInterpolant(eX',eY',eeg(:,300)); 
 interpDat = F(gridX,gridY);
 
+data = [1 4 3 6 2 19];
+datatimes = 1:6;
+newtimes = 1:.001:6;
+newdat = interp1(datatimes,data,newtimes,’spline');
