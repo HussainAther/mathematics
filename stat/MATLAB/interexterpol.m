1f1837d426df4461e2bbc07af703aa3bbd7b919e
Syed Hussain Ather <shussainather@gmail.com>
2019-07-24 23:14:22 -0400

    line line line

--- stat/MATLAB/interexterpol.m
+++ stat/MATLAB/interexterpol.m
@@ -6,7 +6,8 @@ newdata = F(newtimes);
 [eX,eY] = pol2cart(pi/180*[chanlocs.theta],... [chanlocs.radius]);
 intFact = 100;
 interpX = linspace(min(eX),max(eX),intFact); 
-interpY = linspace(min(eY),max(eY),intFact); [gridX,gridY] = ndgrid(interpX,interpY);
+interpY = linspace(min(eY),max(eY),intFact); 
+[gridX,gridY] = ndgrid(interpX,interpY);
 F = scatteredInterpolant(eX',eY',eeg(:,300)); 
 interpDat = F(gridX,gridY);
 
@@ -14,3 +15,7 @@ data = [1 4 3 6 2 19];
 datatimes = 1:6;
 newtimes = 1:.001:6;
 newdat = interp1(datatimes,data,newtimes,’spline');
+origN=10; 
+padN=100;
+data = randn(origN,1);
+datapad = ifft(fft(data)/origN, padN)*padN;
