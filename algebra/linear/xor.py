fc8b014dcba6dafa6e7e5931fe2ee9660698137d
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 05:27:49 -0400

    deriv

--- algebra/linear/xor.py
+++ algebra/linear/xor.py
@@ -19,3 +19,18 @@ hidden_layer_output = sigmoid(hidden_layer_activation)
 output_layer_activation = np.dot(hidden_layer_output,output_weights)
 output_layer_activation += output_bias
 predicted_output = sigmoid(output_layer_activation)
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+#Backpropagation
+error = expected_output - predicted_output
+d_predicted_output = error * sigmoid_derivative(predicted_output)
+ 
+error_hidden_layer = d_predicted_output.dot(output_weights.T)
+d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
+#Updating Weights and Biases
+output_weights += hidden_layer_output.T.dot(d_predicted_output) * lr
+output_bias += np.sum(d_predicted_output,axis=0,keepdims=True) * lr
+hidden_weights += inputs.T.dot(d_hidden_layer) * lr
+hidden_bias += np.sum(d_hidden_layer,axis=0,keepdims=True) * lr
