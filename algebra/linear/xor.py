547667a9bb6921b6d2086f5e6de90dcb245668dc
Syed Hussain Ather <shussainather@gmail.com>
2019-07-25 05:27:13 -0400

    sigmoid

--- algebra/linear/xor.py
+++ algebra/linear/xor.py
@@ -9,3 +9,13 @@ hidden_weights = np.random.uniform(size=(inputLayerNeurons,hiddenLayerNeurons))
 hidden_bias =np.random.uniform(size=(1,hiddenLayerNeurons))
 output_weights = np.random.uniform(size=(hiddenLayerNeurons,outputLayerNeurons))
 output_bias = np.random.uniform(size=(1,outputLayerNeurons))
+
+def sigmoid (x):
+    return 1/(1 + np.exp(-x))
+
+hidden_layer_activation = np.dot(inputs,hidden_weights)
+hidden_layer_activation += hidden_bias
+hidden_layer_output = sigmoid(hidden_layer_activation)
+output_layer_activation = np.dot(hidden_layer_output,output_weights)
+output_layer_activation += output_bias
+predicted_output = sigmoid(output_layer_activation)
