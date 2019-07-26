e036f9b3b9d9d04eea1034ca148ada792599b0cc
Syed Hussain Ather <shussainather@gmail.com>
2019-07-26 00:32:27 -0400

    init

---
+++ calc/multigrid/util.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def l2_norm(a,h):
+    return h*np.sqrt(np.sum(a**2))
+
+def prolong_lin(a): 
+    pshape = (2*np.shape(a)[0]-1,2*np.shape(a)[1]-1) 
+    p = np.empty(pshape,float)
+    p[0: :2,0: :2] = a[0: ,0: ]
+    p[1:-1:2,0: :2] = 0.5*(a[0:-1,0: ]+a[1: ,0: ]) 
+    p[0: :2,1:-1:2] = 0.5*(a[0: ,0:-1]+a[0: ,1: ]) 
+    p[1:-1:2,1:-1:2] = 0.25*(a[0:-1,0:-1]+a[1: ,0:-1] + a[0:-1,1: ]+a[1: ,1: ]) 
+    return p
+
+def restrict_hw(a): 
+    rshape = (np.shape(a)[0]/2+1,np.shape(a)[1]/2+1) 
+    r = np.empty(rshape,float) 
+    r[1:-1,1:-1]=0.5*a[2:-1:2,2:-1:2] + 0.125*(a[2:-1:2,1:-2:2]+a[2:-1:2,3: :2] + a[1:-2:2,2:-1:2]+a[3: :2,2:-1:2])
+    r[0,0: ] = a[0,0: :2] 
+    r[-1,0: ] = a[-1,0: :2] 
+    r[0: ,0] = a[0: :2,0] 
+    r[0: ,-1] = a[0: :2,-1] 
+    return r
