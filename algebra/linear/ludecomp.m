fc6ebfed40afe29772cba4b53176505f4d748112
Syed Hussain Ather <shussainather@gmail.com>
2019-07-30 08:07:37 -0400

    for

--- algebra/linear/ludecomp.m
+++ algebra/linear/ludecomp.m
@@ -11,3 +11,13 @@ function [X]=modlu(A, B)
   end
   SUM1 = 0;
   SUM2 = 0;
+  for J = 2:N
+    for I = J:N
+      SUM1 = 0;
+      for K = 1:J-1
+        SUM1 = SUM1 + [L(I, K) * U(K, J)];
+        end
+        L(I, J) = [A(I, J) - SUM1];
+    end
+    U(J, J) = 1;
+    for I = J+1:N;
