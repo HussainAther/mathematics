cc8cdcaba35d30558fe46f5484a71da30b1afb55
Syed Hussain Ather <shussainather@gmail.com>
2019-07-30 08:06:02 -0400

    sums

--- algebra/linear/ludecomp.m
+++ algebra/linear/ludecomp.m
@@ -4,4 +4,10 @@ LU Decomposition lower-upper lower upper lowerupper
 function [X]=modlu(A, B)
   [N, N] = size(A)
   for I=1:N
-    L(I, 1) = A(I, 1)
+    L(I, 1) = A(I, 1);
+  end
+  for J=1:N
+    U(1, J) = A(1, J)/L(1, 1);
+  end
+  SUM1 = 0;
+  SUM2 = 0;
