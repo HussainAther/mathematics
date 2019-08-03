25bdc25351e05003d71d77de8b90f066750667d8
Syed Hussain Ather <shussainather@gmail.com>
2019-08-03 19:57:55 -0400

    init func

--- calc/eulermilstein.py
+++ calc/eulermilstein.py
@@ -5,9 +5,15 @@ import numpy.random as npr
 Compare Euler-Maruyama and Milstein algorithms.
 """
 
-# Initialize variables
+# Initialize variables.
 M = 1000 # number of paths
 P = 6 # number of discretizations
 T = 1 # time interval endpoint
 N = 2**12 # grid size
 dt = 1.0*T/N # time differential 
+lambda = 2.0
+mu = 1.0
+Xzero = 1.0
+
+# Initialize functions.
+def a(X): return 
