4e22dff471b183e257d55b3483663cbfbdbf6d39
Syed Hussain Ather <shussainather@gmail.com>
2019-08-29 10:29:40 -0400

    sl

--- calc/newtonpolynomial.py
+++ calc/newtonpolynomial.py
@@ -1,5 +1,14 @@
+import scipy.linalg as sl
 
 class NewtonPolynomial(PolyNomial):
     """
     Newton polynomial.
     """
+    def __init__(self, **args):
+        if "coeff" in args:
+            try:
+                self.xi = array(args["xi"])
+            except KeyError:
+                raise ValueError("Coefficients need to be given together" \
+                                 "with abscissae values xi.")
+                super(NewtonPolynomial, self).__init__(**args)
