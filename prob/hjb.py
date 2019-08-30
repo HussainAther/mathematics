52892697508e9ac5b005cb65841277736521dfaa
Syed Hussain Ather <shussainather@gmail.com>
2019-08-30 12:56:02 -0400

    define em

--- prob/hjb.py
+++ prob/hjb.py
@@ -8,5 +8,13 @@ def f(x, u):
     """
     Drift.
     """
+    return x*u
+
+def F(x, u):
+    """
+    Diffusion coefficient.
+    """
+    return 2
     
+# Sample differential formula for drift and 
 dx = f(x, u) * dt + F(x, y) * dw 
