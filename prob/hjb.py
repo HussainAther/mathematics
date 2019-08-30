74f8d710319f5b980a726ea7240914ee2d16f422
Syed Hussain Ather <shussainather@gmail.com>
2019-08-30 12:48:17 -0400

    comment

--- prob/hjb.py
+++ prob/hjb.py
@@ -1,3 +1,7 @@
 """
-Hamilton-Jacobi-Bellman (HJB) hamilton jacobi bellman equations.
+Hamilton-Jacobi-Bellman (HJB) hamilton jacobi bellman equations for controlled
+ito diffusion. Use an n-dimensional Browniawn motion as a stochastic differential
+equation.
 """
+
+
