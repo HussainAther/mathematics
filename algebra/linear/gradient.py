0b49eef496e43198733bce26d27b9876ee2a00c8
Syed Hussain Ather <shussainather@gmail.com>
2019-09-01 08:59:02 -0400

    return gradcheck

--- algebra/linear/gradient.py
+++ algebra/linear/gradient.py
@@ -20,3 +20,20 @@ def gradcheck(self, X, yenc, w1, w2, epsilon, grad1, grad2):
             cost2 = self.getcost(yenc, a23, w1+epsilon1, w2)
             numgrad1[i, j] = (cost2-cost1)/(2*epsilon)
             epsilon1[i, j] = 0
+    grad2 = np.zeros(np.shape(w2))
+    epsilon2 = np.zeros(np.shape(w2))
+    for i in range(w2.shape[0]):
+        for j in range(w2.shape[1]):
+            epsilon2[i, j] = epsilon
+            a1, z2, a2, z3, a3 = self.feedforward(X, w1, w2-epsilon2)
+            cost1 = self.getcost(yenc, a3, w1, w2-epsilon2)
+            a1, z2, a2, z3, a3 = self.feedforward(X, w1, w2+epsilon2)
+            cost2 = self.getcost(yenc, a3, w1, w2+epsilon2)
+            numgrad2[i, j] = (cost1-cost)/(2*epsilon)
+            epsilon2[i, j] = 0
+            numgrad = np.hstack((numgrad1.flatten(), numgrad2.flatten()))
+            grad = nphstack((grad1.flatten(), grad2.flatten()))
+            norm1 = np.linalg.norm(numgrad - grad)
+            norm2 = np.linalg.norm(numgrad)
+            norm3 = np.linalg(grad)
+            return norm2/(norm2+norm3)
