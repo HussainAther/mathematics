fafb814ac4febd2c5d5219300ceadc5063e2c52d
Syed Hussain Ather <shussainather@gmail.com>
2019-09-01 08:53:32 -0400

    second check

--- algebra/linear/gradient.py
+++ algebra/linear/gradient.py
@@ -4,7 +4,7 @@ import numpy as np
 Gradient checking for an artificial neural network.
 """
 
-def gradcheck(self, X, y_enc, w1, w2, epsilon, grad1, grad2):
+def gradcheck(self, X, yenc, w1, w2, epsilon, grad1, grad2):
     """
     Apply gradient checking to return the relative error between
     numerically approximated gradients and the backpropagated gradients. 
@@ -14,4 +14,6 @@ def gradcheck(self, X, y_enc, w1, w2, epsilon, grad1, grad2):
     for i in range(w1.shape[0]): 
         for j in range(w1.shape[1]):
             epsilon1[i, j] = epsilon
-            a1, z2, a2, z3, a3 = self.feedforward(X, w1 - epsilon1, w2)
+            a1, z2, a2, z3, a3 = self.feedforward(X, w1-epsilon1, w2)
+            cost1 = self.get_cost(yenc, a3, w1-epsilon1, w2)
+            a1, z2, a2, z3, a3 = self.feedforward(X, w1+epsilon1, w2)
