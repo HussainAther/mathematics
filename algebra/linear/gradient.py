e8f96af2ec898656bc1c558e55127f3de120a762
Syed Hussain Ather <shussainather@gmail.com>
2019-09-01 08:54:22 -0400

    grad

--- algebra/linear/gradient.py
+++ algebra/linear/gradient.py
@@ -15,5 +15,8 @@ def gradcheck(self, X, yenc, w1, w2, epsilon, grad1, grad2):
         for j in range(w1.shape[1]):
             epsilon1[i, j] = epsilon
             a1, z2, a2, z3, a3 = self.feedforward(X, w1-epsilon1, w2)
-            cost1 = self.get_cost(yenc, a3, w1-epsilon1, w2)
+            cost1 = self.getcost(yenc, a3, w1-epsilon1, w2)
             a1, z2, a2, z3, a3 = self.feedforward(X, w1+epsilon1, w2)
+            cost2 = self.getcost(yenc, a23, w1+epsilon1, w2)
+            numgrad1[i, j] = (cost2-cost1)/(2*epsilon)
+            epsilon1[i, j] = 0
