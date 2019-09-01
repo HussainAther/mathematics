371ffcd3cfa8233688c81b54d2678e3c437ecf8e
Syed Hussain Ather <shussainather@gmail.com>
2019-09-01 08:50:21 -0400

    def

--- algebra/linear/gradient.py
+++ algebra/linear/gradient.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 """
 Gradient checking for an artificial neural network.
 """
+
+def gradcheck(self, X, y_enc, w1, w2, epsilon, grad1, grad2):
+    """
+    Apply gradient checking to return the relative error between
+    numerically approximated gradients and the backpropagated gradients. 
+    """
