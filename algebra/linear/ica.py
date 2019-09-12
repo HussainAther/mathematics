b7d1bec684740f78b0520527cf9d317fcacb434b
Syed Hussain Ather <shussainather@gmail.com>
2019-09-12 15:28:07 -0700

    sanitize data

--- algebra/linear/ica.py
+++ algebra/linear/ica.py
@@ -16,4 +16,8 @@ s1 = np.sin(2 * time)  # Signal 1 : sinusoidal signal
 s2 = np.sign(np.sin(3 * time))  # Signal 2 : square signal
 s3 = signal.sawtooth(2 * np.pi * time)  # Signal 3: sawtooth signal
 S = np.c_[s1, s2, s3]
-S += 0.2 * np.random.normal(size=S.shape)  # Add noise
+S += 0.2 * np.random.normal(size=S.shape)  # Add noise.
+S /= S.std(axis=0)  # Standardize data.
+# Mix data.
+A = np.array([[1, 1, 1], [0.5, 2, 1.0], [1.5, 1.0, 2.0]])  # Mixing matrix
+X = np.dot(S, A.T)  # Generate observations.
