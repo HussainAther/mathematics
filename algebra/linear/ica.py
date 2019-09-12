9bcf5560c43286bb33763c837f9be31f441b6897
Syed Hussain Ather <shussainather@gmail.com>
2019-09-12 15:26:32 -0700

    import

--- algebra/linear/ica.py
+++ algebra/linear/ica.py
@@ -1,3 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy import signal
+from sklearn.decomposition import FastICA, PCA
+
 """
 Independent component analysis on sample simulated data.
 """
