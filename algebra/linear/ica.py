37530516129b9e5425d2eda1831e3c23b39347cb
Syed Hussain Ather <shussainather@gmail.com>
2019-09-12 15:27:18 -0700

    signals

--- algebra/linear/ica.py
+++ algebra/linear/ica.py
@@ -12,3 +12,7 @@ Independent component analysis on sample simulated data.
 np.random.seed(1234)
 n_samples = 2000
 time = np.linspace(0, 8, n_samples)
+s1 = np.sin(2 * time)  # Signal 1 : sinusoidal signal
+s2 = np.sign(np.sin(3 * time))  # Signal 2 : square signal
+s3 = signal.sawtooth(2 * np.pi * time)  # Signal 3: sawtooth signal
+
