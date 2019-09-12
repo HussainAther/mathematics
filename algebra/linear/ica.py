bcb3424853b835ec501615f62633ec8848ea528d
Syed Hussain Ather <shussainather@gmail.com>
2019-09-12 15:27:00 -0700

    generate date

--- algebra/linear/ica.py
+++ algebra/linear/ica.py
@@ -7,3 +7,8 @@ from sklearn.decomposition import FastICA, PCA
 """
 Independent component analysis on sample simulated data.
 """
+
+# Generate data.
+np.random.seed(1234)
+n_samples = 2000
+time = np.linspace(0, 8, n_samples)
