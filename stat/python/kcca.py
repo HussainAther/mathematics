9b09ea82f729d065556e4bd0f6b3606f54be6f17
Syed Hussain Ather <shussainather@gmail.com>
2019-09-13 19:13:46 -0700

    allocate

--- stat/python/kcca.py
+++ stat/python/kcca.py
@@ -13,4 +13,6 @@ def kcca(data, reg=0., numCC=None, kernelcca=True, ktype="linear",
     numCC = min([k.shape[1] for k in kernel]) if numCC is None else numCC
     # Get the auto- and cross-covariance matrices
     crosscovs = [np.dot(ki, kj.T) for ki in kernel for kj in kernel]
-
+    # Allocate left-hand side (LH) and right-hand side (RH):
+    LH = np.zeros((sum(nFs), sum(nFs)))
+    RH = np.zeros((sum(nFs), sum(nFs)))
