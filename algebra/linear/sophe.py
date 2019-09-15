c59a5a8c4c431bc07b2e913e42b2a1ccff36964f
Syed Hussain Ather <shussainather@gmail.com>
2019-09-15 14:40:51 -0700

    def

--- algebra/linear/sophe.py
+++ algebra/linear/sophe.py
@@ -1,3 +1,7 @@
-"""
-SOPHE Second-order polynomial histogram estimators 
-"""
+
+def sophe(interval, threshold, v):
+    """
+    SOPHE Second-order polynomial histogram estimators for interval
+    for which the data has been scaled, threshold value, and v integer
+    used in finding the binwidth.
+    """
