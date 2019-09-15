e305aa57986dc503bc2787b19b33790736da2e01
Syed Hussain Ather <shussainather@gmail.com>
2019-09-15 11:06:47 -0700

    function

--- algebra/linear/dann.py
+++ algebra/linear/dann.py
@@ -1,3 +1,5 @@
-"""
-Discriminant Adaptive Nearest Neighbors (DANN).
-"""
+def dann(X, y, nsize=50, epsilon=1):
+    """
+    Discriminant Adaptive Nearest Neighbors (DANN) for training data X of shape 
+    [n_samples, n_features], target values y of shape [n_samples, 1].
+    """
