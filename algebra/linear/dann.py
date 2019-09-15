226a16b68af7047e2bfb6e46fcb9cdff4d73ef43
Syed Hussain Ather <shussainather@gmail.com>
2019-09-15 11:08:17 -0700

    import

--- algebra/linear/dann.py
+++ algebra/linear/dann.py
@@ -1,5 +1,11 @@
+import numpy as np
+import random
+
+from scipy import stats
+
 def dann(X, y, nsize=50, epsilon=1):
     """
     Discriminant Adaptive Nearest Neighbors (DANN) for training data X of shape 
     [n_samples, n_features], target values y of shape [n_samples, 1].
     """
+    
