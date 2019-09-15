2568499ea00017456b2a7ee06bdcb98b8b326c1d
Syed Hussain Ather <shussainather@gmail.com>
2019-09-15 11:09:35 -0700

    more comment

--- algebra/linear/dann.py
+++ algebra/linear/dann.py
@@ -3,9 +3,11 @@ import random
 
 from scipy import stats
 
-def dann(X, y, nsize=50, epsilon=1):
+def dann(X, y, nsize=50, epsilon=1, maxiter=1000):
     """
     Discriminant Adaptive Nearest Neighbors (DANN) for training data X of shape 
-    [n_samples, n_features], target values y of shape [n_samples, 1].
+    [n_samples, n_features], target values y of shape [n_samples, 1], nsize neighborhood
+    size, epsilon learning rate, maximum number of iterations through Lloyd's algorithm maxiter,
+
     """
     
