ba5e292ecf8dd8a212de89b4237eeddf4dfc5bd0
Syed Hussain Ather <shussainather@gmail.com>
2019-09-15 11:21:06 -0700

    append

--- algebra/linear/dann.py
+++ algebra/linear/dann.py
@@ -12,11 +12,16 @@ def calcdist(x1, x2:
     dist = diff.T.dot([x1, x2]).dot(diff)
     return dist
 
-def dann(X, y, nsize=50, epsilon=1, maxiter=1000):
+def dann(X, y, x, nsize=50, epsilon=1, maxiter=1000):
     """
     Discriminant Adaptive Nearest Neighbors (DANN) for training data X of shape 
     [n_samples, n_features], target values y of shape [n_samples, 1], nsize neighborhood
     size, epsilon learning rate, maximum number of iterations through Lloyd's algorithm maxiter,
-
+    and query point x.
     """
     nfeatures = X.shape[1]
+    dists = [] 
+    for row in X:
+        dist = np.linalg.norm(row-x)
+        dists.append(dist)
+    
