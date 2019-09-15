87cc3478a513e6f6ddfb15cef81f1090871b89f0
Syed Hussain Ather <shussainather@gmail.com>
2019-09-15 14:50:22 -0700

    make it

--- algebra/linear/sophe.py
+++ algebra/linear/sophe.py
@@ -3,8 +3,18 @@ import numpy as np
 def sophe(data, interval, threshold, v):
     """
     SOPHE Second-order polynomial histogram estimators for list of data, interval
-    for which the data has been scaled, threshold value, and v integer
-    used as a scale in finding the binwidth.
+    for which the data has been scaled, threshold value for cutting off data, binwidth v.
     """
-    binwidth = interval/v 
-    obs = np.where(data >= threshold, data) # observations 
+    bins = []
+    tempbin = []
+    for i in data:
+        tempbin.append(i)
+        if len(tempbin) == v:
+            tempbin.append(i)
+            bins.append(tempbin)            
+            tempbin = []
+    obs = [] # observations
+    for i in bins:
+        if sum(i)/len(i) >= threshold:
+            obs.append(i)
+    
