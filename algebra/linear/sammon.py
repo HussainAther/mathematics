32f839db4033495c60f7eedd89450c2652291c04
Syed Hussain Ather <shussainather@gmail.com>
2019-09-16 16:30:14 -0700

    cmdscale

--- algebra/linear/sammon.py
+++ algebra/linear/sammon.py
@@ -5,8 +5,26 @@ from cmdscale import cmdscale
 
 def cmdscale(D):
     """
-    Classical multidimensional scaling (MDS)
+    Classical multidimensional scaling (MDS) for symmetric distance matrix D.
     """
+    # Number of points                                                                        
+    n = len(D)
+    # Centering matrix                                                                        
+    H = np.eye(n) - np.ones((n, n))/n
+    # YY^T                                                                                    
+    B = -H.dot(D**2).dot(H)/2
+    # Diagonalize                                                                             
+    evals, evecs = np.linalg.eigh(B)
+    # Sort by eigenvalue in descending order                                                  
+    idx   = np.argsort(evals)[::-1]
+    evals = evals[idx]
+    evecs = evecs[:,idx]
+    # Compute the coordinates using positive-eigenvalued components only                      
+    w, = np.where(evals > 0)
+    L  = np.diag(np.sqrt(evals[w]))
+    V  = evecs[:,w]
+    Y  = V.dot(L)
+    return Y, evals[evals > 0]
 
 def sammon(x, n, display = 2, inputdist = "raw", maxhalves = 20, maxiter = 500, tolfun = 1e-9, init = "default"): 
     """
