100c0115f0ac336ec12605f3641c697efd13c8e7
Syed Hussain Ather <shussainather@gmail.com>
2019-09-16 16:11:33 -0700

    comment

--- algebra/linear/sammon.py
+++ algebra/linear/sammon.py
@@ -4,5 +4,6 @@ from scipy.spatial.distance import cdist
 
 def sammon(x, n, display = 2, inputdist = "raw", maxhalves = 20, maxiter = 500, tolfun = 1e-9, init = "default"): 
     """
-    Sammon mapping on a dataset.
+    Sammon mapping on a dataset. Use the nonlinear mapping procedure on 
+    multivariate data with rows of patterns and columns of features. 
     """
