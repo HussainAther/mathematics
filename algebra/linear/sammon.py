171b45c5f82fb204315665565156c7e779adcf88
Syed Hussain Ather <shussainather@gmail.com>
2019-09-16 16:17:45 -0700

    commento

--- algebra/linear/sammon.py
+++ algebra/linear/sammon.py
@@ -4,6 +4,12 @@ from scipy.spatial.distance import cdist
 
 def sammon(x, n, display = 2, inputdist = "raw", maxhalves = 20, maxiter = 500, tolfun = 1e-9, init = "default"): 
     """
-    Sammon mapping on a dataset. Use the nonlinear mapping procedure on 
+    Sammon mapping on a dataset x. Use the nonlinear mapping procedure on 
     multivariate data with rows of patterns and columns of features. 
+    maxiter maximum number of iterations, tolfun relative tolerance for the objective
+    function, maxhalves max number of step halvings, input can be "distance" to 
+    use pairwise distances as input, display can be 0 to 2 for verbosity, 
+    init is "pca" for raw input, "cmdscale" for distances, or can be "random"
+    or "default".
     """
+     
