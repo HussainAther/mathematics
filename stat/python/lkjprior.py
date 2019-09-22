c754ee43136174da16f0a387871f766857bfac15
Syed Hussain Ather <shussainather@gmail.com>
2019-09-21 22:01:46 -0700

    import

--- stat/python/lkjprior.py
+++ stat/python/lkjprior.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pymc3 as pm
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 """
 LKJ prior distribution. Use the inverse-Wishart distribution as the conjugate prior
 for the covariance matrix of a multivariate normal distribution. Good for modeling
