d8a8a0f528bdbdf5bb2e8ed060b7a695861f544f
Syed Hussain Ather <shussainather@gmail.com>
2019-09-21 22:04:44 -0700

    dist

--- stat/python/lkjprior.py
+++ stat/python/lkjprior.py
@@ -4,9 +4,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 """
-LKJ prior distribution. Use the inverse-Wishart distribution as the conjugate prior
+LKJ Cholesky prior distribution. Use the inverse-Wishart distribution as the conjugate prior
 for the covariance matrix of a multivariate normal distribution. Good for modeling
 the covariance matrix of a multivariate normal distribution.
 """
 
 np.random.seed(1234)
+
+N = 10000
+muactual = np.array([1, -2])
+sigmaactual = np.array([[.5, -.3], [-.3, 1.]])
+x = np.random.multivariate_normal(muactual, sigmaactual, size=N)
