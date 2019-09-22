4251003bb5b8d5f01b9c4842864f4c6c2de96d89
Syed Hussain Ather <shussainather@gmail.com>
2019-09-22 16:54:57 -0700

    def kmeans

--- algebra/linear/rbf.py
+++ algebra/linear/rbf.py
@@ -5,3 +5,8 @@ def rbf(x, c, s):
     Gaussian radial basis function (rbf) for neural networks. 
     """
     return np.exp(-1 / (2 * s**2) * (x-c)**2)
+
+def kmeans(X, k):
+    """
+    K-means clustering for number of clusters k and array of inputs X.
+    """
