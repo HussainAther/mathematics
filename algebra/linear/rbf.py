a2b923930675804e644da6fcb915f2e875f59e13
Syed Hussain Ather <shussainather@gmail.com>
2019-09-22 16:50:01 -0700

    import and def

--- algebra/linear/rbf.py
+++ algebra/linear/rbf.py
@@ -1,3 +1,7 @@
-"""
-Radial basis function for neural networks. 
-"""
+import numpy as np
+
+def rbf(x, c, s):
+    """
+    Radial basis function for neural networks. 
+    """
+    return np.exp(-1 / (2 * s**2) * (x-c)**2)
