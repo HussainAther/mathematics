c0c6eeefe8c8d7435daa93a7cb5a2b8fafecb4f0
Syed Hussain Ather <shussainather@gmail.com>
2019-09-22 16:01:13 -0700

    def nodesplit

--- algebra/linear/extratrees.py
+++ algebra/linear/extratrees.py
@@ -3,3 +3,9 @@ import numpy as np
 """
 Extra-trees (extra trees extratrees) splitting algorithm (extremely randomized trees).
 """
+
+def nodesplit(s):
+    """
+    For local learning subset s corresponding to the node to split,
+    return a split [a<a_c] if there is one. Otherwise, return False. 
+    """
