466e87d8e37cbf5f1a37464668be2ad8fa3883f9
Syed Hussain Ather <shussainather@gmail.com>
2019-09-22 16:55:27 -0700

    random select

--- algebra/linear/rbf.py
+++ algebra/linear/rbf.py
@@ -10,3 +10,8 @@ def kmeans(X, k):
     """
     K-means clustering for number of clusters k and array of inputs X.
     """
+    # Randomly select initial clusters from input data.
+    clusters = np.random.choice(np.squeeze(X), size=k)
+    prevClusters = clusters.copy()
+    stds = np.zeros(k)
+    converged = False
