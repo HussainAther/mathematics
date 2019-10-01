c609f91fe8043514612f4d0bebaf8ba9e0a5dc6b
Syed Hussain Ather <shussainather@gmail.com>
2019-09-30 20:53:07 -0700

    while

--- algebra/neuralnetworks/perceptron.m
+++ algebra/neuralnetworks/perceptron.m
@@ -1,4 +1,10 @@
 function w=perce(X, y, w_ini)
   [1, N] = size(X);
   max_iter = 10000; % maximum number of iterations
+  w = w_ini; % initialization of the parameter vector
+  iter = 0; % iteratiaon counter
+  mis_clas = N; % number of misclassified vectors
+  while (mis_clas > 0) && (iter < max_iter)
+    iter = iter + 1;
 
+   
