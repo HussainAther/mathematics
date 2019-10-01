6fc198e6a9c409b3a90364d8d64ddfb13bb4917e
Syed Hussain Ather <shussainather@gmail.com>
2019-09-30 21:05:42 -0700

    end

--- algebra/neuralnetworks/perceptron.m
+++ algebra/neuralnetworks/perceptron.m
@@ -6,5 +6,13 @@ function w=perce(X, y, w_ini)
   mis_clas = N; % number of misclassified vectors
   while (mis_clas > 0) && (iter < max_iter)
     iter = iter + 1;
-
-   
+    mis_clas = 0;
+    gradi = zeros(1, 1); % gradient computation
+    for i=1:N
+      if((X(:,i)'*w)*y(i)<0) 
+        mis_clas = mis_clas + 1;
+        gradi = gradi + rho*(-y(i)*X(:,i));
+      end
+    end
+    w = w - rho*gradi; % update parameter
+  end
