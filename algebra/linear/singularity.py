56ba793140a2845b64b3bedcffc722d2ae894a6f
Syed Hussain Ather <shussainather@gmail.com>
2019-10-01 19:32:59 -0700

    kapapa

--- algebra/linear/singularity.py
+++ algebra/linear/singularity.py
@@ -7,11 +7,11 @@ from numpy.linalg import linalg
 Singularity of a matrix.
 """
 
-def getA(k):
+def getA(kappa):
     matrix = np.zeros((n, n), float)
     for i in range(n):
         for j in range(n):
-            matrix[i][j] = 2*np.cos((2*np.pi/n)*(abs(j-i))*k)
+            matrix[i][j] = 2*np.cos((2*np.pi/n)*(abs(j-i))*kappa)
     return matrix
 
 def getF(csi, a):
@@ -24,3 +24,7 @@ def getG(csi, f, a):
     csiInv = linalga.inv(csi)
     valueG = (csiInv*aa*csiInv)/(2*f)
     return valueG
+
+def getE(g, k):
+    KInv = linalg.inv(k)
+    Ktrans = lin
