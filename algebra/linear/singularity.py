06ad38ed845eb1b572f5347ca1d478764b8f48e9
Syed Hussain Ather <shussainather@gmail.com>
2019-10-01 19:30:21 -0700

    return matrix

--- algebra/linear/singularity.py
+++ algebra/linear/singularity.py
@@ -9,4 +9,7 @@ Singularity of a matrix.
 
 def getA(k):
     matrix = np.zeros((n, n), float)
-    
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = 2*np.cos((2*np.pi/n)*(abs(j-i))*k)
+    return matrix 
