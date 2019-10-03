74c59044527784a9a900b56307a25dcea4e53288
Syed Hussain Ather <shussainather@gmail.com>
2019-10-02 19:36:25 -0700

    def

--- algebra/linear/kullbackleibler.py
+++ algebra/linear/kullbackleibler.py
@@ -6,3 +6,13 @@ from scipy.stats import norm
 from matplotlib import pyplot as plt
 
 sns.set()
+
+"""
+Kullback-Leibler (kullback leibler) divergence (kl).
+"""
+
+def kl_divergence(p, q):
+    """
+    Formula from Koch "Multivariate Analysis"
+    """
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
