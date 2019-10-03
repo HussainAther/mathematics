b2be23e36615374d94a0b3f7266976a72d52cc36
Syed Hussain Ather <shussainather@gmail.com>
2019-10-02 19:37:01 -0700

    plot

--- algebra/linear/kullbackleibler.py
+++ algebra/linear/kullbackleibler.py
@@ -16,3 +16,11 @@ def kl_divergence(p, q):
     Formula from Koch "Multivariate Analysis"
     """
     return np.sum(np.where(p != 0, p * np.log(p / q), 0))
+
+x = np.arange(-10, 10, 0.001)
+p = norm.pdf(x, 0, 2)
+q = norm.pdf(x, 2, 2)
+
+plt.title("KL(P||Q) = %1.3f" % kl_divergence(p, q))
+plt.plot(x, p)
+plt.plot(x, q, c="red")
