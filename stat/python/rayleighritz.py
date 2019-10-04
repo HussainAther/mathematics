e09b607f290e58bca73606d166ff94b6c852eee8
Syed Hussain Ather <shussainather@gmail.com>
2019-10-04 13:01:14 -0700

    basis def

--- stat/python/rayleighritz.py
+++ stat/python/rayleighritz.py
@@ -8,20 +8,26 @@ that are difficult to solve analytically, particularly in the context of solving
 physical boundary value problems that can be expressed as matrix differential equations.
 """
 
+def basis(x,i):
+    """
+    Return basis function.
+    """
+    return x**i
+
 x = Symbol("x")
 q = input("Enter the value of q [3]: ")
 y0 = input("Enter the value of y0 [0]: ")
 basis = [x**i for i in range(q+1)]
 ci = [Symbol("ci_%i" %i) for i in range(q+1)]
-y = y0 + sum(ci[i]*np.basis[i] for i in range(1,q+1))
+y = y0 + sum(ci[i]*basis[i] for i in range(1,q+1))
 print("The trial solution is: ", u)
 k = Matrix([[0,0,0],[0,0,0],[0,0,0]])
 f = Matrix(1, q, range(q))
 for i in range(1,q+1):
     for j in range(1,q+1):
-        k[i-1,j-1] = ci[i]*ci[j]*np.diff(np.basis[i], x)*np.diff(np.basis[j], x)
+        k[i-1,j-1] = ci[i]*ci[j]*np.diff(basis[i], x)*np.diff(basis[j], x)
 for i in range(1,q+1):
-    f[i-1]=2*x*ci[i]*np.basis[i]+ci[i]
+    f[i-1]=2*x*ci[i]*basis[i]+ci[i]
 functional= i ntegrate(sum(k.reshape(1,q**2))+sum(f),[x,0,1])
 s1 = Derivative(functional,ci[1])
 d1 = s1.doit()
