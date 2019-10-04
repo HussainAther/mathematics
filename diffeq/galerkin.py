476dd063f3a3c96e4c2f083f4b6061e1f52aec21
Syed Hussain Ather <shussainather@gmail.com>
2019-10-04 13:02:14 -0700

    func

--- diffeq/galerkin.py
+++ diffeq/galerkin.py
@@ -1,4 +1,26 @@
+import numpy as np
+
+from sympy import input, Symbol, 
+
 """
 Galerkin Method to solve the second-order boundary value problem
 d^2u/dx^2 + p(x) = 0, u(0) = 0, du/dx(1) = up1=1
 """
+
+def p(x):
+    """
+    Function of interest.
+    """
+    return x   
+
+def basis(x,i):
+    """
+    Basis function.
+    """
+    return x**i
+
+x = Symbol("x")
+q = input("Enter the value of q: ")
+u0, up0, up1 = input("Enter the boundary conditions [u0,up0,up1]: ")
+
+
