80a971a7fb805b8ac25aa7c8bca7056469dc6472
Syed Hussain Ather <shussainather@gmail.com>
2019-10-04 12:40:54 -0700

    bg

--- stat/python/rayleighritz.py
+++ stat/python/rayleighritz.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 """
-Rayleigh-Ritz (rayleigh ritz) method.
+Rayleigh-Ritz (rayleigh ritz) method finding approximations to eigenvalue equations 
+that are difficult to solve analytically, particularly in the context of solving 
+physical boundary value problems that can be expressed as matrix differential equations.
 """
+
+
