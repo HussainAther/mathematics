64acb36f232c58b3b0474e439d00e594ff7929e9
Syed Hussain Ather <shussainather@gmail.com>
2019-10-04 12:45:25 -0700

    integratea

--- stat/python/rayleighritz.py
+++ stat/python/rayleighritz.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sympy import input, Matrix, Symbol
+from sympy import input, integrate, Matrix, Symbol
 
 """
 Rayleigh-Ritz (rayleigh ritz) method finding approximations to eigenvalue equations 
@@ -20,3 +20,7 @@ f = Matrix(1, q, range(q))
 for i in range(1,q+1):
     for j in range(1,q+1):
         k[i-1,j-1] = ci[i]*ci[j]*np.diff(np.basis[i], x)*np.diff(np.basis[j], x)
+for i in range(1,q+1):
+    f[i-1]=2*x*ci[i]*np.basis[i]+ci[i]
+functional= i ntegrate(sum(k.reshape(1,q**2))+sum(f),[x,0,1])
+
