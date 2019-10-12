cd29ef5a0889cf7bc33c6408b0dd92ead9788884
Syed Hussain Ather <shussainather@gmail.com>
2019-10-12 14:51:41 -0700

    matrix

--- stat/python/fusedlasso.py
+++ stat/python/fusedlasso.py
@@ -21,4 +21,6 @@ sparsity = rr.llnorm(len(Y), lagrange=.8)
 
 3 Laagrange polynomial 
 sparsity.lagrange += 1
- 
+
+# Create fused lasso matrix
+D = (np.identity(500) + np.diag([-1]*499, k=1))[:-1] 
