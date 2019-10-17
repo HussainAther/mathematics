ccc76e2d355939fe536ab376c96987eb1fac19ec
Syed Hussain Ather <shussainather@gmail.com>
2019-10-16 21:07:47 -0700

    init

---
+++ stat/python/mds.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import sklearn
+
+"""
+Multidimensional scaling.
+"""
+
+from sklearn.metrics import pairwise_distances
+from sklearn.manifold import MDS
+from sklearn import preprocessing
+from mpl_toolkits import mplot3d
+
+
