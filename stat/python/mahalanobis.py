d727fa8ad08349ffb71dc3b6557c6fa528c57fe8
Syed Hussain Ather <shussainather@gmail.com>
2019-10-18 12:47:20 -0400

    eq

--- stat/python/mahalanobis.py
+++ stat/python/mahalanobis.py
@@ -3,3 +3,11 @@ import numpy as np
 """
 Mahalanobis distance.
 """
+
+xx = []
+yy = []
+
+X = np.vstack([xx,yy])
+V = np.cov(X.T)
+VI = np.linalg.inv(V)
+m = np.diag(np.sqrt(np.dot(np.dot((xx-yy),VI),(xx-yy).T)))
