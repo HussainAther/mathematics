ae0f1506c316d03812a029db95534f43bf1ed131
Syed Hussain Ather <shussainather@gmail.com>
2019-10-19 19:25:11 -0500

    fill matrix

--- stat/python/mm.py
+++ stat/python/mm.py
@@ -9,3 +9,6 @@ def mle(pmat, max_iter=100):
     wins = np.sum(pmat, axis=0)
     params = np.ones(n, dtype=float)
     for _ in range(max_iter):
+        tiled = np.tile(params, (n, 1))
+        combined = 1.0 / (tiled + tiled.T)
+        np.fill_diagonal(combined, 0)
