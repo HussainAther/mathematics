5761417434f759697230498f4772c189a0b532cb
Syed Hussain Ather <shussainather@gmail.com>
2019-10-19 19:17:43 -0500

    wireframe plot

--- stat/R/regressionsurface.R
+++ stat/R/regressionsurface.R
@@ -4,4 +4,6 @@ x12grid <- expand.grid(x1=x1.points,x2=x2.points)
 y <- matrix(0, nrow=100, ncol=100)
 y <- outer(x1.points, x2.points, f)
 library(lattice)
+wireframe(y~x12grid$x1*x12grid$x2,scales=list(arrows=FALSE),
+  xlab=expression(x^1),ylab=expression(x^2),zlab="y")
 
