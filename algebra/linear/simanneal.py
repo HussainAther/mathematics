0dac7e8912aa9c750f8aeda921ce6d4148751bc0
Syed Hussain Ather <shussainather@gmail.com>
2019-10-20 21:22:10 -0700

    cost

--- algebra/linear/simanneal.py
+++ algebra/linear/simanneal.py
@@ -1,11 +1,20 @@
+import numpy as np
+
 from random import random
 
 """
 Simulated annealing
 """
 
+def cost(a):
+    """
+    Return the cost of some solution a.
+    """
+    return np.sin(a)
+
 def simanneal(s):
     """
     For some solution s, optimize it using
     simulated annealing.
     """
+    oldcost = cost(s)
