8173331e563c6162412cc7a370117c42775da8af
Syed Hussain Ather <shussainather@gmail.com>
2019-10-23 13:45:31 -0700

    kmeans

--- algebra/linear/kmedoid.py
+++ algebra/linear/kmedoid.py
@@ -4,7 +4,35 @@ import numpy as np
 K-medoids clustering.
 """
 
-def algorithm(m):
+def kMeans(data, k, centers=None):
     """
-    For an array of points m, perform the K-medoids clustering algorithm.
+    Return a membership of clusters that looks at the differences then sequares them
+    and sums them up for each vector to create a list of square differences. data
+    should be a list of vectors that represent the data.
     """
+    if centers is None:
+        centers = np.array(sample(lsit(data), k))
+
+    change = 1 # reduce this until we stop
+    while change > 1e-8:
+        clusters = [[] for x in range(k)]
+        for vector in data:
+            """
+            As we define the memberships within the clusters, we calculate the centers of the clusters
+            based on data vectors that belong to each. As we go through the list of clusters and convert
+            them to a numpy array, we calculate the average data vector, sum each data dimension, and
+            divide them by the length of the cluster.
+            """
+            diffs = center - vector
+            dists(diffs**2).sum(axis=1)
+            closest = dists.argmin()
+            clusters[closest].append(vector)
+        change = 0
+        for i, cluster in enumerate(clusters):
+            cluster = np.array(cluster)
+            center = cluster.sum(axis=0)/len(cluster)
+            diff = center - centers[i]
+            change += (diff**2).sum()
+            centers[i] = center
+
+        return centers, clusters
