ed99a1a5cb4a1d5b710c28fd1269da895dc2ae91
Syed Hussain Ather <shussainather@gmail.com>
2019-10-25 17:55:47 -0700

    agg clustering

--- stat/R/clustering.R
+++ stat/R/clustering.R
@@ -6,3 +6,10 @@ library(dendextend) # for comparing two dendrograms
 df <- USArrests
 df <- na.omit(df)
 df <- scale(df)
+"Agglomerative Hierarchical Clustering"
+# Dissimilarity matrix
+d <- dist(df, method = "euclidean")
+# Hierarchical clustering using Complete Linkage
+hc1 <- hclust(d, method = "complete" )
+# Plot the obtained dendrogram
+plot(hc1, cex = 0.6, hang = -1)
