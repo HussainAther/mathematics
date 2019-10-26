b2b4e06733cb48e066163cc9536360812a1be4bc
Syed Hussain Ather <shussainather@gmail.com>
2019-10-25 17:54:21 -0700

    library

--- stat/R/clustering.R
+++ stat/R/clustering.R
@@ -1,2 +1,5 @@
 "Hierarchical Cluster Analysis"
-
+library(tidyverse) # data manipulation
+library(cluster) # clustering algorithms
+library(factoextra) # clustering visualization
+library(dendextend) # for comparing two dendrograms
