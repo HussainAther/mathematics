22f82248bbd46c47fc97a21e02e9bc21b2180dcc
Syed Hussain Ather <shussainather@gmail.com>
2019-10-28 21:51:48 -0700

    isleaf

--- stat/python/sgs.py
+++ stat/python/sgs.py
@@ -6,6 +6,13 @@ from sklearn.tree._tree import TREE_LEAF
 Spirtes-Glymour-Scheines algorithm (SGS)
 """
 
+def is_leaf(inner_tree, index):
+    """
+    Check whether node is leaf node.
+    """
+    return (inner_tree.children_left[index] == TREE_LEAF and 
+            inner_tree.children_right[index] == TREE_LEAF)
+
 def sgs(v):
     """
     For a set of variables v, perform the SGS algorithm 
