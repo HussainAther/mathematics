e77d6cbcaea619a4761a6a3ae5eddd3345824ce9
Syed Hussain Ather <shussainather@gmail.com>
2019-10-28 21:48:51 -0700

    sklearn

--- stat/python/sgs.py
+++ stat/python/sgs.py
@@ -1,7 +1,14 @@
 import numpy as np
 
+from sklearn.tree._tree import TREE_LEAF
+
 """
 Spirtes-Glymour-Scheines algorithm (SGS)
 """
 
-
+def sgs(v):
+    """
+    For a set of variables v, perform the SGS algorithm 
+    for discovering causal structure.
+    """
+    g = colliders(
