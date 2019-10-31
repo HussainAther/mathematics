d93dcb8f66a264c6ff20c4cc147af9021d704fd8
Syed Hussain Ather <shussainather@gmail.com>
2019-10-31 16:52:33 -0700

    import

--- prob/atm.py
+++ prob/atm.py
@@ -1,1 +1,12 @@
+import cv2
+import numpy as np
+import sys
+import scipy
+
+from scipy.sparse.linalg import LinearOperator
+
+"""
 Ambrosio Tortorelli Minimizer of the Mumford-Shah functional.
+Implementation of edge preserving smoothing by minimizing with 
+the Ambrosio-Tortorelli appoach.
+"""
