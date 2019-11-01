dc0f77e55366fef8d76f4bdea8fb6288fa78e9e4
Syed Hussain Ather <shussainather@gmail.com>
2019-10-31 17:22:39 -0700

    solve edges

--- prob/atm.py
+++ prob/atm.py
@@ -19,6 +19,23 @@ def gradients(img):
     y = cv2.filter2D(img, cv2.CV_64F, np.array([[-1, 0, 1]]).T)
     return x, y
 
+def solveedges(g):
+    """
+    Find the edges using the linear oeprator.
+    """
+    size = g.shape[0]* self.g.shape[1]
+    A = LinearOperator( (size, size), matvec = self.edge_linear_operator, dtype = np.float64)
+    b = np.ones(size) * beta / (4 * epsilon)
+    edges, _ = scipy.sparse.linalg.cg(A, b, tol = self.tol, maxiter = self.maxiter)
+    return np.power(edges.reshape(*g.shape), 2)
+
+def minimize():
+    """
+    Use the minimizer on the natural images.
+    """
+    for i in range(0, iter):
+        solveedges
+
 def justdoit(img, iter = 1, maxiter = 10, tol = 0.1, alpha = 1000, beta = 0.01, epsilon = 0.010:
     """
     Just do it. Make your dreams come true.
@@ -35,7 +52,7 @@ result, edges = [], []
 for channel in cv2.split(img):
     # Perform the AT Minimization.
     solver = justdoit(channel, iter = 1, tol = 0.1, maxiter  = 6)
-    f, v = solver.minimize()
+    f, v = minimize(iter)
     result.append(f)
     edges.append(v)
 f = cv2.merge(result)
