0be31064f7726fbd23a0efc4c7de4ea3f2ae0df9
Syed Hussain Ather <shussainather@gmail.com>
2019-11-12 21:06:01 -0800

    comment

--- calc/adaptiveintegration.py
+++ calc/adaptiveintegration.py
@@ -2,7 +2,12 @@ import numpy as np
 
 from convergence import trapezoidal
 
-
+"""
+Adaptive integration lets us iterate over an integral nd compare the 
+results produced by n and 2n intervals. If the difference is smaller
+than some tolerance epsilon, then the value of 2n is returned. Otherwise,
+cut n in half and repeat the iteration.
+"""
 
 def midpoint(f, a, b, n):
     """
