42c5d379edcdb0ad83f69298db3e228bed8521d4
Syed Hussain Ather <shussainather@gmail.com>
2019-11-12 20:44:32 -0800

    import

--- calc/convergence.py
+++ calc/convergence.py
@@ -1,4 +1,13 @@
+import numpy as np
+
+from math import log
+
 """
 Convergence rates. We compute them with an algorithm that uses
 general integrands across different intervals.
 """
+
+def convrates(f, F, a, b, exps=14):
+    """
+    for 
+    """
