e001d7b15ab5a71fe6c4be45f339d800f4bcad0c
Syed Hussain Ather <shussainather@gmail.com>
2019-11-14 22:26:07 -0800

    for prob

--- stat/R/lasso.R
+++ stat/R/lasso.R
@@ -10,9 +10,14 @@ rownames(X) = as.character (1:nrow(X))
 a <- sample(1:ncol(X), 50, prob=rep(.5, lenght(1:ncol(X))))
 for (i in 1:50){
      X[30:40, a[i]]<rnorm(length(30:40), meana=-.7, sd=.5)
-} 
+}
+"Beta response logistic model" 
 Xbeta <- rnorm(100, mean=0, sd=.5)
 Xbeta[a] <- rnorm(length(a), mean=-.7, sd=.5)
 beta <- numeric(100)
 betaa[1:length(beta)] <- -1
 responsep <- numeric(100)
+for(1 in 1:100){
+    coeff <- -beta[i] * Xbeta[ai]
+    responsep[i] <- 1/(1+exp(coeff))
+}
