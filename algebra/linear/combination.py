2c2ca871c750a1bbfdcb2eee8b76e963fdfc07f7
Syed Hussain Ather <shussainather@gmail.com>
2019-11-15 00:24:59 -0800

    def

--- algebra/linear/combination.py
+++ algebra/linear/combination.py
@@ -1,3 +1,10 @@
 """
 Linear combinations. 
 """
+
+@memoize
+def linearcomb(n):
+    """
+    Return a tuple (i, j, k, l) satisfying the 
+    linear combination n = i*1 + j*3 + k*9 + l*27
+    """
