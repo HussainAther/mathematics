b14c90ad544a731a21c8609dc2d41f4a7094365e
Syed Hussain Ather <shussainather@gmail.com>
2019-11-15 00:25:46 -0800

    factors set

--- algebra/linear/combination.py
+++ algebra/linear/combination.py
@@ -2,9 +2,22 @@
 Linear combinations. 
 """
 
+def factors_set():
+    """
+    Find the set of factors satisfying the linear combination.
+    """
+    factors_set = ( (i,j,k,l) for i in [-1,0,1] 
+                          for j in [-1,0,1]
+                          for k in [-1,0,1]
+                          for l in [-1,0,1])
+    for factor in factors_set:
+        yield factor
+
 @memoize
 def linearcomb(n):
     """
     Return a tuple (i, j, k, l) satisfying the 
     linear combination n = i*1 + j*3 + k*9 + l*27
     """
+    weighs = (1, 3, 9, 27)
+    for factors in factors_set()
