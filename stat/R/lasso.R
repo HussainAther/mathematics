cc84e0de8ea2fcfc279bf1e5cf53d4dda3415f59
Syed Hussain Ather <shussainather@gmail.com>
2019-11-14 22:21:54 -0800

    matriax

--- stat/R/lasso.R
+++ stat/R/lasso.R
@@ -1,3 +1,4 @@
 "Fused lasso. For features that can be ordered, look at the spatial structure
 information. Fused lasso takes copy number varition for genomic location into 
 account for number-basead classifiers."
+X <- matrix(0,70,100)
