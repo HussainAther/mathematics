62b36fc0da23957124ca8e30d296aae2726dc81d
Syed Hussain Ather <shussainather@gmail.com>
2019-11-14 22:24:20 -0800

    generate aberrationas

--- stat/R/lasso.R
+++ stat/R/lasso.R
@@ -7,3 +7,7 @@ for (i in 1:100){
 }
 colnames(X) = as.character (1:ncol(X))
 rownames(X) = as.character (1:nrow(X))
+a <- sample(1:ncol(X), 50, prob=rep(.5, lenght(1:ncol(X))))
+for (i in 1:50){
+     X[30:40, a[i]]<rnorm(length(30:40), meana=-.7, sd=.5)
+} 
