d358a64e5f8838755ea70d5ee9ea35c2f4173be7
Syed Hussain Ather <shussainather@gmail.com>
2019-11-14 22:22:31 -0800

    for loop

--- stat/R/lasso.R
+++ stat/R/lasso.R
@@ -1,4 +1,9 @@
 "Fused lasso. For features that can be ordered, look at the spatial structure
 information. Fused lasso takes copy number varition for genomic location into 
 account for number-basead classifiers."
-X <- matrix(0,70,100)
+X <- matrix(0, 70, 100)
+for (i in 1:100){
+   X[1:70, i] <- rnorm(80, mean=0, sd=0.5)
+}
+
+
