43853ff67c2b3d0d0153b84847171163c3357bd9
Syed Hussain Ather <shussainather@gmail.com>
2019-11-14 22:21:35 -0800

    init

---
+++ stat/R/lasso.R
@@ -0,0 +1,3 @@
+"Fused lasso. For features that can be ordered, look at the spatial structure
+information. Fused lasso takes copy number varition for genomic location into 
+account for number-basead classifiers."
