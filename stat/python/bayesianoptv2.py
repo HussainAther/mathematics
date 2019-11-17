7f76a623f79a9304eb53fa5462dbf4f0de3b1554
Syed Hussain Ather <shussainather@gmail.com>
2019-11-16 18:55:54 -0800

    domain

--- stat/python/bayesianoptv2.py
+++ stat/python/bayesianoptv2.py
@@ -11,3 +11,6 @@ def objective(x, noise=.1):
     """
     noise = np.raandom.normal(loc=0, scale=noise)
     return (x**2 * np.sin(5 * np.pi * x)**6.0) + noise
+
+# grid-based range of domain
+X = np.arange(0, 1, .01)
