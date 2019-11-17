82046a53e00e3e840352b9406070f2df4f0db833
Syed Hussain Ather <shussainather@gmail.com>
2019-11-17 02:29:10 -0800

    isolation

--- stat/R/pam.R
+++ stat/R/pam.R
@@ -106,3 +106,36 @@ separation(i.pam2.man.pred, i.std.test[,-5], metric="manhattan")
 separation(i.pam3.man.pred, i.std.test[,-5], metric="manhattan")
 separation(i.pam5.man.pred, i.std.test[,-5], metric="manhattan")
 separation(i.pam7.man.pred, i.std.test[,-5], metric="manhattan")
+
+isolation <- function(clustering, data, metric="euclidean", stand=FALSE)
+{
+  clusters <- sort(unique(clustering))
+  dm <- as.matrix(daisy(data, metric, stand))
+  diam <- diameter(clustering, data, metric, stand)
+  sep <- separation(clustering, data, metric, stand)
+  is <- sapply(clusters,
+               function(d)
+               if (all(apply(dm[clustering==d,clustering==d,drop=FALSE], 1, max)<
+                         apply(dm[clustering==d,clustering!=d,drop=FALSE], 1, min)))
+                 "L*"
+               else if (diam[d]<sep[d])
+"L" else
+                 "no")
+  ‘names<-‘(factor(is, levels=c("no", "L", "L*")), clusters)
+}
+isolation(i.pam2.euc$clustering, i.std.train[,-5])
+isolation(i.pam3.euc$clustering, i.std.train[,-5])
+isolation(i.pam5.euc$clustering, i.std.train[,-5])
+isolation(i.pam7.euc$clustering, i.std.train[,-5])
+isolation(i.pam2.man$clustering, i.std.train[,-5], metric="manhattan")
+isolation(i.pam3.man$clustering, i.std.train[,-5], metric="manhattan")
+isolation(i.pam5.man$clustering, i.std.train[,-5], metric="manhattan")
+isolation(i.pam7.man$clustering, i.std.train[,-5], metric="manhattan")
+isolation(i.pam2.euc.pred, i.std.test[,-5])
+isolation(i.pam3.euc.pred, i.std.test[,-5])
+isolation(i.pam5.euc.pred, i.std.test[,-5])
+isolation(i.pam7.euc.pred, i.std.test[,-5])
+isolation(i.pam2.man.pred, i.std.test[,-5], metric="manhattan")
+isolation(i.pam3.man.pred, i.std.test[,-5], metric="manhattan")
+isolation(i.pam5.man.pred, i.std.test[,-5], metric="manhattan")
+isolation(i.pam7.man.pred, i.std.test[,-5], metric="manhattan")
