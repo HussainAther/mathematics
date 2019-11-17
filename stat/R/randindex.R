3fd0cc241fb37d4dd21fdaf9df3ee13aa3d36553
Syed Hussain Ather <shussainather@gmail.com>
2019-11-17 02:18:25 -0800

    routine

--- stat/R/randindex.R
+++ stat/R/randindex.R
@@ -2,4 +2,9 @@
 randindex <- function(clustering, classes)
 { 
   mean(outer(1:length(clustering), 1:length(classes),
-
+             function(i, j)
+             ifelse(i!=j,
+                    clustering[i]==clustering[j] & classes[i]==classes[j]  |
+                    clustering[i]!=clustering[j] & classes[i]!=classes[j, NA)),
+       na.rm=TRUE)
+}
