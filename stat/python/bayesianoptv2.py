8a16138cdc101c51d6355ad84c70cf1d12966198
Syed Hussain Ather <shussainather@gmail.com>
2019-11-16 18:54:06 -0800

    add the eq

--- stat/python/bayesianoptv2.py
+++ stat/python/bayesianoptv2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Bayesian optimization.
 """
@@ -7,3 +9,5 @@ def objective(x, noise=.1):
     Objective function used as test problem
     that we will optimize.
     """
+    noise = np.raandom.normal(loc=0, scale=noise)
+    return (x**2 * np.sin(5 * np.pi * x)**6.0) + noise
