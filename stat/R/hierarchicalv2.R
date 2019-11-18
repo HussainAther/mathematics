b0cdb007760a013b8bfd683300f9588652e6dcc7
Syed Hussain Ather <shussainather@gmail.com>
2019-11-18 08:08:43 -0800

    iterate

--- stat/R/hierarchicalv2.R
+++ stat/R/hierarchicalv2.R
@@ -59,4 +59,29 @@ ahc <- function(data, linkf=ahc.size, diss=euc.dist, bottom=1:nrow(data))
                    Vectorize(function(i1, i2)
                              if (i1<i2)
                                linkf(clustering, clusters[i1], clusters[i2], data,
-                                     diss, dm) 
+                                     diss, dm)
+  while(length(clusters)>1)
+      {
+        mli <- arrayInd(which.min(links), dim(links))  # minimum link index
+        d1 <- clusters[i1 <- mli[1]]
+        d2 <- clusters[i2 <- mli[2]]
+        d12 <- max(clusters)+1
+            # merge d1 and d2 into d12
+        merge <- rbind(merge, c(clid(d1), clid(d2)))
+        height <- c(height, if (is.null(height) || links[i1,i2]>height[length(height)])
+                              links[i1,i2]
+                            else height[length(height)]+height[1])  # height correction
+        clustering[clustering==d1 | clustering==d2] <- d12
+        clusters <- clusters[-c(i1, i2)]
+        links <- links[-c(i1, i2),,drop=FALSE] # remove links for d1
+        links <- links[,-c(i1, i2),drop=FALSE] # remove links for d2
+        if (length(clusters)>0)
+        {
+          links <- cbind(links, sapply(clusters,
+                                       function(d) linkf(clustering, d, d12, data,
+                                                         diss, dm)))
+          links <- rbind(links, NA) # keep the matrix square
+      }
+        clusters <- c(clusters, d12)
+  }
+ 
