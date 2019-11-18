22942c46f4ba375315e4ed20eaa6761b34b9dbdd
Syed Hussain Ather <shussainather@gmail.com>
2019-11-18 01:49:46 -0800

    input data

--- stat/R/hierarchicalv2.R
+++ stat/R/hierarchicalv2.R
@@ -5,3 +5,10 @@ data(weathercl, package="dmr.data")
 data(iris)
 data(Glass, package="mlbench")
 
+set.seed(12)
+ri <- runif(nrow(iris))
+i.train <- iris[ri>=0.33,]
+i.test <- iris[ri<0.33,]
+rg <- runif(nrow(Glass))
+g.train <- Glass[rg>=0.33,]
+g.test <- Glass[rg<0.33,]
