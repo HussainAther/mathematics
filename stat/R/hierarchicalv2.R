e862cb868f4255e22e0bd4c9954da4967ff8becb
Syed Hussain Ather <shussainather@gmail.com>
2019-11-18 01:50:44 -0800

    predict

--- stat/R/hierarchicalv2.R
+++ stat/R/hierarchicalv2.R
@@ -12,3 +12,8 @@ i.test <- iris[ri<0.33,]
 rg <- runif(nrow(Glass))
 g.train <- Glass[rg>=0.33,]
 g.test <- Glass[rg<0.33,]
+wcl.std <- predict.std(std.all(.~., weathercl), weathercl)
+i.stdm <- std.all(Species̃., i.train) 
+i.std.train <- predict.std(i.stdm, i.train) 
+i.std.test <- predict.std(i.stdm, i.test)
+    
