058a4cd11b7d79b8cc803280d0f1c383b4625c59
Syed Hussain Ather <shussainather@gmail.com>
2019-11-19 22:15:38 -0800

    compute error

--- stat/R/ensemble.R
+++ stat/R/ensemble.R
@@ -16,5 +16,11 @@ hv.test <- HouseVotes84[rhv<0.33,]
 rbh <- runif(nrow(BostonHousing))
 bh.train <- BostonHousing[rbh>=0.33,]
 bh.test <- BostonHousing[rbh<0.33,]
-hv.tree <- rpart(Clas~., hv.train) 
-hv.nb <- naiveBayes(Clas~., hv.train)
+hv.tree <- rpart(Class~., hv.train) 
+hv.nb <- naiveBayes(Class~., hv.train)
+hv.err.tree <- err(predict(hv.tree, hv.test, type="c"), hv.test$Class)
+hv.err.nb <- err(predict(hv.nb, hv.test), hv.test$Class)
+bh.tree <- rpart(medv~., bh.train) 
+bh.lm <- lm(medv~., bh.train)
+bh.mse.tree <- mse(predict(bh.tree, bh.test), bh.test$medv)
+bh.mse.lm <- mse(predict(bh.lm, bh.test), bh.test$medv)
