8bde0e4cef3bfd9416f1294bae0ff410e341ee43
Syed Hussain Ather <shussainather@gmail.com>
2019-11-19 18:56:33 -0800

    input data

--- stat/R/ensemble.R
+++ stat/R/ensemble.R
@@ -8,3 +8,12 @@ library(rpart)
 library(e1071)
 data(HouseVotes84, package="mlbench")
 data(BostonHousing, package="mlbench")
+# Ensemble modeling techniques
+set.seed(12)
+rhv <- runif(nrow(HouseVotes84))
+hv.train <- HouseVotes84[rhv>=0.33,]
+hv.test <- HouseVotes84[rhv<0.33,]
+rbh <- runif(nrow(BostonHousing))
+bh.train <- BostonHousing[rbh>=0.33,]
+bh.test <- BostonHousing[rbh<0.33,]
+hv.tree <- rpart(Class̃., hv.train) hv.nb <- naiveBayes(Class̃., hv.train)
