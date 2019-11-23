fcf8dd845f0889ba627ea7da41d3fa0a3e7c35aa
Syed Hussain Ather <shussainather@gmail.com>
2019-11-23 13:17:38 -0800

    pid

--- stat/R/svm.R
+++ stat/R/svm.R
@@ -17,4 +17,14 @@ set.seed(12)
 rpid <- runif(nrow(PimaIndiansDiabetes))
 pid.train <- PimaIndiansDiabetes[rpid>=0.33,]
 pid.test <- PimaIndiansDiabetes[rpid<0.33,]
-  
+ 
+rbh <- runif(nrow(BostonHousing))
+bh.train <- BostonHousing[rbh>=0.33,-4]
+bh.test <- BostonHousing[rbh<0.33,-4]
+
+pid.stdm <- std.all(diabetes~., pid.train) 
+pid.std.train <- predict.std(pid.stdm, pid.train) 
+pid.std.test <- predict.std(pid.stdm, pid.test)
+bh.stdm <- std.all(medv~., bh.train) 
+bh.std.train <- predict.std(bh.stdm, bh.train) 
+bh.std.test <- predict.std(bh.stdm, bh.test) 
