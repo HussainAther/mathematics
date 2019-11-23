1bf7cae669382e2e3bcff766505b8781f3a9df29
Syed Hussain Ather <shussainather@gmail.com>
2019-11-23 01:16:39 -0800

    more on v2

--- stat/python/dice.py
+++ stat/python/dice.py
@@ -18,5 +18,17 @@ def dicev2(a, b):
     Dice coefficient 2nt/(na + nb).
     """
     if not len(a) or not len(b): return 0.0
-    if len(a) == 1:  a=a+u'.'
-    if len(b) == 1:  b=b+u'.'
+    if len(a) == 1:  a=a+u"."
+    if len(b) == 1:  b=b+u"."
+    a_bigram_list=[]
+    for i in range(len(a)-1):
+      a_bigram_list.append(a[i:i+2])
+    b_bigram_list=[]
+    for i in range(len(b)-1):
+      b_bigram_list.append(b[i:i+2])
+      
+    a_bigrams = set(a_bigram_list)
+    b_bigrams = set(b_bigram_list)
+    overlap = len(a_bigrams & b_bigrams)
+    dice_coeff = overlap * 2.0/(len(a_bigrams) + len(b_bigrams))
+    return dice_coeff
