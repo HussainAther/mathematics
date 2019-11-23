38d7fd73d66e67b9c38a254167ddc907448a1dca
Syed Hussain Ather <shussainather@gmail.com>
2019-11-23 01:15:23 -0800

    def

--- stat/python/dice.py
+++ stat/python/dice.py
@@ -3,3 +3,12 @@ Dice's coefficient (Sørensen–Dice coefficient) measures how similar a set
 and another set are. It can be used to measure how similar two strings are in terms of 
 the number of common bigrams (a bigram is a pair of adjacent letters in the string). 
 """
+
+def dice_coefficient(a, b):
+    """
+    Dice coefficient 2nt/(na + nb).
+    """
+    a_bigrams = set(a)
+    b_bigrams = set(b)
+    overlap = len(a_bigrams & b_bigrams)
+    return overlap * 2.0/(len(a_bigrams) + len(b_bigrams))
