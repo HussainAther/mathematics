335681d61055e3b30564f217a9f3914ab1c8753d
Syed Hussain Ather <shussainather@gmail.com>
2019-11-23 01:14:56 -0800

    init

---
+++ stat/python/dice.py
@@ -0,0 +1,5 @@
+"""
+Dice's coefficient (Sørensen–Dice coefficient) measures how similar a set 
+and another set are. It can be used to measure how similar two strings are in terms of 
+the number of common bigrams (a bigram is a pair of adjacent letters in the string). 
+"""
