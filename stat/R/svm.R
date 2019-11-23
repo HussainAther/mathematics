aa40a0133bbec387bb36db741239497c9e06bf70
Syed Hussain Ather <shussainather@gmail.com>
2019-11-23 13:19:08 -0800

    datasets

--- stat/R/svm.R
+++ stat/R/svm.R
@@ -27,4 +27,19 @@ pid.std.train <- predict.std(pid.stdm, pid.train)
 pid.std.test <- predict.std(pid.stdm, pid.test)
 bh.stdm <- std.all(medv~., bh.train) 
 bh.std.train <- predict.std(bh.stdm, bh.train) 
-bh.std.test <- predict.std(bh.stdm, bh.test) 
+bh.std.test <- predict.std(bh.stdm, bh.test)
+
+# dataset for plots
+kmf.plot <- function(a1, a2) { 2*a1-3*a2+4 }
+kmdat.plot <- 'names<-'(expand.grid(seq(1, 5, 0.05), seq(1, 5, 0.05)), c("a1", "a2"))
+kmdat.plot$f <- kmf.plot(kmdat.plot$a1, kmdat.plot$a2)
+kmdat.plot$c <- as.factor(ustep(kmdat.plot$f))
+
+# datasets for parameter estimation examples
+kmg<-function(a1, a2, a3, a4) { a1^2+2*a^2-a3^2-2*a4^2+2*a1-3*a2+2*a3-3*a4+1 } 
+kmf <- function(a1, a2, a3, a4) { 3*a1+4*a2-2*a3+2*a4-3 }
+kmdat <- data.frame(a1=runif(400, min=1, max=5), a2=runif(400, min=1, max=5),
+                    a3=runif(400, min=1, max=5), a4=runif(400, min=1, max=5))
+kmdat$g <- kmg(kmdat$a1, kmdat$a2, kmdat$a3, kmdat$a4)
+kmdat$c <- as.factor(ustep(kmdat$g))
+kmdat$f <- kmf(kmdat$a1, kmdat$a2, kmdat$a3, kmdat$a4) 
