6cad6267009439566a06b922cf00243cd2a470bd
Syed Hussain Ather <shussainather@gmail.com>
2019-11-23 13:21:12 -0800

    train

--- stat/R/svm.R
+++ stat/R/svm.R
@@ -42,4 +42,12 @@ kmdat <- data.frame(a1=runif(400, min=1, max=5), a2=runif(400, min=1, max=5),
                     a3=runif(400, min=1, max=5), a4=runif(400, min=1, max=5))
 kmdat$g <- kmg(kmdat$a1, kmdat$a2, kmdat$a3, kmdat$a4)
 kmdat$c <- as.factor(ustep(kmdat$g))
-kmdat$f <- kmf(kmdat$a1, kmdat$a2, kmdat$a3, kmdat$a4) 
+kmdat$f <- kmf(kmdat$a1, kmdat$a2, kmdat$a3, kmdat$a4)
+
+kmdat.train <- kmdat[1:200,]
+kmdat.test <- kmdat[201:400,]
+
+# linearly separable training and test subsets 
+kmdat.ls <- linsep.sub(c~a1+a2+a3+a4, kmdat) 
+kmdat.train.ls <- kmdat[1:200,][kmdat.ls[1:200],] 
+kmdat.test.ls <- kmdat[201:400,][kmdat.ls[201:400],] 
