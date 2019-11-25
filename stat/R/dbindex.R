20307b016e38c91e37f2ebbb1994be849fcfa4a4
Syed Hussain Ather <shussainather@gmail.com>
2019-11-25 08:32:24 -0800

    diag

--- stat/R/dbindex.R
+++ stat/R/dbindex.R
@@ -3,4 +3,8 @@ dbindex <- function(clustering, centers, data, metric="euclidean", stand=FALSE)
  {
    clusters <- sort(unique(clustering))
    ds <- as.matrix(daisy(rbind(data, centers), metric, stand))
-
+   to.center <- sapply(clusters, function(d) mean(ds[clustering==d,nrow(data)+d]))
+   between.centers <- ds[(nrow(data)+1):(nrow(data)+length(clusters)),
+                          (nrow(data)+1):(nrow(data)+length(clusters))]
+   diag(between.centers) <- NA
+   'dimnames<-'(outer(clusters, clusters, Vectorize(function(d1, d2)
