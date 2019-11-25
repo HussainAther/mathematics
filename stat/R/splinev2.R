955273ecd3775147bc604b648b439ea502e2997c
Syed Hussain Ather <shussainather@gmail.com>
2019-11-24 22:39:32 -0800

    covar

--- stat/R/splinev2.R
+++ stat/R/splinev2.R
@@ -3,3 +3,6 @@ coords <- expand.grid(x = c(-1, 0, 1), y = c(-1, 0, 1))
 predict.pt <- which(coords$x == 0 & coords$y == 0)
 distances <- dist(coords)
 distances <- as.matrix(distances)
+covars <- exp(-distances)
+Cov.YZ <- covars[predict.pt, -predict.pt]
+Var.Z <- covars[-predict.pt, -predict.pt]
