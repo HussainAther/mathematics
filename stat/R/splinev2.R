a7a2edda140e45535568e6d4cc0152fcd41d66d4
Syed Hussain Ather <shussainather@gmail.com>
2019-11-24 22:39:21 -0800

    dists

--- stat/R/splinev2.R
+++ stat/R/splinev2.R
@@ -1,3 +1,5 @@
 # Spatial smoothing with splines
 coords <- expand.grid(x = c(-1, 0, 1), y = c(-1, 0, 1))
 predict.pt <- which(coords$x == 0 & coords$y == 0)
+distances <- dist(coords)
+distances <- as.matrix(distances)
