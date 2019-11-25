e937157b97beb53b761c5e92109ee4da49165998
Syed Hussain Ather <shussainather@gmail.com>
2019-11-25 08:33:46 -0800

    list

--- stat/R/dbindex.R
+++ stat/R/dbindex.R
@@ -8,3 +8,7 @@ dbindex <- function(clustering, centers, data, metric="euclidean", stand=FALSE)
                           (nrow(data)+1):(nrow(data)+length(clusters))]
    diag(between.centers) <- NA
    'dimnames<-'(outer(clusters, clusters, Vectorize(function(d1, d2)
+                                                   (to.center[d1]+to.center[d2])/
+                                                    between.centers[d1,d2])),
+               list(clusters, clusters))
+}
