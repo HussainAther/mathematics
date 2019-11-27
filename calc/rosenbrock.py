646c659b5cd70ed203ea0b53a1c73189ef20b46f
Syed Hussain Ather <shussainather@gmail.com>
2019-11-26 18:10:51 -0800

    init init init

---
+++ calc/rosenbrock.py
@@ -0,0 +1,17 @@
+"""
+Non-convex bivariate Rosenbrock function.
+"""
+
+def rosenbrock(X):
+    """
+    This R^2 -> R^1 function should be compatible with algopy.
+    http://en.wikipedia.org/wiki/Rosenbrock_function
+    A generalized implementation is available
+    as the scipy.optimize.rosen function.
+    """
+    x = X[0]
+    y = X[1]
+    a = 1. - x
+    b = y - x*x
+    return a*a + b*b*100.
+
