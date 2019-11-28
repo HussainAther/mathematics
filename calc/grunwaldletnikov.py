401f492542a7b1d63fd2c4716518e3b3143b7766
Syed Hussain Ather <shussainather@gmail.com>
2019-11-27 16:10:32 -0800

    comment

--- calc/grunwaldletnikov.py
+++ calc/grunwaldletnikov.py
@@ -5,7 +5,9 @@ Grünwald-Letnikov (grunwaldletnikov grunwald letnikov) fractional derivative.
 # fractional order
 eta = .8
 
-# tempoeral domain and time step size
+# temporal domain and time step size
 dt = 1e-3
 tmax = 1
-tmin = 0  
+tmin = 0 
+
+# Solve the fractional differential equation. 
