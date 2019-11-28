1ea0a255ea99caf6296ee3dcbfff98e1b1370623
Syed Hussain Ather <shussainather@gmail.com>
2019-11-27 16:10:12 -0800

    temp

--- calc/grunwaldletnikov.py
+++ calc/grunwaldletnikov.py
@@ -3,4 +3,9 @@ Grünwald-Letnikov (grunwaldletnikov grunwald letnikov) fractional derivative.
 """
 
 # fractional order
-eta = .8 
+eta = .8
+
+# tempoeral domain and time step size
+dt = 1e-3
+tmax = 1
+tmin = 0  
