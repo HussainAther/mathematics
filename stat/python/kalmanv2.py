cdf103b83191acb32a369cee47038030bce62c08
Syed Hussain Ather <shussainather@gmail.com>
2019-11-30 00:32:44 -0800

    data

--- stat/python/kalmanv2.py
+++ stat/python/kalmanv2.py
@@ -1,3 +1,12 @@
 import numpy as np
 
 from numpy.linalg import inv
+
+"""
+Kalman filter to determine velocity
+of an aircraft.
+"""
+
+x_observations = np.array([4000, 4260, 4550, 4860, 5110])
+v_observations = np.array([280, 282, 285, 286, 290])
+
