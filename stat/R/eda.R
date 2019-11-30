d908dbfbea21d18bb056396177f762119f9116c9
Syed Hussain Ather <shussainather@gmail.com>
2019-11-29 22:17:33 -0800

    tukey

--- stat/R/eda.R
+++ stat/R/eda.R
@@ -9,3 +9,8 @@ library(RSADBE)
 x <- c(13,17,11,115,12,7,24)
 tab <- cbind(order(x),x[order(x)],c(1:7),c(7:1),pmin(c(1:7), c(7:1)))
 colnames(tab) <- c("x_label","x_order","Position_from_min", "Position_from_max","depth")
+
+"Tukey's (tukey) five number inter-difference, Bowley-Tukey (bowley tukey) measure of skewness"
+# For memory recall times (Dunn and Master (1982))
+data(memory)
+lapply(memory,fivenum)
