e2ab6bf10a4a50f273f1bda2232e604e7c7f2f2d
Syed Hussain Ather <shussainather@gmail.com>
2019-11-30 00:33:00 -0800

    conditions

--- stat/python/kalmanv2.py
+++ stat/python/kalmanv2.py
@@ -10,3 +10,9 @@ of an aircraft.
 x_observations = np.array([4000, 4260, 4550, 4860, 5110])
 v_observations = np.array([280, 282, 285, 286, 290])
 
+z = np.c_[x_observations, v_observations]
+
+# Initial Conditions
+a = 2  # Acceleration
+v = 280
+t = 1  # Difference in time
