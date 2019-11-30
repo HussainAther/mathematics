0f2e223806157964aeeaf018d497da4eccfc6170
Syed Hussain Ather <shussainather@gmail.com>
2019-11-29 19:51:56 -0800

    plot

--- stat/R/mcmc.R
+++ stat/R/mcmc.R
@@ -8,10 +8,12 @@ sampling, namely repetitive sampling from the full conditionals for 𝜇
 and 𝜏 = 1∕𝜎2, set out above. In particular, assume a Ga(1, 1) prior 
 and a N(0, 100) prior for 𝜇. Then a code in R for simulating the data, and 
 subsequent Gibbs sampling to estimate 𝜇 and 𝜎2, with T = 10000 iterations and B = 1000 for burn-in, is"
+
 # Observations: sample of n=100 from N(10,9)
 x <- rnorm(100,10,3); n <- 100; mn.x <- mean(x); var.x <- var(x)
 cat("Observed mean ",mn.x, "\n ")
 cat("Observed variance ",var.x, "\n ")
+
 # Gibbs Sampling in conjunction with Normal/Gamma Priors
 # Parameters for Normal priors on mu, and gamma prior on tau
 m<-0;V <-100;h<-1/V;alph <-1;beta <-1
@@ -19,7 +21,8 @@ m<-0;V <-100;h<-1/V;alph <-1;beta <-1
   T <- 10000; B <- 1000; TB <- T-B
   mu <-  tau <- sigma2  <- numeric(T);
   mu[1] <- 0; tau[1] <- 1
-# start Gibbs sampler loop
+
+# Gibbs sampler loop
 for(t in 2:T){  # full conditional for mu
   m1 <- (h*m + n*tau[t-1]*mean(x))/(h+n*tau[t-1])
   V1 <- 1/(h+n*tau[t-1])
@@ -27,3 +30,22 @@ for(t in 2:T){  # full conditional for mu
   # full conditional for tau
 alph1 <- alph + (n/2); beta1 <- (sum((x-mu[t])̂2)/2) + beta tau[t] <- rgamma(1,alph1,beta1)
 sigma2[t] <- 1/tau[t]}
+
+# Retain samples after Burn-in
+mu <- mu[B+1:T]; sigma2 <- sigma2[B+1:T]
+
+# parameter summaries
+summary(mu);       quantile(mu[1:TB], c(0.025, 0.05, 0.90, 0.975))
+summary(sigma2); quantile(sigma2[1:TB], c(0.025, 0.05, 0.90, 0.975))
+
+# Set up subplots
+par(mfrow=c(2,2))
+
+# Trace Plots
+plot(mu,type="l");   plot(sigma2,type="l")
+
+# Marginal Posterior Densities
+plot(density(mu),col="red",main=expression(paste("Posterior
+                                          Density of ",mu)))
+plot(density(sigma2),col="blue",main=expression(paste("Posterior
+Density of ",sigmâ2)))
