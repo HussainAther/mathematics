e46da4c6da2e29a81c7a78ec0175f9defc3dc450
Syed Hussain Ather <shussainather@gmail.com>
2019-12-02 08:24:36 -0800

    spacing

--- algebra/linear/transfermatrix.py
+++ algebra/linear/transfermatrix.py
@@ -6,10 +6,10 @@ Transfer matrix
 
 def transfermatrix(Pi):
     xlen, ylen = Pi.shape
-    S=[0]*xlen
+    S = [0]*xlen
     for a in range(xlen):
-        S[a]=0
+        S[a] = 0
         for b in range(xlen):
-            S[a]+=P[b, a]*Pi[b]
+            S[a] += P[b, a]*Pi[b]
     return S
 
