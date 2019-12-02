a0942992281a08e2a755a932d68e831a4ea512ff
Syed Hussain Ather <shussainather@gmail.com>
2019-12-02 08:01:05 -0800

    return

--- algebra/linear/transfermatrix.py
+++ algebra/linear/transfermatrix.py
@@ -3,3 +3,13 @@ import numpy as np
 """
 Transfer matrix
 """
+
+def transfermatrix(Pi):
+    xlen, ylen = Pi.shape
+    S=[0]*xlen
+    for a in range(xlen):
+        S[a]=0
+        for b in range(xlen):
+            S[a]+=P[b, a]*Pi[b]
+    return S
+
