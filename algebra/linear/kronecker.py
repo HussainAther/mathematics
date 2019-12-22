1a5cc8a85d2d9dfcd21d05afc93920fdb77a1a0f
Syed Hussain Ather <shussainather@gmail.com>
2019-12-21 20:50:53 -0800

    def

--- algebra/linear/kronecker.py
+++ algebra/linear/kronecker.py
@@ -1,3 +1,10 @@
 """
 Kronecker product
 """
+
+def kp(m1, m2):
+    """
+    For matrices m1 and m2, calculate the Kronecker
+    product.
+    """
+    return [[n1 * n2 for n1 in elem2 for n2 in m2[row]] for elem1 in m2 for row in range(len(m2))]
