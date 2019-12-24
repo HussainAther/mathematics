3742744103fee8e50856cff3b526a223d72542d5
Syed Hussain Ather <shussainather@gmail.com>
2019-12-24 05:12:10 -0800

    def leg

--- calc/glq.py
+++ calc/glq.py
@@ -3,3 +3,9 @@ import numpy as np
 """
 Gaussian-Legendre Quadrature (gaussian legendre quadrature)
 """
+
+def legendre(n, x):
+    """
+    Recrusively generate the Legendre polynomial
+    of order n with an x value.
+    """
