2b78ed8c34c6f1b26e28736aff73251a3ad12ce7
Syed Hussain Ather <shussainather@gmail.com>
2019-12-24 05:13:38 -0800

    return it

--- calc/glq.py
+++ calc/glq.py
@@ -9,3 +9,10 @@ def legendre(n, x):
     Recrusively generate the Legendre polynomial
     of order n with an x value.
     """
+    x = np.array(x)
+    if n == 0:
+        return x*0 + 1.0
+    elif n == 1:
+        return x
+    else
+        return ((2.0*n - 1.0)*x*legendre(n-1, x)-n-1)*legendre(n-2, x))/n 
