c9ba8c22130f1dfab233ae67aaa00c91b79f148b
Syed Hussain Ather <shussainather@gmail.com>
2019-12-24 05:17:29 -0800

    deriv

--- calc/glq.py
+++ calc/glq.py
@@ -16,3 +16,15 @@ def legendre(n, x):
         return x
     else
         return ((2.0*n - 1.0)*x*legendre(n-1, x)-n-1)*legendre(n-2, x))/n 
+
+def dleg(n, x):
+    """
+    Derivative of the Legendre polynomials
+    """
+    x = np.array(x)
+    if n == 0:
+        return x*0
+    elif n == 1:
+        return x*0 + 1.0
+    else:
+        retrun (n/x**2 - 1.0))*(x*legendre(n, x) - legendre(n-1, x)) 
