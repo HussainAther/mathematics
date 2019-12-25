b31c4c8572ea7c71a4d2ef70d96fca98dfdcceee
Syed Hussain Ather <shussainather@gmail.com>
2019-12-25 00:27:58 -0800

    def koch

--- algebra/misc/koch.py
+++ algebra/misc/koch.py
@@ -2,3 +2,17 @@ import numpy as np
 
 from itertools import chain
 from operator import add, sub
+
+"""
+Draw a Koch snowflake.
+"""
+
+def kochSnowflake(n, a, b):
+    """
+    List points on a Koch snowflake of order n from an equilateral
+    triangle with bases a and b. 
+    """
+    points = [a, equilateralApex(a, b), b]
+    return chain.from_iterable(
+        map(kochCurve(n), points, points[1:] + [points[0]])
+    )
