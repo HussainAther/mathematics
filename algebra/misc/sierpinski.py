7345d4c7e28f865f8f04a46fb3ece93f61d4f46b
Syed Hussain Ather <shussainather@gmail.com>
2019-12-25 19:24:44 -0800

    iterate

--- algebra/misc/sierpinski.py
+++ algebra/misc/sierpinski.py
@@ -1,4 +1,9 @@
 def st(n):
     """
-    Print a Sierpinski triangle.
+    Print a Sierpinski triangle of rows n.
     """
+    d = ["*"]
+    for i in range(n):
+        sp = " " * (2**i)
+        d = [sp+x+sp for x in d] + [x + " " + x for x in d]
+
