77370a848549d6bb12dec278d428202d840ff356
Syed Hussain Ather <shussainather@gmail.com>
2019-12-31 00:13:22 -0800

    window

--- algebra/misc/peanocurve.py
+++ algebra/misc/peanocurve.py
@@ -8,3 +8,12 @@ Peano curve.
 stack = [] # Mark the current stacks in run.
 def peano(iterations=1):
     global stack
+    # The turtle Ivan:
+    ivan = tt.Turtle(shape = "classic", visible = True)
+ 
+    # The app window:
+    screen = tt.Screen()
+    screen.title("Desenhin do Peano")
+    screen.bgcolor("#232323")
+    screen.delay(0) # Speed on drawing (if higher, more slow)
+    screen.setup(width=0.95, height=0.9)
