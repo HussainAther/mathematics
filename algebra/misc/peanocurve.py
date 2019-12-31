7ceafc74bfb146c1a281e73cf45f1557a525b3ef
Syed Hussain Ather <shussainather@gmail.com>
2019-12-31 00:15:18 -0800

    screenlength

--- algebra/misc/peanocurve.py
+++ algebra/misc/peanocurve.py
@@ -17,3 +17,16 @@ def peano(iterations=1):
     screen.bgcolor("#232323")
     screen.delay(0) # Speed on drawing (if higher, more slow)
     screen.setup(width=0.95, height=0.9)
+
+    # The size of each step walked (here, named simply "walk"). It's not a pixel scale. This may stay still:
+    walk = 1
+ 
+    def screenlength(k):
+        """
+        This is a function to make the image good to see (without it would result in a partial image).
+        This will guarantee that we can see the the voids and it's steps.
+        """
+        if k != 0:
+            length = screenlength(k-1)
+            return 2*length + 1
+        else: return 0
