33e2fc0cf9eafb6145acf21e06afb700c85bbc03
Syed Hussain Ather <shussainather@gmail.com>
2020-01-01 00:09:43 -0800

    start

--- algebra/linear/spiralmatrix.py
+++ algebra/linear/spiralmatrix.py
@@ -2,3 +2,6 @@ def spiral(n):
     """
     Square numbers in a spiral matrix.
     """
+    dx,dy = 1, 0 # starting increments
+    x,y = 0, 0 # starting location
+    myarray = [[None]* n for j in range(n)]
