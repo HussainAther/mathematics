8b26ac79166a3f4b87af77e4ff4e8552474ead95
Syed Hussain Ather <shussainather@gmail.com>
2020-01-01 00:10:37 -0800

    return

--- algebra/linear/spiralmatrix.py
+++ algebra/linear/spiralmatrix.py
@@ -8,4 +8,9 @@ def spiral(n):
     for i in xrange(n**2):
         myarray[x][y] = i
         nx,ny = x+dx, y+dy
-        if 0<=nx<n and 0<=ny<n and myarray[nx][ny] == None:
+        if 0 <= nx< n and 0 <= ny < n and myarray[nx][ny] == None:
+            x, y = nx, ny
+        else:
+            dx, dy = -dy, dx
+            x,y = x+dx, y+dy
+    return myarray
