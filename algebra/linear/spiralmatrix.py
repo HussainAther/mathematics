85a0ba9129b7c9e17dba5c127f76e956fc45c757
Syed Hussain Ather <shussainather@gmail.com>
2020-01-01 00:10:00 -0800

    loop

--- algebra/linear/spiralmatrix.py
+++ algebra/linear/spiralmatrix.py
@@ -5,3 +5,7 @@ def spiral(n):
     dx,dy = 1, 0 # starting increments
     x,y = 0, 0 # starting location
     myarray = [[None]* n for j in range(n)]
+    for i in xrange(n**2):
+        myarray[x][y] = i
+        nx,ny = x+dx, y+dy
+        if 0<=nx<n and 0<=ny<n and myarray[nx][ny] == None:
