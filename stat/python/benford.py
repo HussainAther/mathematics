93e13d1bd99a5cf7da638ec9b41627e52cfebeba
Syed Hussain Ather <shussainather@gmail.com>
2020-01-03 22:13:25 -0800

    show dist

--- stat/python/benford.py
+++ stat/python/benford.py
@@ -29,3 +29,15 @@ def heads(s):
     Header of the distribution
     """
     for a in s: yield int(str(a)[0])
+
+def show_dist(title, s):
+    """
+    Show the distribution.
+    """
+    c = Counter(s)
+    size = sum(c.values())
+    res = [c[d]/size for d in range(1,10)]
+ 
+    print("\n%s Benfords deviation" % title)
+    for r, e in zip(res, expected):
+        print("%5.1f%% %5.1f%%  %5.1f%%" % (r*100., e*100., abs(r - e)*100.))
