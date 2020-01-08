89ce0c9fa4f0c3d867403279c367bf4f08b56818
Syed Hussain Ather <shussainather@gmail.com>
2020-01-08 01:35:51 -0800

    print

--- calc/calculatee.py
+++ calc/calculatee.py
@@ -13,3 +13,8 @@ while(e-e0 > 1e-15):
 	n += 1
 	fact *= 2*n*(2*n+1)
 	e += (2.*n+2)/fact
+
+print("Computed e = " + str(e))
+print("Real e = " + str(math.e))
+print("Error = " + str(math.e-e))
+print("Number of iterations = " + str(n))
