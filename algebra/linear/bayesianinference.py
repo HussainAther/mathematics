e3a99984088542915140b82134545a95f124f238
Syed Hussain Ather <shussainather@gmail.com>
2020-01-11 04:30:19 -0800

    sample posterior

--- algebra/linear/bayesianinference.py
+++ algebra/linear/bayesianinference.py
@@ -113,3 +113,55 @@ for i, b in enumerate(betas.numpy()):
     polynomial_str += "{0:.2f}".format(b) + "x^" + str(i) 
     if i < k - 1: polynomial_str += " + "
 plt.scatter(x_arr, y.numpy()); plt.title(polynomial_str); plt.show()
+
+"""
+2. Exact posterior inference
+
+Since we are using conjugate priors, it is possible to compute the posterior analytically.
+The form of the posterior will be the same as the prior: 
+- Gaussian distribution over betas
+- Gamma distribution over precision
+
+You can find the derivation on the wikipedia page.
+
+"""
+print("~~~Defining the posterior~~~")
+def analytical_posterior(y, x, a_0, b_0, mu_0, k):
+    """
+    Given a dataset (x,y), return posterior parameters 
+    for p(b | sigma^2, y, x) and p(precision | y, x).
+    
+    Inputs
+    ------
+    y: (n,) tensor 
+    x: (n,) x-coordinates
+    a_0, b_0, mu_0: prior parameters
+    k: order of polynomial 
+    
+    Outputs
+    -------
+    a_posterior: () shape parameter for Gamma dist
+    b_posterior: () scale parameter for Gamma dist
+    mu_posterior: (k,) mean
+    lambda_posterior: (k,k) precision matrix
+    
+    """
+    
+    x = features(x, k)
+    
+    ##p(b|tau, y, x) = N(mu_posterior, (1/tau)*lambda_posterior^-1)
+    XTX = x.t().mm(x)
+    lambda_0 = torch.eye(k)
+    lambda_posterior = XTX + lambda_0
+    lambda_inv = lambda_posterior.inverse()
+    
+    mu_0 = mu_0*torch.ones(k,1)
+    inside = lambda_0.mm(mu_0) + x.t().mm(y)
+    mu_posterior = lambda_inv.mm(inside)
+        
+    ##p(tau| y, X)
+    a_posterior = a_0 + len(y)/2.
+    mlm = mu_0.t().mm(lambda_0).mm(mu_0) - mu_posterior.t().mm(lambda_posterior).mm(mu_posterior)
+    b_posterior = b_0 + 0.5*(y.t().mm(y) + mlm)
+    
+    return a_posterior, b_posterior, mu_posterior, lambda_posterior
