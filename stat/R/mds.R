61b454fc93a74e8f4e54f84e37bb500ec856397c
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 11:35:37 -0800

    load data

--- stat/R/mds.R
+++ stat/R/mds.R
@@ -1,4 +1,12 @@
 library(knitr)
+library(rafalib)
+library(tissuesGeneExpression)
 
-# Multi-dimensional scaling
+# Multi-dimensional scaling for gene expression
+
+data(tissuesGeneExpression)
+colind <- tissue%in%c("kidney","colon","liver")
+mat <- e[,colind]
+group <- factor(tissue[colind])
+dim(mat)
 
