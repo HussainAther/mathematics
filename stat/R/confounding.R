2a99a29191c884a583d31a4cfb30cc9bd0826c37
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 11:06:40 -0800

    simpson

--- stat/R/confounding.R
+++ stat/R/confounding.R
@@ -27,3 +27,15 @@ for(i in 1:2){
 tab <- round(tab)
 tab
 print(chisq.test(tab)$p.val)
+
+# percent admissions by major
+y=cbind(dat[1:6,c(1,3)],dat[7:12,3])
+colnames(y)[2:3]=c("Male","Female")
+y
+
+# Simpson's paradox
+y=cbind(dat[1:6,5],dat[7:12,5])
+y=sweep(y,2,colSums(y),"/")*100
+x=rowMeans(cbind(dat[1:6,3],dat[7:12,3]))
+matplot(x,y,xlab="percent that gets in the major",ylab="percent that applies to major",col=c("blue","red"),cex=1.5)
+legend("topleft",c("Male","Female"),col=c("blue","red"),pch=c("1","2"),box.lty=0)
