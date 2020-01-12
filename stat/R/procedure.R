54361904b3c197244e41d38d5c8e681b0dbcade3
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 12:41:00 -0800

    test to reject null hypo

--- stat/R/procedure.R
+++ stat/R/procedure.R
@@ -16,3 +16,21 @@ pvals <- replicate(m,{
     treatment = sample(population,N)
     t.test(treatment,control)$p.value
 })
+
+# Get R
+sum(pvals < 0.05)alpha <- 0.05
+N <- 12
+m <- 10000
+p0 <- 0.90 ##10% of diets work, 90% don't
+m0 <- m*p0
+m1 <- m-m0
+nullHypothesis <- c( rep(TRUE,m0), rep(FALSE,m1))
+delta <- 3set.seed(1)
+calls <- sapply(1:m, function(i){
+  control <- sample(population,N)
+  treatment <- sample(population,N)
+  if(!nullHypothesis[i]) treatment <- treatment + delta
+  ifelse( t.test(treatment,control)$p.value < alpha, 
+          "Called Significant",
+          "Not Called Significant")
+})
