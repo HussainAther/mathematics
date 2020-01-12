6a2709edff2915bc174fbc1369c241fc0c72cd15
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 12:16:28 -0800

    diff in mean

--- stat/R/ttest.R
+++ stat/R/ttest.R
@@ -3,3 +3,8 @@ library(dplyr)
 dat <- read.csv("femaleMiceWeights.csv")
 
 # T-test (ttest t test) in practice
+
+control <- filter(dat,Diet=="chow") %>% select(Bodyweight) %>% unlist
+treatment <- filter(dat,Diet=="hf") %>% select(Bodyweight) %>% unlist
+
+diff <- mean(treatment) - mean(control)
