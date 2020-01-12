543a8beeb2aa34df90a398fa8e79dbc80552a0b5
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 11:50:10 -0800

    import

--- stat/R/montecarlo.R
+++ stat/R/montecarlo.R
@@ -1,5 +1,10 @@
 library(knitr)
+library(rafalib)
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/mice_pheno.csv"
 
 # Monte Carlo simulations
 
-
+set.seed(1)
+filename <- "mice_pheno.csv"
+if (!file.exists(filename)) download(url,destfile=filename)
