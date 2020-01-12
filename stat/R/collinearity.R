5378bfafd388078ebc12bb7bb98816a847669955
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 10:53:24 -0800

    rank

--- stat/R/collinearity.R
+++ stat/R/collinearity.R
@@ -3,3 +3,12 @@ opts_chunk$set(fig.path=paste0("figure/", sub("(.*).Rmd","\\1",basename(knitr:::
 
 # Collinearity - look for this in the design matrix to determine
 # if the model fits.
+
+# Get matrix rank.
+Sex <- c(0,0,0,0,1,1,1,1)
+A <-   c(1,1,0,0,0,0,0,0)
+B <-   c(0,0,1,1,0,0,0,0)
+C <-   c(0,0,0,0,1,1,0,0)
+D <-   c(0,0,0,0,0,0,1,1)
+X <- model.matrix(~Sex+A+B+C+D-1)
+cat("ncol=",ncol(X),"rank=", qr(X)$rank,"\n")
