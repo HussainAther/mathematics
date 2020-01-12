8ed0d39d577bebc51cd3b7957f8b82f0280feb73
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 12:40:29 -0800

    do it now

--- stat/R/procedure.R
+++ stat/R/procedure.R
@@ -1,4 +1,18 @@
+library(downloader)
 library(knitr)
 
 # Procedure of computing a p-value ofr each gene and calling
 # genes with p-values smaller than it significant.
+
+filename <- "femaleControlsPopulation.csv"
+
+set.seed(1)
+population = unlist( read.csv("femaleControlsPopulation.csv") )
+alpha <- 0.05
+N <- 12
+m <- 10000
+pvals <- replicate(m,{
+    control = sample(population,N)
+    treatment = sample(population,N)
+    t.test(treatment,control)$p.value
+})
