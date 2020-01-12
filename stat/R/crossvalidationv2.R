0dee5680870bb8a4ac35c7e2628eded44652bb18
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 12:29:39 -0800

    knn

--- stat/R/crossvalidationv2.R
+++ stat/R/crossvalidationv2.R
@@ -3,3 +3,14 @@ library(tissuesGeneExpression)
 data(tissuesGeneExpression)
 
 # Implementation of cross-validation (CV)
+
+# Drop tissues without many samples.
+table(tissue)
+ind <- which(tissue != "placenta")
+y <- tissue[ind]
+X <- t( e[,ind] )
+
+# K-nearest (k nearest) neighbors for classification (knn)
+library(class)
+pred <- knn(train =  X, test = X, cl=y, k=5)
+mean(y != pred)
