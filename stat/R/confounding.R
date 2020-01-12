16a49c7dabdada83cbb9064b610a84a9fa464be9
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 11:05:51 -0800

    chisquare

--- stat/R/confounding.R
+++ stat/R/confounding.R
@@ -15,3 +15,15 @@ dat$total=dat$Percent*dat$Number/100
 sum(dat$total[dat$Gender==1]/sum(dat$Number[dat$Gender==1]))
 # percent women get in
 sum(dat$total[dat$Gender==0]/sum(dat$Number[dat$Gender==0]))
+
+# chi-square (chi square) test
+indexes<-list(1:6,7:12)
+tab <- matrix(NA,2,2,dimnames=list(c("males","females"),c("accepted","rejected")))
+for(i in 1:2){
+  ind<-indexes[[i]]
+  tab[i,1]<- sum(dat[ind,2]*dat[ind,3]/100)
+  tab[i,2] <- sum(dat[ind,2]*(1-dat[ind,3]/100))
+}
+tab <- round(tab)
+tab
+print(chisq.test(tab)$p.val)
