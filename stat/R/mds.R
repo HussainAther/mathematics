f5b01bb67a8af1a66e8174f59d20b1fd1c0a0433
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 11:40:12 -0800

    svd

--- stat/R/mds.R
+++ stat/R/mds.R
@@ -10,3 +10,10 @@ mat <- e[,colind]
 group <- factor(tissue[colind])
 dim(mat)
 
+# single value decomposition
+s <- svd(mat-rowMeans(mat))
+PC1 <- s$d[1]*s$v[,1]
+PC2 <- s$d[2]*s$v[,2]
+mypar(1,1)
+plot(PC1,PC2,pch=21,bg=as.numeric(group))
+legend("bottomright",levels(group),col=seq(along=levels(group)),pch=15,cex=1.5)
