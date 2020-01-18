c1940b71343ec7dec7d3f7ddba98f5fe8a68c994
Syed Hussain Ather <shussainather@gmail.com>
2020-01-18 11:53:34 -0800

    def logistic

--- diffeq/logisticbifurcation.py
+++ diffeq/logisticbifurcation.py
@@ -1,2 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+"""
+Logistic map bifurcation
+"""
+
+def logistic(r, x):
+    """
+    Logistic map function for nonlinear systems
+    """
+    return r * x * (1 - x)
