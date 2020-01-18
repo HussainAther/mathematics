442ab211e8c90c3aa1ab80c83ad5d8b5e65a6348
Syed Hussain Ather <shussainather@gmail.com>
2020-01-18 12:54:17 -0800

    observs

--- diffeq/logisticbifurcation.py
+++ diffeq/logisticbifurcation.py
@@ -40,7 +40,6 @@ def plotsystem(r, x0, n, ax=None):
 
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
-    ax.set_title(f"$r={r:.1f}, \, x_0={x0:.1f}$")
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6),
                                sharey=True)
@@ -53,32 +52,33 @@ iterations = 1000
 last = 100
 x = 1e-5 * np.ones(n)
 
-lyapunov = np.zeros(n)
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 9),
-                               sharex=True)
+# lyapunov = np.zeros(n)
+# fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 9),
+#                                sharex=True)
 for i in range(iterations):
     x = logistic(r, x)
     # We compute the partial sum of the
     # Lyapunov exponent.
-    lyapunov += np.log(abs(r - 2 * r * x))
+#    lyapunov += np.log(abs(r - 2 * r * x))
     # We display the bifurcation diagram.
     if i >= (iterations - last):
         ax1.plot(r, x, ",k", alpha=.25)
 ax1.set_xlim(2.5, 4)
 ax1.set_title("Bifurcation diagram")
-
-# Display the Lyapunov exponent.
-# Horizontal line.
-ax2.axhline(0, color="k", lw=.5, alpha=.5)
-# Negative Lyapunov exponent.
-ax2.plot(r[lyapunov < 0],
-         lyapunov[lyapunov < 0] / iterations,
-         ".k", alpha=.5, ms=.5)
-# Positive Lyapunov exponent.
-ax2.plot(r[lyapunov >= 0],
-         lyapunov[lyapunov >= 0] / iterations,
-         ".r", alpha=.5, ms=.5)
-ax2.set_xlim(2.5, 4)
-ax2.set_ylim(-2, 1)
-ax2.set_title("Lyapunov exponent")
-plt.tight_layout()
+# 
+## Display the Lyapunov exponent.
+## Horizontal line.
+#ax2.axhline(0, color="k", lw=.5, alpha=.5)
+## Negative Lyapunov exponent.
+#ax2.plot(r[lyapunov < 0],
+#         lyapunov[lyapunov < 0] / iterations,
+#         ".k", alpha=.5, ms=.5)
+#
+## Positive Lyapunov exponent.
+#ax2.plot(r[lyapunov >= 0],
+#         lyapunov[lyapunov >= 0] / iterations,
+#         ".r", alpha=.5, ms=.5)
+#ax2.set_xlim(2.5, 4)
+#ax2.set_ylim(-2, 1)
+#ax2.set_title("Lyapunov exponent")
+#plt.tight_layout()
--- diffeq/logisticiter.py
+++ diffeq/logisticiter.py
@@ -1,3 +1,11 @@
 """
 Iterate through a logistic map.
 """
+
+def iterate(x0, r, iter):
+    for i in range(iter):
+        prod = r*x0*(1-x0)
+        x0 = prod
+    return prod
+
+print(iterate(.2, 2.6, 10))
