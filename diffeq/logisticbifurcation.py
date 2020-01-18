d9ba9a2c27148d91b881c118b6529b3bb92c8af2
Syed Hussain Ather <shussainather@gmail.com>
2020-01-18 11:56:18 -0800

    lyapu

--- diffeq/logisticbifurcation.py
+++ diffeq/logisticbifurcation.py
@@ -46,3 +46,39 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6),
                                sharey=True)
 plotsystem(2.5, .1, 10, ax=ax1)
 plotsystem(3.5, .1, 10, ax=ax2)
+
+n = 10000
+r = np.linspace(2.5, 4.0, n)
+iterations = 1000
+last = 100
+x = 1e-5 * np.ones(n)
+
+lyapunov = np.zeros(n)
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 9),
+                               sharex=True)
+for i in range(iterations):
+    x = logistic(r, x)
+    # We compute the partial sum of the
+    # Lyapunov exponent.
+    lyapunov += np.log(abs(r - 2 * r * x))
+    # We display the bifurcation diagram.
+    if i >= (iterations - last):
+        ax1.plot(r, x, ",k", alpha=.25)
+ax1.set_xlim(2.5, 4)
+ax1.set_title("Bifurcation diagram")
+
+# Display the Lyapunov exponent.
+# Horizontal line.
+ax2.axhline(0, color="k", lw=.5, alpha=.5)
+# Negative Lyapunov exponent.
+ax2.plot(r[lyapunov < 0],
+         lyapunov[lyapunov < 0] / iterations,
+         ".k", alpha=.5, ms=.5)
+# Positive Lyapunov exponent.
+ax2.plot(r[lyapunov >= 0],
+         lyapunov[lyapunov >= 0] / iterations,
+         ".r", alpha=.5, ms=.5)
+ax2.set_xlim(2.5, 4)
+ax2.set_ylim(-2, 1)
+ax2.set_title("Lyapunov exponent")
+plt.tight_layout()
