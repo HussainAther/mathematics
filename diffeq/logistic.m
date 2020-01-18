d617711be68666716bb09cada8efae36a2b23331
Syed Hussain Ather <shussainather@gmail.com>
2020-01-18 10:27:04 -0800

    init

--- diffeq/logistic.m
+++ diffeq/logistic.m
@@ -1,3 +1,12 @@
 %{
 Logistic map
 }%
+
+function [iterates] = logistic(x0, r, n)
+    iterates = [];
+    currentX = x0;
+    for index = [0:n]
+        iterates = [iterates, currentX];
+        currentX = r*currentX*(1-currentX);
+    end
+    end
