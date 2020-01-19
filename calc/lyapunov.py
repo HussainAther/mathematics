fcca9f35a717ef121a1cd744e791b1529d30a69b
Syed Hussain Ather <shussainather@gmail.com>
2020-01-18 21:56:22 -0800

    rm old plot

--- calc/lyapunov.py
+++ calc/lyapunov.py
@@ -9,9 +9,6 @@ m_min = 3.5
 m_max = 4.5
 step = .25
 
-# graph1 = gdisplay(title="Lyapunov coefficient (blue) for LogisticMap (red):
-                xtitle = "m", ytitle = "x, Lap", xmax = 5.0, xmin = 0., ymax = 1.0, ymin = -.6)
-
 for m in range(m_min, m_max, step):
     y = .05
     suma = 0
