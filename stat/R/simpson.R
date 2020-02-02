6412148b10798ba309be66f87535d42ac4009455
Syed Hussain Ather <shussainather@gmail.com>
2020-02-02 09:46:08 -0500

    women

--- stat/R/simpson.R
+++ stat/R/simpson.R
@@ -5,3 +5,6 @@ admissions$total=admissions$Percent*admissions$Number/100
 
 # percent men get in
 sum(admissions$total[admissions$Gender==1]/sum(admissions$Number[admissions$Gender==1]))
+
+# percent women get in
+sum(admissions$total[admissions$Gender==0]/sum(admissions$Number[admissions$Gender==0]))
