63f41768cb3f9e3d0074fd75857110d487957b69
Syed Hussain Ather <shussainather@gmail.com>
2020-02-07 12:35:18 -0500

    multiply mean derivate bt rate

--- stat/python/mv/pipeline.py
+++ stat/python/mv/pipeline.py
@@ -79,3 +79,11 @@ def updateweights(features, targets, weights, lr):
     d_w1 = -x1*(targets - predictions)
     d_w2 = -x2*(targets - predictions)
     d_w3 = -x3*(targets - predictions)
+
+    # Multiply the mean derivative by the learning rate
+    # and subtract from our weights (remember gradient points in direction of steepest ASCENT).
+    weights[0][0] -= (lr * np.mean(d_w1))
+    weights[1][0] -= (lr * np.mean(d_w2))
+    weights[2][0] -= (lr * np.mean(d_w3))
+
+    return weights
