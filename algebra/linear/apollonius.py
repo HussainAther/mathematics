fc9aa158bf6510fc4f8404ad262ce759b0fc3159
Syed Hussain Ather <shussainather@gmail.com>
2020-02-09 21:15:10 -0500

    vector equalties

--- algebra/linear/apollonius.py
+++ algebra/linear/apollonius.py
@@ -19,3 +19,8 @@ def solveApollonius(c1, c2, c3, s1, s2, s3):
     x1, y1, r1 = c1
     x2, y2, r2 = c2
     x3, y3, r3 = c3
+
+    v11 = 2*x2 - 2*x1
+    v12 = 2*y2 - 2*y1
+    v13 = x1*x1 - x2*x2 + y1*y1 - y2*y2 - r1*r1 + r2*r2
+    v14 = 2*s2*r2 - 2*s1*r1
