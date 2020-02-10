d57b8eb47474ca212aeb31d98b3913a0ffce6f7f
Syed Hussain Ather <shussainather@gmail.com>
2020-02-09 21:03:31 -0500

    def Circle

--- algebra/linear/apollonius.py
+++ algebra/linear/apollonius.py
@@ -8,3 +8,5 @@ Find one or more circles that are tangent to three
 given objects in a plane, where an object may be a 
 line, a point or a circle of any size.
 """
+
+Circle = namedtuple("Circle", "x, y, r")
