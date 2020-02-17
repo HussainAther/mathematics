72190e9961fbc65226a3754826c16a0cd475be8b
Syed Hussain Ather <shussainather@gmail.com>
2020-02-16 20:14:49 -0500

    explain it

--- prob/ergodicity.py
+++ prob/ergodicity.py
@@ -1,8 +1,26 @@
-import scipy as sp
+import numpy as np
 
 from scipy.constants import golden
 from scipy.integrate import quad
 
 """
 Ergodicity have the property that "the time average equals the space average. This satisifies the ergodic hypothesis of thermodynamics. A process is ergodic if its time average is the same as its average over the probability space, known as the ensemble average. 
+"""
+
+# The golden angle is an irrational portion of a circle, 
+# and so a sequence of rotations by the golden angle 
+# will not repeat itself.
+goldenangle = 2*np.pi*golden**-2
+
+"""
+If we picked a point x on a circle and rotated it repeatedly by a golden angle,
+ then we can use an integrable function f on the circle and form the average of its
+values at the sequence of rotations. This is the time average.
+"""
+
+
+"""
+We can integrate f over the circle and divide by the circumference to get the space average.
+
+The ergodic theorem says that the time average equals the space average, except possibly for a setting of starting values of measure zero.
 """
