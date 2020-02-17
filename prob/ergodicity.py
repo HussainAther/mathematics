4f15eba1878a44d836b5faec121ee7035534391f
Syed Hussain Ather <shussainather@gmail.com>
2020-02-16 20:12:09 -0500

    import

--- prob/ergodicity.py
+++ prob/ergodicity.py
@@ -1,3 +1,8 @@
+import scipy as sp
+
+from scipy.constants import golden
+from scipy.integrate import quad
+
 """
 Ergodicity have the property that "the time average equals the space average. This satisifies the ergodic hypothesis of thermodynamics. A process is ergodic if its time average is the same as its average over the probability space, known as the ensemble average. 
 """
