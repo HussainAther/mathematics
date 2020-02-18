ab471376a88f087d14d0b220b2f94ba5a6bad4dc
Syed Hussain Ather <shussainather@gmail.com>
2020-02-17 20:11:09 -0500

    cases

--- diffeq/sinegordon.py
+++ diffeq/sinegordon.py
@@ -3,4 +3,10 @@ The sine-Gordon (sine gordon sinegordon) equation is a nonlinear
 partial differential equation:
 
 u_tt - ∆u = -sin(u)
+
+There are then three cases of dimensional lattices given by:
+
+u_tt - u_xx = -sin(u)
+u_tt - u_xx - u_yy = -sin(u)
+u_tt - u_xx - u_yy - u_zz = -sin(u)
 """
