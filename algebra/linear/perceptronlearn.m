701c2e431fc2b595d0c51db7e0d9c4988735f986
Syed Hussain Ather <shussainather@gmail.com>
2020-03-12 16:53:48 -0400

    eval func mistkaes

--- algebra/linear/perceptronlearn.m
+++ algebra/linear/perceptronlearn.m
@@ -135,4 +135,22 @@ function [mistakes0, mistakes1] =  eval_perceptron(neg_examples, pos_examples, w
 %   mistakes0 - A vector containing the indices of the positive examples that have been
 %       incorrectly classified as negative.
 %%
+num_neg_examples = size(neg_examples,1);
+num_pos_examples = size(pos_examples,1);
+mistakes0 = [];
+mistakes1 = [];
+for i=1:num_neg_examples
+    x = neg_examples(i,:)';
+    activation = x'*w;
+    if (activation >= 0)
+        mistakes0 = [mistakes0;i];
+    end
+end
+for i=1:num_pos_examples
+    x = pos_examples(i,:)';
+    activation = x'*w;
+    if (activation < 0)
+        mistakes1 = [mistakes1;i];
+    end
+end
 
