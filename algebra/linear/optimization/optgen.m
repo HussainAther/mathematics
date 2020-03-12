8c556dd606d26f49757dccff3b1d2b829fc384f4
Syed Hussain Ather <shussainather@gmail.com>
2020-03-12 17:14:25 -0400

    fraction of data

--- algebra/linear/optimization/optgen.m
+++ algebra/linear/optimization/optgen.m
@@ -184,3 +184,14 @@ function ret = initial_model(n_hid)
     as_row_vector = cos(0:(n_params-1));
     ret = theta_to_model(as_row_vector(:) * 0.1); % We don't use random initialization, for this assignment. This way, everybody will get the same results.
 end
+
+function ret = classification_performance(model, data)
+    % This returns the fraction of data cases that is incorrectly classified by the model.
+    hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
+    hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
+    class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
+  
+    [dump, choices] = max(class_input); % choices is integer: the chosen class, plus 1.
+    [dump, targets] = max(data.targets); % targets is integer: the target class, plus 1.
+    ret = mean(double(choices ~= targets));
+end
