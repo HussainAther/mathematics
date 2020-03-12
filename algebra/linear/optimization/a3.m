cbf8860354dc4a81f13f51a685a0fbcae6e6cf61
Syed Hussain Ather <shussainather@gmail.com>
2020-03-12 16:58:58 -0400

    optimization routine

--- algebra/linear/optimization/a3.m
+++ algebra/linear/optimization/a3.m
@@ -7,3 +7,16 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
     datas = from_data_file.data;
     n_training_cases = size(datas.training.inputs, 2);
     if n_iters ~= 0, test_gradient(model, datas.training, wd_coefficient); end
+
+    % optimization
+    theta = model_to_theta(model);
+    momentum_speed = theta * 0;
+    training_data_losses = [];
+    validation_data_losses = [];
+    if do_early_stopping,
+        best_so_far.theta = -1; % this will be overwritten soon
+        best_so_far.validation_loss = inf;
+        best_so_far.after_n_iters = -1;
+    end
+    for optimization_iteration_i = 1:n_iters,
+        model = theta_to_model(theta);
