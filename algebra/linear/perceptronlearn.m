8b32c8c527dae71ce6089e1edac1b5bd10897ad8
Syed Hussain Ather <shussainather@gmail.com>
2020-03-12 16:46:38 -0400

    update weights

--- algebra/linear/perceptronlearn.m
+++ algebra/linear/perceptronlearn.m
@@ -83,3 +83,19 @@ while (num_errs > 0)
         break;
     end
 end
+
+function [w] = update_weights(neg_examples, pos_examples, w_current)
+%% 
+% Updates the weights of the perceptron for incorrectly classified points
+% using the perceptron update algorithm. This function makes one sweep
+% over the dataset.
+% Inputs:
+%   neg_examples - The num_neg_examples x 3 matrix for the examples with target 0.
+%       num_neg_examples is the number of examples for the negative class.
+%   pos_examples- The num_pos_examples x 3 matrix for the examples with target 1.
+%       num_pos_examples is the number of examples for the positive class.
+%   w_current - A 3-dimensional weight vector, the last element is the bias.
+% Returns:
+%   w - The weight vector after one pass through the dataset using the perceptron
+%       learning rule.
+%%
