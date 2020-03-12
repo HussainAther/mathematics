20e1b3d3d0c0c4aa03df8683732f127d14b1763b
Syed Hussain Ather <shussainather@gmail.com>
2020-03-12 16:53:27 -0400

    eval func

--- algebra/linear/perceptronlearn.m
+++ algebra/linear/perceptronlearn.m
@@ -118,3 +118,21 @@ for i=1:num_pos_examples
         w += learning_rate.*(pos_examples(i, :))';
     end
 end
+
+function [mistakes0, mistakes1] =  eval_perceptron(neg_examples, pos_examples, w)
+%% 
+% Evaluates the perceptron using a given weight vector. Here, evaluation
+% refers to finding the data points that the perceptron incorrectly classifies.
+% Inputs:
+%   neg_examples - The num_neg_examples x 3 matrix for the examples with target 0.
+%       num_neg_examples is the number of examples for the negative class.
+%   pos_examples- The num_pos_examples x 3 matrix for the examples with target 1.
+%       num_pos_examples is the number of examples for the positive class.
+%   w - A 3-dimensional weight vector, the last element is the bias.
+% Returns:
+%   mistakes0 - A vector containing the indices of the negative examples that have been
+%       incorrectly classified as positive.
+%   mistakes0 - A vector containing the indices of the positive examples that have been
+%       incorrectly classified as negative.
+%%
+
