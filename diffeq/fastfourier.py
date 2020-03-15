0d9cf8d2115a722a859db9eccc87654a950ac474
Syed Hussain Ather <shussainather@gmail.com>
2020-03-15 15:55:45 -0400

    np

--- diffeq/fastfourier.py
+++ diffeq/fastfourier.py
@@ -1,4 +1,5 @@
-from numpy import *
+import numpy as np
+
 from sys import version
 
 if int(version[0]) > 2: # deprecation in python 3
@@ -25,8 +26,8 @@ The total number of multiplications required with this approach is approximately
 
 max = 2100
 points = 1026
-data = zeros((max), float)
-dtr = zeros((points, 2) float)
+data = np.zeros((max), float)
+dtr = np.zeros((points, 2) float)
 
 def fft(nn, isign): 
     """
@@ -54,17 +55,16 @@ def fft(nn, isign):
             j = j−m
             m = m/2
         j = j+m
-
-    print(" Bit-reversed data")
+    print("Bit-reversed data")
     for i in range(1, n+1, 2):
-        print("%2d data[%2d] %9.5f "%(i , i , data [ i ])) # show the reorder
+        print("%2d data[%2d] %9.5f "%(i , i , data [ i ])) # Show the reorder.
     mmax = 2
     while (mmax−n) < 0 :
         istep = 2*mmax
         theta = 6.2831853/(1.0*isign*mmax)
-        sinth = math.sin(theta/2.0)
+        sinth = np.sin(theta/2.0)
         wstpr = -2.0*sinth**2
-        wstpi = math.sin(theta)
+        wstpi = np.sin(theta)
         wr = 1.0
         wi = 0.0
         for m in range(1, mmax+1, 2):
