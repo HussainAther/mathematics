32a67a8075e310d730e3b60257678aa3e9cedbaa
Syed Hussain Ather <shussainather@gmail.com>
2020-03-15 22:40:58 -0400

    spacing

--- diffeq/lorenz.py
+++ diffeq/lorenz.py
@@ -10,7 +10,7 @@ chaos theory (like with the butterfly effect). We can simulate this.
 
 def Lorenz(s):
     """
-    For three coupled differential equations with three state variables (indicated by the three-member s),
+    For three coupled differential equations with three state variables (indicated by the three-members),
     we have three constants (sigma, rho, beta) to demonstrate chaotic behavior.
     """
     x = s[0]
@@ -22,16 +22,16 @@ def Lorenz(s):
     rho = 28.0
     beta = 8.0/3.0
   
-    # return the state derivatives
+    # Return the state derivatives.
     return [sigma * (y-x), (rho-z)*x -y, x*y - beta*z]
 
-# run the function on some sample data
+# Run the function on some sample data.
 state0 = [2.0, 3.0, 4.0]
 t = np.arange(0.0, 30.0, 0.01) # Use numpy's arange function for a .01 step interval
 
 state = odeint(Lorenz, state0, t)
 
-# plot
+# Plot.
 fig = figure()
 ax = fig.gca(projection="3d")
 ax.plot(state[:,0], state[:,1], state[:,2])
@@ -55,13 +55,13 @@ pb, = plot(state1[:,0],state1[:,1], "b-", alpha=0.2)
 xlabel("x")
 ylabel("y")
 
-p, = plot(state1[0:10,0], state1[0:10,1], "b-'"
-pp, = plot(state1[10,0], state1[10,1], "b.", markersize=10)
-p2, = plot(state2[0:10,0], state2[0:10,1], "r-")
-pp2, = plot(state2[10,0], state2[10,1], "r.", markersize=10)
+p = plot(state1[0:10,0], state1[0:10,1], "b-'"
+pp = plot(state1[10,0], state1[10,1], "b.", markersize=10)
+p2 = plot(state2[0:10,0], state2[0:10,1], "r-")
+pp2 = plot(state2[10,0], state2[10,1], "r.", markersize=10)
 tt = title("%4.2f sec" % 0.00)
 
-# animate
+# Animate.
 step = 3
 for i in xrange(1,shape(state1)[0]-10,step):
     p.set_xdata(state1[10+i:20+i,0])
