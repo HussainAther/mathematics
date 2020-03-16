c1f428071b8b4927dd99c8a5514ad816beaf2196
Syed Hussain Ather <shussainather@gmail.com>
2020-03-15 22:33:12 -0400

    plot check

--- diffeq/cubicspline.py
+++ diffeq/cubicspline.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 from gekko import GEKKO
 
@@ -15,14 +15,14 @@ boundary condition that completes the system of m-2 equations.
 xm = np.array([0,1,2,3,4,5])
 ym = np.array([0.1,0.2,0.3,0.5,1.0,0.9])
 
-m = GEKKO() # create GEKKO model
+m = GEKKO() # Create GEKKO model
 m.options.IMODE = 2 # solution mode
 x = m.Param(value=np.linspace(-1,6)) # prediction points
 y = m.Var() # prediction results
 m.cspline(x, y, xm, ym) # cubic spline
 m.solve(disp=False) # solve
 
-# create plot
+# Plot.
 plt.plot(xm, ym, "bo")
 plt.plot(x.value, y.value, "r--", label="cubic spline")
 plt.legend(loc="best")
