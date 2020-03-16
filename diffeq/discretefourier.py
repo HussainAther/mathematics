6bfbf913e5f6457c3b712e814611d04cd6b4002b
Syed Hussain Ather <shussainather@gmail.com>
2020-03-15 22:39:01 -0400

    equals spacing

--- diffeq/discretefourier.py
+++ diffeq/discretefourier.py
@@ -1,20 +1,20 @@
 import cmath # for complex math functions
 import numpy as np
 
-from vpython.graph import *
+from vpython.graph import gcurve, gidsplay, gvbars
 
 """
 Discrete Fourier Transform (DFT) using built-in complex numbers
 """
 
-sgingr =  gdisplay(x=0, y=0, width=600, height=250, title ="Signal", \
-                xtitle="x", ytitle = "signal", xmax = 2.∗math.pi, xmin = 0,\
-                ymax = 30, ymin = 0)
+sgingr =  gdisplay(x=0, y=0, width=600, height=250, title="Signal", \
+                xtitle="x", ytitle="signal", xmax=2.∗np.pi, xmin=0,\
+                ymax=30, ymin=0)
 
 sigfig = gcurve(color = color.yellow, display = signgr)
 imagr = gdisplay(x=0,y=250,width=600,height=250,title ="Imag Fourier TF", \
-                xtitle = "x", ytitle="TF.Imag",xmax=10.,xmin=−1,ymax=100,ymin=−0.2)
-impart = gvbars(delta = 0.05, color = color.red, display = imagr)
+                xtitle="x", ytitle="TF.Imag", xmax=10., xmin=−1, ymax=100, ymin=−0.2)
+impart = gvbars(delta=0.05, color=color.red, display=imagr)
 
 N = 50 # number of points
 Np = N
@@ -34,7 +34,10 @@ def f(signal):
         signal[i] = 30*np.cos(x**4)
         sigfig.plot(pos = (x, signal[i]))
 
-def fouriercomplex(dftz): # fourier transform
+def fouriercomplex(dftz):
+    """
+    Fourier transform
+    """
     for n in range(0, Np):
         zsum = complex(0, 0)
         for k in range(0, N):
@@ -53,24 +56,24 @@ Discrete Fourier Transform using built-in real numbers
 
 # for the original signal
 signgr = gdisplay(x=0,y=0,width=600,height=250, title="Original signal y(t)= 3 cos(wt)+2 cos(3wt)+ cos(5wt) ",\
-                xtitle="x", ytitle="signal", xmax=2.∗math.pi ,xmin=0,ymax=7,ymin=−7)
+                xtitle="x", ytitle="signal", xmax=2.∗np.pi ,xmin=0,ymax=7,ymin=−7)
 
 sigfig = gcurve(color=color.yellow,display=signgr) # For the imaginary part of the transform
 
 imagr = gdisplay(x=0,y=250,width=600,height=250,\
                 title="Fourier transform imaginary part", xtitle="x",\
-                ytitle ="Transf.Imag" , xmax = 1 0 . 0 , xmin=−1,ymax =20 , ymin=−70)
+                ytitle="Transf.Imag", xmax=10.0, xmin=−1, ymax =20, ymin=−70)
     
-impart = gvbars(delta=0.05,color=color.red,display=imagr)
+impart = gvbars(delta=0.05, color=color.red, display=imagr)
 
 def fourierreal(dftimag):
     """
     Calculate and plot the real Fourier transform from the imaginary component.
     """
-    for n in range (0 ,Np) :
+    for n in range(0 ,Np):
         imag = 0.
         for k in range(0, N):
-            imag += signal [k]∗ np.sin (( twopi∗k∗n) /N)
+            imag += signal [k]∗ np.sin((twopi∗k∗n)/N)
         dftimag [n] = −imag∗sq2pi
         if dftimag[n] !=0:
             impart.plot(pos=(n, dftimag[n]))
