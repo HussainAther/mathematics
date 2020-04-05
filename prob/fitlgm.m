bac1a010aec8145d967c03a00830502ea1470ca4
Syed Hussain Ather <shussainather@gmail.com>
2020-04-05 00:03:10 -0400

    comment

--- prob/fitlgm.m
+++ prob/fitlgm.m
@@ -1,3 +1,11 @@
 function [Beta sigma] = FitLG(X, U, W)
 
+% Estimate parameters of the linear Gaussian model:
+% X|U ~ N(Beta(1)*U(1) + ... + Beta(K)*U(K) + Beta(K+1), sigma^2);
 
+% Note that Matlab index from 1, we can't write Beta(0). So Beta(K+1) is
+% essentially Beta(0) in PA3 description (and the text book).
+
+% X: (N x 1), the child variable, N examples
+% U: (N x K), K parent variables, N examples
+% W: (N x 1), weights over the examples.
