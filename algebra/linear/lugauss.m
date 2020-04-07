6265ad3ee7faf3fe855e4f101bcec9491abe9b73
Syed Hussain Ather <shussainather@gmail.com>
2020-04-06 22:41:10 -0400

    init

---
+++ algebra/linear/lugauss.m
@@ -0,0 +1,6 @@
+% Gauss factorization
+function A=lugass(A)
+% LU factorization without pivoting
+% Store an upper triangular matrix in hte upper triagnular 
+% part of A and a lower triangular matrix in the strictly
+% lower part of A.
