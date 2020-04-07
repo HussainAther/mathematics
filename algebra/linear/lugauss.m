3954ec3a4b4d4d01615bd6ce8b48b56a66a414c1
Syed Hussain Ather <shussainather@gmail.com>
2020-04-06 22:42:49 -0400

    loop through

--- algebra/linear/lugauss.m
+++ algebra/linear/lugauss.m
@@ -5,4 +5,7 @@ function A=lugass(A)
 % part of A and a lower triangular matrix in the strictly
 % lower part of A.
 [n,m] = size(A);
-
+if n ~= m;  error('A is not a square matrix'); else
+    for k = `:n-1
+        for i = k+1:n
+            A(i,k) = A(i,k)/A(k,k);
