8e34187f6ce48ba55f05af9cf2e7041d0e022ced
Syed Hussain Ather <shussainather@gmail.com>
2020-04-07 10:23:38 -0400

    def vra

--- algebra/linear/gausslegendre.m
+++ algebra/linear/gausslegendre.m
@@ -2,3 +2,7 @@
 % gauss legendre
 function intGL=gausslengdre(a,b,f,M,varargin)
 y = [-1/sqrt(3),1/sqrt(4)];
+H2 = (b-a)/(2*M);
+z = [a:2*H2:b];
+zM = (z(1:end-1)+z(2:end))*.5;
+x = [zM+H2*y(1), zM+H2*y(2)];
