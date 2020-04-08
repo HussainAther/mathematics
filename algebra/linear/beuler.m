5b1cd73c2750f862c29a950dfa211b4d926912f4
Syed Hussain Ather <shussainather@gmail.com>
2020-04-08 00:05:28 -0400

    def var

--- algebra/linear/beuler.m
+++ algebra/linear/beuler.m
@@ -6,7 +6,7 @@ function [t,u]= beuler(odefun ,tspan ,y0 ,Nh ,varargin )
 % integrates the system of differential equations
 % y'=f(t,y) from time T0 to TF with initial condition
 % Y0 using the backward Euler method on an equispaced
-% grid of NH intervals .
+% grid of NH intervals.
 % Function ODEFUN(T,Y) must return a vector , whose
 % elements hold the evaluation of f(t,y), of the
 % same dimension of Y.
@@ -14,3 +14,11 @@ function [t,u]= beuler(odefun ,tspan ,y0 ,Nh ,varargin )
 % time returned in the column vector T.
 % [T,Y] = BEULER(ODEFUN ,TSPAN ,Y0 ,NH ,P1 ,P2 ,...) passes
 % the additional parameters P1 ,P2 ,...
+tt=linspace (tspan(1), tspan(2), Nh +1);
+y=y0 (:); % always create a vector column
+u=y.’;
+global glob_h glob_t glob_y glob_odefun ;
+glob_h=( tspan(2)- tspan (1))/Nh;
+glob_y=y;
+glob_odefun =odefun;
+glob_t=tt (2);
