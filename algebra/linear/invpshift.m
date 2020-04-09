9e61c04861f606bdf9ae7957addaf48c37efcce6
Syed Hussain Ather <shussainather@gmail.com>
2020-04-09 00:18:10 -0400

    if else

--- algebra/linear/invpshift.m
+++ algebra/linear/invpshift.m
@@ -10,4 +10,11 @@ function [lambda ,x,iter ] = invshift (A,mu ,tol ,nmax ,x0)
 % 100), starting from the initial vector X0.
 % [LAMBDA ,V,ITER ]= INVSHIFT (A,MU ,TOL ,NMAX ,X0) also
 % returns the eigenvector V such that A*V=LAMBDA*V and
-% the iteration number at which V was computed .
+% the iteration number at which V was computed.
+[n,m] = size (A);
+if n ~= m, error('Only for square matrices '); end
+if nargin == 1
+    x0 = rand(n ,1); nmax = 100; tol = 1.e -06; mu = 0;
+elseif nargin == 2
+    x0 = rand(n ,1); nmax = 100; tol = 1.e -06;
+end
