71b86480ca0c654bd4d1650bd09b56e1f3450bd4
Syed Hussain Ather <shussainather@gmail.com>
2020-04-09 11:17:33 -0400

    get size

--- algebra/linear/powermethod.m
+++ algebra/linear/powermethod.m
@@ -4,4 +4,11 @@ function [ lambda0, v ] = PowerMethod( A, x, maxits, illustrate, delay )
 % If illustrate = 1, then the matrices V are printed (with delay in seconds)
 % and graphs that illustrate the convergence are generated.
 % If illustrate == 2, then graphs that illustrate the convergence are generated.
+[ m, n ] = size( A );
     
+% If we illustrate the algorithm, create an array in which to store the 
+% approximations for the eigenvalues
+if illustrate ~= 0
+    lambdas = zeros( 1, maxits );
+end
+ 
