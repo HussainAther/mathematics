f98ddc9227cf0e8b2de2d79c3f7d30e4731913af
Syed Hussain Ather <shussainather@gmail.com>
2020-04-09 11:18:53 -0400

    begin iteration

--- algebra/linear/powermethod.m
+++ algebra/linear/powermethod.m
@@ -7,8 +7,14 @@ function [ lambda0, v ] = PowerMethod( A, x, maxits, illustrate, delay )
 [ m, n ] = size( A );
     
 % If we illustrate the algorithm, create an array in which to store the 
-% approximations for the eigenvalues
+% approximations for the eigenvalues.
 if illustrate ~= 0
     lambdas = zeros( 1, maxits );
 end
- 
+
+% Create initial vector by normalizing vector x to length 1.
+v = x / norm( x );
+  
+% Iterate until the vector doesn't change much anymore or a maximum number
+% of iterations have been performed.
+for k=1:maxits 
