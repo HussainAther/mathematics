dc55baef429b12034030093b892ea79a9b67624e
Syed Hussain Ather <shussainather@gmail.com>
2020-04-09 11:16:51 -0400

    init

---
+++ algebra/linear/powermethod.m
@@ -0,0 +1,7 @@
+function [ lambda0, v ] = PowerMethod( A, x, maxits, illustrate, delay )
+% Performs PowerMethod with vector x
+%
+% If illustrate = 1, then the matrices V are printed (with delay in seconds)
+% and graphs that illustrate the convergence are generated.
+% If illustrate == 2, then graphs that illustrate the convergence are generated.
+    
