6e89ae5354856268f67192b30a14d55c5819a4f2
Syed Hussain Ather <shussainather@gmail.com>
2020-04-10 19:59:38 -0400

    comment

--- algebra/linear/predcor.m
+++ algebra/linear/predcor.m
@@ -1,2 +1,18 @@
 function [t,u] = predcor(odefun, tspan, y, Nh ,...
                         predictor, corrector, varargin )
+%PREDCOR Solves differential equations using a
+% predictor -corrector method
+% [T,Y]= PREDCOR (ODEFUN ,TSPAN ,Y0 ,NH ,PRED ,CORR ) with
+% TSPAN=[T0 TF] integrates the system of differential
+% equations y’ = f(t,y) from time T0 to TF with
+% initial condition Y0 using a general predictor
+% corrector method on an equispaced grid of NH steps.
+% Function ODEFUN(T,Y) must return a vector , whose
+% elements hold the evaluation of f(t,y), of the
+% same dimension of Y.
+% Each row in the solution array Y corresponds to a
+% time returned in the column vector T.
+% [T,Y]= PREDCOR (ODEFUN ,TSPAN ,Y0 ,NH ,PRED ,CORR ,P1 ,..)
+% passes the additional parameters P1 ,... to the
+% functions ODEFUN ,PRED and CORR as ODEFUN(T,Y,P1 ,..),
+% PRED (T,Y,P1 ,P2 ...), CORR
