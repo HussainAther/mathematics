f0e55db274b67b028f4d1c6400991f68f800874d
Syed Hussain Ather <shussainather@gmail.com>
2020-04-12 00:12:23 -0400

    end function

--- algebra/linear/poissonfd.m
+++ algebra/linear/poissonfd.m
@@ -32,3 +32,34 @@ for m = 2:ny+1
         K(i,i+nx1) = kiy; K(i,i-nx1) = kiy;
     end
 end
+rhs1 = zeros(dim ,1); xh = [a:hx:b]’; yh = [c:hy:d];
+rhs1 (1: nx1) = feval(bound ,xh ,c,varargin {:});
+rhs1 (dim -nx -1: dim) = feval(bound ,xh ,d, varargin {:});
+rhs1 (1: nx1:dim -nx -1) = feval(bound ,a,yh ,varargin {:});
+rhs1 (nx1:nx1:dim) = feval(bound ,b,yh ,varargin {:});
+rhs = rhs - K*rhs1 ;
+nbound = [[1: nx1],[dim -nx -1: dim ],[1:nx1:dim -nx -1] ,...
+    [nx1:nx1:dim ]];
+ninternal = setdiff ([1: dim],nbound);
+K = K(ninternal ,ninternal );
+rhs = rhs( ninternal );
+utemp = K\ rhs;
+u = rhs1 ; u (ninternal ) = utemp;
+k = 1; y = c;
+for j = 1:ny1
+    x = a;
+    for i = 1: nx1
+        uh(j,i) = u(k); k = k + 1;
+        ue(j,i) = feval(uex ,x,y,varargin {:});
+        x = x + hx;
+    end
+    y = y + hy;
+end
+if nargout == 4 & nargin >= 9
+    error = max(max(abs(uh -ue )))/ max(max(abs(ue )));
+elseif nargout == 4 & nargin ==8
+    warning('Exact solution not available ');
+    error = [ ];
+else
+end
+end
