b93dae8134f131b3a14acc127bad9d2ca0933d32
Syed Hussain Ather <shussainather@gmail.com>
2020-04-12 00:08:43 -0400

    comment

--- algebra/linear/poissonfd.m
+++ algebra/linear/poissonfd.m
@@ -1,2 +1,6 @@
 function [xh, yh, uh, error] = poissonfd (a, b, c, d, nx, ny,...
                                           fun, bound, uex, varargin)
+%POISSONFD two-dimensional Poisson solver
+% [XH, YH, UH] = POISSONFD(A, B, C, D, NX, NY, FUN, BOUND) solves
+% by the five-point finite difference scheme the
+% problem -LAPL (U) = FUN in the rectangle (A,B)X(C,D)
