bae037446857d3275e77cfffbeb35848e9b5fccf
Syed Hussain Ather <shussainather@gmail.com>
2020-04-12 00:11:10 -0400

    main loop

--- algebra/linear/poissonfd.m
+++ algebra/linear/poissonfd.m
@@ -22,3 +22,13 @@ hx = (b-a)/( nx +1); hy = (d-c)/(ny +1);
     hx2 = hx ^2; hy2 = hy ^2;
 kii = 2/ hx2 +2/ hy2; kix = -1/hx2; kiy = -1/hy2;
 K = speye(dim ,dim); rhs = zeros(dim ,1);
+y = c;
+for m = 2:ny+1
+    x = a; y = y + hy;
+    for n = 2:nx+1
+        i = n+(m -1)* nx1; x = x + hx;
+        rhs(i) = feval(fun ,x,y,varargin {:});
+        K(i,i) = kii; K(i,i-1) = kix; K(i,i+1) = kix;
+        K(i,i+nx1) = kiy; K(i,i-nx1) = kiy;
+    end
+end
