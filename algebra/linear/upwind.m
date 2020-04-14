434efe70bfd30ec864b998b32599333cd7223d4e
Syed Hussain Ather <shussainather@gmail.com>
2020-04-13 21:51:32 -0400

    second comment

--- algebra/linear/upwind.m
+++ algebra/linear/upwind.m
@@ -4,3 +4,8 @@ function [xh ,th ,uh] = hyper(xspan, tspan, u0, ul,...
 % with Lax-Friedrichs, Lax-Wendroff, upwind (lax friendrichs lax wnendroff)
 % [XH ,TH ,UH]= HYPER(XSPAN ,TSPAN ,U0 ,UL ,SCHEME ,CFL ,...
 % DELTAX ,DELTAT)
+% solves the hyperbolic scalar equation
+% DU/DT+ A * DU/DX=0
+% in (XSPAN(1), XSPAN (2)) x(TSPAN(1), TSPAN (2))
+% with A>0, initial condition U(X ,0)= U0(X) and
+% boundary condition U(T)=UL(T) given at XSPAN
