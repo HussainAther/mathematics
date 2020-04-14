6f558160b17bf7257b7f07d96a638fd19ef3e396
Syed Hussain Ather <shussainather@gmail.com>
2020-04-13 21:52:43 -0400

    list schemes

--- algebra/linear/upwind.m
+++ algebra/linear/upwind.m
@@ -9,3 +9,7 @@ function [xh ,th ,uh] = hyper(xspan, tspan, u0, ul,...
 % in (XSPAN(1), XSPAN (2)) x(TSPAN(1), TSPAN (2))
 % with A>0, initial condition U(X ,0)= U0(X) and
 % boundary condition U(T)=UL(T) given at XSPAN
+% with several finite difference schemes .
+% scheme = 1 Lax - Friedrichs
+%          2 Lax - Wendroff
+%          3 Upwind
