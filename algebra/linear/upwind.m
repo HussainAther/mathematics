d265680561efdee5819d5421a87538fdb673a860
Syed Hussain Ather <shussainather@gmail.com>
2020-04-13 21:49:53 -0400

    comment

--- algebra/linear/upwind.m
+++ algebra/linear/upwind.m
@@ -1,2 +1,6 @@
 function [xh ,th ,uh] = hyper(xspan, tspan, u0, ul,... 
-                              scheme, cfl, deltax ,deltat)
+                              scheme, cfl, deltax, deltat)
+% HYPER solves hyperbolic scalar equations 
+% with Lax-Friedrichs, Lax-Wendroff, upwind (lax friendrichs lax wnendroff)
+% [XH ,TH ,UH]= HYPER(XSPAN ,TSPAN ,U0 ,UL ,SCHEME ,CFL ,...
+% DELTAX ,DELTAT)
