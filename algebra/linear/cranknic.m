4704f9cc944072c80681f2ea861e8e8c14763122
Syed Hussain Ather <shussainather@gmail.com>
2020-04-15 17:22:40 -0400

    comments

--- algebra/linear/cranknic.m
+++ algebra/linear/cranknic.m
@@ -5,4 +5,12 @@ function [t,u] = cranknic(odefun, tspan, y0, Nh, varargin)
 % equations y'=f(t,y) from time T0 to TF with initial
 % condition Y0 using the Crank -Nicolson method on an
 % equispaced grid of NH intervals.
-% Function ODEFUN(T,Y) must return a
+% Function ODEFUN(T,Y) must return a vector , whose
+% elements hold the evaluation of f(t,y), of the
+% same dimension of Y.
+% Each row in the solution array Y corresponds to a
+% time returned in the column vector T.
+% [T,Y] = CRANKNIC (ODEFUN, TSPAN, Y0, NH, P1, P2,...)
+% passes the additional parameters P1, P2,... to the
+% function ODEFUN as ODEFUN(T,Y,P1 ,P2 ...).
+tt=linspace (tspan(1), tspan(2), Nh +1);
