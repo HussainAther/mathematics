29c7782fcf83a642d942f0356ba4c2db9f527818
Syed Hussain Ather <shussainather@gmail.com>
2020-04-16 00:32:11 -0400

    begin function

--- algebra/linear/hlq.m
+++ algebra/linear/hlq.m
@@ -1,1 +1,9 @@
 % Householder LQ Decomposition
+function [ A_out, t_out ] = HLQ( A )
+
+  [ m, n ] = size( A );
+  t = zeros( m, 1 );
+  
+  [ ATL, ATR, ...
+    ABL, ABR ] = FLA_Part_2x2( A, ...
+                               0, 0, 'FLA_TL' );
