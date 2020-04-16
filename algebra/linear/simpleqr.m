f5af39cf9c8259123f839f20ec2b8bbad4f3b4e6
Syed Hussain Ather <shussainather@gmail.com>
2020-04-16 00:37:20 -0400

    itreation

--- algebra/linear/simpleqr.m
+++ algebra/linear/simpleqr.m
@@ -1,5 +1,13 @@
 function [ Ak, V ] = SimpleQRAlg( A, maxits, illustrate, delay )
-% Performs a simple QR algorithm (with full matrix)
+% Performs a simple QR algorithm (with full matrix).
 %
 % If illustrate = 1, then the matrices are printed (with delay in seconds)
 % and graphs that illustrate the convergence are generated.
+[ m, n ] = size( A );
+    
+% Initialize the matrix in which the eigenvectors are accumulated.
+% V = I
+V = eye( m, m );
+    
+% Ak holds iteration A^{(k)}
+Ak = A;
