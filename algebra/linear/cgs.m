824f126cc663db704cbd9f013a9942aa2f8668dd
Syed Hussain Ather <shussainather@gmail.com>
2020-04-16 00:11:53 -0400

    fla steps

--- algebra/linear/cgs.m
+++ algebra/linear/cgs.m
@@ -4,3 +4,14 @@ function [ Q, R ] = MGS( A )
   [ m, n ] = size( A );    % Extract sizes of A.
   Q = zeros( m, n );       % Create a matrix in which to compute Q.
   R = zeros( n, n );       % Create a matrix in which to compute R.
+  [ AL, AR ] = FLA_Part_1x2( A, ...
+                               0, 'FLA_LEFT' );
+
+  [ QL, QR ] = FLA_Part_1x2( Q, ...
+                               0, 'FLA_LEFT' );
+                           
+  [ RTL, RTR, ...
+    RBL, RBR ] = FLA_Part_2x2( R, ...
+                               0, 0, 'FLA_TL' );
+
+  while ( size( AL, 2 ) < size( A, 2 ) )
