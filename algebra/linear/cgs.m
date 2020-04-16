9e406dd6c0114e8e54de3aa56462a7754c46f89b
Syed Hussain Ather <shussainather@gmail.com>
2020-04-16 00:11:41 -0400

    init

--- algebra/linear/cgs.m
+++ algebra/linear/cgs.m
@@ -1,1 +1,6 @@
 % Classic Gram-Schmidt algorithm (gram schmidt) 
+function [ Q, R ] = MGS( A )
+
+  [ m, n ] = size( A );    % Extract sizes of A.
+  Q = zeros( m, n );       % Create a matrix in which to compute Q.
+  R = zeros( n, n );       % Create a matrix in which to compute R.
