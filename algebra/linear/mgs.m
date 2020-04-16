e132e0fb61d8c4cef3b58e698c00b2bd352c783f
Syed Hussain Ather <shussainather@gmail.com>
2020-04-16 00:17:38 -0400

    norm

--- algebra/linear/mgs.m
+++ algebra/linear/mgs.m
@@ -20,6 +20,12 @@ function [ Q, R ] = MGS( A )
       R20,  r21,   R22 ] = FLA_Repart_2x2_to_3x3( RTL, RTR, ...
                                                   RBL, RBR, ...
                                                   1, 1, 'FLA_BR' );
+
+    rho11 = norm( a1 );
+    a1 = a1 / rho11;
+    r12t = a1' * A2;
+    A2 = A2 - a1 * r12t;
+
     [ AL, AR ] = FLA_Cont_with_1x3_to_1x2( A0, a1, A2, ...
                                            'FLA_LEFT' );
 
