c6afe85a5b2bfef2ccee1d6171b12591069d0b06
Syed Hussain Ather <shussainather@gmail.com>
2020-04-17 13:56:53 -0400

    comment

--- algebra/linear/qrbasic.m
+++ algebra/linear/qrbasic.m
@@ -1,2 +1,6 @@
 function D = qrbasic(A, tol, nmax)
 % QRBASIC computes all the eigenvalues of a matrix A.
+% D=QRBASIC (A,TOL ,NMAX ) computes by QR iterations all
+% the eigenvalues of A within a tolerance TOL and a
+% maximum number of iteration NMAX. The convergence of
+% this method is not always guaranteed.
