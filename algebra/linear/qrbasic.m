ce683c628d347d577ac5ce438e1112c54e0a59d8
Syed Hussain Ather <shussainather@gmail.com>
2020-04-17 13:57:59 -0400

    basic init

--- algebra/linear/qrbasic.m
+++ algebra/linear/qrbasic.m
@@ -4,3 +4,6 @@ function D = qrbasic(A, tol, nmax)
 % the eigenvalues of A within a tolerance TOL and a
 % maximum number of iteration NMAX. The convergence of
 % this method is not always guaranteed.
+[n, m] = size(A);
+if n ~= m, error('The matrix must be squared'); end
+T = A; niter = 0; test = norm(tril (A,-1), inf);
