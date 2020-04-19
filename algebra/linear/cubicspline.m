64a490cfba53bed371731b5c0e3cc4e5e4464ec8
Syed Hussain Ather <shussainather@gmail.com>
2020-04-18 20:54:04 -0400

    check and set equations

--- algebra/linear/cubicspline.m
+++ algebra/linear/cubicspline.m
@@ -16,3 +16,19 @@ end
 if nargin == 3
     der0 = 0; dern = 0; type = 1;
 else
+    der0 = der (1); dern = der (2);
+end
+h = x(2: end) - x(1:end -1);
+e = 2*[h(1); h(1:end -1) + h(2: end); h(end)];
+A = spdiags([[h; 0] e [0; h]], -1:1, n, n);
+d = (y(2: end)-y(1:end -1))./h;
+rhs = 3*(d(2: end)-d(1:end -1));
+if type == 0
+    A(1,1) = 2*h(1); A(1,2) = h(1);
+    A(n,n) = 2*h(end); A(end ,end -1) = h(end);
+    rhs = [3*( d(1)- der0 ); rhs; 3*(dern -d(end ))];
+else
+    A(1,:) = 0; A(1,1) = 1;
+    A(n,:) = 0; A(n,n) = 1;
+    rhs = [der0 ; rhs; dern];
+end
