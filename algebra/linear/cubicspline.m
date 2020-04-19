e8a58869f177522e8d3433db33ae6af328ce4d67
Syed Hussain Ather <shussainather@gmail.com>
2020-04-18 20:52:28 -0400

    percent comment

--- algebra/linear/cubicspline.m
+++ algebra/linear/cubicspline.m
@@ -1,1 +1,11 @@
 function s = cubicspline(x, y, zi, type, der)
+% CUBICSPLINE Computes a cubic spline
+% S=CUBICSPLINE (X,Y,ZI) computes the value at the
+% abscissae ZI of the natural interpolating cubic
+% spline that interpolates the values Y at the nodes X.
+% S=CUBICSPLINE (X,Y,ZI ,TYPE ,DER) if TYPE = 0 computes the
+% values at the abscissae ZI of the cubic spline
+% interpolating the values Y with first derivative at
+% the endpoints equal to the values DER (1) and DER (2).
+% If TYPE =1 the values DER (1) and DER (2) are
+% the second derivative at the endpoints.
