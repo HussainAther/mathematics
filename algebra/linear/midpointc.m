6b15df2cb552ac33cc0ecf08ba24f8f9e6285cc0
Syed Hussain Ather <shussainather@gmail.com>
2020-04-19 00:23:51 -0400

    first comment

--- algebra/linear/midpointc.m
+++ algebra/linear/midpointc.m
@@ -1,1 +1,4 @@
 function Imp = midpointc(a, b, M, f, varargin)
+%MIDPOINTC Composite midpoint numerical integration.
+% IMP = MIDPOINTC (A,B,M,FUN) computes an approximation
+% of the integral of the function FUN via the midpoint
