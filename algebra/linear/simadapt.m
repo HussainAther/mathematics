b4111b5f1e426fab1253cabaf3316da560d7a7ed
Syed Hussain Ather <shussainather@gmail.com>
2020-04-20 13:32:18 -0400

    comment

--- algebra/linear/simadapt.m
+++ algebra/linear/simadapt.m
@@ -1,2 +1,14 @@
 function [JSf ,nodes] = simpadpt(f, a, b, tol, hmin, varargin)
 %SIMPADPT Adaptive Simpson quadrature formula
+% JSF = SIMPADPT (FUN ,A,B,TOL ,HMIN ) tries to approximate
+% the integral of function FUN from A to B within
+% error TOL using recursive adaptive Simpson
+% quadrature with H>= HMIN . The function FUN should
+% accept a vector argument x and return a vector.
+% FUN can be either an inline function , an anonymous
+% function , or it can be defined by an external m-file.
+% JSF = SIMPADPT(FUN, A, B, TOL, HMIN, P1, P2,...) calls the
+% function FUN passing the optional parameters
+% P1 ,P2 ,... as FUN(X,P1 ,P2 ,...).
+% [JSF ,NODES] = SIMPADPT (...) returns the distribution
+% of nodes used in the quadrature process.
