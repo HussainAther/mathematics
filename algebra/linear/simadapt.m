8657916d036b5e78f7c73ca534d9c1bd562cbed5
Syed Hussain Ather <shussainather@gmail.com>
2020-04-20 13:36:34 -0400

    loop through

--- algebra/linear/simadapt.m
+++ algebra/linear/simadapt.m
@@ -13,3 +13,19 @@ function [JSf ,nodes] = simpadpt(f, a, b, tol, hmin, varargin)
 % [JSF ,NODES] = SIMPADPT (...) returns the distribution
 % of nodes used in the quadrature process.
 A=[a,b]; N=[]; S=[]; JSf = 0; ba = 2*(b - a); nodes =[];
+while ~isempty(A),
+    [deltaI ,ISc ]=caldeltai (A,f,varargin {:});
+    if abs(deltaI) < 15* tol *(A(2)-A(1))/ba;
+        JSf = JSf + ISc; S = union(S,A);
+        nodes = [nodes , A(1) (A(1)+ A(2))*0.5 A(2)];
+        S = [S(1), S(end)]; A = N; N = [];
+    elseif A(2)-A(1) < hmin
+        JSf=JSf+ISc; S = union(S,A);
+        S = [S(1), S(end)]; A=N; N=[];
+        warning('Too small integration -step ');
+    else
+        Am = (A(1)+ A(2))*0.5;
+        A = [A(1) Am]; N = [Am, b];
+    end
+end
+
