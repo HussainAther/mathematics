2cad019bc64761573d01de66aba3c41be9ce0fb9
Syed Hussain Ather <shussainather@gmail.com>
2020-04-28 16:54:27 -0400

    if else begin

--- algebra/linear/gershcircles.m
+++ algebra/linear/gershcircles.m
@@ -1,3 +1,10 @@
 function gershcircles(A)
 % GERSHCIRCLES plots the Gershgorin circles
 % GERSHCIRCLES (A) draws the Gershgorin circles
+% the square matrix A and its transpose.
+n = size(A);
+if n(1) ~= n(2)
+    error('Only square matrices ');
+else
+    n = n(1); circler = zeros(n ,201); circlec = circler;
+end
